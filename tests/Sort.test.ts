1e599005b7445db38dd44c979ba81347f0511792
Simon Knott <info@simonknott.de>
2021-07-30 11:30:17 +0200

    add test for multiple sort order

--- tests/Sort.test.ts
+++ tests/Sort.test.ts
@@ -5,10 +5,10 @@ window.moment = moment;
 import { Task } from '../src/Task';
 import { Sort } from '../src/Sort';
 
-function fromLine(line: string) {
+function fromLine(line: string, path = '') {
     return Task.fromLine({
         line,
-        path: '',
+        path,
         precedingHeader: '',
         sectionIndex: 0,
         sectionStart: 0,
@@ -28,5 +28,19 @@ describe('Sort', () => {
         const b = fromLine('- [x] pet the cat 🗓 2021-09-15 ✅ 2021-09-16');
         expect(Sort.by({ sorting: ['done'] }, [a, b])).toEqual([b, a]);
         expect(Sort.by({ sorting: ['done'] }, [b, a])).toEqual([b, a]);
+    });
+
+    test('by due, path, status', () => {
+        const a = fromLine('- [ ] a 🗓 1970-01-01', '1');
+        const b = fromLine('- [x] b 🗓 1970-01-01', '2');
+        const c = fromLine('- [ ] c 🗓 1970-01-01', '2');
+        const expectedOrder = [
+            a, // date is the same, but path is lower
+            c, // path is lower, but same as b. this is first b/c it's not done yet
+            b, // same as c, but already done. this has lowest priority
+        ];
+        expect(
+            Sort.by({ sorting: ['due', 'path', 'status'] }, [a, b, c]),
+        ).toEqual(expectedOrder);
     });
 });
