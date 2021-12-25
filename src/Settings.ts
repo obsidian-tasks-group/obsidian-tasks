af8d45dbaecb1496d3252e0f8a54c2ccfb3e394e
Holland Wilson <stickfigureonfire@gmail.com>
2021-12-25 14:27:42 -0600

    You can now select for the global filter to land at the end of a task on creation.

--- src/Settings.ts
+++ src/Settings.ts
@@ -1,11 +1,13 @@
 export interface Settings {
     globalFilter: string;
     removeGlobalFilter: boolean;
+    appendGlobalFilter: boolean;
 }
 
 const defaultSettings: Settings = {
     globalFilter: '',
     removeGlobalFilter: false,
+    appendGlobalFilter: false,
 };
 
 let settings: Settings = { ...defaultSettings };
