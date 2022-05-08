016829b65a456dfcc21f67d276002844dfe06b24
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2022-05-08 12:55:25 +0100

    Remove unused remnants of earlier group options

--- src/Query.ts
+++ src/Query.ts
@@ -67,7 +67,7 @@ export class Query {
         /^sort by (urgency|status|priority|start|scheduled|due|done|path|description|tag)( reverse)?/;
 
     private readonly groupByRegexp =
-        /^group by (context|done|due|filename|folder|happens|heading|linktext|path|priority|referenceDateField|scheduled|start|status|urgency)/;
+        /^group by (filename|folder|heading|linktext|path|status)/;
 
     private readonly headingRegexp =
         /^heading (includes|does not include) (.*)/;
