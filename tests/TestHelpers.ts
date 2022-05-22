ff1216477242401139fee2031b28ebec9422334c
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2022-05-22 19:41:45 +0100

    Add 'group by' option, for various file properties and status (#644)
    * Add new 'group by' instruction, that allows grouping tasks by backlink, filename, folder, heading, path and status values.
    
    * Multiple 'group by' instructions may be used, to create nested groups.
    
    * Add 'tasks-group-heading' styling to group headings.
    
    * Add new file 'tests/TestHelpers.ts', containing some re-usable code to help with writing of tests.
    
    * Add new documentation page 'docs/queries/grouping.md'.
    
    * Add 'group by' example to 'docs/queries/examples.md'.
    
    * Add 'Maintaining the tests' section to CONTRIBUTING.md.

---
+++ tests/TestHelpers.ts
@@ -0,0 +1,41 @@
+import { Task } from '../src/Task';
+
+export function fromLine({
+    line,
+    path = '',
+    precedingHeader = '',
+}: {
+    line: string;
+    path?: string;
+    precedingHeader?: string | null;
+}) {
+    return Task.fromLine({
+        line,
+        path,
+        precedingHeader,
+        sectionIndex: 0,
+        sectionStart: 0,
+    })!;
+}
+
+export function createTasksFromMarkdown(
+    tasksAsMarkdown: string,
+    path: string,
+    precedingHeader: string,
+): Task[] {
+    const taskLines = tasksAsMarkdown.split('\n');
+    const tasks: Task[] = [];
+    for (const line of taskLines) {
+        const task = Task.fromLine({
+            line: line,
+            path: path,
+            precedingHeader: precedingHeader,
+            sectionIndex: 0,
+            sectionStart: 0,
+        });
+        if (task) {
+            tasks.push(task);
+        }
+    }
+    return tasks;
+}
