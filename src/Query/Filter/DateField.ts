898ad92f17714ca04e3b177cdfa53fbbcda3066e
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2022-05-25 21:19:35 +0100

    Simplify DateField.createFilterOrErrorMessage()

--- src/Query/Filter/DateField.ts
+++ src/Query/Filter/DateField.ts
@@ -6,30 +6,30 @@ import { FilterOrErrorMessage } from './Filter';
 
 export abstract class DateField extends Field {
     public createFilterOrErrorMessage(line: string): FilterOrErrorMessage {
-        let filter;
-        let error;
+        const result = new FilterOrErrorMessage();
         const match = line.match(this.filterRegexp());
         if (match !== null) {
             const filterDate = Query.parseDate(match[2]);
             if (!filterDate.isValid()) {
-                error = 'do not understand ' + this.fieldName() + ' date';
+                result.error =
+                    'do not understand ' + this.fieldName() + ' date';
             } else {
                 if (match[1] === 'before') {
-                    filter = (task: Task) => {
+                    result.filter = (task: Task) => {
                         const date = this.date(task);
                         return date
                             ? date.isBefore(filterDate)
                             : this.filterResultIfFieldMissing();
                     };
                 } else if (match[1] === 'after') {
-                    filter = (task: Task) => {
+                    result.filter = (task: Task) => {
                         const date = this.date(task);
                         return date
                             ? date.isAfter(filterDate)
                             : this.filterResultIfFieldMissing();
                     };
                 } else {
-                    filter = (task: Task) => {
+                    result.filter = (task: Task) => {
                         const date = this.date(task);
                         return date
                             ? date.isSame(filterDate)
@@ -38,14 +38,11 @@ export abstract class DateField extends Field {
                 }
             }
         } else {
-            error =
+            result.error =
                 'do not understand query filter (' +
                 this.fieldName() +
                 ' date)';
         }
-        const result = new FilterOrErrorMessage();
-        result.filter = filter;
-        result.error = error;
         return result;
     }
 
