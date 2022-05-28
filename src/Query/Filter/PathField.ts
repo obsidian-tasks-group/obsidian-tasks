8e1491e04859b620dc32a59887231002d9f67a04
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2022-05-28 22:16:32 +0100

    Make PathField.createFilterOrErrorMessage() general

--- src/Query/Filter/PathField.ts
+++ src/Query/Filter/PathField.ts
@@ -9,26 +9,26 @@ export class PathField extends Field {
 
     public createFilterOrErrorMessage(line: string): FilterOrErrorMessage {
         const result = new FilterOrErrorMessage();
-        const pathMatch = line.match(PathField.pathRegexp);
-        if (pathMatch !== null) {
-            const filterMethod = pathMatch[1];
+        const match = line.match(this.filterRegexp());
+        if (match !== null) {
+            const filterMethod = match[1];
             if (filterMethod === 'includes') {
                 result.filter = (task: Task) =>
                     Query.stringIncludesCaseInsensitive(
-                        task.path,
-                        pathMatch[2],
+                        this.value(task),
+                        match[2],
                     );
-            } else if (pathMatch[1] === 'does not include') {
+            } else if (match[1] === 'does not include') {
                 result.filter = (task: Task) =>
                     !Query.stringIncludesCaseInsensitive(
-                        task.path,
-                        pathMatch[2],
+                        this.value(task),
+                        match[2],
                     );
             } else {
-                result.error = 'do not understand query filter (path)';
+                result.error = `do not understand query filter (${this.fieldName()})`;
             }
         } else {
-            result.error = 'do not understand query filter (path)';
+            result.error = `do not understand query filter (${this.fieldName()})`;
         }
         return result;
     }
@@ -39,5 +39,9 @@ export class PathField extends Field {
 
     protected fieldName(): string {
         return 'path';
+    }
+
+    protected value(task: Task): string {
+        return task.path;
     }
 }
