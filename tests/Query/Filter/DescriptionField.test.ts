a425410295ebaccb7817fc21210812e5dcf0fc75
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2022-05-29 21:00:27 +0100

    Port in 2 tests from Query.test.ts

--- tests/Query/Filter/DescriptionField.test.ts
+++ tests/Query/Filter/DescriptionField.test.ts
@@ -1,4 +1,7 @@
 import { DescriptionField } from '../../../src/Query/Filter/DescriptionField';
+import { getSettings, updateSettings } from '../../../src/Settings';
+import { testTaskFilter } from '../../TestingTools/FilterTestHelpers';
+import { fromLine } from '../../TestHelpers';
 
 describe('description', () => {
     it('can instantiate', () => {
@@ -7,5 +10,68 @@ describe('description', () => {
         );
         expect(filter.filter).toBeDefined();
         expect(filter.error).toBeUndefined();
+    });
+
+    it('ignores the global filter when filtering', () => {
+        // Arrange
+        const originalSettings = getSettings();
+        updateSettings({ globalFilter: '#task' });
+        const filter = new DescriptionField().createFilterOrErrorMessage(
+            'description includes task',
+        );
+
+        // Act, Assert
+        testTaskFilter(
+            filter,
+            fromLine({
+                line: '- [ ] #task this does not include the word; only in the global filter',
+            })!,
+            false,
+        );
+        testTaskFilter(
+            filter,
+            fromLine({ line: '- [ ] #task this does: task' })!,
+            true,
+        );
+
+        // Cleanup
+        updateSettings(originalSettings);
+    });
+
+    it('works without a global filter', () => {
+        // Arrange
+        const originalSettings = getSettings();
+        updateSettings({ globalFilter: '' });
+        const filter = new DescriptionField().createFilterOrErrorMessage(
+            'description includes task',
+        );
+
+        // Act, Assert
+        testTaskFilter(
+            filter,
+            fromLine({
+                line: '- [ ] this does not include the word at all',
+            })!,
+            false,
+        );
+
+        testTaskFilter(
+            filter,
+            fromLine({
+                line: '- [ ] #task this includes the word as a tag',
+            })!,
+            true,
+        );
+
+        testTaskFilter(
+            filter,
+            fromLine({
+                line: '- [ ] #task this does: task',
+            })!,
+            true,
+        );
+
+        // Cleanup
+        updateSettings(originalSettings);
     });
 });
