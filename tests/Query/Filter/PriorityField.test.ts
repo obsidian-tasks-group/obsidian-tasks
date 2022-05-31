0142054f29d3bc7fa1af20221aee44751ce0063d
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2022-05-31 07:47:48 +0100

    Improve coverage on priority tests

--- tests/Query/Filter/PriorityField.test.ts
+++ tests/Query/Filter/PriorityField.test.ts
@@ -13,18 +13,54 @@ function testTaskFilterForTaskWithPriority(
 }
 
 // /^priority (is )?(above|below)? ?(low|none|medium|high)/;
-describe('priority', () => {
+describe('priority is', () => {
     it('priority is high', () => {
         const filter = 'priority is high';
-        testTaskFilterForTaskWithPriority(filter, Priority.Low, false);
         testTaskFilterForTaskWithPriority(filter, Priority.High, true);
+        testTaskFilterForTaskWithPriority(filter, Priority.Medium, false);
+        testTaskFilterForTaskWithPriority(filter, Priority.None, false);
+        testTaskFilterForTaskWithPriority(filter, Priority.Low, false);
     });
 
+    it('priority is medium', () => {
+        const filter = 'priority is medium';
+        testTaskFilterForTaskWithPriority(filter, Priority.High, false);
+        testTaskFilterForTaskWithPriority(filter, Priority.Medium, true);
+        testTaskFilterForTaskWithPriority(filter, Priority.None, false);
+        testTaskFilterForTaskWithPriority(filter, Priority.Low, false);
+    });
+
+    it('priority is none', () => {
+        const filter = 'priority is none';
+        testTaskFilterForTaskWithPriority(filter, Priority.High, false);
+        testTaskFilterForTaskWithPriority(filter, Priority.Medium, false);
+        testTaskFilterForTaskWithPriority(filter, Priority.None, true);
+        testTaskFilterForTaskWithPriority(filter, Priority.Low, false);
+    });
+
+    it('priority is low', () => {
+        const filter = 'priority is low';
+        testTaskFilterForTaskWithPriority(filter, Priority.High, false);
+        testTaskFilterForTaskWithPriority(filter, Priority.Medium, false);
+        testTaskFilterForTaskWithPriority(filter, Priority.None, false);
+        testTaskFilterForTaskWithPriority(filter, Priority.Low, true);
+    });
+});
+
+describe('priority above', () => {
     it('priority above none', () => {
         const filter = 'priority above none';
         testTaskFilterForTaskWithPriority(filter, Priority.Low, false);
         testTaskFilterForTaskWithPriority(filter, Priority.None, false);
         testTaskFilterForTaskWithPriority(filter, Priority.Medium, true);
         testTaskFilterForTaskWithPriority(filter, Priority.High, true);
+    });
+});
+
+describe('priority below', () => {
+    it('priority below none', () => {
+        const filter = 'priority below none';
+        testTaskFilterForTaskWithPriority(filter, Priority.Low, true);
+        testTaskFilterForTaskWithPriority(filter, Priority.None, false);
     });
 });
