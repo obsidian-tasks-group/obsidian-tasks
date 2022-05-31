35029e083cf85a8bbe28f510f3c304cd780b6008
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2022-05-31 06:12:44 +0100

    Document file extensions for TaskBuilder and PathField

--- src/Query/Filter/PathField.ts
+++ src/Query/Filter/PathField.ts
@@ -3,6 +3,9 @@ import { TextField } from './TextField';
 
 /** Support the 'path' search instruction.
  *
+ * Note that the current implementation also searches the file extension,
+ * so 'path includes .md' will typically match all tasks.
+ *
  */
 export class PathField extends TextField {
     private static readonly pathRegexp =
