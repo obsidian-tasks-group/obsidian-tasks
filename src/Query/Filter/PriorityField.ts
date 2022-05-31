8048e262685bdfae12abb6127d9531828fb2af67
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2022-05-31 08:31:59 +0100

    refactor: Create priority filter class (#706)
    * Test priority filter, using new helper testTaskFilterViaQuery()
    
    * Improve coverage on priority tests
    
    * Reimplement 'priority' filter using PriorityField class
    
    * Rework PriorityField.test.ts to use PriorityField directly.
    
    No longer need to create Query objects
    
    * Add a test for the error text from PriorityField
    
    * Remove stray comment from test

---
+++ src/Query/Filter/PriorityField.ts
@@ -0,0 +1,66 @@
+import { Priority, Task } from '../../Task';
+import { Field } from './Field';
+import { FilterOrErrorMessage } from './Filter';
+
+export class PriorityField extends Field {
+    private static readonly priorityRegexp =
+        /^priority (is )?(above|below)? ?(low|none|medium|high)/;
+
+    createFilterOrErrorMessage(line: string): FilterOrErrorMessage {
+        const result = new FilterOrErrorMessage();
+        const priorityMatch = line.match(this.filterRegexp());
+        if (priorityMatch !== null) {
+            const filterPriorityString = priorityMatch[3];
+            let filterPriority: Priority | null = null;
+
+            switch (filterPriorityString) {
+                case 'low':
+                    filterPriority = Priority.Low;
+                    break;
+                case 'none':
+                    filterPriority = Priority.None;
+                    break;
+                case 'medium':
+                    filterPriority = Priority.Medium;
+                    break;
+                case 'high':
+                    filterPriority = Priority.High;
+                    break;
+            }
+
+            if (filterPriority === null) {
+                result.error = 'do not understand priority';
+                return result;
+            }
+
+            let filter;
+            if (priorityMatch[2] === 'above') {
+                filter = (task: Task) =>
+                    task.priority
+                        ? task.priority.localeCompare(filterPriority!) < 0
+                        : false;
+            } else if (priorityMatch[2] === 'below') {
+                filter = (task: Task) =>
+                    task.priority
+                        ? task.priority.localeCompare(filterPriority!) > 0
+                        : false;
+            } else {
+                filter = (task: Task) =>
+                    task.priority ? task.priority === filterPriority : false;
+            }
+
+            result.filter = filter;
+        } else {
+            result.error = 'do not understand query filter (priority date)';
+        }
+        return result;
+    }
+
+    protected fieldName(): string {
+        return 'priority';
+    }
+
+    protected filterRegexp(): RegExp {
+        return PriorityField.priorityRegexp;
+    }
+}
---
+++ tests/Query/Filter/PriorityField.test.ts
@@ -0,0 +1,81 @@
+import { Priority } from '../../../src/Task';
+import { TaskBuilder } from '../../TestingTools/TaskBuilder';
+import { testTaskFilter } from '../../TestingTools/FilterTestHelpers';
+import { PriorityField } from '../../../src/Query/Filter/PriorityField';
+
+function testTaskFilterForTaskWithPriority(
+    filter: string,
+    priority: Priority,
+    expected: boolean,
+) {
+    const builder = new TaskBuilder();
+    const filterOrError = new PriorityField().createFilterOrErrorMessage(
+        filter,
+    );
+    testTaskFilter(filterOrError, builder.priority(priority).build(), expected);
+}
+
+describe('priority is', () => {
+    it('priority is high', () => {
+        const filter = 'priority is high';
+        testTaskFilterForTaskWithPriority(filter, Priority.High, true);
+        testTaskFilterForTaskWithPriority(filter, Priority.Medium, false);
+        testTaskFilterForTaskWithPriority(filter, Priority.None, false);
+        testTaskFilterForTaskWithPriority(filter, Priority.Low, false);
+    });
+
+    it('priority is medium', () => {
+        const filter = 'priority is medium';
+        testTaskFilterForTaskWithPriority(filter, Priority.High, false);
+        testTaskFilterForTaskWithPriority(filter, Priority.Medium, true);
+        testTaskFilterForTaskWithPriority(filter, Priority.None, false);
+        testTaskFilterForTaskWithPriority(filter, Priority.Low, false);
+    });
+
+    it('priority is none', () => {
+        const filter = 'priority is none';
+        testTaskFilterForTaskWithPriority(filter, Priority.High, false);
+        testTaskFilterForTaskWithPriority(filter, Priority.Medium, false);
+        testTaskFilterForTaskWithPriority(filter, Priority.None, true);
+        testTaskFilterForTaskWithPriority(filter, Priority.Low, false);
+    });
+
+    it('priority is low', () => {
+        const filter = 'priority is low';
+        testTaskFilterForTaskWithPriority(filter, Priority.High, false);
+        testTaskFilterForTaskWithPriority(filter, Priority.Medium, false);
+        testTaskFilterForTaskWithPriority(filter, Priority.None, false);
+        testTaskFilterForTaskWithPriority(filter, Priority.Low, true);
+    });
+});
+
+describe('priority above', () => {
+    it('priority above none', () => {
+        const filter = 'priority above none';
+        testTaskFilterForTaskWithPriority(filter, Priority.Low, false);
+        testTaskFilterForTaskWithPriority(filter, Priority.None, false);
+        testTaskFilterForTaskWithPriority(filter, Priority.Medium, true);
+        testTaskFilterForTaskWithPriority(filter, Priority.High, true);
+    });
+});
+
+describe('priority below', () => {
+    it('priority below none', () => {
+        const filter = 'priority below none';
+        testTaskFilterForTaskWithPriority(filter, Priority.Low, true);
+        testTaskFilterForTaskWithPriority(filter, Priority.None, false);
+    });
+});
+
+describe('priority error cases', () => {
+    it('priority is something', () => {
+        const field = new PriorityField();
+        const filter = field.createFilterOrErrorMessage(
+            'priority is no-such-priority',
+        );
+        expect(filter.filter).toBeUndefined();
+        expect(filter.error).toBe(
+            'do not understand query filter (priority date)',
+        );
+    });
+});
