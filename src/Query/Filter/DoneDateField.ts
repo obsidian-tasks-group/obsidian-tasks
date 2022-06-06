8f24a790e129934219c120029f51dd75a0193b6a
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2022-06-06 15:23:00 +0100

    Teach DoneDateField.ts 'has done date' and 'no done date'

--- src/Query/Filter/DoneDateField.ts
+++ src/Query/Filter/DoneDateField.ts
@@ -1,12 +1,31 @@
 import type { Moment } from 'moment';
 import type { Task } from '../../Task';
 import { DateField } from './DateField';
+import { FilterOrErrorMessage } from './Filter';
 
 /**
  * Support the 'done' search instruction.
  */
 export class DoneDateField extends DateField {
     private static readonly doneRegexp = /^done (before|after|on)? ?(.*)/;
+    private static readonly instructionForFieldPresence = 'has done date';
+    private static readonly instructionForFieldAbsence = 'no done date';
+
+    public createFilterOrErrorMessage(line: string): FilterOrErrorMessage {
+        if (line === DoneDateField.instructionForFieldPresence) {
+            const result = new FilterOrErrorMessage();
+            result.filter = (task: Task) => this.date(task) !== null;
+            return result;
+        }
+
+        if (line === DoneDateField.instructionForFieldAbsence) {
+            const result = new FilterOrErrorMessage();
+            result.filter = (task: Task) => this.date(task) === null;
+            return result;
+        }
+
+        return super.createFilterOrErrorMessage(line);
+    }
 
     protected filterRegexp(): RegExp {
         return DoneDateField.doneRegexp;
