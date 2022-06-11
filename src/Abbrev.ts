9facd9bba6c55300d5b172bb9d91c5b247c5fc1c
focccus <58370462+focccus@users.noreply.github.com>
2022-06-11 10:55:47 +0200

    feat: Add date abbreviations to 'Create or edit Task' modal (#579)
    * Abbreviations for Dates, Refactoring of Parsing
    
    * Removed Debug Messages
    
    * fixed close i tags in date parser
    
    * fixed Abbreviations not parsed on submit
    
    Co-authored-by: focccus <focccus@users.noreply.github.com>

---
+++ src/Abbrev.ts
@@ -0,0 +1,10 @@
+// Abbreviations for entering dates with chrono
+export default {
+    td: 'today',
+    tm: 'tomorrow',
+    yd: 'yesterday',
+    tw: 'this week',
+    nw: 'next week',
+    weekend: 'sat',
+    we: 'sat',
+};
