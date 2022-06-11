79dfe781fd57d3dfc3aa60c895f5531d65a1b3a0
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2022-06-11 11:17:56 +0100

    feat: Update 'Create or edit Task' to show new abbreviations (#755)
    * Remove some debug output
    
    * Extract repeated text to a variable
    
    * Rename Abbrev.ts to DateAbbreviations.ts
    
    * Try to make the abbreviations discoverable in placeholder
    
    * Make the date abbreviations easier to read

--- src/DateAbbreviations.ts
+++ src/DateAbbreviations.ts
@@ -1,4 +1,6 @@
 // Abbreviations for entering dates with chrono
+// MAINTENANCE NOTE:
+//      If adding more abbreviations, please review datePlaceholder in src/ui/EditTask.svelte
 export default {
     td: 'today',
     tm: 'tomorrow',
