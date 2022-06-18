14f3f5f0dc04b68b33c42ccae151c1b54a711290
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2022-06-18 09:53:57 +0100

    refactor: Add RecurringField class, for handling recurring/not recurring filters (#806)
    * refactor: Add RecurringField class
    
    * test: Remove repetitive text in tests
    
    * refactor: Reimplement 'is recurring' and 'is not recurring' using RecurringField
    
    * test: Add a test of recurrence filters with invalid recurrence rule

---
+++ tests/Query/Filter/RecurringField.test.ts
@@ -0,0 +1,50 @@
+/**
+ * @jest-environment jsdom
+ */
+import moment from 'moment';
+import { RecurringField } from '../../../src/Query/Filter/RecurringField';
+import type { FilterOrErrorMessage } from '../../../src/Query/Filter/Filter';
+import { testTaskFilter } from '../../TestingTools/FilterTestHelpers';
+import { fromLine } from '../../TestHelpers';
+
+window.moment = moment;
+
+function testRecurringFilter(
+    filter: FilterOrErrorMessage,
+    line: string,
+    expected: boolean,
+) {
+    const task = fromLine({ line });
+    testTaskFilter(filter, task, expected);
+}
+
+describe('recurring', () => {
+    const non_recurring = '- [ ] non-recurring task';
+    const recurring = '- [ ] recurring 🔁 every day 📅 2022-06-17';
+    // Invalid recurrence rules are discarded, and treated as non-recurring
+    const invalid = '- [ ] recurring 🔁 invalid rule 📅 2022-06-17';
+
+    it('is recurring', () => {
+        // Arrange
+        const filter = new RecurringField().createFilterOrErrorMessage(
+            'is recurring',
+        );
+
+        // Assert
+        testRecurringFilter(filter, non_recurring, false);
+        testRecurringFilter(filter, recurring, true);
+        testRecurringFilter(filter, invalid, false);
+    });
+
+    it('is not recurring', () => {
+        // Arrange
+        const filter = new RecurringField().createFilterOrErrorMessage(
+            'is not recurring',
+        );
+
+        // Assert
+        testRecurringFilter(filter, non_recurring, true);
+        testRecurringFilter(filter, recurring, false);
+        testRecurringFilter(filter, invalid, true);
+    });
+});
