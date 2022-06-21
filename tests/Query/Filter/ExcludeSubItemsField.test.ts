edba31be0868db06698485b1987501c00e57dd2b
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2022-06-21 17:48:46 +0100

    test: Clean up tests for ExcludeSubItemsField

--- tests/Query/Filter/ExcludeSubItemsField.test.ts
+++ tests/Query/Filter/ExcludeSubItemsField.test.ts
@@ -1,18 +1,8 @@
 // 'exclude sub-items'
 
-import {
-    testTaskFilter,
-    testTaskFilterViaQuery,
-} from '../../TestingTools/FilterTestHelpers';
+import { testTaskFilter } from '../../TestingTools/FilterTestHelpers';
 import { fromLine } from '../../TestHelpers';
 import { ExcludeSubItemsField } from '../../../src/Query/Filter/ExcludeSubItemsField';
-
-/*
- * 1. [x] Write tests with testTaskFilterViaQuery()
- * 2. [x] Introduce ExcludeSubItemsField
- * 3. [ ] Update Query
- * 4. [ ] Convert tests to use testTaskFilter()
- */
 
 describe('sub-items', () => {
     it('exclude sub-items', () => {
@@ -25,23 +15,6 @@ describe('sub-items', () => {
         testTaskFilter(filter, fromLine({ line: '- [ ] I am a task' }), true);
         testTaskFilter(
             filter,
-            fromLine({ line: '    - [ ] I am a sub-task' }),
-            false,
-        );
-    });
-
-    it('exclude sub-items - old-style test - delete me soon', () => {
-        // Arrange
-        const filterName = 'exclude sub-items';
-
-        // Assert
-        testTaskFilterViaQuery(
-            filterName,
-            fromLine({ line: '- [ ] I am a task' }),
-            true,
-        );
-        testTaskFilterViaQuery(
-            filterName,
             fromLine({ line: '    - [ ] I am a sub-task' }),
             false,
         );
