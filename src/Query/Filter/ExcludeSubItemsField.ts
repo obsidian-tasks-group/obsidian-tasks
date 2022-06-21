c3567029849ebb01b0aebc100f2e59e3c34378c8
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2022-06-21 17:43:22 +0100

    feat: Add ExcludeSubItemsField class - for 'exclude sub-items'

---
+++ src/Query/Filter/ExcludeSubItemsField.ts
@@ -0,0 +1,32 @@
+import { Field } from './Field';
+import type { FilterOrErrorMessage } from './Filter';
+import { FilterInstructions } from './FilterInstructions';
+
+export class ExcludeSubItemsField extends Field {
+    private readonly _filters = new FilterInstructions();
+
+    constructor() {
+        super();
+
+        this._filters.add(
+            'exclude sub-items',
+            (task) => task.indentation === '',
+        );
+    }
+
+    public canCreateFilterForLine(line: string): boolean {
+        return this._filters.canCreateFilterForLine(line);
+    }
+
+    public createFilterOrErrorMessage(line: string): FilterOrErrorMessage {
+        return this._filters.createFilterOrErrorMessage(line);
+    }
+
+    protected fieldName(): string {
+        return 'exclude';
+    }
+
+    protected filterRegexp(): RegExp | null {
+        return null;
+    }
+}
--- tests/Query/Filter/ExcludeSubItemsField.test.ts
+++ tests/Query/Filter/ExcludeSubItemsField.test.ts
@@ -1,17 +1,36 @@
 // 'exclude sub-items'
 
-import { testTaskFilterViaQuery } from '../../TestingTools/FilterTestHelpers';
+import {
+    testTaskFilter,
+    testTaskFilterViaQuery,
+} from '../../TestingTools/FilterTestHelpers';
 import { fromLine } from '../../TestHelpers';
+import { ExcludeSubItemsField } from '../../../src/Query/Filter/ExcludeSubItemsField';
 
 /*
- * 1. Write tests with testTaskFilterViaQuery()
- * 2. Introduce ExcludeSubItemsField
- * 3. Update Query
- * 4. Convert tests to use testTaskFilter()
+ * 1. [x] Write tests with testTaskFilterViaQuery()
+ * 2. [x] Introduce ExcludeSubItemsField
+ * 3. [ ] Update Query
+ * 4. [ ] Convert tests to use testTaskFilter()
  */
 
 describe('sub-items', () => {
     it('exclude sub-items', () => {
+        // Arrange
+        const filter = new ExcludeSubItemsField().createFilterOrErrorMessage(
+            'exclude sub-items',
+        );
+
+        // Assert
+        testTaskFilter(filter, fromLine({ line: '- [ ] I am a task' }), true);
+        testTaskFilter(
+            filter,
+            fromLine({ line: '    - [ ] I am a sub-task' }),
+            false,
+        );
+    });
+
+    it('exclude sub-items - old-style test - delete me soon', () => {
         // Arrange
         const filterName = 'exclude sub-items';
 
