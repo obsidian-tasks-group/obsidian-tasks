8faaa5ccfba532d310d8a70f5dfb7ea792113eec
Evan Light <evan.light@hey.com>
2022-07-01 14:46:43 -0400

    Basic regex matching using PathField; need to support slash delimiters and regex params later

--- src/Query/Filter/TextField.ts
+++ src/Query/Filter/TextField.ts
@@ -8,34 +8,28 @@ import { FilterOrErrorMessage } from './Filter';
  * value, such as the description or file path.
  */
 export abstract class TextField extends Field {
-    private static readonly regexpPattern = /^\/(.*)\/$/;
-
     public createFilterOrErrorMessage(line: string): FilterOrErrorMessage {
         const result = new FilterOrErrorMessage();
         const match = Field.getMatch(this.filterRegexp(), line);
         if (match !== null) {
             const filterMethod = match[1];
             if (filterMethod === 'includes') {
-                const queryString = match[2];
-                const regexpMatch = queryString.match(TextField.regexpPattern);
-                if (regexpMatch !== null) {
-                    const pattern = regexpMatch[1];
-                    const regexp = new RegExp(pattern);
-                    result.filter = (task: Task) =>
-                        this.value(task).match(regexp) !== null;
-                } else {
-                    result.filter = (task: Task) =>
-                        TextField.stringIncludesCaseInsensitive(
-                            this.value(task),
-                            match[2],
-                        );
-                }
+                result.filter = (task: Task) =>
+                    TextField.stringIncludesCaseInsensitive(
+                        this.value(task),
+                        match[2],
+                    );
             } else if (match[1] === 'does not include') {
                 result.filter = (task: Task) =>
                     !TextField.stringIncludesCaseInsensitive(
                         this.value(task),
                         match[2],
                     );
+            } else if (match[1] === 'regex matches') {
+                const pattern = match[2];
+                const regexp = new RegExp(pattern);
+                result.filter = (task: Task) =>
+                    this.value(task).match(regexp) !== null;
             } else {
                 result.error = `do not understand query filter (${this.fieldName()})`;
             }
