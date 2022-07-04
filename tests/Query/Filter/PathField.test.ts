05084729e510d07f3a3a0fdda2ef2d4b7a05ffad
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2022-07-04 20:23:22 +0100

    test: Add custom matcher for FilterOrErrorMessage
    Initially testing the PathField.
    
    Will be moved to a separate file later.
    
    Co-Authored-By: Evan Light <10112+elight@users.noreply.github.com>

--- tests/Query/Filter/PathField.test.ts
+++ tests/Query/Filter/PathField.test.ts
@@ -12,6 +12,77 @@ function testTaskFilterForTaskWithPath(
     testFilter(filter, builder.path(path), expected);
 }
 
+declare global {
+    namespace jest {
+        interface Matchers<R> {
+            toMatchTaskWithPath(path: string): R;
+            toBeValid(): R;
+        }
+
+        interface Expect {
+            toMatchTaskWithPath(path: string): any;
+            toBeValid(): any;
+        }
+
+        interface InverseAsymmetricMatchers {
+            toMatchTaskWithPath(path: string): any;
+            toBeValid(): any;
+        }
+    }
+}
+
+export function toBeValid(filter: FilterOrErrorMessage) {
+    if (filter.filter === undefined) {
+        return {
+            message: () =>
+                'unexpected null filter: check your instruction matches your filter class',
+            pass: false,
+        };
+    }
+
+    if (filter.error !== undefined) {
+        return {
+            message: () =>
+                'unexpected error message in filter: check your instruction matches your filter class',
+            pass: false,
+        };
+    }
+
+    return {
+        message: () => 'filter is unexpectedly valid',
+        pass: true,
+    };
+}
+
+export function toMatchTaskWithPath(
+    filter: FilterOrErrorMessage,
+    path: string,
+) {
+    const builder = new TaskBuilder();
+    const task = builder.path(path).build();
+
+    const matches = filter.filter!(task);
+    if (!matches) {
+        return {
+            message: () => `unexpected failure to match task: ${path}`,
+            pass: false,
+        };
+    }
+
+    return {
+        message: () => `filter should not have matched task: ${path}`,
+        pass: true,
+    };
+}
+
+expect.extend({
+    toMatchTaskWithPath,
+});
+
+expect.extend({
+    toBeValid,
+});
+
 describe('path', () => {
     it('by path (includes)', () => {
         // Arrange
@@ -38,17 +109,18 @@ describe('path', () => {
         testTaskFilterForTaskWithPath(filter, '/other/path/file.md', true);
     });
 
-    it('by path (includes regex)', () => {
+    it('by path (regex matches)', () => {
         // Arrange
         const filter = new PathField().createFilterOrErrorMessage(
             'path regex matches /w.bble/',
         );
 
         // Assert
-        testTaskFilterForTaskWithPath(filter, '', false);
-        testTaskFilterForTaskWithPath(filter, '/some/path/wibble.md', true);
-        testTaskFilterForTaskWithPath(filter, '/some/path/wobble.md', true);
-        testTaskFilterForTaskWithPath(filter, '/some/path/WobblE.md', false); // confirm case-sensitive
-        testTaskFilterForTaskWithPath(filter, '/other/path/file.md', false);
+        expect(filter).toBeValid();
+        expect(filter).not.toMatchTaskWithPath('');
+        expect(filter).toMatchTaskWithPath('/some/path/wibble.md');
+        expect(filter).toMatchTaskWithPath('/some/path/wobble.md');
+        expect(filter).not.toMatchTaskWithPath('/some/path/WobblE.md'); // confirm case-sensitive
+        expect(filter).not.toMatchTaskWithPath('/other/path/file.md');
     });
 });
