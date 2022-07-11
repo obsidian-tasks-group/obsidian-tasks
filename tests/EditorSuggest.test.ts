2d8375acdefc652737c4c6fd8956b27d2d717f0a
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2022-07-11 18:42:10 +0100

    test: Format snapshot content as table columns

--- tests/EditorSuggest.test.ts
+++ tests/EditorSuggest.test.ts
@@ -122,7 +122,7 @@ describe('auto-complete', () => {
         ];
         const allSuggestions: string[] = [];
         for (const line of lines) {
-            allSuggestions.push(line);
+            allSuggestions.push(`| ${line} | |`);
             const suggestions: SuggestInfo[] = buildSuggestions(
                 line,
                 line.length - 1,
@@ -136,78 +136,79 @@ describe('auto-complete', () => {
                 if (replacementText === '\n') {
                     replacementText = '<new line>';
                 }
-                const suggestionAsText = `${suggestion.displayText}: ${replacementText}`;
+                // Format for pasting in to a Markdown table:
+                const suggestionAsText = `| ${suggestion.displayText} | ${replacementText} |`;
                 if (!allSuggestions.includes(suggestionAsText)) {
                     allSuggestions.push(suggestionAsText);
                 }
             }
         }
 
-        expect(allSuggestions).toMatchInlineSnapshot(`
-            Array [
-              "- [ ] some task",
-              "⏎: <new line>",
-              "📅 due date: 📅 ",
-              "🛫 start date: 🛫 ",
-              "⏳ scheduled date: ⏳ ",
-              "⏫ high priority: ⏫ ",
-              "🔼 medium priority: 🔼 ",
-              "🔽 low priority: 🔽 ",
-              "🔁 recurring (repeat): 🔁 ",
-              "- [ ] some task 🔁 ",
-              "every: 🔁 every ",
-              "every day: 🔁 every day ",
-              "every week: 🔁 every week ",
-              "every month: 🔁 every month ",
-              "every month on the: 🔁 every month on the ",
-              "every year: 🔁 every year ",
-              "every week on Sunday: 🔁 every week on Sunday ",
-              "every week on Monday: 🔁 every week on Monday ",
-              "every week on Tuesday: 🔁 every week on Tuesday ",
-              "every week on Wednesday: 🔁 every week on Wednesday ",
-              "every week on Thursday: 🔁 every week on Thursday ",
-              "every week on Friday: 🔁 every week on Friday ",
-              "every week on Saturday: 🔁 every week on Saturday ",
-              "- [ ] some task 📅 ",
-              "today (2022-07-11): 📅 2022-07-11 ",
-              "tomorrow (2022-07-12): 📅 2022-07-12 ",
-              "Sunday (2022-07-17): 📅 2022-07-17 ",
-              "Monday (2022-07-18): 📅 2022-07-18 ",
-              "Tuesday (2022-07-12): 📅 2022-07-12 ",
-              "Wednesday (2022-07-13): 📅 2022-07-13 ",
-              "Thursday (2022-07-14): 📅 2022-07-14 ",
-              "Friday (2022-07-15): 📅 2022-07-15 ",
-              "Saturday (2022-07-16): 📅 2022-07-16 ",
-              "next week (2022-07-18): 📅 2022-07-18 ",
-              "next month (2022-08-11): 📅 2022-08-11 ",
-              "next year (2023-07-11): 📅 2023-07-11 ",
-              "- [ ] some task ⏳ ",
-              "today (2022-07-11): ⏳ 2022-07-11 ",
-              "tomorrow (2022-07-12): ⏳ 2022-07-12 ",
-              "Sunday (2022-07-17): ⏳ 2022-07-17 ",
-              "Monday (2022-07-18): ⏳ 2022-07-18 ",
-              "Tuesday (2022-07-12): ⏳ 2022-07-12 ",
-              "Wednesday (2022-07-13): ⏳ 2022-07-13 ",
-              "Thursday (2022-07-14): ⏳ 2022-07-14 ",
-              "Friday (2022-07-15): ⏳ 2022-07-15 ",
-              "Saturday (2022-07-16): ⏳ 2022-07-16 ",
-              "next week (2022-07-18): ⏳ 2022-07-18 ",
-              "next month (2022-08-11): ⏳ 2022-08-11 ",
-              "next year (2023-07-11): ⏳ 2023-07-11 ",
-              "- [ ] some task 🛫 ",
-              "today (2022-07-11): 🛫 2022-07-11 ",
-              "tomorrow (2022-07-12): 🛫 2022-07-12 ",
-              "Sunday (2022-07-17): 🛫 2022-07-17 ",
-              "Monday (2022-07-18): 🛫 2022-07-18 ",
-              "Tuesday (2022-07-12): 🛫 2022-07-12 ",
-              "Wednesday (2022-07-13): 🛫 2022-07-13 ",
-              "Thursday (2022-07-14): 🛫 2022-07-14 ",
-              "Friday (2022-07-15): 🛫 2022-07-15 ",
-              "Saturday (2022-07-16): 🛫 2022-07-16 ",
-              "next week (2022-07-18): 🛫 2022-07-18 ",
-              "next month (2022-08-11): 🛫 2022-08-11 ",
-              "next year (2023-07-11): 🛫 2023-07-11 ",
-            ]
+        expect('\n' + allSuggestions.join('\n') + '\n').toMatchInlineSnapshot(`
+            "
+            | - [ ] some task | |
+            | ⏎ | <new line> |
+            | 📅 due date | 📅  |
+            | 🛫 start date | 🛫  |
+            | ⏳ scheduled date | ⏳  |
+            | ⏫ high priority | ⏫  |
+            | 🔼 medium priority | 🔼  |
+            | 🔽 low priority | 🔽  |
+            | 🔁 recurring (repeat) | 🔁  |
+            | - [ ] some task 🔁  | |
+            | every | 🔁 every  |
+            | every day | 🔁 every day  |
+            | every week | 🔁 every week  |
+            | every month | 🔁 every month  |
+            | every month on the | 🔁 every month on the  |
+            | every year | 🔁 every year  |
+            | every week on Sunday | 🔁 every week on Sunday  |
+            | every week on Monday | 🔁 every week on Monday  |
+            | every week on Tuesday | 🔁 every week on Tuesday  |
+            | every week on Wednesday | 🔁 every week on Wednesday  |
+            | every week on Thursday | 🔁 every week on Thursday  |
+            | every week on Friday | 🔁 every week on Friday  |
+            | every week on Saturday | 🔁 every week on Saturday  |
+            | - [ ] some task 📅  | |
+            | today (2022-07-11) | 📅 2022-07-11  |
+            | tomorrow (2022-07-12) | 📅 2022-07-12  |
+            | Sunday (2022-07-17) | 📅 2022-07-17  |
+            | Monday (2022-07-18) | 📅 2022-07-18  |
+            | Tuesday (2022-07-12) | 📅 2022-07-12  |
+            | Wednesday (2022-07-13) | 📅 2022-07-13  |
+            | Thursday (2022-07-14) | 📅 2022-07-14  |
+            | Friday (2022-07-15) | 📅 2022-07-15  |
+            | Saturday (2022-07-16) | 📅 2022-07-16  |
+            | next week (2022-07-18) | 📅 2022-07-18  |
+            | next month (2022-08-11) | 📅 2022-08-11  |
+            | next year (2023-07-11) | 📅 2023-07-11  |
+            | - [ ] some task ⏳  | |
+            | today (2022-07-11) | ⏳ 2022-07-11  |
+            | tomorrow (2022-07-12) | ⏳ 2022-07-12  |
+            | Sunday (2022-07-17) | ⏳ 2022-07-17  |
+            | Monday (2022-07-18) | ⏳ 2022-07-18  |
+            | Tuesday (2022-07-12) | ⏳ 2022-07-12  |
+            | Wednesday (2022-07-13) | ⏳ 2022-07-13  |
+            | Thursday (2022-07-14) | ⏳ 2022-07-14  |
+            | Friday (2022-07-15) | ⏳ 2022-07-15  |
+            | Saturday (2022-07-16) | ⏳ 2022-07-16  |
+            | next week (2022-07-18) | ⏳ 2022-07-18  |
+            | next month (2022-08-11) | ⏳ 2022-08-11  |
+            | next year (2023-07-11) | ⏳ 2023-07-11  |
+            | - [ ] some task 🛫  | |
+            | today (2022-07-11) | 🛫 2022-07-11  |
+            | tomorrow (2022-07-12) | 🛫 2022-07-12  |
+            | Sunday (2022-07-17) | 🛫 2022-07-17  |
+            | Monday (2022-07-18) | 🛫 2022-07-18  |
+            | Tuesday (2022-07-12) | 🛫 2022-07-12  |
+            | Wednesday (2022-07-13) | 🛫 2022-07-13  |
+            | Thursday (2022-07-14) | 🛫 2022-07-14  |
+            | Friday (2022-07-15) | 🛫 2022-07-15  |
+            | Saturday (2022-07-16) | 🛫 2022-07-16  |
+            | next week (2022-07-18) | 🛫 2022-07-18  |
+            | next month (2022-08-11) | 🛫 2022-08-11  |
+            | next year (2023-07-11) | 🛫 2023-07-11  |
+            "
         `);
 
         // todaySpy.mockClear();
