5b50b3acfc7d8969ce37e26fb29c67679789566c
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2022-07-23 17:10:11 +0100

    feat: Add HappensDateField.earliestDate()
    This will make it easier to group and sort by 'happens'.
    
    The tests are horribly repetitious, and will be shortened soon.

--- tests/Query/Filter/HappensDateField.test.ts
+++ tests/Query/Filter/HappensDateField.test.ts
@@ -49,3 +49,59 @@ describe('happens date', () => {
         testFilter(filter, new TaskBuilder().doneDate('2022-04-15'), true);
     });
 });
+
+describe('accessing earliest happens date', () => {
+    it('should return none if no dates set', () => {
+        expect(
+            new HappensDateField().earliestDate(new TaskBuilder().build()),
+        ).toBeNull();
+    });
+
+    it('should return due if only date set', () => {
+        const earliest = new HappensDateField().earliestDate(
+            new TaskBuilder().dueDate('1989-12-17').build(),
+        );
+        expect({
+            earliest: earliest?.format('YYYY-MM-DD'),
+        }).toMatchObject({
+            earliest: '1989-12-17',
+        });
+    });
+
+    it('should return start if only date set', () => {
+        const earliest = new HappensDateField().earliestDate(
+            new TaskBuilder().startDate('1989-12-17').build(),
+        );
+        expect({
+            earliest: earliest?.format('YYYY-MM-DD'),
+        }).toMatchObject({
+            earliest: '1989-12-17',
+        });
+    });
+
+    it('should return scheduled if only date set', () => {
+        const earliest = new HappensDateField().earliestDate(
+            new TaskBuilder().scheduledDate('1989-12-17').build(),
+        );
+        expect({
+            earliest: earliest?.format('YYYY-MM-DD'),
+        }).toMatchObject({
+            earliest: '1989-12-17',
+        });
+    });
+
+    it('should return earliest if all dates set', () => {
+        const earliest = new HappensDateField().earliestDate(
+            new TaskBuilder()
+                .dueDate('1989-12-17')
+                .startDate('1999-12-17')
+                .scheduledDate('2009-12-17')
+                .build(),
+        );
+        expect({
+            earliest: earliest?.format('YYYY-MM-DD'),
+        }).toMatchObject({
+            earliest: '1989-12-17',
+        });
+    });
+});
