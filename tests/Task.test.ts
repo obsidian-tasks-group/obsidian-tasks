86419464f1c3268e0540940c499c0a50b5ae4057
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2022-07-26 10:55:21 +0100

    fix: Escape $ character in global filter

--- tests/Task.test.ts
+++ tests/Task.test.ts
@@ -1030,6 +1030,9 @@ describe('check removal of the global filter exhaustively', () => {
         {
             globalFilter: '^',
         },
+        {
+            globalFilter: '$',
+        },
     ])(
         'should parse global filter "$globalFilter" edge cases correctly',
         ({ globalFilter }) => {
