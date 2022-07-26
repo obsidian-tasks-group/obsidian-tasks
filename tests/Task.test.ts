b75256691e7147118b9a98cd3a7fe4f3aa23f1ac
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2022-07-26 11:03:12 +0100

    fix: Escape } character in global filter

--- tests/Task.test.ts
+++ tests/Task.test.ts
@@ -1039,6 +1039,9 @@ describe('check removal of the global filter exhaustively', () => {
         {
             globalFilter: '{',
         },
+        {
+            globalFilter: '}',
+        },
     ])(
         'should parse global filter "$globalFilter" edge cases correctly',
         ({ globalFilter }) => {
