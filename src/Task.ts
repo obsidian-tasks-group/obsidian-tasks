5aaacf6c3c5416c87fff403a1bc94c66cdcab8fe
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2022-08-04 18:28:21 +0100

    fix: Fix unreleased global filter code on unusual characters (#976)
    * test: Extra global filter tests - 2 fail, git hooks disabled
    
    TODO Make the test more succinct
    
    * test: Interim commit - 1 fail, git hooks disabled
    
    Work towards making the new tests more succinct
    
    * test: Interim commit - multiple fails, git hooks disabled
    
    Work towards making the new tests more succinct
    
    * fix: Escape * character in global filter
    
    * fix: Escape . character in global filter
    
    * test: Remove unwanted console output in test
    
    * fix: Escape + character in global filter
    
    * fix: Escape ? character in global filter
    
    * fix: Escape ^ character in global filter
    
    * fix: Escape $ character in global filter
    
    * comment: Note some special characters that do not fail
    
    when used on their own as global filter.
    So a better test will be needed to show why they need to
    be escaped.
    
    * fix: Escape { character in global filter
    
    * fix: Escape } character in global filter
    
    * fix: Escape ( character in global filter
    
    * fix: Escape ) character in global filter
    
    * comment: Add comment-out copy of original regexp
    
    * fix: Escape | character in global filter
    
    * fix: Escape [ character in global filter
    
    * fix: Escape ] character in global filter
    
    This fixes the missing escape in the original implementation
    
    * fix: Escape \ character in global filter
    
    * fix: Escape / character in global filter
    
    NB No failing test for this. I was not able to
    find a global filter pattern with '/' in that made the tests fail
    
    * test: Tests indicated I should not escape = character
    
    * test: Tests indicated I should not escape ! character
    
    * test: Tests indicated I should not escape : character
    
    * comment: Remove comments showing original escapeRegExp()
    
    This is a manual revert of 2fe7b654d818e243a780d1357b91f35e49233168
    
    * comment: Reinstate * which I lost in merge
    
    * comment: Add explanation of non-escaped characters
    
    I copied the text from @AnnaKornfeldSimpson's explanation in:
    https://github.com/esm7/obsidian-tasks/pull/18#issuecomment-1196115407
    
    Co-Authored-By: Anna Kornfeld Simpson <AnnaKornfeldSimpson@users.noreply.github.com>
    
    * comment: Add to explain the character selection in test
    
    Co-authored-by: Erez Shermer <erezshermer@gmail.com>
    Co-authored-by: Anna Kornfeld Simpson <AnnaKornfeldSimpson@users.noreply.github.com>

--- src/Task.ts
+++ src/Task.ts
@@ -867,7 +867,20 @@ export class Task {
      * Taken from https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_Expressions#escaping
      */
     private escapeRegExp(s: string) {
-        return s.replace(/([.*+?^=!:${}()|[]\/\\])/g, '\\$1');
+        // NOTE: = is not escaped, as doing so gives error:
+        //         Invalid regular expression: /(^|\s)hello\=world($|\s)/: Invalid escape
+        // NOTE: ! is not escaped, as doing so gives error:
+        //         Invalid regular expression: /(^|\s)hello\!world($|\s)/: Invalid escape
+        // NOTE: : is not escaped, as doing so gives error:
+        //         Invalid regular expression: /(^|\s)hello\:world($|\s)/: Invalid escape
+        //
+        // Explanation from @AnnaKornfeldSimpson in:
+        // https://github.com/esm7/obsidian-tasks/pull/18#issuecomment-1196115407
+        // From what I can tell, the three missing characters from the original regex - : ! =
+        // are all only considered to have special meanings if they directly follow
+        // a ? (all 3) or a ?< (! and =).
+        // So theoretically if the ? are all escaped, those three characters do not have to be.
+        return s.replace(/([.*+?^${}()|[\]/\\])/g, '\\$1');
     }
 
     /**
