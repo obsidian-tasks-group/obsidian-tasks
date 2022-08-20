2716a5c91f6b28654e70450da799bb26581bffdd
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2022-08-20 18:23:07 +0100

    refactor: Extract interface StringMatcher

---
+++ src/Query/Matchers/StringMatcher.ts
@@ -0,0 +1,3 @@
+export interface StringMatcher {
+    matches(stringToSearch: string): boolean;
+}
--- src/Query/Matchers/SubstringMatcher.ts
+++ src/Query/Matchers/SubstringMatcher.ts
@@ -1,13 +1,14 @@
 import { TextField } from '../Filter/TextField';
+import type { StringMatcher } from './StringMatcher';
 
-export class SubstringMatcher {
+export class SubstringMatcher implements StringMatcher {
     private readonly stringToFind: string;
 
     constructor(stringToFind: string) {
         this.stringToFind = stringToFind;
     }
 
-    public matches(stringToSearch: string): boolean {
+    matches(stringToSearch: string): boolean {
         return TextField.stringIncludesCaseInsensitive(
             stringToSearch,
             this.stringToFind,
