9c908cb2488d60accfdbec82a42429142463e9df
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2022-08-20 21:44:01 +0100

    comment: Add JSDoc docs for new classes

--- src/Query/Matchers/RegexMatcher.ts
+++ src/Query/Matchers/RegexMatcher.ts
@@ -1,12 +1,28 @@
 import type { IStringMatcher } from './IStringMatcher';
 
+/**
+ * Regular-expression-based implementation of IStringMatcher.
+ */
 export class RegexMatcher implements IStringMatcher {
     private readonly regex: RegExp;
 
+    /**
+     * Construct a RegexMatcher object.
+     *
+     * @param regex {RegExp} - A valid regular expression
+     */
     constructor(regex: RegExp) {
         this.regex = regex;
     }
 
+    /**
+     * Construct a RegexMatcher object if the supplied string is a valid regular expression.
+     * and null if not.
+     *
+     * @param {string} regexInput - A string that can be converted to a regular expression.
+     *                              It must begin with a /, and end either with / and optionally any
+     *                              valid flags.
+     */
     static validateAndConstruct(regexInput: string): RegexMatcher | null {
         // Courtesy of https://stackoverflow.com/questions/17843691/javascript-regex-to-match-a-regex
         const regexPattern =
--- src/Query/Matchers/SubstringMatcher.ts
+++ src/Query/Matchers/SubstringMatcher.ts
@@ -1,9 +1,20 @@
 import { TextField } from '../Filter/TextField';
 import type { IStringMatcher } from './IStringMatcher';
 
+/**
+ * Substring-based implementation of IStringMatcher.
+ *
+ * This does a case-insensitive search for the given string.
+ */
 export class SubstringMatcher implements IStringMatcher {
     private readonly stringToFind: string;
 
+    /**
+     * Construct a SubstringMatcher object
+     *
+     * @param {string} stringToFind - The string to search for.
+     *                                Searches will be case-insensitive.
+     */
     constructor(stringToFind: string) {
         this.stringToFind = stringToFind;
     }
