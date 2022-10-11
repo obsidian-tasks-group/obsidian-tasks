564eb1db42dc3c2b30cff5bf3ac2fb38fe97706f
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2022-10-11 17:47:47 +0100

    comment: Update TODOs

--- src/Query/Filter/Filter.ts
+++ src/Query/Filter/Filter.ts
@@ -4,8 +4,11 @@ import type { Task } from '../../Task';
  * A filtering function, that takes a Task object and returns
  * whether it matches a particular filtering instruction.
  */
+// TODO Rename to FilterFunction
 export type Filter = (task: Task) => boolean;
 
+// TODO Rename to Filter
+// TODO Add docs
 export class NewFilter {
     instruction: string;
     private _filterFunction: Filter;
@@ -15,10 +18,14 @@ export class NewFilter {
         this._filterFunction = filterFunction;
     }
 
+    // TODO Add operator that tests a Task matches - to remove need to call newFilter.filterFunction
+
+    // TODO Can I remove getter?
     public get filterFunction(): Filter {
         return this._filterFunction;
     }
 
+    // TODO Can I remove setter?
     public set filterFunction(value: Filter) {
         this._filterFunction = value;
     }
@@ -46,10 +53,12 @@ export class FilterOrErrorMessage {
         this.instruction = instruction;
     }
 
+    // TODO Rename this to filter()
     public get newFilter(): NewFilter | undefined {
         return this._filter;
     }
 
+    // TODO Can I remove this?
     public get filter(): Filter | undefined {
         if (this._filter) {
             return this._filter.filterFunction;
@@ -58,6 +67,7 @@ export class FilterOrErrorMessage {
         }
     }
 
+    // TODO Can I remove this?
     public set filter(value: Filter | undefined) {
         if (value) {
             this._filter = new NewFilter(this.instruction, value);
