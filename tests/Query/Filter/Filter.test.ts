88a387f1b992b59f291a7b81d20ed7de862d23e6
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2022-10-11 09:16:17 +0100

    test: Strengthen tests in Filter.test.ts

--- tests/Query/Filter/Filter.test.ts
+++ tests/Query/Filter/Filter.test.ts
@@ -1,4 +1,11 @@
 import { Filter, FilterOrErrorMessage } from '../../../src/Query/Filter/Filter';
+import type { FilterFunction } from '../../../src/Query/Filter/Filter';
+import type { Task } from '../../../src/Task';
+import { toMatchTaskFromLine } from '../../CustomMatchers/CustomMatchersForFilters';
+
+expect.extend({
+    toMatchTaskFromLine,
+});
 
 describe('Filter', () => {
     it('should create an undefined Filter object', () => {
@@ -12,5 +19,23 @@ describe('FilterOrErrorMessage', () => {
         const filterOrErrorMessage = new FilterOrErrorMessage();
         expect(filterOrErrorMessage.filter).toBeUndefined();
         expect(filterOrErrorMessage.error).toBeUndefined();
+    });
+
+    it('should create FilterOrErrorMessage object for filter', () => {
+        const filterFunction: FilterFunction = (task: Task) => {
+            return task.description.length > 20;
+        };
+        const filterOrErrorMessage = FilterOrErrorMessage.fromFilter(filterFunction);
+        expect(filterOrErrorMessage.filter).not.toBeUndefined();
+        expect(filterOrErrorMessage.error).toBeUndefined();
+
+        expect(filterOrErrorMessage).toMatchTaskFromLine('- [ ] long task name .....................');
+        expect(filterOrErrorMessage).not.toMatchTaskFromLine('- [ ] short name');
+    });
+
+    it('should create FilterOrErrorMessage object for error', () => {
+        const filterOrErrorMessage = FilterOrErrorMessage.fromError('error happened');
+        expect(filterOrErrorMessage.filter).toBeUndefined();
+        expect(filterOrErrorMessage.error).toEqual('error happened');
     });
 });
