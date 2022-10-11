143e8074977225582adbab8cf1924cf39e431677
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2022-10-11 09:05:25 +0100

    test: Add test for FilterOrErrorMessage

--- tests/Query/Filter/Filter.test.ts
+++ tests/Query/Filter/Filter.test.ts
@@ -1,8 +1,16 @@
-import { Filter } from '../../../src/Query/Filter/Filter';
+import { Filter, FilterOrErrorMessage } from '../../../src/Query/Filter/Filter';
 
 describe('Filter', () => {
     it('should create an undefined Filter object', () => {
         const filter = new Filter(undefined);
         expect(filter.filterFunction).toBeUndefined();
+    });
+});
+
+describe('FilterOrErrorMessage', () => {
+    it('should create an empty FilterOrErrorMessage object', () => {
+        const filterOrErrorMessage = new FilterOrErrorMessage();
+        expect(filterOrErrorMessage.filter).toBeUndefined();
+        expect(filterOrErrorMessage.error).toBeUndefined();
     });
 });
