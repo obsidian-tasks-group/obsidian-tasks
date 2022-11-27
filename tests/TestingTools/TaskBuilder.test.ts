3500bbd2050512283493873b36acf7260ca0b2a3
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2022-11-27 12:22:18 +0000

    test: Make TaskBuilder.status() set consistent originalStatusCharacter

--- tests/TestingTools/TaskBuilder.test.ts
+++ tests/TestingTools/TaskBuilder.test.ts
@@ -1,3 +1,4 @@
+import { Status } from '../../src/Task';
 import { TaskBuilder } from './TaskBuilder';
 
 export {};
@@ -7,5 +8,39 @@ describe('TaskBuilder', () => {
         const builder = new TaskBuilder().description('hello').tags(['#tag1', '#tag2']);
         const task = builder.build();
         expect(task.toFileLineString()).toStrictEqual('- [ ] hello #tag1 #tag2');
+    });
+
+    describe('status and originalStatusCharacter', () => {
+        it('should set custom status character when setting status TODO', () => {
+            const builder = new TaskBuilder().status(Status.TODO);
+            const task = builder.build();
+            expect(task.status).toStrictEqual(Status.TODO);
+            expect(task.originalStatusCharacter).toStrictEqual(' ');
+            expect(task.toFileLineString()).toStrictEqual('- [ ] my description');
+        });
+
+        it('should set custom status character when setting status DONE', () => {
+            const builder = new TaskBuilder().status(Status.DONE);
+            const task = builder.build();
+            expect(task.status).toStrictEqual(Status.DONE);
+            expect(task.originalStatusCharacter).toStrictEqual('x');
+            expect(task.toFileLineString()).toStrictEqual('- [x] my description');
+        });
+
+        it('should not change status when changing originalStatusCharacter to TODO', () => {
+            const builder = new TaskBuilder().status(Status.DONE).originalStatusCharacter(' ');
+            const task = builder.build();
+            expect(task.status).toStrictEqual(Status.DONE);
+            expect(task.originalStatusCharacter).toStrictEqual(' ');
+            expect(task.toFileLineString()).toStrictEqual('- [ ] my description');
+        });
+
+        it('should not change status when changing originalStatusCharacter to Half Done', () => {
+            const builder = new TaskBuilder().status(Status.DONE).originalStatusCharacter('/');
+            const task = builder.build();
+            expect(task.status).toStrictEqual(Status.DONE);
+            expect(task.originalStatusCharacter).toStrictEqual('/');
+            expect(task.toFileLineString()).toStrictEqual('- [/] my description');
+        });
     });
 });
