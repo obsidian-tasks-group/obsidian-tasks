2cd698fac5bb6bec5a4687106c453f64a4aa654f
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2022-12-11 18:25:06 +0000

    test: More test cases for CreateOrEditTaskParser.ts

--- tests/Commands/CreateOrEditTaskParser.test.ts
+++ tests/Commands/CreateOrEditTaskParser.test.ts
@@ -8,4 +8,20 @@ describe('CreateOrEditTaskParser', () => {
         expect(task.toFileLineString()).toStrictEqual(line);
         expect(task.path).toStrictEqual(path);
     });
+
+    it('should process a valid indented Tasks task', () => {
+        const line = '    - [ ] Hello World';
+        const path = 'a/b/c.md';
+        const task = taskFromLine({ line, path });
+        expect(task.toFileLineString()).toStrictEqual(line);
+        expect(task.path).toStrictEqual(path);
+    });
+
+    it('should support running command on blank line', () => {
+        const line = '';
+        const path = 'a/b/c.md';
+        const task = taskFromLine({ line, path });
+        expect(task.toFileLineString()).toStrictEqual('- [ ] ');
+        expect(task.path).toStrictEqual(path);
+    });
 });
