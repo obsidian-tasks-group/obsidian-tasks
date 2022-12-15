9490844663670ef62de80da4046f76a3ac0b2d80
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2022-12-15 20:34:30 +0000

    comment: Add a TODO

--- src/Query/Filter/DueDateField.ts
+++ src/Query/Filter/DueDateField.ts
@@ -72,6 +72,7 @@ export class DueDateField extends DateField {
      * Return a function to compare two Task objects, for use in sorting by due.
      */
     public static comparator(): Comparator {
+        // TODO Refactor to make non-static and use this.date(a), this.date(b)
         return (a: Task, b: Task) => {
             return Sort.compareByDate(a.dueDate, b.dueDate);
         };
