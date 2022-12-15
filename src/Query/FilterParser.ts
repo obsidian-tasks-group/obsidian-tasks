6ce4f72940d74b15786bf048c460f03f62ab2271
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2022-12-15 22:02:28 +0000

    refactor: Move parsing of new sort lines to FilterParser

--- src/Query/FilterParser.ts
+++ src/Query/FilterParser.ts
@@ -15,6 +15,7 @@ import { BooleanField } from './Filter/BooleanField';
 import { FilenameField } from './Filter/FilenameField';
 
 import type { FilterOrErrorMessage } from './Filter/Filter';
+import type { Sorting } from './Sort';
 
 const fieldCreators = [
     () => new StatusField(),
@@ -40,4 +41,45 @@ export function parseFilter(filterString: string): FilterOrErrorMessage | null {
         if (field.canCreateFilterForLine(filterString)) return field.createFilterOrErrorMessage(filterString);
     }
     return null;
+}
+
+export function parseSorter(sorterString: string): Sorting | null {
+    // New style parsing, which is done by the Field classes.
+    // Initially this is only implemented for a few fields.
+    // TODO Once a few more Field classes have comparator implementations,
+    //      convert this to look like parseFilter(), looping over all field types.
+
+    const sortByRegexp = /^sort by (\S+)( reverse)?/;
+    const fieldMatch = sorterString.match(sortByRegexp);
+    if (fieldMatch === null) {
+        return null;
+    }
+    const propertyName = fieldMatch[1];
+    const reverse = !!fieldMatch[2];
+
+    let field;
+    switch (propertyName) {
+        case 'status':
+            field = new StatusField();
+            break;
+        case 'due':
+            field = new DueDateField();
+            break;
+    }
+
+    if (!field) {
+        return null;
+    }
+
+    let sorter;
+    if (reverse) {
+        sorter = field.createReverseSorter();
+    } else {
+        sorter = field.createNormalSorter();
+    }
+
+    if (!sorter) {
+        return null;
+    }
+    return sorter;
 }
