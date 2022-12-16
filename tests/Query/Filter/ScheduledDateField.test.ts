fc82c0e8589490d8ead8c44e1f97db6270239e00
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2022-12-16 21:37:45 +0000

    test: Add new-style tests for sorting of Done, Scheduled and Start dates.
    These are cut-down versions of the more thorough tests in DueDateField.test.ts

--- tests/Query/Filter/ScheduledDateField.test.ts
+++ tests/Query/Filter/ScheduledDateField.test.ts
@@ -4,6 +4,9 @@
 import moment from 'moment';
 import { toHaveExplanation } from '../../CustomMatchers/CustomMatchersForFilters';
 import { ScheduledDateField } from '../../../src/Query/Filter/ScheduledDateField';
+import { TaskBuilder } from '../../TestingTools/TaskBuilder';
+import { expectTaskComparesAfter, expectTaskComparesBefore } from '../../CustomMatchers/CustomMatchersForSorting';
+import { Sort } from '../../../src/Query/Sort';
 
 window.moment = moment;
 
@@ -20,5 +23,26 @@ describe('explain scheduled date queries', () => {
     it('implicit "on" gets added to explanation', () => {
         const filterOrMessage = new ScheduledDateField().createFilterOrErrorMessage('scheduled 2023-01-02');
         expect(filterOrMessage).toHaveExplanation('scheduled date is on 2023-01-02 (Monday 2nd January 2023)');
+    });
+});
+
+describe('sorting by scheduled', () => {
+    it('supports Field sorting methods correctly', () => {
+        const field = new ScheduledDateField();
+        expect(field.supportsSorting()).toEqual(false);
+    });
+
+    // These are minimal tests just to confirm basic behaviour is set up for this field.
+    // Thorough testing is done in DueDateField.test.ts.
+
+    const date1 = new TaskBuilder().scheduledDate('2021-01-12').build();
+    const date2 = new TaskBuilder().scheduledDate('2022-12-23').build();
+
+    it('sort by scheduled', () => {
+        expectTaskComparesBefore(Sort.makeLegacySorting(false, 1, 'scheduled'), date1, date2);
+    });
+
+    it('sort by scheduled reverse', () => {
+        expectTaskComparesAfter(Sort.makeLegacySorting(true, 1, 'scheduled'), date1, date2);
     });
 });
