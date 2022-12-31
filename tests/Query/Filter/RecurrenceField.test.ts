dc4b9e11844c898d772ec802463dfef8047cbab8
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2022-12-31 19:21:08 +0000

    test: Add more tests of recurrence filters.

--- tests/Query/Filter/RecurrenceField.test.ts
+++ tests/Query/Filter/RecurrenceField.test.ts
@@ -5,12 +5,13 @@ import moment from 'moment';
 
 import { RecurrenceField } from '../../../src/Query/Filter/RecurrenceField';
 import { TaskBuilder } from '../../TestingTools/TaskBuilder';
-import { toMatchTask } from '../../CustomMatchers/CustomMatchersForFilters';
+import { toBeValid, toMatchTask } from '../../CustomMatchers/CustomMatchersForFilters';
 import { RecurrenceBuilder } from '../../TestingTools/RecurrenceBuilder';
 
 window.moment = moment;
 
 expect.extend({
+    toBeValid,
     toMatchTask,
 });
 
@@ -25,6 +26,9 @@ describe('recurrence', () => {
         const field = new RecurrenceField();
         expect(field.value(new TaskBuilder().build())).toStrictEqual('');
         expect(field.value(with_recurrence('every Sunday when done'))).toStrictEqual('every week on Sunday when done');
+        expect(field.value(with_recurrence('every 6 months on the 2nd Wednesday'))).toStrictEqual(
+            'every 6 months on the 2nd Wednesday',
+        );
     });
 
     it('by recurrence (includes)', () => {
@@ -32,6 +36,19 @@ describe('recurrence', () => {
         const filter = new RecurrenceField().createFilterOrErrorMessage('recurrence includes wednesday');
 
         // Assert
+        expect(filter).toBeValid();
         expect(filter).toMatchTask(with_recurrence('every Wednesday'));
+        expect(filter).not.toMatchTask(new TaskBuilder().build());
+    });
+
+    it('by recurrence (does not include)', () => {
+        // Arrange
+        const filter = new RecurrenceField().createFilterOrErrorMessage('recurrence does not include when done');
+
+        // Assert
+        expect(filter).toBeValid();
+        expect(filter).toMatchTask(new TaskBuilder().build());
+        expect(filter).toMatchTask(with_recurrence('every week on Sunday'));
+        expect(filter).not.toMatchTask(with_recurrence('every 10 days when done'));
     });
 });
