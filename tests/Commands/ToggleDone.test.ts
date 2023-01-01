df58e23e520da79f44bf343c2b9129391f79440c
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2023-01-01 20:41:04 +0000

    test: Make test data a bit more varied

--- tests/Commands/ToggleDone.test.ts
+++ tests/Commands/ToggleDone.test.ts
@@ -188,27 +188,27 @@ describe('ToggleDone', () => {
         it('when there is a global filter and task with global filter is toggled', () => {
             updateSettings({ globalFilter: '#task' });
 
-            const line1 = '- [P] #task this is a task starting at Pro';
+            const line1 = '- [C] #task this is a task starting at Con';
 
             // Assert
             const line2 = toggleLine(line1, 'x.md');
-            expect(line2).toStrictEqual('- [C] #task this is a task starting at Pro');
+            expect(line2).toStrictEqual('- [P] #task this is a task starting at Con');
 
             const line3 = toggleLine(line2, 'x.md');
-            expect(line3).toStrictEqual('- [P] #task this is a task starting at Pro');
+            expect(line3).toStrictEqual('- [C] #task this is a task starting at Con');
         });
 
         it('when there is a global filter and task without global filter is toggled', () => {
             updateSettings({ globalFilter: '#task' });
 
-            const line1 = '- [P] this is a task starting at Pro';
+            const line1 = '- [P] this is a task starting at Pro, not matching the global filter';
 
             // Assert
             const line2 = toggleLine(line1, 'x.md');
-            expect(line2).toStrictEqual('- [C] this is a task starting at Pro');
+            expect(line2).toStrictEqual('- [C] this is a task starting at Pro, not matching the global filter');
 
             const line3 = toggleLine(line2, 'x.md');
-            expect(line3).toStrictEqual('- [P] this is a task starting at Pro');
+            expect(line3).toStrictEqual('- [P] this is a task starting at Pro, not matching the global filter');
         });
     });
 
