efae35c423a6a962df6167db42c4a436ab37a952
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2023-01-04 23:28:37 +0000

    test: Address 5 TODOs in Sort.test.ts (#1470)
    * test: Move DateField tests to better location
    
    * test: Address some TODOs in Sort.test.ts
    
    The test removed no longer has any value, as there
    is test code for the individual fields.

---
+++ tests/Query/Filter/DateField.test.ts
@@ -0,0 +1,32 @@
+/**
+ * @jest-environment jsdom
+ */
+import moment from 'moment';
+
+window.moment = moment;
+
+import {
+    expectDateComparesAfter,
+    expectDateComparesBefore,
+    expectDateComparesEqual,
+} from '../../CustomMatchers/CustomMatchersForSorting';
+
+// These are lower-level tests that the Task-based ones above, for ease of test coverage.
+describe('compareBy', () => {
+    it('compares correctly by date', () => {
+        const earlierDate = '2022-01-01';
+        const laterDate = '2022-02-01';
+        const invalidDate = '2022-02-30';
+
+        expectDateComparesBefore(earlierDate, laterDate);
+        expectDateComparesEqual(earlierDate, earlierDate);
+        expectDateComparesAfter(laterDate, earlierDate);
+
+        expectDateComparesAfter(null, earlierDate); // no date sorts after valid dates
+        expectDateComparesEqual(null, null);
+
+        expectDateComparesBefore(invalidDate, null); // invalid dates sort before no date
+        expectDateComparesEqual(invalidDate, invalidDate);
+        expectDateComparesAfter(invalidDate, earlierDate); // invalid dates sort after valid ones
+    });
+});
