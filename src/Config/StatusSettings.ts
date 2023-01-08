8eea65ecf8a74d7ee239be7d3e1387f834039c95
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2023-01-08 12:56:40 +0000

    refactor: Add SettingsTab.deleteCustomStatus()
    And adjust SettingsTab to use it.

--- src/Config/StatusSettings.ts
+++ src/Config/StatusSettings.ts
@@ -13,4 +13,18 @@ export class StatusSettings {
     public addCustomStatus(newStatus: StatusConfiguration) {
         this.customStatusTypes.push(newStatus);
     }
+
+    /**
+     * Delete the given custom status.
+     * Returns true if deleted, and false if not.
+     * @param status
+     */
+    public deleteCustomStatus(status: StatusConfiguration) {
+        const index = this.customStatusTypes.indexOf(status);
+        if (index <= -1) {
+            return false;
+        }
+        this.customStatusTypes.splice(index, 1);
+        return true;
+    }
 }
--- tests/Config/StatusSettings.test.ts
+++ tests/Config/StatusSettings.test.ts
@@ -1,6 +1,6 @@
 import { verifyAsJson } from 'approvals/lib/Providers/Jest/JestApprovals';
 import { StatusSettings } from '../../src/Config/StatusSettings';
-import { Status, StatusConfiguration } from '../../src/Status';
+import { StatusConfiguration } from '../../src/Status';
 
 describe('StatusSettings', () => {
     it('verify default status settings', () => {
@@ -14,11 +14,36 @@ describe('StatusSettings', () => {
         expect(settings.customStatusTypes.length).toEqual(0);
 
         // Act
-        const newStatus = new Status(new StatusConfiguration('!', 'Important', 'x', false));
+        const newStatus = new StatusConfiguration('!', 'Important', 'x', false);
         settings.addCustomStatus(newStatus);
 
         // Assert
         expect(settings.customStatusTypes.length).toEqual(1);
         expect(settings.customStatusTypes[0]).toStrictEqual(newStatus);
+    });
+
+    it('should delete a status', () => {
+        // Arrange
+        const settings = new StatusSettings();
+        const pro = new StatusConfiguration('P', 'Pro', 'C', false);
+        const imp = new StatusConfiguration('!', 'Important', 'x', false);
+        const con = new StatusConfiguration('C', 'Con', 'P', false);
+        settings.addCustomStatus(pro);
+        settings.addCustomStatus(imp);
+        settings.addCustomStatus(con);
+        expect(settings.customStatusTypes.length).toEqual(3);
+
+        // Act
+        const result = settings.deleteCustomStatus(imp);
+
+        // Assert
+        expect(result).toEqual(true);
+        expect(settings.customStatusTypes.length).toEqual(2);
+        expect(settings.customStatusTypes[0]).toStrictEqual(pro);
+        expect(settings.customStatusTypes[1]).toStrictEqual(con);
+
+        // Delete a second time. It should now report that nothing was deleted.
+        const result2 = settings.deleteCustomStatus(imp);
+        expect(result2).toEqual(false);
     });
 });
