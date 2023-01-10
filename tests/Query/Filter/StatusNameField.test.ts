b0f2ddcb8767cbd843b1a1f8c5ce85a2fe869386
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2023-01-10 17:35:58 +0000

    feat: Add status.name text search

---
+++ tests/Query/Filter/StatusNameField.test.ts
@@ -0,0 +1,31 @@
+import { StatusNameField } from '../../../src/Query/Filter/StatusNameField';
+import * as TestHelpers from '../../TestHelpers';
+import { toBeValid, toMatchTaskFromLine } from '../../CustomMatchers/CustomMatchersForFilters';
+
+expect.extend({
+    toBeValid,
+    toMatchTaskFromLine,
+});
+
+describe('status.name', () => {
+    it('value', () => {
+        // Arrange
+        const filter = new StatusNameField();
+
+        // Assert
+        expect(filter.value(TestHelpers.fromLine({ line: '- [ ] Xxx' }))).toStrictEqual('Todo');
+        expect(filter.value(TestHelpers.fromLine({ line: '- [/] Xxx' }))).toStrictEqual('In Progress');
+        expect(filter.value(TestHelpers.fromLine({ line: '- [x] Xxx' }))).toStrictEqual('Done');
+        expect(filter.value(TestHelpers.fromLine({ line: '- [-] Xxx' }))).toStrictEqual('Cancelled');
+    });
+
+    it('status.name includes', () => {
+        // Arrange
+        const filter = new StatusNameField().createFilterOrErrorMessage('status.name includes todo');
+
+        // Assert
+        expect(filter).toBeValid();
+        expect(filter).toMatchTaskFromLine('- [ ] Xxx');
+        expect(filter).not.toMatchTaskFromLine('- [x] Xxx');
+    });
+});
