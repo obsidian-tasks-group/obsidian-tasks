32df09268239054064e1d88af2c34a095138e90c
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2023-01-15 14:15:04 +0000

    feat: Add editing of status types
    These are not yet used outside of settings.

--- tests/Status.test.ts
+++ tests/Status.test.ts
@@ -11,21 +11,25 @@ window.moment = moment;
 describe('Status', () => {
     it('preview text', () => {
         const configuration = new Status(new StatusConfiguration('P', 'Pro', 'Con', true));
-        expect(configuration.previewText()).toEqual("- [P] Pro, next status is 'Con'. ");
+        expect(configuration.previewText()).toEqual("- [P] Pro, next status is 'Con', type is 'TODO'. ");
     });
 
     it('default configurations', () => {
-        expect(Status.DONE.previewText()).toEqual("- [x] Done, next status is ' '. ");
-        expect(Status.EMPTY.previewText()).toEqual("- [] EMPTY, next status is ''. ");
-        expect(Status.TODO.previewText()).toEqual("- [ ] Todo, next status is 'x'. ");
+        expect(Status.DONE.previewText()).toEqual("- [x] Done, next status is ' ', type is 'DONE'. ");
+        expect(Status.EMPTY.previewText()).toEqual("- [] EMPTY, next status is '', type is 'EMPTY'. ");
+        expect(Status.TODO.previewText()).toEqual("- [ ] Todo, next status is 'x', type is 'TODO'. ");
     });
 
     it('factory methods for default statuses', () => {
-        expect(Status.makeDone().previewText()).toEqual("- [x] Done, next status is ' '. ");
-        expect(Status.makeEmpty().previewText()).toEqual("- [] EMPTY, next status is ''. ");
-        expect(Status.makeTodo().previewText()).toEqual("- [ ] Todo, next status is 'x'. ");
-        expect(Status.makeCancelled().previewText()).toEqual("- [-] Cancelled, next status is ' '. ");
-        expect(Status.makeInProgress().previewText()).toEqual("- [/] In Progress, next status is 'x'. ");
+        expect(Status.makeDone().previewText()).toEqual("- [x] Done, next status is ' ', type is 'DONE'. ");
+        expect(Status.makeEmpty().previewText()).toEqual("- [] EMPTY, next status is '', type is 'EMPTY'. ");
+        expect(Status.makeTodo().previewText()).toEqual("- [ ] Todo, next status is 'x', type is 'TODO'. ");
+        expect(Status.makeCancelled().previewText()).toEqual(
+            "- [-] Cancelled, next status is ' ', type is 'CANCELLED'. ",
+        );
+        expect(Status.makeInProgress().previewText()).toEqual(
+            "- [/] In Progress, next status is 'x', type is 'IN_PROGRESS'. ",
+        );
     });
 
     it('should initialize with valid properties', () => {
