a89c418a7b6bc7d4ed4cb7f559b76335f2dcd185
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2023-01-20 14:27:09 +0000

    chore: Update the names in copyright names in main.js

--- esbuild.config.mjs
+++ esbuild.config.mjs
@@ -13,7 +13,7 @@ if you want to view the source visit the plugins github repository
 License obsidian-tasks:
 MIT License
 
-Copyright (c) 2021 Martin Schenck
+Copyright (c) 2021 Martin Schenck and Clare Macrae
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
