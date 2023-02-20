3f848af347a96460d3b875b8f7e600bb39969a8b
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2023-02-20 16:55:14 +0000

    feat: Add partial handling of invalid dates in ranges
    Co-Authored-By: Ilyas Landikov <93825870+ilandikov@users.noreply.github.com>

--- src/Query/DateParser.ts
+++ src/Query/DateParser.ts
@@ -1,5 +1,5 @@
 import * as chrono from 'chrono-node';
-import type moment from 'moment';
+import moment from 'moment';
 
 export class DateParser {
     public static parseDate(input: string, forwardDate: boolean = false): moment.Moment {
@@ -17,11 +17,16 @@ export class DateParser {
      * Parse a line and extract a pair of dates, returned in a tuple, sorted by date.
      * @param input - any pair of dates, separate by one or more spaces '17 August 2013 19 August 2013',
      *                or a single date.
+     * @return - A Tuple of dates. If both input dates are invalid, then both ouput dates will be invalid.
      */
     public static parseDateRange(input: string): [moment.Moment, moment.Moment] {
         const result = chrono.parse(input, undefined, {
             forwardDate: true,
         });
+
+        if (result.length === 0) {
+            return [moment.invalid(), moment.invalid()];
+        }
 
         const startDate = result[0].start;
         const endDate = result[1] && result[1].start ? result[1].start : startDate;
