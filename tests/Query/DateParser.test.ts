a445b6ce52ac3429a22b7970d1d57c872eb92eaa
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2023-02-20 16:05:49 +0000

    test: Extract method testParsingDateRange()
    Co-Authored-By: Ilyas Landikov <93825870+ilandikov@users.noreply.github.com>

--- tests/Query/DateParser.test.ts
+++ tests/Query/DateParser.test.ts
@@ -12,6 +12,20 @@ function testParsingeSingleDate(input: string, result: string) {
     expect(moment.format(TaskRegularExpressions.dateFormat)).toEqual(result);
 }
 
+function testParsingDateRange(input: string, expectedStart: string, expectedEnd: string) {
+    // Act
+    const result = DateParser.parseDateRange(input);
+
+    // Assert
+    const start = result[0];
+    expect(start).toBeDefined();
+    expect(start.format(TaskRegularExpressions.dateFormat)).toEqual(expectedStart);
+
+    const end = result[1];
+    expect(end).toBeDefined();
+    expect(end.format(TaskRegularExpressions.dateFormat)).toEqual(expectedEnd);
+}
+
 describe('DateParser - single dates', () => {
     it('should parse a valid fixed date correctly', () => {
         const input = '2021-03-17';
@@ -26,52 +40,16 @@ describe('DateParser - single dates', () => {
 describe('DateParser - date ranges', () => {
     it('should parse date range from natural dates', () => {
         // Arrange
-        const input = '17 August 2013 19 August 2013';
-
-        // Act
-        const result = DateParser.parseDateRange(input);
-
-        // Assert
-        const start = result[0];
-        expect(start).toBeDefined();
-        expect(start.format(TaskRegularExpressions.dateFormat)).toEqual('2013-08-17');
-
-        const end = result[1];
-        expect(end).toBeDefined();
-        expect(end.format(TaskRegularExpressions.dateFormat)).toEqual('2013-08-19');
+        testParsingDateRange('17 August 2013 19 August 2013', '2013-08-17', '2013-08-19');
     });
 
     it('should parse date range with  multiple spaces', () => {
-        // Arrange
-        const input = '2013-08-17   2014-08-19';
-
-        // Act
-        const result = DateParser.parseDateRange(input);
-
-        // Assert
-        const start = result[0];
-        expect(start).toBeDefined();
-        expect(start.format(TaskRegularExpressions.dateFormat)).toEqual('2013-08-17');
-
-        const end = result[1];
-        expect(end).toBeDefined();
-        expect(end.format(TaskRegularExpressions.dateFormat)).toEqual('2014-08-19');
+        testParsingDateRange('2013-08-17   2014-08-19', '2013-08-17', '2014-08-19');
     });
 
     it('should parse single date as date range', () => {
         // Arrange
         const input = '2019-12-28';
-
-        // Act
-        const result = DateParser.parseDateRange(input);
-
-        // Assert
-        const start = result[0];
-        expect(start).toBeDefined();
-        expect(start.format(TaskRegularExpressions.dateFormat)).toEqual(input);
-
-        const end = result[1];
-        expect(end).toBeDefined();
-        expect(end.format(TaskRegularExpressions.dateFormat)).toEqual(input);
+        testParsingDateRange(input, input, input);
     });
 });
