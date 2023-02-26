4089620e41957fe39d3ce5ee7165ad67aea5026b
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2023-02-26 20:43:15 +0000

    refactor: TaskLineLocation class now stores path

--- src/TaskLineLocation.ts
+++ src/TaskLineLocation.ts
@@ -2,4 +2,14 @@
  * TaskLineLocation will evolve in to the place where all information about a task line's location
  * in a markdown file will be stored, so that testable algorithms can then be added here.
  */
-export class TaskLineLocation {}
+export class TaskLineLocation {
+    private readonly _path: string;
+
+    public constructor(path: string) {
+        this._path = path;
+    }
+
+    public get path(): string {
+        return this._path;
+    }
+}
