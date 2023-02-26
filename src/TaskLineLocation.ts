81bba5729026a83133a7a6d1525ad7da06fb9771
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2023-02-26 20:36:38 +0000

    refactor: Add empty TaskLineLocation class
    I will gradually move code from File.ts to here

---
+++ src/TaskLineLocation.ts
@@ -0,0 +1,5 @@
+/**
+ * TaskLineLocation will evolve in to the place where all information about a task line's location
+ * in a markdown file will be stored, so that testable algorithms can then be added here.
+ */
+export class TaskLineLocation {}
