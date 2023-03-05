aff33171509ce3f78852af88f1d13f424d4998e4
kedestin <26238343+kedestin@users.noreply.github.com>
2023-03-05 02:57:11 -0500

    Make DefaultTaskSerializerSymbols interface, accepted by DefaultTaskSerializer

--- src/TaskSerializer/DefaultTaskSerializer.ts
+++ src/TaskSerializer/DefaultTaskSerializer.ts
@@ -1,8 +1,68 @@
 import type { Task } from '../Task';
 import type { TaskDetails, TaskSerializer } from '.';
 
+/* Interface describing the symbols that {@link DefaultTaskSerializer}
+ * uses to serialize and deserialize tasks.
+ *
+ * @export
+ * @interface DefaultTaskSerializerSymbols
+ */
+export interface DefaultTaskSerializerSymbols {
+    readonly prioritySymbols: {
+        High: string;
+        Medium: string;
+        Low: string;
+        None: string;
+    };
+    readonly startDateSymbol: string;
+    readonly createdDateSymbol: string;
+    readonly scheduledDateSymbol: string;
+    readonly dueDateSymbol: string;
+    readonly doneDateSymbol: string;
+    readonly recurrenceSymbol: string;
+    readonly TaskFormatRegularExpressions: {
+        priorityRegex: RegExp;
+        startDateRegex: RegExp;
+        createdDateRegex: RegExp;
+        scheduledDateRegex: RegExp;
+        dueDateRegex: RegExp;
+        doneDateRegex: RegExp;
+        recurrenceRegex: RegExp;
+    };
+}
+
+/**
+ * A symbol map for obsidian-task's default task style.
+ * Uses emojis to concisely convey meaning
+ */
+export const DEFAULT_SYMBOLS: DefaultTaskSerializerSymbols = {
+    prioritySymbols: {
+        High: '⏫',
+        Medium: '🔼',
+        Low: '🔽',
+        None: '',
+    },
+    startDateSymbol: '🛫',
+    createdDateSymbol: '➕',
+    scheduledDateSymbol: '⏳',
+    dueDateSymbol: '📅',
+    doneDateSymbol: '✅',
+    recurrenceSymbol: '🔁',
+    TaskFormatRegularExpressions: {
+        // The following regex's end with `$` because they will be matched and
+        // removed from the end until none are left.
+        priorityRegex: /([⏫🔼🔽])$/u,
+        startDateRegex: /🛫 *(\d{4}-\d{2}-\d{2})$/u,
+        createdDateRegex: /➕ *(\d{4}-\d{2}-\d{2})$/u,
+        scheduledDateRegex: /[⏳⌛] *(\d{4}-\d{2}-\d{2})$/u,
+        dueDateRegex: /[📅📆🗓] *(\d{4}-\d{2}-\d{2})$/u,
+        doneDateRegex: /✅ *(\d{4}-\d{2}-\d{2})$/u,
+        recurrenceRegex: /🔁 ?([a-zA-Z0-9, !]+)$/iu,
+    },
+} as const;
+
 export class DefaultTaskSerializer implements TaskSerializer {
-    constructor() {}
+    constructor(public readonly symbols: DefaultTaskSerializerSymbols) {}
 
     /* Convert a task to its string representation
      *
