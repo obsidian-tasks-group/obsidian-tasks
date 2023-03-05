54cfb1f0ef8b5c9fa9866632131208dc72ee3b4b
kedestin <26238343+kedestin@users.noreply.github.com>
2023-03-05 03:26:25 -0500

    Implement DefaultTaskSerializer.serialize
        Basically a verbatim copy of Task.toString

--- src/TaskSerializer/DefaultTaskSerializer.ts
+++ src/TaskSerializer/DefaultTaskSerializer.ts
@@ -1,4 +1,6 @@
-import type { Task } from '../Task';
+import { TaskLayout } from '../TaskLayout';
+import type { TaskLayoutComponent } from '../TaskLayout';
+import { Priority, type Task, TaskRegularExpressions } from '../Task';
 import type { TaskDetails, TaskSerializer } from '.';
 
 /* Interface describing the symbols that {@link DefaultTaskSerializer}
@@ -71,8 +73,78 @@ export class DefaultTaskSerializer implements TaskSerializer {
      * @return The string representation of the task
      */
     public serialize(task: Task): string {
-        task;
-        throw new Error('Not implemented');
+        const taskLayout = new TaskLayout();
+        let taskString = '';
+        for (const component of taskLayout.layoutComponents) {
+            taskString += this.componentToString(task, taskLayout, component);
+        }
+        return taskString;
+    }
+
+    /**
+     * Renders a specific TaskLayoutComponent of the task (its description, priority, etc) as a string.
+     */
+    public componentToString(task: Task, layout: TaskLayout, component: TaskLayoutComponent) {
+        const {
+            prioritySymbols,
+            startDateSymbol,
+            createdDateSymbol,
+            scheduledDateSymbol,
+            doneDateSymbol,
+            recurrenceSymbol,
+            dueDateSymbol,
+        } = this.symbols;
+
+        switch (component) {
+            case 'description':
+                return task.description;
+            case 'priority': {
+                let priority: string = '';
+
+                if (task.priority === Priority.High) {
+                    priority = ' ' + prioritySymbols.High;
+                } else if (task.priority === Priority.Medium) {
+                    priority = ' ' + prioritySymbols.Medium;
+                } else if (task.priority === Priority.Low) {
+                    priority = ' ' + prioritySymbols.Low;
+                }
+                return priority;
+            }
+            case 'startDate':
+                if (!task.startDate) return '';
+                return layout.options.shortMode
+                    ? ' ' + startDateSymbol
+                    : ` ${startDateSymbol} ${task.startDate.format(TaskRegularExpressions.dateFormat)}`;
+            case 'createdDate':
+                if (!task.createdDate) return '';
+                return layout.options.shortMode
+                    ? ' ' + createdDateSymbol
+                    : ` ${createdDateSymbol} ${task.createdDate.format(TaskRegularExpressions.dateFormat)}`;
+            case 'scheduledDate':
+                if (!task.scheduledDate || task.scheduledDateIsInferred) return '';
+                return layout.options.shortMode
+                    ? ' ' + scheduledDateSymbol
+                    : ` ${scheduledDateSymbol} ${task.scheduledDate.format(TaskRegularExpressions.dateFormat)}`;
+            case 'doneDate':
+                if (!task.doneDate) return '';
+                return layout.options.shortMode
+                    ? ' ' + doneDateSymbol
+                    : ` ${doneDateSymbol} ${task.doneDate.format(TaskRegularExpressions.dateFormat)}`;
+            case 'dueDate':
+                if (!task.dueDate) return '';
+                return layout.options.shortMode
+                    ? ' ' + dueDateSymbol
+                    : ` ${dueDateSymbol} ${task.dueDate.format(TaskRegularExpressions.dateFormat)}`;
+            case 'recurrenceRule':
+                if (!task.recurrence) return '';
+                return layout.options.shortMode
+                    ? ' ' + recurrenceSymbol
+                    : ` ${recurrenceSymbol} ${task.recurrence.toText()}`;
+            case 'blockLink':
+                return task.blockLink ?? '';
+            default:
+                throw new Error(`Don't know how to render task component of type '${component}'`);
+        }
     }
 
     /* Try to parse Task Details from a string
