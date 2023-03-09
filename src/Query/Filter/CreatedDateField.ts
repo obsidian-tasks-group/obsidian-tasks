ec876a7b6cd5a9a7014f2335e96f4893078bef50
Ivan Chernov <chernoffivan@gmail.com>
2023-03-10 02:43:28 +0400

    feat: Add filter, group and sorting for created date (#1744)
    * feat: Add filters for createdDate field in Task
    
    * feat: add grouping by createdDate in Task
    
    * docs: update all docs on created date filters
    
    ---------
    
    Co-authored-by: Clare Macrae <github@cfmacrae.fastmail.co.uk>

---
+++ src/Query/Filter/CreatedDateField.ts
@@ -0,0 +1,18 @@
+import type { Moment } from 'moment';
+import type { Task } from '../../Task';
+import { DateField } from './DateField';
+
+/**
+ * Support the 'created' search instruction.
+ */
+export class CreatedDateField extends DateField {
+    public fieldName(): string {
+        return 'created';
+    }
+    public date(task: Task): Moment | null {
+        return task.createdDate;
+    }
+    protected filterResultIfFieldMissing() {
+        return false;
+    }
+}
