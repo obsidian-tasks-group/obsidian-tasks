fce86451236c7da038b5479a97624443a9fe0be3
Ilyas Landikov <ilyas.landikov@icloud.com>
2023-03-11 04:08:20 +0600

    test: add erroneous year case

--- tests/Query/DateParser.test.ts
+++ tests/Query/DateParser.test.ts
@@ -186,5 +186,8 @@ describe('DateParser - specific date ranges', () => {
         // Month
         testParsingDateRange('2017-13', 'Invalid date', 'Invalid date');
         testParsingDateRange('2023-14', 'Invalid date', 'Invalid date');
+
+        // Year
+        testParsingDateRange('20167', 'Invalid date', 'Invalid date');
     });
 });
