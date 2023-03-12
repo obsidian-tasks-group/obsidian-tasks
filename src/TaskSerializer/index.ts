d9c8f7b327dd9d3a9bd70abc44437123f2c33265
kedestin <26238343+kedestin@users.noreply.github.com>
2023-03-11 20:12:09 -0500

    Add documentation for TaskSerializer types

--- src/TaskSerializer/index.ts
+++ src/TaskSerializer/index.ts
@@ -2,9 +2,17 @@ import type { Task } from '../Task';
 
 type Writeable<T> = { -readonly [P in keyof T]: T[P] };
 
-// A propertity bag of fields that can be parsed by a TaskSerializer
-// All fields are writeable for convenience
-
+/**
+ * A subset of fields of {@link Task} that can be parsed from the textual
+ * description of that Task.
+ *
+ * All fields are writeable for convenience.
+ *
+ * Some previously non-nullable fields are marked nullable so that they
+ *    can optionally share a common code path for parsing.
+ *    example: {@link TaskDetails.tags} can be left nullable by {@link TaskSerializer.deserialize})
+ *             so that {@link Task.fromLine} parses tags from the description.
+ */
 export type TaskDetails = Writeable<
     Pick<
         Task,
@@ -20,8 +28,34 @@ export type TaskDetails = Writeable<
         Partial<Pick<Task, 'tags'>>
 >;
 
+/**
+ * An abstraction that manages how a {@link Task} is read from and written
+ * to a file.
+ *
+ * A {@link TaskSerializer} is only responsible for the single line of text that follows
+ * after the checkbox:
+ *
+ *        - [ ] This is a task description
+ *              ~~~~~~~~~~~~~~~~~~~~~~~~~~
+ *
+ * @exports
+ * @interface TaskSerializer
+ */
 export interface TaskSerializer {
+    /**
+     * Parses task details from the string representation of a task
+     *
+     * @param line The single line of text to parse
+     * @returns The parsed {@link TaskDetails}, or null the string could not be parsed
+     */
     deserialize(line: string): TaskDetails | null;
+
+    /**
+     * Creates the string representation of a {@link Task}
+     *
+     * @param task The {@link Task} to stringify
+     * @returns {string}
+     */
     serialize(task: Task): string;
 }
 
