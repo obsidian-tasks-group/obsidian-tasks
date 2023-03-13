c4867a16fd885c0927275a6fcc0623e871c3d7cd
Achim Gmeiner <achim@gmeiner.cc>
2023-03-13 16:08:38 +0100

    Configure jest to work with Svelte components.

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,17 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
     verbose: true,
     preset: 'ts-jest',
     transform: {
+        '^.+\\.svelte$': [
+            'svelte-jester',
+            {
+                preprocess: true,
+            },
+        ],
         '^.+\\.ts$': 'ts-jest',
     },
-    moduleFileExtensions: ['js', 'ts'],
+    moduleFileExtensions: ['js', 'ts', 'svelte'],
 
     // A list of paths to modules that run some code to configure or
     // set up the testing framework before each test.
