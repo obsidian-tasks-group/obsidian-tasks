2ed7e8bddb9cbeafcaa0e6f65f9926ccd313acf0
Ilyas Landikov <ilyas.landikov@icloud.com>
2023-03-31 23:33:16 +0600

    test: simplify submit test code

--- tests/EditTask.test.ts
+++ tests/EditTask.test.ts
@@ -12,28 +12,17 @@ import { DateFallback } from '../src/DateFallback';
 import { GlobalFilter } from '../src/Config/GlobalFilter';
 
 window.moment = moment;
-const task = taskFromLine({ line: '- [X] test task 1', path: '' });
 const statusOptions: Status[] = [Status.DONE, Status.TODO];
 
 describe('Task rendering', () => {
-    let resolvePromise: (input: string) => void;
-    let onSubmit: (updatedTasks: Task[]) => void;
-
-    beforeEach(() => {
-        onSubmit = (updatedTasks: Task[]): void => {
-            const serializedTask = DateFallback.removeInferredStatusIfNeeded(task, updatedTasks)
-                .map((task: Task) => task.toFileLineString())
-                .join('\n');
-            resolvePromise(serializedTask);
-        };
-    });
-
     afterEach(() => {
         GlobalFilter.reset();
     });
 
     function testDescriptionRender(taskDescription: string, expectedDescription: string) {
         const task = taskFromLine({ line: `- [ ] ${taskDescription}`, path: '' });
+
+        const onSubmit = (_: Task[]): void => {};
         const { container } = render(EditTask, { task, statusOptions, onSubmit });
         expect(() => container).toBeTruthy();
         const renderedDescription = container.ownerDocument.getElementById('description') as HTMLInputElement;
@@ -57,28 +46,24 @@ describe('Task rendering', () => {
 });
 
 describe('Task editing', () => {
-    let resolvePromise: (input: string) => void;
-    let waitForClose: Promise<string>;
-    let onSubmit: (updatedTasks: Task[]) => void;
-
-    beforeEach(() => {
-        waitForClose = new Promise<string>((resolve, _) => {
-            resolvePromise = resolve;
-        });
-        onSubmit = (updatedTasks: Task[]): void => {
-            const serializedTask = DateFallback.removeInferredStatusIfNeeded(task, updatedTasks)
-                .map((task: Task) => task.toFileLineString())
-                .join('\n');
-            resolvePromise(serializedTask);
-        };
-    });
-
     afterEach(() => {
         GlobalFilter.reset();
     });
 
     async function testDescriptionEdit(taskDescription: string, newDescription: string, expectedDescription: string) {
         const task = taskFromLine({ line: `- [ ] ${taskDescription}`, path: '' });
+
+        let resolvePromise: (input: string) => void;
+        const waitForClose = new Promise<string>((resolve, _) => {
+            resolvePromise = resolve;
+        });
+        const onSubmit = (updatedTasks: Task[]): void => {
+            const serializedTask = DateFallback.removeInferredStatusIfNeeded(task, updatedTasks)
+                .map((task: Task) => task.toFileLineString())
+                .join('\n');
+            resolvePromise(serializedTask);
+        };
+
         const result = render(EditTask, { task, statusOptions, onSubmit });
         const { container } = result;
         expect(() => container).toBeTruthy();
