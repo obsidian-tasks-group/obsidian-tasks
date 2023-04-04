ee1a8972bee726fa0057e83eb75695414cbeffca
Ilyas Landikov <ilyas.landikov@icloud.com>
2023-04-04 23:40:17 +0600

    test: tests on next and last M & Q

--- tests/Query/DateRange.test.ts
+++ tests/Query/DateRange.test.ts
@@ -1,19 +1,6 @@
 import moment from 'moment';
 import { DateRange } from '../../src/Query/DateRange';
 import { DateParser } from '../../src/Query/DateParser';
-import { TaskRegularExpressions } from '../../src/Task';
-
-function testParsingDateRange(input: string, expectedStart: string, expectedEnd: string) {
-    // Act
-    const result = DateParser.parseDateRange(input);
-
-    // Assert
-    expect(result.start).toBeDefined();
-    expect(result.end).toBeDefined();
-    const startFmt = result.start.format(TaskRegularExpressions.dateFormat);
-    const endFmt = result.end.format(TaskRegularExpressions.dateFormat);
-    expect([startFmt, endFmt]).toStrictEqual([expectedStart, expectedEnd]);
-}
 
 function testDateRange(dateRange: DateRange, start: string, end: string) {
     expect(dateRange.start).toBeDefined();
@@ -61,12 +48,23 @@ describe('Date Parser - correct delta for next & last month & quarter (Today is 
     });
 
     it('should return longer range even if the current range is shorter in days', () => {
-        testParsingDateRange('last month', '2021-03-01', '2021-03-31');
-        testParsingDateRange('next month', '2021-05-01', '2021-05-31');
-        testParsingDateRange('last quarter', '2021-01-01', '2021-03-31');
-        testParsingDateRange('next quarter', '2021-07-01', '2021-09-30');
-        // The latest test case is not representative eg won't fail without the fix
+        const lastMonth = DateRange.buildRelative('month');
+        lastMonth.subtract('month');
+        testDateRange(lastMonth, '2021-03-01', '2021-03-31');
+
+        const nextMonth = DateRange.buildRelative('month');
+        nextMonth.add('month');
+        testDateRange(nextMonth, '2021-05-01', '2021-05-31');
+
+        const lastQuarter = DateRange.buildRelative('quarter');
+        lastQuarter.subtract('quarter');
+        testDateRange(lastQuarter, '2021-01-01', '2021-03-31');
+
+        // This test case is not representative eg won't fail without the fix
         // because the length of Q2 in days is same as Q3.
+        const nextQuarter = DateRange.buildRelative('quarter');
+        nextQuarter.add('quarter');
+        testDateRange(nextQuarter, '2021-07-01', '2021-09-30');
     });
 });
 
