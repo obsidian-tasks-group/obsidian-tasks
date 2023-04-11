f3a6a7271ae293b244a297f1ecd0e38925d08243
Ilyas Landikov <ilyas.landikov@icloud.com>
2023-04-12 03:13:59 +0600

    refactor: migrate PriorityField to new grouping

--- src/Query/Group.ts
+++ src/Query/Group.ts
@@ -1,5 +1,4 @@
 import type { Task } from '../Task';
-import { Priority } from '../Task';
 import { TaskGroups } from './TaskGroups';
 import { Grouper } from './Grouper';
 import type { GrouperFunction, GroupingProperty } from './Grouper';
@@ -41,7 +40,6 @@ export class Group {
     private static groupers: Record<GroupingProperty, GrouperFunction> = {
         backlink: Group.groupByBacklink,
         folder: Group.groupByFolder,
-        priority: Group.groupByPriority,
         recurrence: Group.groupByRecurrence,
         recurring: Group.groupByRecurring,
         root: Group.groupByRoot,
@@ -51,25 +49,6 @@ export class Group {
     public static escapeMarkdownCharacters(filename: string) {
         // https://wilsonmar.github.io/markdown-text-for-github-from-html/#special-characters
         return filename.replace(/\\/g, '\\\\').replace(/_/g, '\\_');
-    }
-
-    private static groupByPriority(task: Task): string[] {
-        let priorityName = 'ERROR';
-        switch (task.priority) {
-            case Priority.High:
-                priorityName = 'High';
-                break;
-            case Priority.Medium:
-                priorityName = 'Medium';
-                break;
-            case Priority.None:
-                priorityName = 'None';
-                break;
-            case Priority.Low:
-                priorityName = 'Low';
-                break;
-        }
-        return [`Priority ${task.priority}: ${priorityName}`];
     }
 
     private static groupByRecurrence(task: Task): string[] {
