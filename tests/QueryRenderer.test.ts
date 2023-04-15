f82f8001e92c0ff9843c2215f837cca4e4d0d07a
kedestin <26238343+kedestin@users.noreply.github.com>
2023-04-15 00:34:33 -0400

    test: Add a test to check that QueryRenderer uses the correct query

--- tests/QueryRenderer.test.ts
+++ tests/QueryRenderer.test.ts
@@ -4,7 +4,7 @@
 import moment from 'moment';
 import { Query } from '../src/Query/Query';
 import { resetSettings, updateSettings } from '../src/Config/Settings';
-import { explainResults } from '../src/lib/QueryRenderer';
+import { explainResults, getQueryForQueryRenderer } from '../src/lib/QueryRenderer';
 import { GlobalFilter } from '../src/Config/GlobalFilter';
 
 window.moment = moment;
@@ -75,5 +75,23 @@ Explanation of this Tasks code block query:
 No filters supplied. All tasks will match the query.`;
 
         expect(explainResults(query.source)).toEqual(expectedDisplayText);
+    });
+});
+
+/**
+ * @note Test suite deliberately omits any tests on the functionality of the query the QueryRenderer uses.
+ *       Since it is just running a Query, we defer to the Query tests. We just check that we're getting
+ *       the right query.
+ */
+describe('query used for QueryRenderer', () => {
+    afterEach(() => {
+        resetSettings();
+    });
+
+    it('should be the result of combining the global query and the actual query', () => {
+        const querySource = 'description includes world';
+        const globalQuerySource = 'description includes hello';
+        updateSettings({ globalQuery: globalQuerySource });
+        expect(getQueryForQueryRenderer(querySource).source).toEqual(`${globalQuerySource}\n${querySource}`);
     });
 });
