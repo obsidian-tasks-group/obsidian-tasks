eaff280c5816e05e72b45acf342d8be06f2e7578
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2023-04-20 17:40:53 +0100

    test: Remove manual test that is now in DateFieldReference.test.ts

--- tests/Query/Filter/DueDateField.test.ts
+++ tests/Query/Filter/DueDateField.test.ts
@@ -2,7 +2,6 @@
  * @jest-environment jsdom
  */
 import moment from 'moment';
-import { verify } from 'approvals/lib/Providers/Jest/JestApprovals';
 import { DueDateField } from '../../../src/Query/Filter/DueDateField';
 import type { FilterOrErrorMessage } from '../../../src/Query/Filter/Filter';
 import { TaskBuilder } from '../../TestingTools/TaskBuilder';
@@ -105,33 +104,6 @@ describe('due date', () => {
         // This test fails to parse after fixing #1901
         const filterOrMessage = new DueDateField().createFilterOrErrorMessage('due in two weeks');
         expect(filterOrMessage).toHaveExplanation('due date is on 2023-03-20 (Monday 20th March 2023)');
-    });
-
-    it('due on named day of week', () => {
-        // See https://github.com/obsidian-tasks-group/obsidian-tasks/issues/1901
-        jest.useFakeTimers();
-        jest.setSystemTime(new Date('2023-04-19'));
-
-        const increments = ['', 'last', 'this', 'next'];
-        const weekdays = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday'];
-
-        const instructions: string[] = [];
-
-        const divider = 'description includes ------------------------------';
-        instructions.push('explain');
-        instructions.push(divider);
-        instructions.push('due today');
-        instructions.push(divider);
-        for (const weekday of weekdays) {
-            for (const increment of increments) {
-                instructions.push(`due ${increment} ${weekday}`);
-            }
-            instructions.push(divider);
-        }
-        const source = instructions.join('\n');
-        const query = new Query({ source });
-        const explanation = query.explainQuery();
-        verify(explanation);
     });
 });
 
