4cf3dde309d496b56389c06bcd7c91c25d2c450a
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2023-04-20 20:21:46 +0100

    comment: Add explanatory info to DateField.createFilterOrErrorMessage()

--- src/Query/Filter/DateField.ts
+++ src/Query/Filter/DateField.ts
@@ -53,10 +53,17 @@ export abstract class DateField extends Field {
 
         const fieldNameKeywordDate = Field.getMatch(this.filterRegExp(), line);
         if (fieldNameKeywordDate !== null) {
-            const keywordAndDateString = fieldNameKeywordDate[1];
-            const fieldKeyword = fieldNameKeywordDate[2];
-            const fieldDateString = fieldNameKeywordDate[3];
+            const keywordAndDateString = fieldNameKeywordDate[1]; // Will contain the whole line except the field name
+            const fieldKeyword = fieldNameKeywordDate[2]; // Will be 'before', 'after', 'in', 'on' or undefined
+            const fieldDateString = fieldNameKeywordDate[3]; // Will contain the remainder of the instruction
+
+            // Try interpreting everything after the keyword as a date range:
             let fieldDates = DateParser.parseDateRange(fieldDateString);
+
+            // If the date range parsing failed, try again to parse the whole line except the field name
+            // as a single date, using the pre-date-ranges parsing mechanism.
+            // This is needed to keep 'due in two weeks' working, as 'two weeks' is not actually a valid date range
+            // if the futureDates value parsed in to chrono's parsing functions is false.
             if (!fieldDates.isValid()) {
                 // Try the old way of parsing
                 const date = DateParser.parseDate(keywordAndDateString);
@@ -64,6 +71,7 @@ export abstract class DateField extends Field {
                     fieldDates = new DateRange(date, date);
                 }
             }
+
             if (!fieldDates.isValid()) {
                 result.error = 'do not understand ' + this.fieldName() + ' date';
             } else {
