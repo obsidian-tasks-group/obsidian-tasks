a327be7fd37d8a43006fa04989381e384fd023e9
esm7 <esm7@users.noreply.github.com>
2023-04-24 11:36:54 +0300

    fix: Allow filename filters to contain parentheses (#1907)
    By making sure that BooleanField is always last in the list of field creators.
    This fixes https://github.com/obsidian-tasks-group/obsidian-tasks/issues/1852

--- src/Query/FilterParser.ts
+++ src/Query/FilterParser.ts
@@ -1,3 +1,4 @@
+import type { Field } from './Filter/Field';
 import { DescriptionField } from './Filter/DescriptionField';
 import { CreatedDateField } from './Filter/CreatedDateField';
 import { DoneDateField } from './Filter/DoneDateField';
@@ -27,7 +28,12 @@ import { FolderField } from './Filter/FolderField';
 import { RootField } from './Filter/RootField';
 import { BacklinkField } from './Filter/BacklinkField';
 
-const fieldCreators = [
+// When parsing a query the fields are tested one by one according to this order.
+// Since BooleanField is a meta-field, which needs to aggregate a few fields together, it is intended to
+// be kept last.
+// When adding new fields keep this order in mind, putting fields that are more specific before fields that
+// may contain them, and keep BooleanField last.
+const fieldCreators: EndsWith<BooleanField> = [
     () => new StatusNameField(), // status.name is before status, to avoid ambiguity
     () => new StatusTypeField(), // status.type is before status, to avoid ambiguity
     () => new StatusField(),
@@ -47,11 +53,14 @@ const fieldCreators = [
     () => new TagsField(),
     () => new HeadingField(),
     () => new ExcludeSubItemsField(),
-    () => new BooleanField(),
     () => new FilenameField(),
     () => new UrgencyField(),
     () => new RecurrenceField(),
+    () => new BooleanField(), // --- Please make sure to keep BooleanField last (see comment above) ---
 ];
+
+// This type helps verify that BooleanField is kept last
+type EndsWith<End, T extends Field = Field> = [...Array<() => T>, () => End];
 
 export function parseFilter(filterString: string): FilterOrErrorMessage | null {
     for (const creator of fieldCreators) {
