427f73a41ac79ad2a28b17f0da61e7b0169545b6
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2023-04-25 20:47:43 +0100

    refactor: Rename IntermediateTaskGroupsStorage to TaskGroupingTreeStorage
    And update some comments referring to renamed symbols in TaskGroupingTree.ts

--- src/Query/GroupDisplayHeadingSelector.ts
+++ src/Query/GroupDisplayHeadingSelector.ts
@@ -1,5 +1,5 @@
 import { GroupDisplayHeading } from './GroupDisplayHeading';
-import type { IntermediateTaskGroupsStorage } from './TaskGroupingTree';
+import type { TaskGroupingTreeStorage } from './TaskGroupingTree';
 
 /*
  * This file contains implementation details of Group.ts
@@ -68,7 +68,7 @@ import type { IntermediateTaskGroupsStorage } from './TaskGroupingTree';
  * What the code does
  * ==================
  *
- * The IntermediateTaskGroups class below does the initial grouping and sorting.
+ * The TaskGroupingTree class below does the initial grouping and sorting.
  *
  * The GroupHeadings class below implements pjeby's heading detection algorithm, but instead of doing the printing directly,
  * it returns the calculated heading levels in an array of GroupHeading objects, for later use in QueryRenderer.ts.
@@ -83,7 +83,7 @@ import type { IntermediateTaskGroupsStorage } from './TaskGroupingTree';
 export class GroupDisplayHeadingSelector {
     private lastHeadingAtLevel = new Array<string>();
 
-    constructor(groupedTasks: IntermediateTaskGroupsStorage) {
+    constructor(groupedTasks: TaskGroupingTreeStorage) {
         const firstGroup = groupedTasks.keys().next().value;
         const groupCount = firstGroup.length;
         for (let i = 0; i < groupCount; i++) {
