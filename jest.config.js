ea882e79b43a5ff33d1df6270d286b411fa9d1ae
kedestin <26238343+kedestin@users.noreply.github.com>
2023-04-25 02:03:30 -0400

    test: fix timezone test issues by normalizing to UTC (#1906)
    Co-authored-by: sytone <github@sytone.com>

--- jest.config.js
+++ jest.config.js
@@ -16,4 +16,5 @@ module.exports = {
     // A list of paths to modules that run some code to configure or
     // set up the testing framework before each test.
     setupFilesAfterEnv: ['<rootDir>/tests/CustomMatchers/jest.custom_matchers.setup.ts'],
+    globalSetup: "./tests/global-setup.js"
 };
