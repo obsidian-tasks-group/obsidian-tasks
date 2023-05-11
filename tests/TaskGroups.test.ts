obsidian-tasks-group/obsidian-tasks

commit afcb2fea953123582d2c06b40cce1335161f0011

refactor: Move the sorting of task groups by name from TaskGroupingTree to TaskGroups
Sorting of maps was hard to understand and reason about, and harder still to control.

This paves the way for providing more control over sorting of groups in future.

tests/TaskGroups.test.ts
@@ -129,6 +129,39 @@ describe('Grouping tasks', () => {
         `);
     });
 
+    it('sorts group names beginning with numeric values correctly', () => {
+        const a = fromLine({
+            line: '- [ ] first, as 9 is less then 10',
+            path: '9 something.md',
+        });
+        const b = fromLine({
+            line: '- [ ] second, as 10 is more than 9',
+            path: '10 something.md',
+        });
+        const inputs = [a, b];
+
+        const grouping = [new FilenameField().createGrouper()];
+        const groups = new TaskGroups(grouping, inputs);
+        // TODO Fix this - the two groups currently are in the wrong order.
+        expect(groups.toString()).toMatchInlineSnapshot(`
+            "
+            Group names: [[[10 something]]]
+            #### [[10 something]]
+            - [ ] second, as 10 is more than 9
+
+            ---
+
+            Group names: [[[9 something]]]
+            #### [[9 something]]
+            - [ ] first, as 9 is less then 10
+
+            ---
+
+            2 tasks
+            "
+        `);
+    });
+
     it('handles tasks matching multiple groups correctly', () => {
         const a = fromLine({
             line: '- [ ] Task 1 #group1',

src/Query/TaskGroupingTree.ts
@@ -38,7 +38,7 @@ class TaskGroupingTreeNode extends GroupingTreeNode<Task> {}
 
 /**
  * TaskGroupingTree does the initial grouping together of tasks,
- * in alphabetical order by group names.
+ * in arbitrary order. Caller is responsible for sorting groups in to desired order.
  *
  * It is essentially a thin wrapper around Map - see {@link TaskGroupingTreeStorage}.
  *
@@ -58,7 +58,6 @@ export class TaskGroupingTree {
     constructor(groupers: Grouper[], tasks: Task[]) {
         const tree = this.buildGroupingTree(groupers, tasks);
         this.groups = tree.generateAllPaths();
-        this.groups = this.getSortedGroups();
     }
 
     /**
@@ -95,12 +94,5 @@ export class TaskGroupingTree {
         }
 
         return root;
-    }
-
-    private getSortedGroups() {
-        // groups.keys() will initially be in the order the entries were added,
-        // so effectively random.
-        // Return a duplicate map, with the keys (that is, group names) sorted in alphabetical order:
-        return new TaskGroupingTreeStorage([...this.groups.entries()].sort());
     }
 }

src/Query/TaskGroups.ts
@@ -81,9 +81,30 @@ export class TaskGroups {
             const taskGroup = new TaskGroup(groups, groupHeadings, tasks);
             this.add(taskGroup);
         }
+        this.sortTaskGroups();
     }
 
     private add(taskGroup: TaskGroup) {
         this._groups.push(taskGroup);
+    }
+
+    private sortTaskGroups() {
+        const compareFn = (group1: TaskGroup, group2: TaskGroup) => {
+            // Compare two TaskGroup objects, sorting them by the group names at each grouping level.
+            const groupNames1 = group1.groups;
+            const groupNames2 = group2.groups;
+            // TODO Replace with localeCompare()
+            for (let i = 0; i < groupNames1.length; i++) {
+                if (groupNames1[i] < groupNames2[i]) {
+                    return -1;
+                }
+                if (groupNames1[i] > groupNames2[i]) {
+                    return 1;
+                }
+            }
+            // identical if we reach here
+            return 0;
+        };
+        this._groups.sort(compareFn);
     }
 }

commit d11891534ca899ea7994bdebd0863104e4f03f43

test: Add a test that shows an error in nested grouping display
I accidentally caused this in afcb2fea953123582d2c06b40cce1335161f0011.

tests/TaskGroups.test.ts
@@ -9,6 +9,8 @@ import { PathField } from '../src/Query/Filter/PathField';
 import { TagsField } from '../src/Query/Filter/TagsField';
 import { FolderField } from '../src/Query/Filter/FolderField';
 import { TaskGroups } from '../src/Query/TaskGroups';
+import { StatusTypeField } from '../src/Query/Filter/StatusTypeField';
+import { HappensDateField } from '../src/Query/Filter/HappensDateField';
 import { fromLine } from './TestHelpers';
 
 window.moment = moment;
@@ -237,6 +239,51 @@ describe('Grouping tasks', () => {
             Group names: [folder\\_b/folder\\_c/,[[file_d]]]
             ##### [[file_d]]
             - [ ] Task 2 - but path is 2nd, alphabetically
+
+            ---
+
+            3 tasks
+            "
+        `);
+    });
+
+    it('should create nested headings if multiple groups used - case 2', () => {
+        const b = fromLine({
+            line: '- [ ] Task a - early date 📅 2022-09-19',
+        });
+        const a = fromLine({
+            line: '- [ ] Task b - later date ⏳ 2022-12-06',
+        });
+        const c = fromLine({
+            line: '- [ ] Task c - intermediate date ⏳ 2022-10-06',
+        });
+        const inputs = [a, b, c];
+
+        const grouping = [
+            new StatusTypeField().createGrouper(), // Two group levels
+            new HappensDateField().createGrouper(),
+        ];
+        const groups = new TaskGroups(grouping, inputs);
+        // This result is incorrect. The '2 TODO' heading is shown before
+        // the last group instead of before the first one.
+        expect(groups.toString()).toMatchInlineSnapshot(`
+            "
+            Group names: [2 TODO,2022-09-19 Monday]
+            ##### 2022-09-19 Monday
+            - [ ] Task a - early date 📅 2022-09-19
+
+            ---
+
+            Group names: [2 TODO,2022-10-06 Thursday]
+            ##### 2022-10-06 Thursday
+            - [ ] Task c - intermediate date ⏳ 2022-10-06
+
+            ---
+
+            Group names: [2 TODO,2022-12-06 Tuesday]
+            #### 2 TODO
+            ##### 2022-12-06 Tuesday
+            - [ ] Task b - later date ⏳ 2022-12-06
 
             ---
 