f384807ae08c66e2bdc92e5feb649eb0b8b0137e
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2023-06-04 22:57:48 +0100

    test: Extract helper verifyFieldDataForReferenceDocs()

--- tests/Query/Filter/ReferenceDocs/FilterReference/FunctionFieldReference.test.ts
+++ tests/Query/Filter/ReferenceDocs/FilterReference/FunctionFieldReference.test.ts
@@ -18,15 +18,19 @@ function formatToRepresentType(x: any) {
 }
 
 describe('task', () => {
-    it('status', () => {
+    function verifyFieldDataForReferenceDocs(fields: string[]) {
         const markdownTable = new MarkdownTable(['Field', 'Type', 'Example']);
         const task = TaskBuilder.createFullyPopulatedTask();
-        const fields = ['task.status.name', 'task.status.type', 'task.status.symbol', 'task.status.nextStatusSymbol'];
         for (const field of fields) {
             const x = evaluateExpression(task, field);
             const cells = [field, typeof x, formatToRepresentType(x)];
             markdownTable.addRow(cells);
         }
         markdownTable.verifyForDocs();
+    }
+
+    it('status', () => {
+        const fields = ['task.status.name', 'task.status.type', 'task.status.symbol', 'task.status.nextStatusSymbol'];
+        verifyFieldDataForReferenceDocs(fields);
     });
 });
