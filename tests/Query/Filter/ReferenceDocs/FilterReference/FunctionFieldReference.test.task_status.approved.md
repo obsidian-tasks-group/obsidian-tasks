5d858dd3ddc76f3cd1962f498b41301a20a2addd
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2023-06-04 22:50:40 +0100

    test: Generate a table of status fields for custom functions

---
+++ tests/Query/Filter/ReferenceDocs/FilterReference/FunctionFieldReference.test.task_status.approved.md
@@ -0,0 +1,11 @@
+<!-- placeholder to force blank line before included text -->
+
+| Field | Type | Example |
+| ----- | ----- | ----- |
+| task.status.name | string | 'Todo' |
+| task.status.type | string | 'TODO' |
+| task.status.symbol | string | ' ' |
+| task.status.nextStatusSymbol | string | 'x' |
+
+
+<!-- placeholder to force blank line after included text -->
--- tests/Query/Filter/ReferenceDocs/FilterReference/FunctionFieldReference.test.ts
+++ tests/Query/Filter/ReferenceDocs/FilterReference/FunctionFieldReference.test.ts
@@ -6,13 +6,27 @@ import moment from 'moment';
 import { evaluateExpression } from '../../../../../src/Query/Filter/FunctionField';
 
 import { TaskBuilder } from '../../../../TestingTools/TaskBuilder';
+import { MarkdownTable } from '../../../../TestingTools/VerifyMarkdownTable';
 
 window.moment = moment;
 
-describe('task.status', () => {
-    it('document-fields', () => {
+function formatToRepresentType(x: any) {
+    if (typeof x === 'string') {
+        return "'" + x + "'";
+    }
+    return x;
+}
+
+describe('task', () => {
+    it('status', () => {
+        const markdownTable = new MarkdownTable(['Field', 'Type', 'Example']);
         const task = TaskBuilder.createFullyPopulatedTask();
-        const x = evaluateExpression(task, 'task.status.name');
-        expect(x).toStrictEqual('Todo');
+        const fields = ['task.status.name', 'task.status.type', 'task.status.symbol', 'task.status.nextStatusSymbol'];
+        for (const field of fields) {
+            const x = evaluateExpression(task, field);
+            const cells = [field, typeof x, formatToRepresentType(x)];
+            markdownTable.addRow(cells);
+        }
+        markdownTable.verifyForDocs();
     });
 });
