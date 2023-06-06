46b3f45e77e1cd5a33b961115c62a619217b9b16
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2023-06-06 15:41:00 +0100

    docs: Fix the type displayed for task.tags

--- tests/Query/Filter/ReferenceDocs/FilterReference/FunctionFieldReference.test.ts
+++ tests/Query/Filter/ReferenceDocs/FilterReference/FunctionFieldReference.test.ts
@@ -25,6 +25,17 @@ function addBackticks(x: any) {
     return '`' + x + '`';
 }
 
+function determineExpression(value: any) {
+    if (Array.isArray(value)) {
+        if (value.length > 0) {
+            return `${typeof value[0]}[]`;
+        } else {
+            return 'any[]';
+        }
+    }
+    return typeof value;
+}
+
 // TODO Show a task in a callout, or an ordered list
 // TODO Also create an empty task, and combine its types together with that of the populated one - to find any types that could be none/null/undefined
 
@@ -35,12 +46,14 @@ describe('task', () => {
         const task2 = new TaskBuilder().description('minimal task').status(Status.makeInProgress()).build();
         for (const field of fields) {
             // TODO better type label for string[] (tags)
+            const value1 = evaluateExpression(task1, field);
+            const value2 = evaluateExpression(task2, field);
             const cells = [
                 addBackticks(field),
-                addBackticks(typeof evaluateExpression(task1, field)),
-                addBackticks(formatToRepresentType(evaluateExpression(task1, field))),
-                addBackticks(typeof evaluateExpression(task2, field)),
-                addBackticks(formatToRepresentType(evaluateExpression(task2, field))),
+                addBackticks(determineExpression(value1)),
+                addBackticks(formatToRepresentType(value1)),
+                addBackticks(determineExpression(value2)),
+                addBackticks(formatToRepresentType(value2)),
             ];
             markdownTable.addRow(cells);
         }
