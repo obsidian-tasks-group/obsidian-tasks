76fadcf0d3e3025f84f245c1b7008a117dbabed8
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2023-06-07 20:40:17 +0100

    test: Move test of evaluateExpression() to Scripting/Expression.test.ts

--- tests/Query/Filter/FunctionField.test.ts
+++ tests/Query/Filter/FunctionField.test.ts
@@ -4,11 +4,9 @@
 import moment from 'moment';
 
 import { FunctionField } from '../../../src/Query/Filter/FunctionField';
-import { evaluateExpression } from '../../../src/Scripting/Expression';
 import { Status } from '../../../src/Status';
 import { Priority } from '../../../src/Task';
 import { toGroupTaskFromBuilder, toGroupTaskWithPath } from '../../CustomMatchers/CustomMatchersForGrouping';
-import { verifyAll } from '../../TestingTools/ApprovalTestHelpers';
 import { TaskBuilder } from '../../TestingTools/TaskBuilder';
 
 window.moment = moment;
@@ -147,31 +145,6 @@ describe('FunctionField - grouping return types', () => {
         const line = `group by function ${expression}`;
         const grouper = createGrouper(line);
         toGroupTaskFromBuilder(grouper, new TaskBuilder(), result);
-    });
-
-    it('group result on expressions', () => {
-        const task = TaskBuilder.createFullyPopulatedTask();
-        const expressionsAndResults = [
-            '"hello"',
-            '""',
-            '[]',
-            '"" || "No value"',
-            'false',
-            'true',
-            '1',
-            '0',
-            '0 || "No value"',
-            '1.0765456',
-            '["heading1", "heading2"]', // return two headings, indicating that this task should be displayed twice, once in each heading
-            '[1, 2]', // return two headings, that need to be converted to strings
-            'null',
-            'null || "No value"',
-            'undefined',
-            'undefined || "No value"',
-            // Should allow manual escaping of markdown
-            String.raw`"I _am_ not _italic_".replaceAll("_", "\\_")`,
-        ];
-        verifyAll((a) => `${evaluateExpression(task, a)}`, expressionsAndResults);
     });
 });
 
---
+++ tests/Scripting/Expression.test.ts
@@ -0,0 +1,37 @@
+/**
+ * @jest-environment jsdom
+ */
+import moment from 'moment';
+
+import { evaluateExpression } from '../../src/Scripting/Expression';
+import { verifyAll } from '../TestingTools/ApprovalTestHelpers';
+import { TaskBuilder } from '../TestingTools/TaskBuilder';
+
+window.moment = moment;
+
+describe('Expression', () => {
+    it('result', () => {
+        const task = TaskBuilder.createFullyPopulatedTask();
+        const expressionsAndResults = [
+            '"hello"',
+            '""',
+            '[]',
+            '"" || "No value"',
+            'false',
+            'true',
+            '1',
+            '0',
+            '0 || "No value"',
+            '1.0765456',
+            '["heading1", "heading2"]', // return two headings, indicating that this task should be displayed twice, once in each heading
+            '[1, 2]', // return two headings, that need to be converted to strings
+            'null',
+            'null || "No value"',
+            'undefined',
+            'undefined || "No value"',
+            // Should allow manual escaping of markdown
+            String.raw`"I _am_ not _italic_".replaceAll("_", "\\_")`,
+        ];
+        verifyAll((a) => `${evaluateExpression(task, a)}`, expressionsAndResults);
+    });
+});
