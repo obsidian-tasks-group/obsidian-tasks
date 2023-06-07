5d02cd57dfe95db870986f2c2e0a258f7bb789a3
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2023-06-07 20:23:09 +0100

    test: Add a test of expression calculation results

--- tests/Query/Filter/FunctionField.test.ts
+++ tests/Query/Filter/FunctionField.test.ts
@@ -5,6 +5,7 @@ import { verifyAllCombinations1 } from 'approvals/lib/Providers/Jest/Combination
 import moment from 'moment';
 
 import { FunctionField } from '../../../src/Query/Filter/FunctionField';
+import { evaluateExpression } from '../../../src/Scripting/Expression';
 import { Status } from '../../../src/Status';
 import { Priority } from '../../../src/Task';
 import { toGroupTaskFromBuilder, toGroupTaskWithPath } from '../../CustomMatchers/CustomMatchersForGrouping';
@@ -149,7 +150,29 @@ describe('FunctionField - grouping return types', () => {
     });
 
     it('group result on expressions', () => {
-        verifyAllCombinations1((a) => `${a}`, expressionsAndResults);
+        const task = TaskBuilder.createFullyPopulatedTask();
+        const expressionsAndResults = [
+            '"hello"',
+            '""',
+            '[]',
+            '"" || "No value"',
+            'false',
+            'true',
+            '1',
+            '0',
+            '0 || "No value"',
+            '1.0765456',
+            '["heading1", "heading2"]', // return two headings, indicating that this task should be displayed twice, once in each heading
+            '[1, 2]', // return two headings, that need to be converted to strings
+            'null',
+            'null || "No value"',
+            'undefined',
+            'undefined || "No value"',
+            // Should allow manual escaping of markdown
+            String.raw`"I _am_ not _italic_".replaceAll("_", "\\_")`,
+        ];
+        // TODO Remove [] from around a in the approved file (due to use of combinations)
+        verifyAllCombinations1((a) => `${evaluateExpression(task, a)}`, expressionsAndResults);
     });
 });
 
