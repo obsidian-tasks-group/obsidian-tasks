b612cb48b74bb66d3c4a20e1b179bf990f09bef6
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2023-06-09 22:24:47 +0100

    test: Parameterize a test, to make it easier to test more properties

--- tests/Query/Filter/ReferenceDocs/FilterReference/TaskPropertyExamples.test.ts
+++ tests/Query/Filter/ReferenceDocs/FilterReference/TaskPropertyExamples.test.ts
@@ -1,31 +1,37 @@
+import type { Task } from '../../../../../src/Task';
 import { SampleTasks } from '../../../../TestHelpers';
 import {
     verifyFunctionFieldGrouperSamplesForDocs,
     verifyFunctionFieldGrouperSamplesOnTasks,
 } from './VerifyFunctionFieldSamples';
 
-describe('custom grouping by description', () => {
-    const customGroups = [
-        [
-            'group by function task.description',
-            'group by description. This might be useful for finding completed recurrences of the same task',
-        ],
-        ['group by function task.description.toUpperCase()', 'Convert the description to capitals'],
-        [
-            'group by function task.description.slice(0, 25)',
-            'Truncate descriptions to at most their first 25 characters, and group by that string',
-        ],
-        [
-            "group by function task.description.replace('short', '==short==')",
-            'Highlight the word "short" in any group descriptions',
-        ],
+const customGroups = [
+    [
+        'group by function task.description',
+        'group by description. This might be useful for finding completed recurrences of the same task',
+    ],
+    ['group by function task.description.toUpperCase()', 'Convert the description to capitals'],
+    [
+        'group by function task.description.slice(0, 25)',
+        'Truncate descriptions to at most their first 25 characters, and group by that string',
+    ],
+    [
+        "group by function task.description.replace('short', '==short==')",
+        'Highlight the word "short" in any group descriptions',
+    ],
+];
+
+describe('custom grouping by', () => {
+    type CustomGroupingPropertyTestData = [string, string[][], Task[]];
+    const testData: CustomGroupingPropertyTestData[] = [
+        ['description', customGroups, SampleTasks.withAllRepresentativeDescriptions()],
     ];
 
-    it('results', () => {
-        verifyFunctionFieldGrouperSamplesOnTasks(customGroups, SampleTasks.withAllRepresentativeDescriptions());
+    it.each(testData)('%s results', (_: string, groups: string[][], tasks: Task[]) => {
+        verifyFunctionFieldGrouperSamplesOnTasks(groups, tasks);
     });
 
-    it('docs', () => {
-        verifyFunctionFieldGrouperSamplesForDocs(customGroups);
+    it.each(testData)('%s docs', (_: string, groups: string[][], _tasks: Task[]) => {
+        verifyFunctionFieldGrouperSamplesForDocs(groups);
     });
 });
