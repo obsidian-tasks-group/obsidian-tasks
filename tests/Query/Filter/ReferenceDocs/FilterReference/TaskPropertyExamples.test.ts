e48cb5c41a2a2ad3dde2d57174aadfa1ea0a1855
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2023-06-09 22:29:22 +0100

    test: Inline the list of custom groups

--- tests/Query/Filter/ReferenceDocs/FilterReference/TaskPropertyExamples.test.ts
+++ tests/Query/Filter/ReferenceDocs/FilterReference/TaskPropertyExamples.test.ts
@@ -5,26 +5,28 @@ import {
     verifyFunctionFieldGrouperSamplesOnTasks,
 } from './VerifyFunctionFieldSamples';
 
-const customGroups = [
-    [
-        'group by function task.description',
-        'group by description. This might be useful for finding completed recurrences of the same task',
-    ],
-    ['group by function task.description.toUpperCase()', 'Convert the description to capitals'],
-    [
-        'group by function task.description.slice(0, 25)',
-        'Truncate descriptions to at most their first 25 characters, and group by that string',
-    ],
-    [
-        "group by function task.description.replace('short', '==short==')",
-        'Highlight the word "short" in any group descriptions',
-    ],
-];
-
 describe('custom grouping by', () => {
     type CustomGroupingPropertyTestData = [string, string[][], Task[]];
     const testData: CustomGroupingPropertyTestData[] = [
-        ['description', customGroups, SampleTasks.withAllRepresentativeDescriptions()],
+        [
+            'description',
+            [
+                [
+                    'group by function task.description',
+                    'group by description. This might be useful for finding completed recurrences of the same task',
+                ],
+                ['group by function task.description.toUpperCase()', 'Convert the description to capitals'],
+                [
+                    'group by function task.description.slice(0, 25)',
+                    'Truncate descriptions to at most their first 25 characters, and group by that string',
+                ],
+                [
+                    "group by function task.description.replace('short', '==short==')",
+                    'Highlight the word "short" in any group descriptions',
+                ],
+            ],
+            SampleTasks.withAllRepresentativeDescriptions(),
+        ],
     ];
 
     it.each(testData)('%s results', (_: string, groups: string[][], tasks: Task[]) => {
