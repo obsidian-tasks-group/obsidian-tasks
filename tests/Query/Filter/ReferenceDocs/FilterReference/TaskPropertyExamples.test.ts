3d7b2bb933be6eae38ddb543eb90a4dec8f2213a
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2023-06-10 21:11:37 +0100

    test: Move 'group by function task.created' docs tests to TaskPropertyExamples.test.ts

--- tests/Query/Filter/ReferenceDocs/FilterReference/TaskPropertyExamples.test.ts
+++ tests/Query/Filter/ReferenceDocs/FilterReference/TaskPropertyExamples.test.ts
@@ -21,6 +21,17 @@ describe('custom grouping by', () => {
         // ---------------------------------------------------------------------------------
 
         [
+            'task.created',
+            [
+                [
+                    'group by function task.created?.format("YYYY-MM-DD dddd") || ""',
+                    'Like "group by task.created", except it does not write "No created date" if there is no created date. The question mark (`?`) and `|| ""` are needed because the created date value may be null',
+                ],
+            ],
+            SampleTasks.withAllRepresentativeCreatedDates(),
+        ],
+
+        [
             'task.done',
             [
                 [
