16fdfb54b50f434bcd427a6b12883760796896da
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2023-06-12 10:56:11 +0100

    jsdocs: Update Task docs for introduction of TasksDate

--- src/Task.ts
+++ src/Task.ts
@@ -410,35 +410,35 @@ export class Task {
     }
 
     /**
-     * An alias for {@link createdDate}, so the field names in scripting docs are consistent with the existing search instruction names.
+     * Return {@link createdDate} as a {@link TasksDate}, so the field names in scripting docs are consistent with the existing search instruction names, and null values are easy to deal with.
      */
     public get created(): TasksDate {
         return new TasksDate(this.createdDate);
     }
 
     /**
-     * An alias for {@link doneDate}, so the field names in scripting docs are consistent with the existing search instruction names.
+     * Return {@link doneDate} as a {@link TasksDate}, so the field names in scripting docs are consistent with the existing search instruction names, and null values are easy to deal with.
      */
     public get done(): TasksDate {
         return new TasksDate(this.doneDate);
     }
 
     /**
-     * An alias for {@link dueDate}, so the field names in scripting docs are consistent with the existing search instruction names.
+     * Return {@link dueDate} as a {@link TasksDate}, so the field names in scripting docs are consistent with the existing search instruction names, and null values are easy to deal with.
      */
     public get due(): TasksDate {
         return new TasksDate(this.dueDate);
     }
 
     /**
-     * An alias for {@link scheduledDate}, so the field names in scripting docs are consistent with the existing search instruction names.
+     * Return {@link scheduledDate} as a {@link TasksDate}, so the field names in scripting docs are consistent with the existing search instruction names, and null values are easy to deal with.
      */
     public get scheduled(): TasksDate {
         return new TasksDate(this.scheduledDate);
     }
 
     /**
-     * An alias for {@link startDate}, so the field names in scripting docs are consistent with the existing search instruction names.
+     * Return {@link startDate} as a {@link TasksDate}, so the field names in scripting docs are consistent with the existing search instruction names, and null values are easy to deal with.
      */
     public get start(): TasksDate {
         return new TasksDate(this.startDate);
@@ -455,7 +455,7 @@ export class Task {
     }
 
     /**
-     * Return the earliest of the dates used by 'happens' in this given task, or null if none set.
+     * Return the earliest of the dates used by 'happens' in this given task as a {@link TasksDate}.
      *
      * Generally speaking, the earliest date is considered to be the highest priority,
      * as it is the first point at which the user might wish to act on the task.
