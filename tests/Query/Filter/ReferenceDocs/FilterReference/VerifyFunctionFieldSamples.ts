262daf23c08ce4afebca1868455bf703d53a0423
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2023-06-13 09:46:47 +0100

    test: Move type QueryInstructionLineAndDescription to VerifyFunctionFieldSamples.ts

--- tests/Query/Filter/ReferenceDocs/FilterReference/VerifyFunctionFieldSamples.ts
+++ tests/Query/Filter/ReferenceDocs/FilterReference/VerifyFunctionFieldSamples.ts
@@ -4,7 +4,18 @@ import type { Task } from '../../../../../src/Task';
 import { groupHeadingsForTask } from '../../../../CustomMatchers/CustomMatchersForGrouping';
 import { verifyMarkdownForDocs } from '../../../../TestingTools/VerifyMarkdownTable';
 
-export function verifyFunctionFieldGrouperSamplesOnTasks(customGroups: string[][], tasks: Task[]) {
+/**
+ * The first value is an example Tasks query block instruction line.
+ *
+ * The second and subsequent values are descriptive text for the user docs, explaining the instruction line.
+ * The descriptions will be written in a bullet list.
+ */
+export type QueryInstructionLineAndDescription = string[];
+
+export function verifyFunctionFieldGrouperSamplesOnTasks(
+    customGroups: QueryInstructionLineAndDescription[],
+    tasks: Task[],
+) {
     verifyAll('Results of custom groupers', customGroups, (group) => {
         const instruction = group[0];
         const comment = group[1];
@@ -20,7 +31,7 @@ ${headings.join('\n')}
     });
 }
 
-export function verifyFunctionFieldGrouperSamplesForDocs(customGroups: string[][]) {
+export function verifyFunctionFieldGrouperSamplesForDocs(customGroups: QueryInstructionLineAndDescription[]) {
     let markdown = '';
     for (const group of customGroups) {
         const instruction = group[0];
