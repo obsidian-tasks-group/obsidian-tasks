e943e9266204fc2a5065d3ecfdfc282e7b078ad8
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2023-06-14 21:35:55 +0100

    docs: Add an example showing how to order priority groups by - erm - priority

--- tests/Scripting/TaskPropertyExamples.test.ts
+++ tests/Scripting/TaskPropertyExamples.test.ts
@@ -288,6 +288,13 @@ describe('custom grouping by', () => {
                 [
                     'group by function task.priorityName',
                     "Group by the task's priority name",
+                    'The priority names are displayed in alphabetical order.',
+                    "Note that the default priority is called 'Normal', as opposed to with `group by priority` which calls the default 'None'",
+                ],
+                [
+                    "group by function '%%' + task.priorityNumber.toString() + '%%' + task.priorityName +' priority'",
+                    "Group by the task's priority name",
+                    'The hidden priority number ensures that the headings are written from highest to lowest priority.',
                     "Note that the default priority is called 'Normal', as opposed to with `group by priority` which calls the default 'None'",
                 ],
             ],
