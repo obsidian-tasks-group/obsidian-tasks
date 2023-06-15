c53ffa15a984f635e02c3444d4509a98e3591891
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2023-06-15 17:14:08 +0100

    test: Make the unusual sort order in urgency more explicit in tests

--- tests/Query/Filter/UrgencyField.test.ts
+++ tests/Query/Filter/UrgencyField.test.ts
@@ -97,11 +97,18 @@ describe('grouping by urgency', () => {
         expect(grouper(fromLine({ line: taskLine }))).toEqual(groups);
     });
 
-    it('should sort groups for UrgencyField', () => {
-        const grouper = new UrgencyField().createReverseGrouper();
+    describe('should sort groups for UrgencyField', () => {
         const taskLines = ['- [ ] a ⏫', '- [ ] a 🔼', '- [ ] a', '- [ ] a 🔽'];
         const tasks = taskLines.map((taskLine) => fromLine({ line: taskLine }));
 
-        expect({ grouper, tasks }).groupHeadingsToBe(['0.00', '1.95', '3.90', '6.00']);
+        it('highest urgency first with normal grouper', () => {
+            const grouper = new UrgencyField().createNormalGrouper();
+            expect({ grouper, tasks }).groupHeadingsToBe(['0.00', '1.95', '3.90', '6.00'].reverse());
+        });
+
+        it('lowest urgency first with reverse grouper', () => {
+            const grouper = new UrgencyField().createReverseGrouper();
+            expect({ grouper, tasks }).groupHeadingsToBe(['0.00', '1.95', '3.90', '6.00']);
+        });
     });
 });
