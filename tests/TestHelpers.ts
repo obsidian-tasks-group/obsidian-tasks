8ffe707491af5b1dd157e46646f8044428bd259e
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2023-06-17 09:34:00 +0100

    test: Simplify imports in TestHelpers.ts

--- tests/TestHelpers.ts
+++ tests/TestHelpers.ts
@@ -1,10 +1,10 @@
 import { Priority, Task } from '../src/Task';
 import { TaskLocation } from '../src/TaskLocation';
 import { PriorityTools } from '../src/lib/PriorityTools';
-import { Status } from './../src/Status';
-import { StatusType } from './../src/StatusConfiguration';
+import { Status } from '../src/Status';
+import { StatusType } from '../src/StatusConfiguration';
 import { TaskBuilder } from './TestingTools/TaskBuilder';
-import { Recurrence } from './../src/Recurrence';
+import { Recurrence } from '../src/Recurrence';
 
 export function fromLine({
     line,
