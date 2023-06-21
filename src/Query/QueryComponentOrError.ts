9103f9dc1c8640587e24d300eb1bea49f94c6810
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2023-06-21 18:19:16 +0100

    refactor: Extract generic class QueryComponentOrError  (#2058)
    * refactor: Rename _filter to _object in FilterOrErrorMessage.
    
    And update code in the class to use it.
    
    This is a step on the way to generalising FilterOrErrorMessage for things
    other than filters.
    
    * refactor: Extract super class ObjectOrErrorMessage
    
    This is a step on the way to generalising FilterOrErrorMessage for things
    other than filters.
    
    * refactor: Make a setter private and remove a now-unused setter.
    
    This is a step on the way to generalising FilterOrErrorMessage for things
    other than filters.
    
    * refactor: Move ObjectOrErrorMessage to own file
    
    This is a step on the way to generalising FilterOrErrorMessage for things
    other than filters.
    
    * refactor: Remove unintended use of Filter.instruction in ObjectOrErrorMessage
    
    This is a step on the way to generalising FilterOrErrorMessage for things
    other than filters.
    
    * refactor: FilterOrErrorMessage now contains ObjectOrErrorMessage, not inherit.
    
    This is a step on the way to generalising FilterOrErrorMessage for things
    other than filters.
    
    * refactor: Make constructor FilterOrErrorMessage private.
    
    This is a step on the way to generalising FilterOrErrorMessage for things
    other than filters.
    
    * refactor: Make ObjectOrErrorMessage generic.
    
    This is a step on the way to generalising FilterOrErrorMessage for things
    other than filters.
    
    * refactor: Make ObjectOrErrorMessage constructor private
    
    * jsdocs: Reinstate lost comment
    
    * jsdocs: Minor corrections to docs of ObjectOrErrorMessage.ts
    
    * refactor: Inline an unnecessary return variable
    
    * jsdocs: Remove obsolete docs on FilterOrErrorMessage
    
    * refactor: Rename ObjectOrErrorMessage to QueryComponentOrError
    
    * refactor: Rename object to queryComponent, for consistency with class name
    
    * jsdocs: Clarify docs of QueryComponentOrError

---
+++ src/Query/QueryComponentOrError.ts
@@ -0,0 +1,62 @@
+/**
+ * Generic class for storing:
+ * - a text instruction.
+ * - an object of type QueryComponent constructed from the instruction, if the instruction is valid.
+ * - otherwise, an error message explaining in what wat the instruction is invalid.
+ *
+ * An example type of QueryComponent is {@link Filter}. See {@link FilterOrErrorMessage}.
+ */
+export class QueryComponentOrError<QueryComponent> {
+    readonly instruction: string;
+    private _queryComponent: QueryComponent | undefined;
+    private _error: string | undefined;
+
+    private constructor(instruction: string) {
+        this.instruction = instruction;
+    }
+
+    public get queryComponent(): QueryComponent | undefined {
+        return this._queryComponent;
+    }
+
+    private set queryComponent(value: QueryComponent | undefined) {
+        this._queryComponent = value;
+    }
+
+    public get error(): string | undefined {
+        return this._error;
+    }
+
+    private set error(value: string | undefined) {
+        this._error = value;
+    }
+
+    /**
+     * Construct an ObjectOrErrorMessage with the given QueryComponent.
+     *
+     * @param instruction
+     * @param object - a {@link Filter}
+     */
+    public static fromObject<QueryComponent>(
+        instruction: string,
+        object: QueryComponent,
+    ): QueryComponentOrError<QueryComponent> {
+        const result = new QueryComponentOrError<QueryComponent>(instruction);
+        result._queryComponent = object;
+        return result;
+    }
+
+    /**
+     * Construct a ObjectOrErrorMessage with the given error message.
+     * @param instruction
+     * @param errorMessage
+     */
+    public static fromError<QueryComponent>(
+        instruction: string,
+        errorMessage: string,
+    ): QueryComponentOrError<QueryComponent> {
+        const result = new QueryComponentOrError<QueryComponent>(instruction);
+        result._error = errorMessage;
+        return result;
+    }
+}
