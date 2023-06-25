c99e8992aa5114c494c83411ee92b06c2830270a
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2023-06-25 17:21:00 +0100

    test: Add function to generate cell content

--- tests/DocumentationSamples/UrgencyDocs/DocsSamplesForUrgency.test.ts
+++ tests/DocumentationSamples/UrgencyDocs/DocsSamplesForUrgency.test.ts
@@ -1,6 +1,10 @@
 import { verifyMarkdownForDocs } from '../../TestingTools/VerifyMarkdownTable';
 
 describe('UrgencyTable', () => {
+    function cell(text: string): string {
+        return `<td>${text}</td>`;
+    }
+
     function rowSpanningCell(span: number, text: string): string {
         return `<td rowspan="${span}">${text}</td>`;
     }
@@ -30,7 +34,7 @@ describe('UrgencyTable', () => {
         const row1 = `
   <tr>
     ${rowSpanningCell(5, 'Due')}
-    <td>More than 7 days overdue</td>
+    ${cell('More than 7 days overdue')}
     ${urgencyCell(12.0)}
   </tr>`;
         table += row1;
@@ -41,65 +45,65 @@ describe('UrgencyTable', () => {
     <td>Range of ${urgencyValue(12.0)} to ${urgencyValue(0.2)}</td>
   </tr>
   <tr>
-    <td>Example for "today": <code>9.0</code></td>
+    ${cell('Example for "today": <code>9.0</code>')}
   </tr>
   <tr>
-    <td>More than 14 days until due</td>
+    ${cell('More than 14 days until due')}
     ${urgencyCell(0.2)}
   </tr>
   <tr>
-    <td>None</td>
+    ${cell('None')}
     ${urgencyCell(0.0)}
   </tr>
   <tr>
     ${rowSpanningCell(6, 'Priority')}
-    <td>Highest</td>
+    ${cell('Highest')}
     ${urgencyCell(9.0)}
   </tr>
   <tr>
-    <td>High</td>
+    ${cell('High')}
     ${urgencyCell(6.0)}
   </tr>
   <tr>
-    <td>Medium</td>
+    ${cell('Medium')}
     ${urgencyCell(3.9)}
   </tr>
   <tr>
-    <td>None</td>
+    ${cell('None')}
     ${urgencyCell(1.95, 2)}
   </tr>
   <tr>
-    <td>Low</td>
+    ${cell('Low')}
     ${urgencyCell(0.0)}
   </tr>
   <tr>
-    <td>Lowest</td>
+    ${cell('Lowest')}
     ${urgencyCell(-1.8)}
   </tr>
   <tr>
     ${rowSpanningCell(3, 'Scheduled')}
-    <td>Today or earlier</td>
+    ${cell('Today or earlier')}
     ${urgencyCell(5.0)}
   </tr>
   <tr>
-    <td>Tomorrow or later</td>
+    ${cell('Tomorrow or later')}
     ${urgencyCell(0.0)}
   </tr>
   <tr>
-    <td>None</td>
+    ${cell('None')}
     ${urgencyCell(0.0)}
   </tr>
   <tr>
     ${rowSpanningCell(3, 'Starts')}
-    <td>Today or earlier</td>
+    ${cell('Today or earlier')}
     ${urgencyCell(0.0)}
   </tr>
   <tr>
-    <td>Tomorrow or later</td>
+    ${cell('Tomorrow or later')}
     ${urgencyCell(-3.0)}
   </tr>
   <tr>
-    <td>None</td>
+    ${cell('None')}
     ${urgencyCell(0.0)}
   </tr>
 </tbody>
