309ee5c9c5122eecb45652d6c0fddf44957e0968
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2023-06-25 22:05:59 +0100

    test: Convert rest of code to use property() function

--- tests/DocumentationSamples/UrgencyDocs/DocsSamplesForUrgency.test.ts
+++ tests/DocumentationSamples/UrgencyDocs/DocsSamplesForUrgency.test.ts
@@ -50,26 +50,32 @@ describe('UrgencyTable', () => {
         let table = '';
         table += heading;
 
-        table += row([rowSpanningCell(5, 'Due'), cell('More than 7 days overdue'), urgencyCell(12.0)]);
+        table += property([[rowSpanningCell(5, 'Due'), cell('More than 7 days overdue'), urgencyCell(12.0)]]);
 
-        table += row([
-            rowSpanningCell(2, 'Due between 7 days ago and in 14 days'),
-            `<td>Range of ${urgencyValue(12.0)} to ${urgencyValue(0.2)}</td>`,
+        table += property([
+            [
+                rowSpanningCell(2, 'Due between 7 days ago and in 14 days'),
+                `<td>Range of ${urgencyValue(12.0)} to ${urgencyValue(0.2)}</td>`,
+            ],
+            [cell('Example for "today": <code>9.0</code>')],
+            [cell('More than 14 days until due'), urgencyCell(0.2)],
+            [cell('None'), urgencyCell(0.0)],
         ]);
-        table += row([cell('Example for "today": <code>9.0</code>')]);
-        table += row([cell('More than 14 days until due'), urgencyCell(0.2)]);
-        table += row([cell('None'), urgencyCell(0.0)]);
 
-        table += row([rowSpanningCell(6, 'Priority'), cell('Highest'), urgencyCell(9.0)]);
-        table += row([cell('High'), urgencyCell(6.0)]);
-        table += row([cell('Medium'), urgencyCell(3.9)]);
-        table += row([cell('None'), urgencyCell(1.95, 2)]);
-        table += row([cell('Low'), urgencyCell(0.0)]);
-        table += row([cell('Lowest'), urgencyCell(-1.8)]);
+        table += property([
+            [rowSpanningCell(6, 'Priority'), cell('Highest'), urgencyCell(9.0)],
+            [cell('High'), urgencyCell(6.0)],
+            [cell('Medium'), urgencyCell(3.9)],
+            [cell('None'), urgencyCell(1.95, 2)],
+            [cell('Low'), urgencyCell(0.0)],
+            [cell('Lowest'), urgencyCell(-1.8)],
+        ]);
 
-        table += row([rowSpanningCell(3, 'Scheduled'), cell('Today or earlier'), urgencyCell(5.0)]);
-        table += row([cell('Tomorrow or later'), urgencyCell(0.0)]);
-        table += row([cell('None'), urgencyCell(0.0)]);
+        table += property([
+            [rowSpanningCell(3, 'Scheduled'), cell('Today or earlier'), urgencyCell(5.0)],
+            [cell('Tomorrow or later'), urgencyCell(0.0)],
+            [cell('None'), urgencyCell(0.0)],
+        ]);
 
         table += property([
             [rowSpanningCell(3, 'Starts'), cell('Today or earlier'), urgencyCell(0.0)],
