170af6f8a03aeda660f65bc8803786dbb0680f8f
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2023-07-04 14:12:53 +0100

    refactor: Teach QueryResult to store search errors

--- src/Query/QueryResult.ts
+++ src/Query/QueryResult.ts
@@ -1,11 +1,20 @@
-import type { TaskGroups } from './TaskGroups';
+import { TaskGroups } from './TaskGroups';
 import type { TaskGroup } from './TaskGroup';
 
 export class QueryResult {
     public readonly taskGroups: TaskGroups;
+    private _searchErrorMessage: string | undefined = undefined;
 
     constructor(groups: TaskGroups) {
         this.taskGroups = groups;
+    }
+
+    public get searchErrorMessage(): string | undefined {
+        return this._searchErrorMessage;
+    }
+
+    private set searchErrorMessage(value: string | undefined) {
+        this._searchErrorMessage = value;
     }
 
     public get totalTasksCount(): number {
@@ -14,5 +23,11 @@ export class QueryResult {
 
     public get groups(): TaskGroup[] {
         return this.taskGroups.groups;
+    }
+
+    static fromError(message: string): QueryResult {
+        const result = new QueryResult(new TaskGroups([], []));
+        result._searchErrorMessage = message;
+        return result;
     }
 }
