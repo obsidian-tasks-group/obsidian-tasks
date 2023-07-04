f4a54a307b8187e69757381bda93911b1dc18003
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2023-07-04 11:10:40 +0100

    refactor: extract errorMessageForException() to ExceptionTools.ts

---
+++ src/lib/ExceptionTools.ts
@@ -0,0 +1,15 @@
+/**
+ * Get the text to report after an exception is caught.
+ * @param whatWasHappening - a description of what was happening at the time, preferably including any user inputs.
+ * @param exception - object that was caught in a try/catch block.
+ */
+export function errorMessageForException(whatWasHappening: string, exception: any): string {
+    const errorMessage = `Error: ${whatWasHappening}. The error message was:`;
+    let detail: string = '';
+    if (exception instanceof Error) {
+        detail += exception.message;
+    } else {
+        detail += 'Unknown error';
+    }
+    return `${errorMessage} ${detail}`;
+}
---
+++ tests/lib/ExceptionTools.test.ts
@@ -0,0 +1,35 @@
+import { errorMessageForException } from '../../src/lib/ExceptionTools';
+
+describe('errorMessageForException()', () => {
+    it('should include exception type and text in error message', () => {
+        // Arrange
+        const exceptionText = 'that was out of range';
+        let messageReceived = 'Error - there was no exception thrown - test is invalid';
+
+        // Put the 'throw' in a function, to prevent WebStorm reporting 'throw' of exception caught locally
+        // when the throw is directly inside the try block:
+        function throwAnException() {
+            throw RangeError(exceptionText);
+        }
+
+        // Act
+        try {
+            throwAnException();
+        } catch (e) {
+            messageReceived = errorMessageForException('testing', e);
+        }
+
+        // Assert
+        expect(messageReceived).toEqual('Error: testing. The error message was: that was out of range');
+        expect(messageReceived.includes(exceptionText)).toEqual(true);
+    });
+
+    it('should give meaningful text for non-exceptions', () => {
+        // Arrange
+        const nonExceptionValue = 42;
+        const messageReceived = errorMessageForException('testing', nonExceptionValue);
+
+        // Assert
+        expect(messageReceived).toEqual('Error: testing. The error message was: Unknown error');
+    });
+});
