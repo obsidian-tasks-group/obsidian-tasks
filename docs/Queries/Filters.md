b086c73433fbf251e4af51c2e08e1b21cf5b6273
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2023-07-09 08:15:59 +0100

    docs: Add examples for 'filter by function' for task.file.filename.

--- docs/Queries/Filters.md
+++ docs/Queries/Filters.md
@@ -945,7 +945,12 @@ Since Tasks X.Y.Z, **[[Custom Filters|custom filtering]] by file name** is now p
 
 <!-- placeholder to force blank line before included text --> <!-- include: CustomFilteringExamples.test.file_properties_task.file.filename_docs.approved.md -->
 
-==TODO==
+- ```filter by function task.file.filename === "4.1.0 Release.md"```
+  - Find tasks in files with the exact file name, but in any folder.
+  - The equality test, `===`, requires that the file extension `.md` be included.
+- ```filter by function task.file.filename.includes("4.1.0 Release")```
+  - Find tasks in files whose name contains the given text.
+  - By using `.includes()` and leaving out the file extension, this will also find files such as `14.1.0 Release.md` and `4.1.0 Release Notes.md`.
 
 <!-- placeholder to force blank line after included text --> <!-- endInclude -->
 
--- tests/Scripting/ScriptingReference/CustomFiltering/CustomFilteringExamples.test.ts
+++ tests/Scripting/ScriptingReference/CustomFiltering/CustomFilteringExamples.test.ts
@@ -246,8 +246,18 @@ describe('file properties', () => {
 
         [
             'task.file.filename',
-            // comment to force line break
-            [],
+            [
+                [
+                    'filter by function task.file.filename === "4.1.0 Release.md"',
+                    'Find tasks in files with the exact file name, but in any folder.',
+                    'The equality test, `===`, requires that the file extension `.md` be included',
+                ],
+                [
+                    'filter by function task.file.filename.includes("4.1.0 Release")',
+                    'Find tasks in files whose name contains the given text.',
+                    'By using `.includes()` and leaving out the file extension, this will also find files such as `14.1.0 Release.md` and `4.1.0 Release Notes.md`',
+                ],
+            ],
             tasks,
         ],
 
