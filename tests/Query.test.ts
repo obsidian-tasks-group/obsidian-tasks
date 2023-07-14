66b9db2d7b602c5546b807d388cbe100572591d1
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2023-07-14 15:24:33 +0100

    test: Test there's both normal and reverse 'group by' samples

--- tests/Query.test.ts
+++ tests/Query.test.ts
@@ -349,6 +349,36 @@ urgency
         it('sample lines really are in alphabetical order', () => {
             expect(filters).toStrictEqual(sortInstructionLines(filters));
         });
+
+        function linesMatchingField(field: Field | BooleanField) {
+            return filters.filter((instruction) => field.createGrouperFromLine(instruction) !== null);
+        }
+
+        const namedFieldCreators = fieldCreators.map((creator) => {
+            const field = creator();
+            return [field.fieldName(), field];
+        });
+        namedFieldCreators.sort((a, b) => (a[0] as string).localeCompare(b[0] as string, undefined, { numeric: true }));
+
+        describe.each(namedFieldCreators)(
+            'has sufficient sample "group by" lines for field "%s"',
+            (_, fieldNeedsCasting) => {
+                const field = fieldNeedsCasting as Field;
+                if (!field.supportsGrouping()) {
+                    return;
+                }
+
+                const matchingLines = linesMatchingField(field);
+
+                it('has at least one test for normal grouping', () => {
+                    expect(matchingLines.filter((line) => !line.includes(' reverse')).length).toBeGreaterThan(0);
+                });
+
+                it('has at least one test for reverse grouping', () => {
+                    expect(matchingLines.filter((line) => line.includes(' reverse')).length).toBeGreaterThan(0);
+                });
+            },
+        );
     });
 
     describe('should recognise every other instruction', () => {
