187174a71a0929ca4219c60c030b8d5683363eb8
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2023-07-16 19:04:14 +0100

    fix: Report invalid regex flags to user (#2137)
    This might happen in the case where the final closing
    slash is missing from the filter.

--- src/Query/Matchers/RegexMatcher.ts
+++ src/Query/Matchers/RegexMatcher.ts
@@ -27,7 +27,12 @@ export class RegexMatcher extends IStringMatcher {
      * @throws {SyntaxError} Throws an exception if there was an error in {@link regexInput}.
      */
     public static validateAndConstruct(regexInput: string): RegexMatcher | null {
-        const regexPattern = /^\/(.+)\/([igmu]*)/;
+        // This expression has two parts.
+        // 1. The regex source:  Match every character from the start of the line to
+        //                       just before the final forward slash ('/').
+        // 2. The flags, if any: Match every character after the last slash,
+        //                       to the end of the line.
+        const regexPattern = /^\/(.+)\/([^/]*)$/;
         const query = regexInput.match(regexPattern);
 
         if (query !== null) {
