5fb1c29cc80b58a389a669eca51e1b880d001dc7
Ilyas Landikov <ilyas.landikov@icloud.com>
2023-08-09 10:58:40 +0600

    refactor: implement a static getter

--- src/Config/GlobalQuery.ts
+++ src/Config/GlobalQuery.ts
@@ -1,6 +1,12 @@
+import { Query } from './../Query/Query';
 import { getSettings } from './Settings';
 
 export class GlobalQuery {
+    static get(): Query {
+        const globalQuerySource = GlobalQuery.source();
+        return new Query(globalQuerySource);
+    }
+
     /**
      * Retrieves the source of the global {@link Query}
      */
