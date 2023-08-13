02095b1fd6b62c1f3991815404000da113dd997d
kedestin <26238343+kedestin@users.noreply.github.com>
2023-08-12 20:31:21 -0400

    test: Add tests for onlySuggestIfBracketOpen

--- tests/Suggestor/Suggestor.test.ts
+++ tests/Suggestor/Suggestor.test.ts
@@ -4,8 +4,8 @@
 import moment from 'moment';
 import * as chrono from 'chrono-node';
 import { getSettings } from '../../src/Config/Settings';
-import type { SuggestInfo } from '../../src/Suggestor';
-import { makeDefaultSuggestionBuilder } from '../../src/Suggestor/Suggestor';
+import type { SuggestInfo, SuggestionBuilder } from '../../src/Suggestor';
+import { makeDefaultSuggestionBuilder, onlySuggestIfBracketOpen } from '../../src/Suggestor/Suggestor';
 import { DEFAULT_SYMBOLS } from '../../src/TaskSerializer/DefaultTaskSerializer';
 import { DATAVIEW_SYMBOLS } from '../../src/TaskSerializer/DataviewTaskSerializer';
 import { MarkdownTable } from '../TestingTools/VerifyMarkdownTable';
@@ -148,5 +148,70 @@ describe.each([
         // For help if this test fails and you are new to Approval Tests, see:
         //    https://publish.obsidian.md/tasks-contributing/Testing/Approval+Tests
         markdownTable.verify();
+    });
+});
+
+describe('onlySuggestIfBracketOpen', () => {
+    const buildSuggestions: SuggestionBuilder = onlySuggestIfBracketOpen(
+        // A dummy SuggestionBuilder. Just used to validate that results are returned, results aren't meaningful
+        () => [{} as unknown as SuggestInfo],
+        [
+            ['(', ')'],
+            ['[', ']'],
+        ],
+    );
+
+    /**
+     * Given a string with **exactly** one vertical bar (`|`), returns the string with the vertical bar removed
+     * and the index of the bar.
+     *
+     * This is used as a helper when writing tests, since it provides a less error prone
+     * and more readable way to represent a cursor's position, denoted by the vertical bar, and the string.
+     *
+     * @param line - A string that contains exactly one vertical bar (`|`)
+     * @returns A tuple of the line without the vertical bar, and the index of the vertical bar.
+     */
+    function cursorPosition(line: string): [line_without_cursor: string, custor_index: number] {
+        const line_without_cursor = line.replace('|', '');
+        // Check that the cursor marker appears exactly once in each input string:
+        expect(line_without_cursor.length).toEqual(line.length - 1);
+        console.log([line.replace('|', ''), line.indexOf('|')]);
+        return [line.replace('|', ''), line.indexOf('|')];
+    }
+
+    it('should suggest if cursor at end of line with an open pair', () => {
+        for (const line of ['(hello world|', '[hello world|']) {
+            const suggestions = buildSuggestions(...cursorPosition(line), getSettings());
+            expect(suggestions).not.toEqual([]);
+        }
+    });
+
+    it('should suggest if cursor at end of line with an nested open pairs', () => {
+        for (const line of ['(((hello world))|', '[[[hello world]]|']) {
+            const suggestions = buildSuggestions(...cursorPosition(line), getSettings());
+            expect(suggestions).not.toEqual([]);
+        }
+    });
+
+    it('should suggest if cursor in middle of closed pair', () => {
+        for (const line of ['(hello world|)', '[hello world|]']) {
+            const suggestions = buildSuggestions(...cursorPosition(line), getSettings());
+            expect(suggestions).not.toEqual([]);
+        }
+    });
+
+    it('should suggest if there is an opening bracket after many closing brackets', () => {
+        const suggestions = buildSuggestions(...cursorPosition(']]]]]]](hello|'), getSettings());
+        expect(suggestions).not.toEqual([]);
+    });
+
+    it('should not suggest on an empty line', () => {
+        const suggestions = buildSuggestions(...cursorPosition('|'), getSettings());
+        expect(suggestions).toEqual([]);
+    });
+
+    it("should not suggest if there's no open bracket at cursor position", () => {
+        const suggestions = buildSuggestions(...cursorPosition('(hello world)|'), getSettings());
+        expect(suggestions).toEqual([]);
     });
 });
