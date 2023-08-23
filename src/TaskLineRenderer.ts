58e5ed03eb579634798a1394ceb5faaec5c604a9
Ilyas Landikov <ilyas.landikov@icloud.com>
2023-08-23 12:36:42 +0600

    refactor: - unify identical switch-cases

--- src/TaskLineRenderer.ts
+++ src/TaskLineRenderer.ts
@@ -266,8 +266,6 @@ function getTaskClasses(component: TaskLayoutComponent, task: Task) {
             break;
         case 'createdDate':
         case 'dueDate':
-            addGenericDateClasses(task[component], LayoutClasses[component]);
-            break;
         case 'startDate':
             addGenericDateClasses(task[component], LayoutClasses[component]);
             break;
