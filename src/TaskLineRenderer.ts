5c096683a55b8b8fe2a9a4ed4c48098e0cf1316e
Ilyas Landikov <ilyas.landikov@icloud.com>
2023-08-23 14:17:09 +0600

    refactor: . inline createdDateAttributeName

--- src/TaskLineRenderer.ts
+++ src/TaskLineRenderer.ts
@@ -289,14 +289,13 @@ function getTaskDataAttributes(component: TaskLayoutComponent, task: Task) {
         }
     }
 
-    const createdDateAttributeName = 'taskCreated';
     const dueDateAttributeName = 'taskDue';
     const startDateAttributeName = 'taskStart';
     const scheduledDateAttributeName = 'taskScheduled';
     const doneDateAttributeName = 'taskDone';
 
     const dataAttributeNames: { [c in TaskLayoutComponent]: string } = {
-        createdDate: createdDateAttributeName,
+        createdDate: 'taskCreated',
         dueDate: dueDateAttributeName,
         startDate: startDateAttributeName,
         scheduledDate: scheduledDateAttributeName,
