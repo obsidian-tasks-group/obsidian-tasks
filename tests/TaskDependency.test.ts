826c33ec0f1bc1ef6347db315da246022e896d35
Daniel Molloy <danieltmolloy919@gmail.com>
2023-08-24 16:38:18 +1000

    Adds dependency on existing id

---
+++ tests/TaskDependency.test.ts
@@ -0,0 +1,22 @@
+import { Task } from '../src/Task';
+import { TaskBuilder } from './TestingTools/TaskBuilder';
+
+function addDependency(parentTask: Task, childTask: Task) {
+    const newDependsOn = [...parentTask.dependsOn];
+    newDependsOn.push(childTask.id);
+
+    return new Task({ ...parentTask, dependsOn: newDependsOn });
+}
+
+describe('TaskDependency', () => {
+    it('Should add dependency on existing id', () => {
+        const childTask = new TaskBuilder().id('123456').build();
+        const parentTask = new TaskBuilder().description('parent task').build();
+
+        const newParent = addDependency(parentTask, childTask);
+
+        expect(parentTask.dependsOn).toEqual([]);
+        expect(newParent.dependsOn).toEqual(['123456']);
+        expect(childTask.id).toEqual('123456');
+    });
+});
