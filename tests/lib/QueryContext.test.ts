971764b5b8bf31a1a42e9b65acbd97e7e1232286
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2023-09-09 21:54:24 +0100

    test: Remove a test that now serves no value

--- tests/lib/QueryContext.test.ts
+++ tests/lib/QueryContext.test.ts
@@ -1,4 +1,3 @@
-import { verifyAsJson } from 'approvals/lib/Providers/Jest/JestApprovals';
 import { TaskBuilder } from '../TestingTools/TaskBuilder';
 import { FilenameField } from '../../src/Query/Filter/FilenameField';
 import { FolderField } from '../../src/Query/Filter/FolderField';
@@ -7,13 +6,6 @@ import { RootField } from '../../src/Query/Filter/RootField';
 import { makeQueryContextFromPath } from '../../src/lib/QueryContext';
 
 describe('QueryContext', () => {
-    it('should construct a QueryContext from a path', () => {
-        const path = 'a/b/c.md';
-        const queryContext = makeQueryContextFromPath(path);
-
-        verifyAsJson(queryContext);
-    });
-
     describe('values should all match their corresponding filters', () => {
         const path = 'a/b/c.md';
         const task = new TaskBuilder().path(path).build();
