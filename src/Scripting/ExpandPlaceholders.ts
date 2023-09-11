78f9b15ea922384167e83fb7ed5dddde643bdf90
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2023-09-11 13:32:49 +0100

    jsdoc: Document new functions and types

--- src/Scripting/ExpandPlaceholders.ts
+++ src/Scripting/ExpandPlaceholders.ts
@@ -3,6 +3,18 @@ import proxyData from 'mustache-validator';
 
 // https://github.com/janl/mustache.js
 
+/**
+ * Expand any placeholder strings - {{....}} - in the given template, and return the result.
+ * @param template - A template string, typically with placeholders such as {{query.task.folder}}
+ * @param view - The property values
+ *
+ * @throws Error
+ *
+ *      By using mustache-validator's proxyData, we ensure that any accesses of property names that are
+ *      not in the view, we ensure that errors are detected immediately, with the error:
+ *
+ *         `Missing Mustache data property: ${pathSegments.join(" > ")`
+ */
 export function expandPlaceholders(template: string, view: any): string {
     // Turn off HTML escaping of things like '/' in file paths:
     // https://github.com/janl/mustache.js#variables
