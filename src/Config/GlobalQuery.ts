1acc0e94f86f5307ee8fb927ee0681d2cf7f851a
Ilyas Landikov <ilyas.landikov@icloud.com>
2023-09-21 23:49:09 +0600

    refactor: . add _path optional parameter

--- src/Config/GlobalQuery.ts
+++ src/Config/GlobalQuery.ts
@@ -22,7 +22,7 @@ export class GlobalQuery {
         this._source = source;
     }
 
-    public query(): Query {
+    public query(_path: string | undefined = undefined): Query {
         return new Query({ source: this._source });
     }
 
