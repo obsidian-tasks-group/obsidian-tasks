ce04b442fa99d916db4b77155fb9c5e0a65a152f
Ilyas Landikov <ilyas.landikov@icloud.com>
2023-09-25 01:26:32 +0600

    refactor: - explicit return when the header is null

--- src/Query/Filter/BacklinkField.ts
+++ src/Query/Filter/BacklinkField.ts
@@ -38,7 +38,7 @@ export class BacklinkField extends TextField {
             // Only append the heading if it differs from the filename:
             const header = task.precedingHeader;
             if (header === null) {
-                /* empty */
+                return ['[[' + filename + ']]'];
             } else {
                 if (header !== filename) {
                     return [`[[${filename}#${header}|${filename} > ${header}]]`];
