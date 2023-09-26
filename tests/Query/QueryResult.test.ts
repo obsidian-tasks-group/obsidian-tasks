af15d786dc84ff334e173630c89e3bd71cb875cd
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2023-09-26 08:59:39 +0100

    test: - Group together related tasks for readability (and I'm going to add more)

--- tests/Query/QueryResult.test.ts
+++ tests/Query/QueryResult.test.ts
@@ -36,24 +36,26 @@ describe('QueryResult', () => {
         expect(result.taskGroups.totalTasksCount()).toEqual(0);
     });
 
-    it('should pluralise "tasks" if 0 matches', () => {
-        const tasks: Task[] = [];
-        const queryResult = createUngroupedQueryResult(tasks);
-        expect(queryResult.totalTasksCountDisplayText()).toEqual('0 tasks');
-    });
+    describe('Text representation of tasks count', () => {
+        it('should pluralise "tasks" if 0 matches', () => {
+            const tasks: Task[] = [];
+            const queryResult = createUngroupedQueryResult(tasks);
+            expect(queryResult.totalTasksCountDisplayText()).toEqual('0 tasks');
+        });
 
-    it('should not pluralise "task" if only 1 match', () => {
-        const tasks = [fromLine({ line: '- [ ] Do something' })];
-        const queryResult = createUngroupedQueryResult(tasks);
-        expect(queryResult.totalTasksCountDisplayText()).toEqual('1 task');
-    });
+        it('should not pluralise "task" if only 1 match', () => {
+            const tasks = [fromLine({ line: '- [ ] Do something' })];
+            const queryResult = createUngroupedQueryResult(tasks);
+            expect(queryResult.totalTasksCountDisplayText()).toEqual('1 task');
+        });
 
-    it('should pluralise "tasks" if 2 matches', () => {
-        const tasks = [
-            fromLine({ line: '- [ ] Do something more complicated 1' }),
-            fromLine({ line: '- [ ] Do something more complicated 2' }),
-        ];
-        const queryResult = createUngroupedQueryResult(tasks);
-        expect(queryResult.totalTasksCountDisplayText()).toEqual('2 tasks');
+        it('should pluralise "tasks" if 2 matches', () => {
+            const tasks = [
+                fromLine({ line: '- [ ] Do something more complicated 1' }),
+                fromLine({ line: '- [ ] Do something more complicated 2' }),
+            ];
+            const queryResult = createUngroupedQueryResult(tasks);
+            expect(queryResult.totalTasksCountDisplayText()).toEqual('2 tasks');
+        });
     });
 });
