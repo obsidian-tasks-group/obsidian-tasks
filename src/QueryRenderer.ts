7d9d2613258e71a0d55d9e78b6eff934513159c9
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2023-09-26 08:22:56 +0100

    refactor: . Add unused QueryResult parameter to QueryRenderer.addTaskCount()

--- src/QueryRenderer.ts
+++ src/QueryRenderer.ts
@@ -14,6 +14,7 @@ import type { Task } from './Task';
 import { DateFallback } from './DateFallback';
 import { TaskLayout } from './TaskLayout';
 import { explainResults, getQueryForQueryRenderer } from './lib/QueryRendererHelper';
+import type { QueryResult } from './Query/QueryResult';
 import type { TaskGroups } from './Query/TaskGroups';
 
 export class QueryRenderer {
@@ -188,7 +189,7 @@ class QueryRenderChild extends MarkdownRenderChild {
         if (debug) {
             console.debug(`${totalTasksCount} of ${tasks.length} tasks displayed in a block in "${this.filePath}"`);
         }
-        this.addTaskCount(content, totalTasksCount);
+        this.addTaskCount(content, totalTasksCount, queryResult);
     }
 
     private renderErrorMessage(content: HTMLDivElement, errorMessage: string) {
@@ -388,7 +389,7 @@ class QueryRenderChild extends MarkdownRenderChild {
         }
     }
 
-    private addTaskCount(content: HTMLDivElement, tasksCount: number) {
+    private addTaskCount(content: HTMLDivElement, tasksCount: number, _queryResult: QueryResult) {
         if (!this.query.layoutOptions.hideTaskCount) {
             content.createDiv({
                 text: `${tasksCount} task${tasksCount !== 1 ? 's' : ''}`,
