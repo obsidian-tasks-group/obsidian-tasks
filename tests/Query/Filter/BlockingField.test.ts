420af1a45d41f720edcf4c54b0b8b8beaffcb343
Daniel Molloy <danieltmolloy919@gmail.com>
2023-10-02 18:13:01 +1100

    test blocking filter using dependsOn

--- tests/Query/Filter/BlockingField.test.ts
+++ tests/Query/Filter/BlockingField.test.ts
@@ -1,10 +1,22 @@
 import { TaskBuilder } from '../../TestingTools/TaskBuilder';
 import { FilterInstructionsBasedField } from '../../../src/Query/Filter/FilterInstructionsBasedField';
+import type { Task } from '../../../src/Task';
 
 class BlockingField extends FilterInstructionsBasedField {
-    constructor() {
+    // @ts-ignore
+    private readonly _allTasks: Task[];
+    constructor(allTasks: Task[]) {
         super();
-        this._filters.add('is blocking', (task) => task.id !== '');
+        this._allTasks = allTasks;
+        this._filters.add('is blocking', (task) => {
+            if (task.id === '') return false;
+
+            for (const cacheTask of this._allTasks) {
+                if (cacheTask.dependsOn.includes(task.id)) return true;
+            }
+
+            return false;
+        });
     }
 
     fieldName(): string {
@@ -13,16 +25,20 @@ class BlockingField extends FilterInstructionsBasedField {
 }
 
 describe('blocking', () => {
-    const not_blocking = new TaskBuilder().build();
+    const notBlocking = new TaskBuilder().build();
     const child = new TaskBuilder().id('12345').build();
+    const childWithoutParent = new TaskBuilder().id('23456').build();
     const parent = new TaskBuilder().dependsOn(['12345']).build();
 
-    const filter = new BlockingField().createFilterOrErrorMessage('is blocking');
+    const allTasks = [notBlocking, child, parent];
+
+    const filter = new BlockingField(allTasks).createFilterOrErrorMessage('is blocking');
 
     it('is blocking', () => {
         expect(filter).toBeValid();
-        expect(filter).not.toMatchTask(not_blocking);
+        expect(filter).not.toMatchTask(notBlocking);
         expect(filter).toMatchTask(child);
         expect(filter).not.toMatchTask(parent);
+        expect(filter).not.toMatchTask(childWithoutParent);
     });
 });
