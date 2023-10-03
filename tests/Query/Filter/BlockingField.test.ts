9b7d3988fb0577e7f2a9ac9c55d5074151823a97
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2023-10-03 16:06:18 +0100

    test: Rename shared filter to clarify its meaning

--- tests/Query/Filter/BlockingField.test.ts
+++ tests/Query/Filter/BlockingField.test.ts
@@ -2,7 +2,7 @@ import { TaskBuilder } from '../../TestingTools/TaskBuilder';
 import { BlockingField } from '../../../src/Query/Filter/BlockingField';
 
 describe('blocking', () => {
-    const filter = new BlockingField().createFilterOrErrorMessage('is blocking');
+    const isBlocking = new BlockingField().createFilterOrErrorMessage('is blocking');
 
     it('is blocking', () => {
         const notBlocking = new TaskBuilder().build();
@@ -11,11 +11,11 @@ describe('blocking', () => {
         const parent = new TaskBuilder().dependsOn(['12345']).build();
         const allTasks = [notBlocking, child, parent];
 
-        expect(filter).toBeValid();
-        expect(filter).not.toMatchTaskInTaskList(notBlocking, allTasks);
-        expect(filter).toMatchTaskInTaskList(child, allTasks);
-        expect(filter).not.toMatchTaskInTaskList(parent, allTasks);
-        expect(filter).not.toMatchTaskInTaskList(childWithoutParent, allTasks);
+        expect(isBlocking).toBeValid();
+        expect(isBlocking).not.toMatchTaskInTaskList(notBlocking, allTasks);
+        expect(isBlocking).toMatchTaskInTaskList(child, allTasks);
+        expect(isBlocking).not.toMatchTaskInTaskList(parent, allTasks);
+        expect(isBlocking).not.toMatchTaskInTaskList(childWithoutParent, allTasks);
     });
 
     it('is blocking - with circular dependencies, all tasks are matched', () => {
@@ -27,8 +27,8 @@ describe('blocking', () => {
         const task3 = new TaskBuilder().id(id3).dependsOn([id1]).build();
         const allTasks = [task1, task2, task3];
 
-        expect(filter).toMatchTaskInTaskList(task1, allTasks);
-        expect(filter).toMatchTaskInTaskList(task2, allTasks);
-        expect(filter).toMatchTaskInTaskList(task3, allTasks);
+        expect(isBlocking).toMatchTaskInTaskList(task1, allTasks);
+        expect(isBlocking).toMatchTaskInTaskList(task2, allTasks);
+        expect(isBlocking).toMatchTaskInTaskList(task3, allTasks);
     });
 });
