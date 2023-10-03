a244da473c0b471fbdb9690eb31d83a86882f63a
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2023-10-03 16:05:26 +0100

    test: Reduce scope of variables

--- tests/Query/Filter/BlockingField.test.ts
+++ tests/Query/Filter/BlockingField.test.ts
@@ -2,16 +2,15 @@ import { TaskBuilder } from '../../TestingTools/TaskBuilder';
 import { BlockingField } from '../../../src/Query/Filter/BlockingField';
 
 describe('blocking', () => {
-    const notBlocking = new TaskBuilder().build();
-    const child = new TaskBuilder().id('12345').build();
-    const childWithoutParent = new TaskBuilder().id('23456').build();
-    const parent = new TaskBuilder().dependsOn(['12345']).build();
-
-    const allTasks = [notBlocking, child, parent];
-
     const filter = new BlockingField().createFilterOrErrorMessage('is blocking');
 
     it('is blocking', () => {
+        const notBlocking = new TaskBuilder().build();
+        const child = new TaskBuilder().id('12345').build();
+        const childWithoutParent = new TaskBuilder().id('23456').build();
+        const parent = new TaskBuilder().dependsOn(['12345']).build();
+        const allTasks = [notBlocking, child, parent];
+
         expect(filter).toBeValid();
         expect(filter).not.toMatchTaskInTaskList(notBlocking, allTasks);
         expect(filter).toMatchTaskInTaskList(child, allTasks);
