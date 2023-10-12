36c63c833aec1ab3f84ea7316177ebdd2fb33284
Spencer Bliven <spencer.bliven@gmail.com>
2023-10-12 21:24:04 +0200

    Fix #1351: Implement line continuations

---
+++ src/Query/Scanner.ts
@@ -0,0 +1,25 @@
+/**
+ * Removes newlines escaped by a backslash.
+ * A trailing backslash at the end of a line can be escaped by doubling it.
+ *
+ * @param input input string
+ * @returns modified input
+ */
+export function continue_lines(input: string): string {
+    return input.replace(/(?<!\\)\\\n/g, '').replace(/\\\\\n/g, '\\\n');
+}
+/**
+ * Take an input string and split it into a list of statements.
+ *
+ * Generally this is similar to splitting the string into lines, but handles line
+ * continuations and escape sequences.
+ *
+ * @param input Input string
+ * @returns List of statements
+ */
+export function scan(input: string): string[] {
+    return continue_lines(input)
+        .split('\n')
+        .map((rawLine: string) => rawLine.trim())
+        .filter((line) => line !== '');
+}
