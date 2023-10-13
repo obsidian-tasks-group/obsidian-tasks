01955181e7de647eec46557250e233953c9dfcc5
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2023-10-13 20:36:58 +0100

    test: Simplify template tests in DateField.test.ts
    The thorough testing is now in TemplatingPluginTools tests.

--- tests/Query/Filter/DateField.test.ts
+++ tests/Query/Filter/DateField.test.ts
@@ -18,24 +18,17 @@ afterEach(() => {
 });
 
 describe('DateField', () => {
-    describe('Error Checking', () => {
-        // All instructions should be scheduled, as test uses ScheduledDateField
-        it.each([
-            // A Templater 'dynamic command', which has a + after <%
-            // From https://github.com/obsidian-tasks-group/obsidian-tasks/issues/2324:
-            'scheduled before <%+ tp.date.now("YYYY-MM-DD", 0, tp.file.title , "YYYY-MM-DD") %>',
-            // From https://publish.obsidian.md/tasks/Advanced/Daily+Agenda.
-            // In practice, when run via Query, this will give the following error instead:
-            //      Tasks query: There was an error expanding one or more placeholders.
-            // Nevertheless, it's useful to guard against any date formatting text leaking in to filters:
-            'scheduled on {{date+14d:YYYY-MM-DD}}',
-        ])(
-            'should reject date search containing unexpanded template text for instruction "%s"',
-            async (instruction: string) => {
-                const filter = new ScheduledDateField().createFilterOrErrorMessage(instruction);
-                expect(filter).not.toBeValid();
-                expect(filter.error).toContain('Instruction contains unexpanded template text');
-            },
-        );
+    it('should reject date search containing unexpanded template text', () => {
+        // Thorough checks are done in TemplatingPluginTools tests.
+
+        // Arrange
+        const instruction = 'scheduled before <%+ tp.date.now("YYYY-MM-DD", 0, tp.file.title , "YYYY-MM-DD") %>';
+
+        // Act
+        const filter = new ScheduledDateField().createFilterOrErrorMessage(instruction);
+
+        // Assert
+        expect(filter).not.toBeValid();
+        expect(filter.error).toContain('Instruction contains unexpanded template text');
     });
 });
