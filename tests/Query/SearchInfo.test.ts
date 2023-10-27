f40f2463ccaf9436a6e07c196f8a8bd8c745ec2e
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2023-10-27 13:30:16 +0100

    test: Add failing test that shows SearchInfo.allTasks can be updated externally

--- tests/Query/SearchInfo.test.ts
+++ tests/Query/SearchInfo.test.ts
@@ -2,7 +2,7 @@ import { SearchInfo } from '../../src/Query/SearchInfo';
 import { TaskBuilder } from '../TestingTools/TaskBuilder';
 
 describe('SearchInfo', () => {
-    it('should not be able to modify the tasks in SearchInfo.allTasks', () => {
+    it('should not be able to modify the tasks in SearchInfo.allTasks directly', () => {
         const tasks = [new TaskBuilder().build()];
         const searchInfo = new SearchInfo(tasks);
         expect(searchInfo.allTasks.length).toEqual(1);
@@ -12,5 +12,14 @@ describe('SearchInfo', () => {
 
         // Success: Does not compile
         // searchInfo.allTasks[0] = new TaskBuilder().description('cannot replace a task').build();
+    });
+
+    it.failing('should not be able to modify the tasks in SearchInfo.allTasks indirectly', () => {
+        const tasks = [new TaskBuilder().build()];
+        const searchInfo = new SearchInfo(tasks);
+
+        // Check that updating the original list of tasks does not change the tasks saved in searchInfo.allTasks
+        tasks.push(new TaskBuilder().description('I should not be added to searchInfo.allTasks').build());
+        expect(searchInfo.allTasks.length).toEqual(1);
     });
 });
