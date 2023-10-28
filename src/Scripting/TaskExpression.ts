c72cdcded38d40c8a7dc9c8cd56584cc9e602fb2
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2023-10-28 15:42:58 +0100

    refactor: . Move constructArguments() & parseAndEvaluateExpression() to TaskExpression.ts

--- src/Scripting/TaskExpression.ts
+++ src/Scripting/TaskExpression.ts
@@ -1,11 +1,41 @@
 import type { Task } from '../Task';
-import {
-    FunctionOrError,
-    constructArguments,
-    evaluateExpression,
-    evaluateExpressionOrCatch,
-    parseExpression,
-} from './Expression';
+import { FunctionOrError, evaluateExpression, evaluateExpressionOrCatch, parseExpression } from './Expression';
+
+/**
+ *  From: https://www.educative.io/answers/parameter-vs-argument
+ *      A parameter is a variable in a function definition. It is a placeholder and hence does not have a concrete value.
+ *      An argument is a value passed during function invocation.
+ * @param task - during parsing, this can be null. During evaluation, it must be a Task
+ */
+export function constructArguments(task: Task | null) {
+    const paramsArgs: [string, any][] = [
+        // TODO Later, pass in the Query too, for access to file properties
+        ['task', task],
+    ];
+    return paramsArgs;
+}
+
+/**
+ * Evaluate an arbitrary JavaScript expression on a Task object
+ * @param task - a {@link Task} object
+ * @param arg - a string, such as `task.path.startsWith("journal/") ? "journal/" : task.path`
+ *
+ * Currently any errors are returned as string error messages, starting with the word 'Error'.
+ *
+ * @todo Implement a type-safe mechanism to report error messages distinct from expression results.
+ *
+ * See also {@link FunctionField} which exposes this facility to users.
+ */
+export function parseAndEvaluateExpression(task: Task, arg: string) {
+    const paramsArgs = constructArguments(task);
+
+    const functionOrError = parseExpression(paramsArgs, arg);
+    if (functionOrError.error) {
+        return functionOrError.error;
+    }
+
+    return evaluateExpressionOrCatch(functionOrError.queryComponent!, paramsArgs, arg);
+}
 
 /**
  * Encapsulate an expression that can be calculated from a {@link Task} object
