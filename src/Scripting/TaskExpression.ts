334058532bfa4a7b4901c0053a6de99330423b02
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2023-10-29 10:02:59 +0000

    Revert "comment: Remove a TODO that turned out not to work."
    This reverts commit 1d9240ad1e91a3b73aaf1a28ee634e13a0263cd3.

--- src/Scripting/TaskExpression.ts
+++ src/Scripting/TaskExpression.ts
@@ -68,6 +68,7 @@ export class TaskExpression {
      * @see evaluateOrCatch
      */
     public evaluate(task: Task, queryContext?: QueryContext) {
+        // TODO When 'filter by function' supports query properties, make queryContext non-optional and simplify its use below.
         if (!this.isValid()) {
             throw Error(
                 `Error: Cannot evaluate an expression which is not valid: "${this.line}" gave error: "${this.parseError}"`,
