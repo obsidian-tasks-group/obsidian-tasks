51cc58cf6d67eb4ab1c25bdcab0a768fed74e18e
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2023-10-29 18:40:12 +0000

    comment: Move some comments to StatusRegistryReport.ts

--- src/StatusRegistryReport.ts
+++ src/StatusRegistryReport.ts
@@ -1,6 +1,11 @@
 import type { StatusRegistry } from './StatusRegistry';
 
 export function createStatusRegistryReport(statusRegistry: StatusRegistry, buttonName: string, versionString: string) {
+    // Ideas for further improvement
+    // - Actually make it an informative report, that shows any issues in settings with duplicate symbols.
+    // - Show any 'next status symbols' that are not known to the plugin.
+    // - Show any status transitions that won't work with recurring tasks currently, as DONE not followed by TODO.
+
     const detailed = true;
     const mermaidText = statusRegistry.mermaidDiagram(detailed);
     return `# ${buttonName}
