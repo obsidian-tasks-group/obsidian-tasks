7d9db4afe10506646d9e85336066735bc4cefe4e
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2023-10-30 09:23:17 +0000

    test: . Extract function createStatuses()

--- tests/StatusRegistryReport.test.ts
+++ tests/StatusRegistryReport.test.ts
@@ -5,6 +5,27 @@ import type { StatusCollection, StatusCollectionEntry } from '../src/StatusColle
 import { Status } from '../src/Status';
 import { verifyWithFileExtension } from './TestingTools/ApprovalTestHelpers';
 
+function createStatuses(
+    coreStatusesData: Array<StatusCollectionEntry>,
+    customStatusesData: Array<StatusCollectionEntry>,
+) {
+    // Populate StatusSettings:
+    const statusSettings = new StatusSettings();
+
+    const core = statusSettings.coreStatuses;
+    StatusSettings.replaceStatus(core, core[0], Status.createFromImportedValue(coreStatusesData[0]));
+    StatusSettings.replaceStatus(core, core[1], Status.createFromImportedValue(coreStatusesData[1]));
+
+    customStatusesData.map((entry: StatusCollectionEntry) => {
+        StatusSettings.addStatus(statusSettings.customStatuses, Status.createFromImportedValue(entry));
+    });
+
+    // Populate StatusRegistry:
+    const statusRegistry = new StatusRegistry();
+    StatusSettings.applyToStatusRegistry(statusSettings, statusRegistry);
+    return { statusSettings, statusRegistry };
+}
+
 describe('StatusRegistryReport', function () {
     it('should create a report', () => {
         // Arrange
@@ -18,21 +39,7 @@ describe('StatusRegistryReport', function () {
             ['Q', 'Question', 'A', 'NON_TASK'],
             ['A', 'Answer', 'Q', 'NON_TASK'],
         ];
-
-        // Populate StatusSettings:
-        const statusSettings = new StatusSettings();
-
-        const core = statusSettings.coreStatuses;
-        StatusSettings.replaceStatus(core, core[0], Status.createFromImportedValue(coreStatusesData[0]));
-        StatusSettings.replaceStatus(core, core[1], Status.createFromImportedValue(coreStatusesData[1]));
-
-        customStatusesData.map((entry: StatusCollectionEntry) => {
-            StatusSettings.addStatus(statusSettings.customStatuses, Status.createFromImportedValue(entry));
-        });
-
-        // Populate StatusRegistry:
-        const statusRegistry = new StatusRegistry();
-        StatusSettings.applyToStatusRegistry(statusSettings, statusRegistry);
+        const { statusSettings, statusRegistry } = createStatuses(coreStatusesData, customStatusesData);
 
         const reportName = 'Review and check your Statuses';
 
