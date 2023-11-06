621145f17ba9f5bf659ff73ab7dbc03c49d49e9c
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2023-11-06 21:28:41 +0000

    test: Add JestReporter as fallback diff tool - will help CI builds

--- tests/TestingTools/VerifyMarkdown.ts
+++ tests/TestingTools/VerifyMarkdown.ts
@@ -1,23 +1,24 @@
 import { type ConfigModifier, Options } from 'approvals/lib/Core/Options';
 import { verify } from 'approvals/lib/Providers/Jest/JestApprovals';
 
-// This import fails with Approvals.NodeJS 6.2.1:
-// error TS1371: This import is never used as a value and must use 'import type' because 'importsNotUsedAsValues' is set to 'error'.
-// import { JestReporter } from 'approvals/lib/Providers/Jest/JestReporter';
+import { JestReporter } from 'approvals/lib/Providers/Jest/JestReporter';
 
 export function verifyMarkdown(output: string) {
     let options = new Options();
     options = options.forFile().withFileExtention('md');
 
+    // TODO Move this to somewhere where it is picked up by all tests, not just ones
+    //      that use this function.
     const configModifier: ConfigModifier = (c) => {
         c.reporters = [
             // Built-in reporters listed at:
             // https://github.com/approvals/Approvals.NodeJS#built-in-reporters
             'vscode', // VS Code diff works well with files containing emojis
             //-----------------
-            // Last one is jest reporter, that should write diffs to console in
-            // Continuous Integration builds, such as GitHub Actions
-            // new JestReporter(), // disabled due to import error - see above
+            // Last one is jest reporter, that writes diffs to console in
+            // Continuous Integration builds, such as GitHub Actions,
+            // or when the development environment has no supported diff tools.
+            new JestReporter(),
         ];
         return c;
     };
