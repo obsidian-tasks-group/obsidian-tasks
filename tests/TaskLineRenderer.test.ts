3039b26c5330c14fea5b6b99ca5ce243e545aab6
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2023-11-09 13:52:16 +0000

    comment: Add to existing comment, to explain why the plain HTML is needed.

--- tests/TaskLineRenderer.test.ts
+++ tests/TaskLineRenderer.test.ts
@@ -604,7 +604,8 @@ describe('Visualise HTML', () => {
         const mockHTMLRenderer = async (text: string, element: HTMLSpanElement, _path: string) => {
             // Contrary to the default mockTextRenderer() in createMockParentAndRender(),
             // instead of the rendered HTMLSpanElement.innerText,
-            // we need the plain HTML here like in TaskLineRenderer.renderComponentText().
+            // we need the plain HTML here like in TaskLineRenderer.renderComponentText(),
+            // in order to ensure that any description and tags are retained.
             element.innerHTML = text;
         };
 
