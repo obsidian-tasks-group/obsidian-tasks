6b6d487f2403d8f2a2a710180a33cac59a9a35e1
Victor DUSART <dusartvictor@gmail.com>
2023-11-16 19:17:42 +0100

    starts adding case insensitivity

--- src/Query/Filter/DateField.ts
+++ src/Query/Filter/DateField.ts
@@ -38,6 +38,7 @@ export abstract class DateField extends Field {
     }
 
     public canCreateFilterForLine(line: string): boolean {
+        line = line?.toLowerCase();
         if (this.filterInstructions.canCreateFilterForLine(line)) {
             return true;
         }
@@ -54,7 +55,7 @@ export abstract class DateField extends Field {
             return FilterOrErrorMessage.fromError(line, errorText);
         }
 
-        const filterResult = this.filterInstructions.createFilterOrErrorMessage(line);
+        const filterResult = this.filterInstructions.createFilterOrErrorMessage(line.toLowerCase());
         if (filterResult.filter !== undefined) {
             return filterResult;
         }
@@ -148,6 +149,7 @@ export abstract class DateField extends Field {
     protected filterRegExp(): RegExp {
         return new RegExp(
             `^${this.fieldNameForFilterInstruction()} (((?:on|in) or before|before|(?:on|in) or after|after|on|in)? ?(.*))`,
+            'i',
         );
     }
 
