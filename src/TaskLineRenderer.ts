9ade80f607abfe14b7aff94d9574e37f051f6e8c
Ilyas Landikov <ilyas.landikov@icloud.com>
2023-11-21 17:35:30 +0600

    refactor: . move FieldLayouts to TaskLineRenderer.ts

--- src/TaskLineRenderer.ts
+++ src/TaskLineRenderer.ts
@@ -5,9 +5,11 @@ import { TASK_FORMATS, getSettings } from './Config/Settings';
 import { replaceTaskWithTasks } from './File';
 import type { Task } from './Task';
 import * as taskModule from './Task';
-import { type AttributesDictionary, FieldLayoutDetails, FieldLayouts } from './TaskFieldRenderer';
+import { type AttributesDictionary, FieldLayoutDetails, FieldLayoutsContainer } from './TaskFieldRenderer';
 import type { LayoutOptions, TaskLayoutComponent } from './TaskLayout';
 import { TaskLayout } from './TaskLayout';
+
+const FieldLayouts = new FieldLayoutsContainer();
 
 /**
  * The function used to render a Markdown task line into an existing HTML element.
