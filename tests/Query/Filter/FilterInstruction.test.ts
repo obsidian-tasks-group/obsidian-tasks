426c41eacad59e9eb1a42e2cf5f2b08f8865309a
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2023-11-27 19:57:25 +0000

    refactor: FilterInstruction is now case-insensitive

---
+++ tests/Query/Filter/FilterInstruction.test.ts
@@ -0,0 +1,25 @@
+import { FilterInstruction } from '../../../src/Query/Filter/FilterInstruction';
+import type { FilterFunction } from '../../../src/Query/Filter/Filter';
+import type { Task } from '../../../src/Task';
+
+describe('FilterInstruction', () => {
+    const filter: FilterFunction = (_task: Task) => true;
+    const filterInstruction = new FilterInstruction('find me', filter);
+
+    it('canCreateFilterForLine should be case-insensitive exact match', () => {
+        expect(filterInstruction.canCreateFilterForLine('FIND ME')).toEqual(true);
+        expect(filterInstruction.canCreateFilterForLine('xFIND ME')).toEqual(false);
+        expect(filterInstruction.canCreateFilterForLine('FIND MEx')).toEqual(false);
+    });
+
+    it('createFilterOrErrorMessage should be case-insensitive', () => {
+        const filterOrErrorMessage = filterInstruction.createFilterOrErrorMessage('FIND ME');
+        expect(filterOrErrorMessage).toBeValid();
+    });
+
+    it('explanation should be case-insensitive', () => {
+        const line = 'FIND ME';
+        const filterOrErrorMessage = filterInstruction.createFilterOrErrorMessage(line);
+        expect(filterOrErrorMessage).toHaveExplanation(line);
+    });
+});
