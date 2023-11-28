890c3a5e02e7fa9d055e06508ed6d97f3a4b55e6
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2023-11-28 14:18:07 +0000

    test: - Make tests easier to skim-read, by removing need to read date strings

--- tests/lib/QueryRendererHelper.test.ts
+++ tests/lib/QueryRendererHelper.test.ts
@@ -139,50 +139,52 @@ describe('postpone - date field choice', () => {
         checkPostponeField(taskBuilder, null);
     }
 
+    // Since the actual date values do not affect the calculation, we use the same value for all tests,
+    // so that the field names stand out when comparing tests.
+    const date = '2023-11-26';
+
     it('should not postpone if no happens dates on task', () => {
         const taskBuilder = new TaskBuilder();
         checkDoesNotPostpone(taskBuilder);
     });
 
     it('should not postpone, if start is only happens field', () => {
-        const taskBuilder = new TaskBuilder().startDate('2023-11-28');
+        const taskBuilder = new TaskBuilder().startDate(date);
         checkDoesNotPostpone(taskBuilder);
     });
 
     it('should not postpone created or done dates', () => {
-        const taskBuilder = new TaskBuilder().createdDate('2023-11-26').doneDate('2023-11-27');
+        const taskBuilder = new TaskBuilder().createdDate(date).doneDate(date);
         checkDoesNotPostpone(taskBuilder);
     });
 
     it('should postpone due date', () => {
-        const taskBuilder = new TaskBuilder().dueDate('2023-11-26');
+        const taskBuilder = new TaskBuilder().dueDate(date);
         checkPostponeField(taskBuilder, 'dueDate');
     });
 
     it('should postpone scheduled date', () => {
-        const taskBuilder = new TaskBuilder().scheduledDate('2023-11-26');
+        const taskBuilder = new TaskBuilder().scheduledDate(date);
         checkPostponeField(taskBuilder, 'scheduledDate');
     });
 
     it('should postpone when scheduled date is inferred', () => {
-        const taskBuilder = new TaskBuilder().scheduledDate('2023-11-26').scheduledDateIsInferred(true);
+        const taskBuilder = new TaskBuilder().scheduledDate(date).scheduledDateIsInferred(true);
         checkPostponeField(taskBuilder, 'scheduledDate');
     });
 
     it('should postpone due date in preference to start and scheduled dates', () => {
-        const date = '2023-11-26';
         const taskBuilder = new TaskBuilder().dueDate(date).scheduledDate(date).startDate(date);
         checkPostponeField(taskBuilder, 'dueDate');
     });
 
     it('should postpone scheduled date in preference to start date', () => {
-        const date = '2023-11-26';
         const taskBuilder = new TaskBuilder().scheduledDate(date).startDate(date);
         checkPostponeField(taskBuilder, 'scheduledDate');
     });
 
     it('should not postpone scheduled date', () => {
-        const taskBuilder = new TaskBuilder().startDate('2023-11-26');
+        const taskBuilder = new TaskBuilder().startDate(date);
         checkDoesNotPostpone(taskBuilder);
     });
 });
