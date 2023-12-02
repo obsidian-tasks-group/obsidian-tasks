5c76e2106bea9f4eaf34786e050cb82576421495
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2023-12-02 21:53:49 +0000

    fix: Remove accidental hard-coded string

--- src/lib/LogTasksHelper.ts
+++ src/lib/LogTasksHelper.ts
@@ -3,7 +3,7 @@ import type { Logger } from './logging';
 
 export function logStartOfTaskEdit(logger: Logger, codeLocation: string, originalTask: Task) {
     logger.debug(`${codeLocation} entered. ${originalTask.path}`);
-    logger.debug(`replaceTaskWithTasks() original: ${originalTask.originalMarkdown}`);
+    logger.debug(`${codeLocation} original: ${originalTask.originalMarkdown}`);
 }
 
 export function logEndOfTaskEdit(logger: Logger, codeLocation: string, newTasks: Task[]) {
