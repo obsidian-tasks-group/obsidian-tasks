bbb18f0766f8bd57acef53cbd20df7bc632d5ffb
Ilyas Landikov <ilyas.landikov@icloud.com>
2023-12-03 16:42:11 +0600

    test: - add expectedDateField parameter to the helper

--- tests/Scripting/Postponer.test.ts
+++ tests/Scripting/Postponer.test.ts
@@ -154,16 +154,16 @@ describe('postpone - new task creation', () => {
         jest.useRealTimers();
     });
 
-    function testPostponedTaskAndDate(task: Task, expectedPostponedDate: string) {
+    function testPostponedTaskAndDate(task: Task, expectedPostponedDate: string, expectedDateField: HappensDate) {
         const { postponedDate, newTasks } = createPostponedTask(task, 'dueDate', 'day', 1);
         expect(postponedDate.format('YYYY-MM-DD')).toEqual(expectedPostponedDate);
-        expect(newTasks.due.formatAsDate()).toEqual(expectedPostponedDate);
+        expect(newTasks[expectedDateField]?.format('YYYY-MM-DD')).toEqual(expectedPostponedDate);
     }
 
     it('should postpone an overdue task to today', () => {
         const task = new TaskBuilder().dueDate('2023-11-01').build();
         const expectedPostponedDate = '2023-12-04';
-        testPostponedTaskAndDate(task, expectedPostponedDate);
+        testPostponedTaskAndDate(task, expectedPostponedDate, 'dueDate');
     });
 
     it('should postpone a task scheduled today to tomorrow', () => {
