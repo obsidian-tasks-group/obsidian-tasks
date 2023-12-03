1f6154cfd9cff8c17ee69e6b00085f86c3ea4072
Ilyas Landikov <ilyas.landikov@icloud.com>
2023-12-03 15:33:00 +0600

    test: . extract postponedDate variable

--- tests/Scripting/TasksDate.test.ts
+++ tests/Scripting/TasksDate.test.ts
@@ -118,26 +118,41 @@ describe('TasksDate - postpone', () => {
 
     it('should postpone an older date (before yesterday) to tomorrow', () => {
         const tasksDate = new TasksDate(moment('2023-11-20'));
-        expect(tasksDate.postpone().format('YYYY-MM-DD')).toEqual('2023-11-29');
+
+        const postponedDate = tasksDate.postpone();
+
+        expect(postponedDate.format('YYYY-MM-DD')).toEqual('2023-11-29');
     });
 
     it('should postpone yesterday date to tomorrow', () => {
         const tasksDate = new TasksDate(moment('2023-11-27'));
-        expect(tasksDate.postpone().isSame(moment('2023-11-29'))).toEqual(true);
+
+        const postponedDate = tasksDate.postpone();
+
+        expect(postponedDate.isSame(moment('2023-11-29'))).toEqual(true);
     });
 
     it('should postpone today date to tomorrow', () => {
         const tasksDate = new TasksDate(moment('2023-11-28'));
-        expect(tasksDate.postpone().isSame(moment('2023-11-29'))).toEqual(true);
+
+        const postponedDate = tasksDate.postpone();
+
+        expect(postponedDate.isSame(moment('2023-11-29'))).toEqual(true);
     });
 
     it('should postpone tomorrow date to after tomorrow', () => {
         const tasksDate = new TasksDate(moment('2023-11-29'));
-        expect(tasksDate.postpone().isSame(moment('2023-11-30'))).toEqual(true);
+
+        const postponedDate = tasksDate.postpone();
+
+        expect(postponedDate.isSame(moment('2023-11-30'))).toEqual(true);
     });
 
     it('should postpone a future date (after tomorrow)', () => {
         const tasksDate = new TasksDate(moment('2023-11-29'));
-        expect(tasksDate.postpone().isSame(moment('2023-11-30'))).toEqual(true);
+
+        const postponedDate = tasksDate.postpone();
+
+        expect(postponedDate.isSame(moment('2023-11-30'))).toEqual(true);
     });
 });
