27f93ca17428da86697752142d39e99cfdb0a7e0
Ilyas Landikov <93825870+ilandikov@users.noreply.github.com>
2023-12-07 15:17:28 +0600

    test: test postponed task and date creation (#2473)
    * test: - test createPostponedTask()
    
    * test: - test TasksDate.postpone()
    
    * test: . extract postponedDate variable
    
    * test: . test TaskDate object to use the formatAsDate() method
    
    * test: - fix test data to reflect the test meaning
    
    * test: . extract expectedPostponedDate variable
    
    * test: - add expectedDateField parameter to the helper
    
    * test: . reorder helper's parameters for readability
    
    * test: . inline variable
    
    * test: - use the helper function in other tests
    
    * test: add failing test for postponing tasks with inferred scheduled dates
    
    * test: . Extract variables for constants in one test
    
    Preparing to extract a helper function
    
    * test: . Extract helper function checkDatePostponesTo()
    
    * test: . Inline constants to make test behaviour clearer
    
    * test: - Adjust tests to use helper function
    
    * test: Confirm that advancing by 1 month works with shorter months
    
    * test: Add some TODOs for future tests
    
    * test: Show that postponing overdue tasks by > 1 day is unhelpful now.
    
    * test: Add a test to visualise postponing by 1 week
    
    * test: . Extract helper function postponeMultipleDatesBy()
    
    * test: . Inline 'amount' and 'unitOfTime'; rename 'output' to indicate intent
    
    * test: Fix the test name
    
    * test: Visualise postponing by 1 day
    
    * test: Move TODO to where needed
    
    * test: Add failing test for postponing future date changing stored date
    
    * test: - Extract function checkPostponingDoesNotModifyOriginalDate()
    
    * test: . Inline variables
    
    * test: Better coverage for checking postpone() does not modify date
    
    ---------
    
    Co-authored-by: Clare Macrae <github@cfmacrae.fastmail.co.uk>

--- tests/Scripting/Postponer.test.ts
+++ tests/Scripting/Postponer.test.ts
@@ -2,15 +2,17 @@
  * @jest-environment jsdom
  */
 import moment from 'moment';
+import type { Task } from 'Task';
 import {
     type HappensDate,
+    createPostponedTask,
     getDateFieldToPostpone,
     postponementSuccessMessage,
     shouldShowPostponeButton,
 } from '../../src/Scripting/Postponer';
-import { TaskBuilder } from '../TestingTools/TaskBuilder';
-import { StatusConfiguration, StatusType } from '../../src/StatusConfiguration';
 import { Status } from '../../src/Status';
+import { StatusConfiguration, StatusType } from '../../src/StatusConfiguration';
+import { TaskBuilder } from '../TestingTools/TaskBuilder';
 
 window.moment = moment;
 
@@ -67,6 +69,8 @@ describe('postpone - date field choice', () => {
         const taskBuilder = new TaskBuilder().scheduledDate(date).startDate(date);
         checkPostponeField(taskBuilder, 'scheduledDate');
     });
+
+    // TODO Check it refuses to postpone an invalid date (failing test)
 });
 
 describe('postpone - whether to show button', () => {
@@ -139,6 +143,51 @@ describe('postpone - whether to show button', () => {
         const task = new TaskBuilder().dueDate('20233-12-03').build();
 
         expect(shouldShowPostponeButton(task)).toEqual(false);
+    });
+});
+
+describe('postpone - new task creation', () => {
+    beforeEach(() => {
+        jest.useFakeTimers();
+        jest.setSystemTime(new Date('2023-12-03'));
+    });
+
+    afterEach(() => {
+        jest.useRealTimers();
+    });
+
+    function testPostponedTaskAndDate(task: Task, expectedDateField: HappensDate, expectedPostponedDate: string) {
+        const { postponedDate, newTasks } = createPostponedTask(task, expectedDateField, 'day', 1);
+        expect(postponedDate.format('YYYY-MM-DD')).toEqual(expectedPostponedDate);
+        expect(newTasks[expectedDateField]?.format('YYYY-MM-DD')).toEqual(expectedPostponedDate);
+
+        // If the scheduled date was inferred from the filename, and it is the scheduledDate that was postponed,
+        // we must ensure that the 'inferred' flag has been reset to false.
+        // Otherwise, the new scheduled date will be ignored in some locations, like rendering of dates.
+        if (task.scheduledDateIsInferred && expectedDateField === 'scheduledDate') {
+            expect(newTasks.scheduledDateIsInferred).toEqual(false);
+        }
+    }
+
+    it('should postpone an overdue task to today', () => {
+        const task = new TaskBuilder().dueDate('2023-11-01').build();
+        const expectedPostponedDate = '2023-12-04';
+        testPostponedTaskAndDate(task, 'dueDate', expectedPostponedDate);
+    });
+
+    it('should postpone a task scheduled today to tomorrow', () => {
+        const task = new TaskBuilder().scheduledDate('2023-12-03').build();
+        testPostponedTaskAndDate(task, 'scheduledDate', '2023-12-04');
+    });
+
+    it.failing('should postpone a task scheduled today to tomorrow, when the scheduled date is inferred', () => {
+        const task = new TaskBuilder().scheduledDate('2023-12-03').scheduledDateIsInferred(true).build();
+        testPostponedTaskAndDate(task, 'scheduledDate', '2023-12-04');
+    });
+
+    it('should postpone a task that starts in the future to the next day', () => {
+        const task = new TaskBuilder().startDate('2024-03-05').build();
+        testPostponedTaskAndDate(task, 'startDate', '2024-03-06');
     });
 });
 
