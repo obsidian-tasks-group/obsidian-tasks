98f99ada9be59fddec6f6959c139685314c2eb19
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2023-12-08 19:44:48 +0000

    fix: Remove variable name from description of postponed field

--- src/Scripting/Postponer.ts
+++ src/Scripting/Postponer.ts
@@ -59,11 +59,12 @@ export function postponeButtonTitle(task: Task, amount: number, timeUnit: unitOf
 
 export function postponeMenuItemTitle(task: Task, amount: number, timeUnit: unitOfTime.DurationConstructor) {
     const updatedDateType = getDateFieldToPostpone(task)!;
-
     const dateToUpdate = task[updatedDateType] as Moment;
+    const updatedDateDisplayText = updatedDateType.replace('Date', '');
+
     const postponedDate = new TasksDate(dateToUpdate).postpone(timeUnit, amount);
     const formattedNewDate = postponedDate.format('ddd Do MMM, YYYY');
 
     const amountOrArticle = amount > 1 ? amount : 'a';
-    return `Postpone ${updatedDateType} for ${amountOrArticle} ${timeUnit} - to ${formattedNewDate}`;
+    return `Postpone ${updatedDateDisplayText} for ${amountOrArticle} ${timeUnit} - to ${formattedNewDate}`;
 }
