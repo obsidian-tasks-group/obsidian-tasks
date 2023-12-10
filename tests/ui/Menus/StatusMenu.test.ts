cf52a32af68b90367645cb74d45b9a76f2a450b2
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2023-12-10 14:40:31 +0000

    test: Fix test name

--- tests/ui/Menus/StatusMenu.test.ts
+++ tests/ui/Menus/StatusMenu.test.ts
@@ -20,7 +20,7 @@ function menuToString(menu: StatusMenu) {
 }
 
 describe('StatusMenu', () => {
-    it('creation', () => {
+    it('should show checkmark against the current task status', () => {
         // Arrange
         const task = new TaskBuilder().status(Status.makeInProgress()).build();
         const statusRegistry = new StatusRegistry();
