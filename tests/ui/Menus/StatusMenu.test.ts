029be5f914205bb3a7bf788aafe9878acc7bb489
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2023-12-10 13:19:31 +0000

    test: Add test of the text of menu items created by StatusMenu

--- tests/ui/Menus/StatusMenu.test.ts
+++ tests/ui/Menus/StatusMenu.test.ts
@@ -1,10 +1,31 @@
 import { StatusMenu } from '../../../src/ui/Menus/StatusMenu';
+import { TaskBuilder } from '../../TestingTools/TaskBuilder';
+import { MenuItem } from '../../__mocks__/obsidian';
 
 export {};
 
 describe('StatusMenu', () => {
+    it('MenuItem', () => {
+        const item = new MenuItem();
+        expect(item.title).toEqual('');
+
+        item.setTitle('hello');
+        expect(item.title).toEqual('hello');
+    });
+
     it('creation', () => {
-        // @ts-expect-error unused
-        const menu = new StatusMenu();
+        const task = new TaskBuilder().build();
+        const menu = new StatusMenu(task);
+        // @ts-expect-error TS2339: Property 'items' does not exist on type 'StatusMenu'.
+        const items: MenuItem[] = menu.items;
+        expect(items.length).toEqual(4);
+
+        const itemsAsText = items.map((item) => item.title).join('\n');
+        expect(itemsAsText).toMatchInlineSnapshot(`
+            "Change status to:   Todo
+            Change status to:   Done
+            Change status to:   In Progress
+            Change status to:   Cancelled"
+        `);
     });
 });
