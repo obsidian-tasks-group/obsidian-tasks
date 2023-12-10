342c8d6826a1c1dfefc56b38737c40c0ac23acde
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2023-12-10 13:19:31 +0000

    test: Add test of the text of menu items created by StatusMenu

--- tests/__mocks__/obsidian.ts
+++ tests/__mocks__/obsidian.ts
@@ -1,13 +1,35 @@
 export {};
 
-export class MenuItem {}
+export class MenuItem {
+    public title: string | DocumentFragment = '';
+    public callback: (evt: MouseEvent | KeyboardEvent) => any;
+
+    constructor() {
+        this.callback = (_evt: MouseEvent | KeyboardEvent) => console.log('callback not defined');
+    }
+
+    public setTitle(title: string | DocumentFragment): this {
+        this.title = title;
+        return this;
+    }
+
+    public onClick(callback: (evt: MouseEvent | KeyboardEvent) => any): this {
+        this.callback = callback;
+        return this;
+    }
+}
 
 export class Menu {
+    public items: MenuItem[] = [];
+
     /**
      * Adds a menu item. Only works when menu is not shown yet.
      * @public
      */
-    addItem(_cb: (item: MenuItem) => any): this {
+    addItem(cb: (item: MenuItem) => any): this {
+        const item = new MenuItem();
+        cb(item);
+        this.items.push(item);
         return this;
     }
 }
