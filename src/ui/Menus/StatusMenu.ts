6d055f89f31b8a546ad4981880996eeb08529808
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2023-12-10 18:46:25 +0000

    refactor: . Move TaskEditingMenu and related symbols to TaskEditingMenu.ts

--- src/ui/Menus/StatusMenu.ts
+++ src/ui/Menus/StatusMenu.ts
@@ -1,47 +1,8 @@
-import { Menu, MenuItem } from 'obsidian';
+import type { MenuItem } from 'obsidian';
 import type { StatusRegistry } from '../../StatusRegistry';
-import { replaceTaskWithTasks } from '../../File';
 import type { Task } from '../../Task';
 import { StatusSettings } from '../../Config/StatusSettings';
-
-/**
- * A function for replacing one task with zero or more new tasks.
- * @see {@link defaultTaskSaver}
- */
-type TaskSaver = (originalTask: Task, newTasks: Task | Task[]) => Promise<void>;
-
-/**
- * A default implementation of {@link TaskSaver} that calls {@link replaceTaskWithTasks}
- * @param originalTask
- * @param newTasks
- */
-async function defaultTaskSaver(originalTask: Task, newTasks: Task | Task[]) {
-    await replaceTaskWithTasks({
-        originalTask,
-        newTasks,
-    });
-}
-
-/**
- * Base class for Menus that offer editing one or more properties of a Task object.
- *
- * A {@link TaskSaver} function must be supplied, in order for any edits to be saved.
- * Derived classes should default to using {@link defaultTaskSaver}, but allow
- * alternative implementations to be used in tests.
- */
-class TaskEditingMenu extends Menu {
-    protected readonly taskSaver: TaskSaver;
-
-    /**
-     * Constructor, which sets up the menu items.
-     * @param taskSaver - a {@link TaskSaver} function, for saving any edits.
-     */
-    constructor(taskSaver: TaskSaver) {
-        super();
-
-        this.taskSaver = taskSaver;
-    }
-}
+import { TaskEditingMenu, type TaskSaver, defaultTaskSaver } from './TaskEditingMenu';
 
 /**
  * A Menu of options for editing the status of a Task object.
