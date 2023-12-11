7ad1d376816d16d9a02714814b1a978139da9612
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2023-12-11 10:54:34 +0000

    feat: Add Priorities menu (not yet used anywhere in the plugin)
    See the jsdoc example for how to add it to the Pencil button in QueryRenderer.

---
+++ src/ui/Menus/PriorityMenu.ts
@@ -0,0 +1,54 @@
+import type { MenuItem } from 'obsidian';
+import { Priority, Task } from '../../Task';
+import { PriorityTools } from '../../lib/PriorityTools';
+import { TaskEditingMenu, type TaskSaver, defaultTaskSaver } from './TaskEditingMenu';
+
+/**
+ * A Menu of options for editing the status of a Task object.
+ *
+ * @example
+ *     editTaskPencil.addEventListener('contextmenu', (ev: MouseEvent) => {
+ *         const menu = new PriorityMenu(task);
+ *         menu.showAtPosition({ x: ev.clientX, y: ev.clientY });
+ *     });
+ *     editTaskPencil.setAttribute('title', 'Right-click for options');
+ */
+export class PriorityMenu extends TaskEditingMenu {
+    /**
+     * Constructor, which sets up the menu items.
+     * @param task - the Task to be edited.
+     * @param taskSaver - an optional {@link TaskSaver} function. For details, see {@link TaskEditingMenu}.
+     */
+    constructor(task: Task, taskSaver: TaskSaver = defaultTaskSaver) {
+        super(taskSaver);
+
+        const commonTitle = 'Priority:';
+
+        const getMenuItemCallback = (task: Task, item: MenuItem, newPriority: Priority) => {
+            const title = `${commonTitle} ${PriorityTools.priorityNameUsingNormal(newPriority)}`;
+            item.setTitle(title)
+                .setChecked(newPriority === task.priority)
+                .onClick(async () => {
+                    if (newPriority !== task.priority) {
+                        const newTask = new Task({
+                            ...task,
+                            priority: newPriority,
+                        });
+                        await this.taskSaver(task, newTask);
+                    }
+                });
+        };
+
+        const allPriorities = [
+            Priority.Highest,
+            Priority.High,
+            Priority.Medium,
+            Priority.None,
+            Priority.Low,
+            Priority.Lowest,
+        ];
+        for (const priority of allPriorities) {
+            this.addItem((item) => getMenuItemCallback(task, item, priority));
+        }
+    }
+}
