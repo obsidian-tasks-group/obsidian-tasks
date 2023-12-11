515c8d17c31d9ec17bb12f5c120e34c3151c17c9
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2023-12-11 13:53:06 +0000

    jsdoc: Add documentation for TaskEditingInstruction

--- src/ui/EditInstructions/TaskEditingInstruction.ts
+++ src/ui/EditInstructions/TaskEditingInstruction.ts
@@ -1,9 +1,20 @@
 import type { Task } from '../../Task';
 
+/**
+ * An instruction interface, for editing a {@link Task} object.
+ */
 export interface TaskEditingInstruction {
     /**
      * Apply the edit to a copy of the given task.
+     *
+     * @note {@link Task} objects are immutable, so any edits are returned in a new task or tasks.
      * @param task
+     * @returns An array of 0 or more tasks:
+     *
+     *            - Typically, this contains a single value, containing an edited copy of {@link task}.
+     *            - An extra task would indicate that a new occurrence has been created.
+     *            - An empty array would indicate to delete the task.
+     *            - If no edit was made, the returned value will be an array of one element, which is the input task.
      */
     apply(task: Task): Task[];
 
