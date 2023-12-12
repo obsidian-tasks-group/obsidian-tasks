04aa3003577b79ae3f03abe2753cb1d2655724ec
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2023-12-12 08:14:47 +0000

    test: Convert menuToString() in PriorityMenu.test.ts to generics
    This should allow it to be used in other tests files.

--- tests/ui/Menus/PriorityMenu.test.ts
+++ tests/ui/Menus/PriorityMenu.test.ts
@@ -6,8 +6,8 @@ import { Priority } from '../../../src/Task';
 
 export {};
 
-function menuToString(menu: PriorityMenu) {
-    // @ts-expect-error TS2339: Property 'items' does not exist on type 'PriorityMenu'.
+function menuToString<MenuType>(menu: MenuType) {
+    // @ts-expect-error TS2339: Property 'items' does not exist on type 'MenuType'.
     const items: MenuItem[] = menu.items;
     return '\n' + items.map((item) => `${item.checked ? 'x' : ' '} ${item.title}`).join('\n');
 }
