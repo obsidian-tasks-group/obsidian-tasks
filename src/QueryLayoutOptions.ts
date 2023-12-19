e509b5882ebc1f647a0fabf76aa87efe3789f1e6
Ilyas Landikov <ilyas.landikov@icloud.com>
2023-12-19 15:52:48 +0600

    refactor: . add QueryLayoutOptions base class

---
+++ src/QueryLayoutOptions.ts
@@ -0,0 +1,9 @@
+export class QueryLayoutOptions {
+    hidePostponeButton: boolean = false;
+    hideTaskCount: boolean = false;
+    hideBacklinks: boolean = false;
+    hideEditButton: boolean = false;
+    hideUrgency: boolean = true;
+    shortMode: boolean = false;
+    explainQuery: boolean = false;
+}
