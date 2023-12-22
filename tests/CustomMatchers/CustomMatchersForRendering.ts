0ee5ae7a4b3f575b29a95fd51e45184417aec758
Ilyas Landikov <ilyas.landikov@icloud.com>
2023-12-22 15:37:06 +0600

    test: - add toHaveAChildSpanWithClassAndDataAttributes() matcher

--- tests/CustomMatchers/CustomMatchersForRendering.ts
+++ tests/CustomMatchers/CustomMatchersForRendering.ts
@@ -4,16 +4,23 @@ declare global {
     namespace jest {
         interface Matchers<R> {
             toHaveDataAttributes(expectedDataAttributes: string): R;
+            toHaveAChildSpanWithClassAndDataAttributes(expectedClass: string, expectedDataAttributes: string): R;
         }
 
         interface Expect {
             toHaveDataAttributes(expectedDataAttributes: string): any;
+            toHaveAChildSpanWithClassAndDataAttributes(expectedClass: string, expectedDataAttributes: string): any;
         }
 
         interface InverseAsymmetricMatchers {
             toHaveDataAttributes(expectedDataAttributes: string): any;
+            toHaveAChildSpanWithClassAndDataAttributes(expectedClass: string, expectedDataAttributes: string): any;
         }
     }
+}
+
+function getTextSpan(listItem: HTMLLIElement) {
+    return listItem.children[1] as HTMLSpanElement;
 }
 
 function getDataAttributesAsString(element: HTMLElement): string {
@@ -34,5 +41,39 @@ export function toHaveDataAttributes(htmlElement: HTMLElement, expectedDataAttri
     return {
         message,
         pass,
+    };
+}
+
+export function toHaveAChildSpanWithClassAndDataAttributes(
+    listItem: HTMLLIElement,
+    expectedClass: string,
+    expectedDataAttributes: string,
+) {
+    const textSpan = getTextSpan(listItem);
+    const childSpans = Array.from(textSpan.children) as HTMLSpanElement[];
+
+    for (const childSpan of childSpans) {
+        if (childSpan.className === expectedClass) {
+            const renderedDataAttributes = getDataAttributesAsString(childSpan);
+            const pass: boolean = renderedDataAttributes === expectedDataAttributes;
+            const message: () => string = () =>
+                pass
+                    ? `Data attributes for the span with '${expectedClass}' class should not be\n${renderedDataAttributes}`
+                    : `Data attributes for the span with '${expectedClass}' class are not the same as expected:\n${diff(
+                          expectedDataAttributes,
+                          renderedDataAttributes,
+                      )}`;
+            return {
+                message,
+                pass,
+            };
+        }
+    }
+
+    const foundChildSpans = childSpans.map((childSpan) => childSpan.className).join('\n');
+    return {
+        message: () =>
+            `The rendered list item does not contain a span with class '${expectedClass}'. Found spans with classes:\n${foundChildSpans}`,
+        pass: false,
     };
 }
