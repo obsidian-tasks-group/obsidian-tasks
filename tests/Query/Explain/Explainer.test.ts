f25accb8895fb8602510486a75c6df87812db4b8
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2023-12-26 13:56:26 +0000

    refactor: . Make Query extend new Explainer class, temporarily

---
+++ tests/Query/Explain/Explainer.test.ts
@@ -0,0 +1,8 @@
+import { Explainer } from '../../../src/Query/Explain/Explainer';
+
+describe('Explainer', () => {
+    it('construct', () => {
+        // @ts-expect-error explainer unused
+        const explainer = new Explainer();
+    });
+});
