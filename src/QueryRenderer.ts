a4d9215e28a835a9e86e283dd585be18df0cdefa
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2024-01-02 09:32:27 +0000

    refactor: . Convert taskListHiddenClasses getter to function in TaskLayout

--- src/QueryRenderer.ts
+++ src/QueryRenderer.ts
@@ -216,7 +216,7 @@ class QueryRenderChild extends MarkdownRenderChild {
         const layout = new TaskLayout(this.query.taskLayoutOptions, this.query.queryLayoutOptions);
         const taskList = content.createEl('ul');
         taskList.addClasses(['contains-task-list', 'plugin-tasks-query-result']);
-        taskList.addClasses(layout.taskListHiddenClasses);
+        taskList.addClasses(layout.taskListHiddenClasses());
         const groupingAttribute = this.getGroupingAttribute();
         if (groupingAttribute && groupingAttribute.length > 0) taskList.dataset.taskGroupBy = groupingAttribute;
 
