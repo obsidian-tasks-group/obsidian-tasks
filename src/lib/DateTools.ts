da2e46a558d129a6debc1ca621bb6982e3074e2a
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2024-01-17 13:39:36 +0000

    refactor: . Unwrap else in compareByDate() to reduce nesting

--- src/lib/DateTools.ts
+++ src/lib/DateTools.ts
@@ -7,19 +7,19 @@ export function compareByDate(a: moment.Moment | null, b: moment.Moment | null):
     }
     if (!(a !== null && b !== null)) {
         return 0;
-    } else {
-        if (a.isValid() && !b.isValid()) {
-            return -1;
-        } else if (!a.isValid() && b.isValid()) {
-            return 1;
-        }
+    }
 
-        if (a.isAfter(b)) {
-            return 1;
-        } else if (a.isBefore(b)) {
-            return -1;
-        } else {
-            return 0;
-        }
+    if (a.isValid() && !b.isValid()) {
+        return -1;
+    } else if (!a.isValid() && b.isValid()) {
+        return 1;
+    }
+
+    if (a.isAfter(b)) {
+        return 1;
+    } else if (a.isBefore(b)) {
+        return -1;
+    } else {
+        return 0;
     }
 }
