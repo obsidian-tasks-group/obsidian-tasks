2fa62f4e0fdde6791cb317ea0ae2ff9b03da0e4a
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2024-01-23 19:04:02 +0000

    comment: Add NEW_TASK_FIELD_EDIT_REQUIRED comments

--- src/ui/EditableTask.ts
+++ src/ui/EditableTask.ts
@@ -2,6 +2,7 @@ import type { Status } from '../Statuses/Status';
 import type { Task } from '../Task/Task';
 
 export type EditableTask = {
+    // NEW_TASK_FIELD_EDIT_REQUIRED
     description: string;
     status: Status;
     priority: 'none' | 'lowest' | 'low' | 'medium' | 'high' | 'highest';
