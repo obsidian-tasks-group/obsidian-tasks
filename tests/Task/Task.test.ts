2ef93410a0b3290a0bd8711de0fe8465e1bbf613
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2024-02-01 16:49:37 +0000

    test: - Move withWideSelectionOfDependencyScenarios() to SampleTasks

--- tests/Task/Task.test.ts
+++ tests/Task/Task.test.ts
@@ -11,10 +11,11 @@ import type { StatusCollection } from '../../src/Statuses/StatusCollection';
 import { StatusRegistry } from '../../src/Statuses/StatusRegistry';
 import { TaskLocation } from '../../src/Task/TaskLocation';
 import { StatusConfiguration, StatusType } from '../../src/Statuses/StatusConfiguration';
-import { fromLine, fromLines } from '../TestingTools/TestHelpers';
+import { fromLine } from '../TestingTools/TestHelpers';
 import { TaskBuilder } from '../TestingTools/TaskBuilder';
 import { RecurrenceBuilder } from '../TestingTools/RecurrenceBuilder';
 import { Priority } from '../../src/Task/Priority';
+import { SampleTasks } from '../TestingTools/SampleTasks';
 
 window.moment = moment;
 
@@ -551,53 +552,6 @@ describe('properties for scripting', () => {
     });
 });
 
-function withWideSelectionOfDependencyScenarios() {
-    const lines = [
-        '- [ ] No dependency - TODO',
-        '- [x] No dependency - DONE',
-        //
-        '- [ ] scenario 1 - TODO depends on TODO 🆔 scenario1',
-        '- [ ] scenario 1 - TODO depends on TODO ⛔️ scenario1',
-        //
-        '- [x] scenario 2 - TODO depends on DONE 🆔 scenario2',
-        '- [ ] scenario 2 - TODO depends on DONE ⛔️ scenario2',
-        //
-        '- [ ] scenario 3 - DONE depends on TODO 🆔 scenario3',
-        '- [x] scenario 3 - DONE depends on TODO ⛔️ scenario3',
-        //
-        '- [x] scenario 4 - DONE depends on DONE 🆔 scenario4',
-        '- [x] scenario 4 - DONE depends on DONE ⛔️ scenario4',
-        //
-        '- [ ] scenario 5 - TODO depends on non-existing ID ⛔️ nosuchid',
-        //
-        '- [ ] scenario 6 - TODO depends on self 🆔 self ⛔️ self',
-        //
-        '- [x] scenario 7 - task with duplicated id - this is DONE                                  - 🆔 scenario7',
-        '- [ ] scenario 7 - task with duplicated id - this is TODO - and is blocking                - 🆔 scenario7',
-        '- [ ] scenario 7 - TODO depends on id that is duplicated - ensure all tasks are checked    - ⛔️ scenario7',
-        //
-        '- [ ] scenario 8 - mutually dependant 🆔 scenario8a ⛔️ scenario8b',
-        '- [ ] scenario 8 - mutually dependant 🆔 scenario8b ⛔️ scenario8a',
-        //
-        '- [ ] scenario 9 - cyclic dependency 🆔 scenario9a ⛔️ scenario9c',
-        '- [ ] scenario 9 - cyclic dependency 🆔 scenario9b ⛔️ scenario9a',
-        '- [ ] scenario 9 - cyclic dependency 🆔 scenario9c ⛔️ scenario9b',
-        //
-        '- [ ] scenario 10 - multiple dependencies TODO         - 🆔 scenario10a',
-        '- [/] scenario 10 - multiple dependencies IN_PROGRESS  - 🆔 scenario10b',
-        '- [x] scenario 10 - multiple dependencies DONE         - 🆔 scenario10c',
-        '- [-] scenario 10 - multiple dependencies CANCELLED    - 🆔 scenario10d',
-        '- [Q] scenario 10 - multiple dependencies NON_TASK     - 🆔 scenario10e',
-        '- [ ] scenario 10 - multiple dependencies              - ⛔️ scenario10a,scenario10b,scenario10c,scenario10d,scenario10e',
-        //
-        '- [ ] scenario 11 - indirect dependency - indirect blocking of scenario11c ignored - 🆔 scenario11a',
-        '- [x] scenario 11 - indirect dependency - DONE                                     - 🆔 scenario11b ⛔️ scenario11a',
-        '- [ ] scenario 11 - indirect dependency - indirect blocking of scenario11a ignored - 🆔 scenario11c ⛔️ scenario11b',
-    ];
-    const tasks = fromLines({ lines });
-    return tasks;
-}
-
 describe('task dependencies', () => {
     beforeEach(() => {
         const nonTaskStatus = new StatusConfiguration('Q', 'Question', 'A', true, StatusType.NON_TASK);
@@ -613,7 +567,7 @@ describe('task dependencies', () => {
     }
 
     it('blocking and blocked', () => {
-        const tasks = withWideSelectionOfDependencyScenarios();
+        const tasks = SampleTasks.withWideSelectionOfDependencyScenarios();
 
         verifyAll('Visualise blocking methods on Task, for a collection of tasks', tasks, (task) => {
             return `
