b7d32fc814527d0b7e70810a4252cbe1d480ac80
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2024-02-01 17:01:12 +0000

    test: Add some JSDocs links to related methods in TestHelpers.ts, for easy navigation

--- tests/TestingTools/TestHelpers.ts
+++ tests/TestingTools/TestHelpers.ts
@@ -1,6 +1,10 @@
 import { Task } from '../../src/Task/Task';
 import { TaskLocation } from '../../src/Task/TaskLocation';
 
+/**
+ * @see fromLines
+ * @see toLine
+ */
 export function fromLine({
     line,
     path = '',
@@ -17,6 +21,11 @@ export function fromLine({
     })!;
 }
 
+/**
+ * @see fromLine
+ * @see createTasksFromMarkdown
+ * @see toLines
+ */
 export function fromLines({
     lines,
     path = '',
@@ -29,14 +38,25 @@ export function fromLines({
     return lines.map((line) => fromLine({ line, path, precedingHeader }));
 }
 
+/**
+ * @see toLines
+ * @see fromLine
+ */
 export function toLine(task: Task) {
     return task.toFileLineString();
 }
 
+/**
+ * @see toLine
+ * @see fromLines
+ */
 export function toLines(tasks: Task[]) {
     return tasks.map((task) => toLine(task));
 }
 
+/**
+ * @see fromLines
+ */
 export function createTasksFromMarkdown(tasksAsMarkdown: string, path: string, precedingHeader: string): Task[] {
     const taskLines = tasksAsMarkdown.split('\n');
     const tasks: Task[] = [];
