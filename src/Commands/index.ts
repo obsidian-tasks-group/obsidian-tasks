efe09c5f89c04a4e4a3224008fec5f3a71f53313
Thomas Herden <tom.herden@dfa.state.ny.us>
2024-02-03 12:30:51 -0500

    Add four (temporary) commands for planned but (yet to be coded) OnCompletion actions

--- src/Commands/index.ts
+++ src/Commands/index.ts
@@ -1,8 +1,8 @@
 import type { App, Editor, MarkdownFileInfo, MarkdownView, View } from 'obsidian';
 import type TasksPlugin from '../main';
 import { createOrEdit } from './CreateOrEdit';
-
 import { toggleDone } from './ToggleDone';
+import { deleteLine, moveLineToArchive, moveLineToListEnd, moveLineToLogList } from './OnCompletion';
 
 export class Commands {
     private readonly plugin: TasksPlugin;
@@ -29,6 +29,39 @@ export class Commands {
             name: 'Toggle task done',
             icon: 'check-in-circle',
             editorCheckCallback: toggleDone,
+        });
+
+        plugin.addCommand({
+            id: 'delete-line',
+            name: 'Delete the current line',
+            editorCheckCallback: (checking: boolean, editor: Editor, view: MarkdownView | MarkdownFileInfo) => {
+                // TODO Need to explore what happens if a tasks code block is rendered before the Cache has been created.
+                return deleteLine(checking, editor, view as View);
+            },
+        });
+
+        plugin.addCommand({
+            id: 'archive-line',
+            name: 'Move line to an Archive note',
+            editorCheckCallback: (checking: boolean, editor: Editor, view: MarkdownView | MarkdownFileInfo) => {
+                return moveLineToArchive(checking, editor, view as View);
+            },
+        });
+
+        plugin.addCommand({
+            id: 'line-to-log-list',
+            name: 'Move line to a log list within note',
+            editorCheckCallback: (checking: boolean, editor: Editor, view: MarkdownView | MarkdownFileInfo) => {
+                return moveLineToListEnd(checking, editor, view as View);
+            },
+        });
+
+        plugin.addCommand({
+            id: 'line-to-list-end',
+            name: 'Move line to the end of current list',
+            editorCheckCallback: (checking: boolean, editor: Editor, view: MarkdownView | MarkdownFileInfo) => {
+                return moveLineToLogList(checking, editor, view as View);
+            },
         });
     }
 }
