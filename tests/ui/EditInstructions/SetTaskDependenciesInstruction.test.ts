fba8409f4270d2be558fc05830f8f248b02f0ee4
Ilyas Landikov <ilyas.landikov@icloud.com>
2024-02-06 15:27:51 +0600

    test: - improve removal of dependency

--- tests/ui/EditInstructions/SetTaskDependenciesInstruction.test.ts
+++ tests/ui/EditInstructions/SetTaskDependenciesInstruction.test.ts
@@ -8,12 +8,13 @@ import { TaskBuilder } from '../../TestingTools/TaskBuilder';
 
 window.moment = moment;
 
-function setDependencies(task: Task, _allTasks: Task[], _dependsOn: Task[], _dependedUpon: Task[]) {
+function setDependencies(task: Task, _allTasks: Task[], dependsOn: Task[], _dependedUpon: Task[]) {
     if (task.dependsOn.length === 0) {
         return task;
     }
 
-    return new Task({ ...task, dependsOn: [] });
+    const newDependsOn = dependsOn.map((task) => task.id);
+    return new Task({ ...task, dependsOn: newDependsOn });
 }
 
 describe('Edit dependencies', () => {
@@ -38,5 +39,20 @@ describe('Edit dependencies', () => {
         const doSecondWithoutTheFirst = setDependencies(doSecond, allTasks, dependsOn, dependedUpon);
 
         expect(doSecondWithoutTheFirst.dependsOn).toEqual([]);
+    });
+
+    it('should remove one of two dependencies', () => {
+        const id1 = '12345';
+        const doFirst = new TaskBuilder().id(id1).build();
+        const id2 = '67890';
+        const dontDependOnMe = new TaskBuilder().id(id2).build();
+        const doSecond = new TaskBuilder().dependsOn([id1, id2]).build();
+        const dependsOn: Task[] = [doFirst];
+        const dependedUpon: Task[] = [];
+        const allTasks = [doFirst, doSecond, dontDependOnMe];
+
+        const doSecondOnlyWithDoFirst = setDependencies(doSecond, allTasks, dependsOn, dependedUpon);
+
+        expect(doSecondOnlyWithDoFirst.dependsOn).toEqual([id1]);
     });
 });
