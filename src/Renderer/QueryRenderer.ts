7df340c5ad83aabd018815f02ba8ffe3e78ae21f
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2024-02-14 20:27:48 +0000

    refactor: . Rename createElement() to createAndAppendElement()
    Code review feedback.

--- src/Renderer/QueryRenderer.ts
+++ src/Renderer/QueryRenderer.ts
@@ -19,7 +19,7 @@ import { TaskModal } from '../Obsidian/TaskModal';
 import type { TasksEvents } from '../Obsidian/TasksEvents';
 import { getTaskLineAndFile, replaceTaskWithTasks } from '../Obsidian/File';
 import { State } from '../Obsidian/Cache';
-import { TaskLineRenderer, createElement } from './TaskLineRenderer';
+import { TaskLineRenderer, createAndAppendElement } from './TaskLineRenderer';
 
 export class QueryRenderer {
     private readonly app: App;
@@ -163,7 +163,7 @@ class QueryRenderChild extends MarkdownRenderChild {
         // console messages in large vaults, if Obsidian was opened with any
         // notes with tasks code blocks in Reading or Live Preview mode.
 
-        const content = createElement(this.containerEl, 'div');
+        const content = createAndAppendElement(this.containerEl, 'div');
         if (state === State.Warm && this.query.error === undefined) {
             await this.renderQuerySearchResults(tasks, state, content);
         } else if (this.query.error !== undefined) {
@@ -215,14 +215,14 @@ class QueryRenderChild extends MarkdownRenderChild {
             this.filePath,
         );
 
-        const explanationsBlock = createElement(content, 'pre');
+        const explanationsBlock = createAndAppendElement(content, 'pre');
         explanationsBlock.addClasses(['plugin-tasks-query-explanation']);
         explanationsBlock.setText(explanationAsString);
         content.appendChild(explanationsBlock);
     }
 
     private async createTaskList(tasks: Task[], content: HTMLDivElement): Promise<void> {
-        const taskList = createElement(content, 'ul');
+        const taskList = createAndAppendElement(content, 'ul');
 
         taskList.addClasses(['contains-task-list', 'plugin-tasks-query-result']);
         const taskLayout = new TaskLayout(this.query.taskLayoutOptions);
@@ -360,7 +360,7 @@ class QueryRenderChild extends MarkdownRenderChild {
             backLink.append(' (');
         }
 
-        const link = createElement(backLink, 'a');
+        const link = createAndAppendElement(backLink, 'a');
 
         link.rel = 'noopener';
         link.target = '_blank';
