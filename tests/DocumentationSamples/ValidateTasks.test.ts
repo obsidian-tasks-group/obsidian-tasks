d29adaf98c95672c10786b57f8b16867d24a7322
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2024-02-16 08:37:17 +0000

    test: . Extract function createTasksValidationCodeBlock()

--- tests/DocumentationSamples/ValidateTasks.test.ts
+++ tests/DocumentationSamples/ValidateTasks.test.ts
@@ -2,6 +2,22 @@ import { DEFAULT_SYMBOLS } from '../../src/TaskSerializer/DefaultTaskSerializer'
 import { verifyWithFileExtension } from '../TestingTools/ApprovalTestHelpers';
 import { Task } from '../../src/Task/Task';
 
+function createTasksValidationCodeBlock(filters: string[]) {
+    return `
+\`\`\`\`text
+\`\`\`tasks
+# These description instructions need to be all on one line:
+${filters.join(' OR ')}
+
+# Optionally, uncomment this line and exclude your templates location
+# path does not include _templates
+
+group by path
+\`\`\`
+\`\`\`\`
+`;
+}
+
 describe('validate-tasks', () => {
     it('find-unread-emojis', () => {
         const allEmojis: string[] = [];
@@ -40,19 +56,7 @@ group by path
     it('find problem dates', () => {
         const dateFields = Task.allDateFields();
         const instructions = dateFields.sort().map((field) => `(${field.replace('Date', ' date')} is invalid)`);
-        const output = `
-\`\`\`\`text
-\`\`\`tasks
-# These description instructions need to be all on one line:
-${instructions.join(' OR ')}
-
-# Optionally, uncomment this line and exclude your templates location
-# path does not include _templates
-
-group by path
-\`\`\`
-\`\`\`\`
-`;
+        const output = createTasksValidationCodeBlock(instructions);
         verifyWithFileExtension(output, 'text');
     });
 });
