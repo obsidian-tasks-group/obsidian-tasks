197df1982c529819f8d9da41082cb21d97af0761
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2024-02-18 06:48:08 +0000

    refactor: . Inline variable vault

--- src/Renderer/QueryRenderer.ts
+++ src/Renderer/QueryRenderer.ts
@@ -381,9 +381,8 @@ class QueryRenderChild extends MarkdownRenderChild {
         }
 
         // Go to the line the task is defined at
-        const vault = this.app.vault;
         link.addEventListener('click', async (ev: MouseEvent) => {
-            const result = await getTaskLineAndFile(task, vault);
+            const result = await getTaskLineAndFile(task, this.app.vault);
             if (result) {
                 const [line, file] = result;
                 const leaf = this.app.workspace.getLeaf(Keymap.isModEvent(ev));
@@ -403,7 +402,7 @@ class QueryRenderChild extends MarkdownRenderChild {
             // (for regular left-click we prefer the 'click' event, and not to just do everything here, because
             // the 'click' event is more generic for touch devices etc.)
             if (ev.button === 1) {
-                const result = await getTaskLineAndFile(task, vault);
+                const result = await getTaskLineAndFile(task, this.app.vault);
                 if (result) {
                     const [line, file] = result;
                     const leaf = this.app.workspace.getLeaf('tab');
