7ca7cc2479e0ff8af816f20a15a8c7e5fac79f08
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2024-02-18 06:57:20 +0000

    refactor: . Extract function addEventsToLinkToTaskLine()

--- src/Renderer/QueryRenderer.ts
+++ src/Renderer/QueryRenderer.ts
@@ -74,6 +74,40 @@ function createLinkToTaskLine(listItem: HTMLElement, shortMode: boolean, linkTex
     return link;
 }
 
+function addEventsToLinkToTaskLine(link: HTMLAnchorElement, task: Task, app: App) {
+    // Go to the line the task is defined at
+    link.addEventListener('click', async (ev: MouseEvent) => {
+        const result = await getTaskLineAndFile(task, app.vault);
+        if (result) {
+            const [line, file] = result;
+            const leaf = app.workspace.getLeaf(Keymap.isModEvent(ev));
+            // When the corresponding task has been found,
+            // suppress the default behavior of the mouse click event
+            // (which would interfere e.g. if the query is rendered inside a callout).
+            ev.preventDefault();
+            // Instead of the default behavior, open the file with the required line highlighted.
+            await leaf.openFile(file, { eState: { line: line } });
+        }
+    });
+
+    link.addEventListener('mousedown', async (ev: MouseEvent) => {
+        // Open in a new tab on middle-click.
+        // This distinction is not available in the 'click' event, so we handle the 'mousedown' event
+        // solely for this.
+        // (for regular left-click we prefer the 'click' event, and not to just do everything here, because
+        // the 'click' event is more generic for touch devices etc.)
+        if (ev.button === 1) {
+            const result = await getTaskLineAndFile(task, app.vault);
+            if (result) {
+                const [line, file] = result;
+                const leaf = app.workspace.getLeaf('tab');
+                ev.preventDefault();
+                await leaf.openFile(file, { eState: { line: line } });
+            }
+        }
+    });
+}
+
 class QueryRenderChild extends MarkdownRenderChild {
     private readonly app: App;
     private plugin: TasksPlugin;
@@ -384,38 +418,8 @@ class QueryRenderChild extends MarkdownRenderChild {
         }
         const link = createLinkToTaskLine(listItem, shortMode, linkText);
 
-        // Go to the line the task is defined at
         const app = this.app;
-        link.addEventListener('click', async (ev: MouseEvent) => {
-            const result = await getTaskLineAndFile(task, app.vault);
-            if (result) {
-                const [line, file] = result;
-                const leaf = app.workspace.getLeaf(Keymap.isModEvent(ev));
-                // When the corresponding task has been found,
-                // suppress the default behavior of the mouse click event
-                // (which would interfere e.g. if the query is rendered inside a callout).
-                ev.preventDefault();
-                // Instead of the default behavior, open the file with the required line highlighted.
-                await leaf.openFile(file, { eState: { line: line } });
-            }
-        });
-
-        link.addEventListener('mousedown', async (ev: MouseEvent) => {
-            // Open in a new tab on middle-click.
-            // This distinction is not available in the 'click' event, so we handle the 'mousedown' event
-            // solely for this.
-            // (for regular left-click we prefer the 'click' event, and not to just do everything here, because
-            // the 'click' event is more generic for touch devices etc.)
-            if (ev.button === 1) {
-                const result = await getTaskLineAndFile(task, app.vault);
-                if (result) {
-                    const [line, file] = result;
-                    const leaf = app.workspace.getLeaf('tab');
-                    ev.preventDefault();
-                    await leaf.openFile(file, { eState: { line: line } });
-                }
-            }
-        });
+        addEventsToLinkToTaskLine(link, task, app);
     }
 
     private addPostponeButton(listItem: HTMLElement, task: Task, shortMode: boolean) {
