2dfc05ff7190242b1dec0d39b44ee39c6c014a3e
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2024-02-18 07:05:54 +0000

    refactor: . Move linkToTaskLine() & implementation to LinkToTaskLine.ts

---
+++ src/Renderer/LinkToTaskLine.ts
@@ -0,0 +1,67 @@
+import { App, Keymap } from 'obsidian';
+import type { Task } from '../Task/Task';
+import { getTaskLineAndFile } from '../Obsidian/File';
+import { createAndAppendElement } from './TaskLineRenderer';
+
+export function linkToTaskLine(task: Task, linkText: string, listItem: HTMLElement, shortMode: boolean, app: App) {
+    const link = createLinkToTaskLine(listItem, shortMode, linkText);
+    addEventsToLinkToTaskLine(link, task, app);
+}
+
+function createLinkToTaskLine(listItem: HTMLElement, shortMode: boolean, linkText: string) {
+    const backLink = listItem.createSpan({ cls: 'tasks-backlink' });
+
+    if (!shortMode) {
+        backLink.append(' (');
+    }
+
+    const link = createAndAppendElement('a', backLink);
+
+    link.rel = 'noopener';
+    link.target = '_blank';
+    link.addClass('internal-link');
+    if (shortMode) {
+        link.addClass('internal-link-short-mode');
+    }
+
+    link.setText(linkText);
+
+    if (!shortMode) {
+        backLink.append(')');
+    }
+    return link;
+}
+
+function addEventsToLinkToTaskLine(link: HTMLAnchorElement, task: Task, app: App) {
+    // Go to the line the task is defined at
+    link.addEventListener('click', async (ev: MouseEvent) => {
+        const result = await getTaskLineAndFile(task, app.vault);
+        if (result) {
+            const [line, file] = result;
+            const leaf = app.workspace.getLeaf(Keymap.isModEvent(ev));
+            // When the corresponding task has been found,
+            // suppress the default behavior of the mouse click event
+            // (which would interfere e.g. if the query is rendered inside a callout).
+            ev.preventDefault();
+            // Instead of the default behavior, open the file with the required line highlighted.
+            await leaf.openFile(file, { eState: { line: line } });
+        }
+    });
+
+    link.addEventListener('mousedown', async (ev: MouseEvent) => {
+        // Open in a new tab on middle-click.
+        // This distinction is not available in the 'click' event, so we handle the 'mousedown' event
+        // solely for this.
+        // (for regular left-click we prefer the 'click' event, and not to just do everything here, because
+        // the 'click' event is more generic for touch devices etc.)
+        if (ev.button === 1) {
+            const result = await getTaskLineAndFile(task, app.vault);
+            if (result) {
+                const [line, file] = result;
+                const leaf = app.workspace.getLeaf('tab');
+                ev.preventDefault();
+                await leaf.openFile(file, { eState: { line: line } });
+            }
+        }
+    });
+}
--- src/Renderer/QueryRenderer.ts
+++ src/Renderer/QueryRenderer.ts
@@ -1,5 +1,5 @@
 import type { EventRef, MarkdownPostProcessorContext } from 'obsidian';
-import { App, Keymap, MarkdownRenderChild, MarkdownRenderer, TFile } from 'obsidian';
+import { App, MarkdownRenderChild, MarkdownRenderer, TFile } from 'obsidian';
 import { GlobalFilter } from '../Config/GlobalFilter';
 import { GlobalQuery } from '../Config/GlobalQuery';
 import { QueryLayout } from '../Layout/QueryLayout';
@@ -17,9 +17,10 @@ import { PostponeMenu } from '../ui/Menus/PostponeMenu';
 import type TasksPlugin from '../main';
 import { TaskModal } from '../Obsidian/TaskModal';
 import type { TasksEvents } from '../Obsidian/TasksEvents';
-import { getTaskLineAndFile, replaceTaskWithTasks } from '../Obsidian/File';
+import { replaceTaskWithTasks } from '../Obsidian/File';
 import { State } from '../Obsidian/Cache';
 import { TaskLineRenderer, createAndAppendElement } from './TaskLineRenderer';
+import { linkToTaskLine } from './LinkToTaskLine';
 
 export class QueryRenderer {
     private readonly app: App;
@@ -48,69 +49,6 @@ export class QueryRenderer {
             }),
         );
     }
-}
-
-function createLinkToTaskLine(listItem: HTMLElement, shortMode: boolean, linkText: string) {
-    const backLink = listItem.createSpan({ cls: 'tasks-backlink' });
-
-    if (!shortMode) {
-        backLink.append(' (');
-    }
-
-    const link = createAndAppendElement('a', backLink);
-
-    link.rel = 'noopener';
-    link.target = '_blank';
-    link.addClass('internal-link');
-    if (shortMode) {
-        link.addClass('internal-link-short-mode');
-    }
-
-    link.setText(linkText);
-
-    if (!shortMode) {
-        backLink.append(')');
-    }
-    return link;
-}
-
-function addEventsToLinkToTaskLine(link: HTMLAnchorElement, task: Task, app: App) {
-    // Go to the line the task is defined at
-    link.addEventListener('click', async (ev: MouseEvent) => {
-        const result = await getTaskLineAndFile(task, app.vault);
-        if (result) {
-            const [line, file] = result;
-            const leaf = app.workspace.getLeaf(Keymap.isModEvent(ev));
-            // When the corresponding task has been found,
-            // suppress the default behavior of the mouse click event
-            // (which would interfere e.g. if the query is rendered inside a callout).
-            ev.preventDefault();
-            // Instead of the default behavior, open the file with the required line highlighted.
-            await leaf.openFile(file, { eState: { line: line } });
-        }
-    });
-
-    link.addEventListener('mousedown', async (ev: MouseEvent) => {
-        // Open in a new tab on middle-click.
-        // This distinction is not available in the 'click' event, so we handle the 'mousedown' event
-        // solely for this.
-        // (for regular left-click we prefer the 'click' event, and not to just do everything here, because
-        // the 'click' event is more generic for touch devices etc.)
-        if (ev.button === 1) {
-            const result = await getTaskLineAndFile(task, app.vault);
-            if (result) {
-                const [line, file] = result;
-                const leaf = app.workspace.getLeaf('tab');
-                ev.preventDefault();
-                await leaf.openFile(file, { eState: { line: line } });
-            }
-        }
-    });
-}
-
-function linkToTaskLine(task: Task, linkText: string, listItem: HTMLElement, shortMode: boolean, app: App) {
-    const link = createLinkToTaskLine(listItem, shortMode, linkText);
-    addEventsToLinkToTaskLine(link, task, app);
 }
 
 class QueryRenderChild extends MarkdownRenderChild {
