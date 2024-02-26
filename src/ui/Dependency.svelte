1da6eff465874d0f13ea8be9b83bbfb9c2789c44
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2024-02-26 11:48:54 +0000

    refactor: ! Move searchForCandidateTasksForDependency() to TypeScript

--- src/ui/Dependency.svelte
+++ src/ui/Dependency.svelte
@@ -2,6 +2,7 @@
     import type { Task } from "../Task/Task";
     import { computePosition, flip, offset, shift, size } from "@floating-ui/dom";
     import type { EditableTask } from "./EditableTask";
+    import { searchForCandidateTasksForDependency } from './DependencyHelpers';
 
     export let task: Task;
     export let editableTask: EditableTask;
@@ -10,8 +11,6 @@
     export let type: "blocking" | "blockedBy";
     export let accesskey: (key: string) => string | null;
     export let accesskeyLetter: string = '';
-
-    const MAX_SEARCH_RESULTS = 20;
 
     let search: string = '';
     let searchResults: Task[] | null = null;
@@ -74,42 +73,7 @@
         if (!search && !showDropdown) return [];
 
         showDropdown = false;
-        return searchForCandidateTasksForDependency(search);
-    }
-
-    function searchForCandidateTasksForDependency(search: string) {
-        let results = allTasks.filter(task => task.description.toLowerCase().includes(search.toLowerCase()));
-
-        // remove itself, and tasks this task already has a relationship with from results
-        results = results.filter((item) => {
-            // line number is unavailable for the task being edited
-            // Known issue - filters out duplicate lines in task file
-            const sameFile = item.description === task.description &&
-                             item.taskLocation.path === task.taskLocation.path &&
-                             item.originalMarkdown === task.originalMarkdown
-
-            return ![...editableTask.blockedBy, ...editableTask.blocking].includes(item) && !sameFile;
-        });
-
-        // search results favour tasks from the same file as this task
-        results.sort((a, b) => {
-            const aInSamePath = a.taskLocation.path === task.taskLocation.path;
-            const bInSamePath = b.taskLocation.path === task.taskLocation.path;
-
-            // prioritise tasks close to this task in the same file
-            if (aInSamePath && bInSamePath) {
-                return Math.abs(a.taskLocation.lineNumber - task.taskLocation.lineNumber)
-                       - Math.abs(b.taskLocation.lineNumber - task.taskLocation.lineNumber);
-            } else if (aInSamePath) {
-                return -1;
-            } else if (bInSamePath) {
-                return 1;
-            } else {
-                return 0;
-            }
-        });
-
-        return results.slice(0, MAX_SEARCH_RESULTS);
+        return searchForCandidateTasksForDependency(search, allTasks, task, editableTask);
     }
 
     function onFocused() {
