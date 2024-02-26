ce5caa51761ea72c984fcae3763471ff6bd6759e
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2024-02-26 13:04:04 +0000

    test: Start adding tests for filtering of tasks in Edit modal

---
+++ tests/ui/DependencyHelpers.test.ts
@@ -0,0 +1,48 @@
+import { createTasksFromMarkdown } from '../TestingTools/TestHelpers';
+import { searchForCandidateTasksForDependency } from '../../src/ui/DependencyHelpers';
+import { StatusRegistry } from '../../src/Statuses/StatusRegistry';
+import { StatusConfiguration, StatusType } from '../../src/Statuses/StatusConfiguration';
+
+describe('searching for tasks', () => {
+    beforeEach(() => {
+        StatusRegistry.getInstance().resetToDefaultStatuses();
+        const nonTaskStatus = new StatusConfiguration('Q', 'Question', 'A', true, StatusType.NON_TASK);
+        StatusRegistry.getInstance().add(nonTaskStatus);
+    });
+
+    afterEach(() => {
+        StatusRegistry.getInstance().resetToDefaultStatuses();
+    });
+
+    it('should show tasks other than the one being edited', () => {
+        const markdown = `
+- [ ] Task being edited
+- [ ] Some other task
+`;
+        const allTasks = createTasksFromMarkdown(markdown, 'filename.md', 'header');
+        const suggestions = searchForCandidateTasksForDependency('', allTasks, allTasks[0], [], []);
+
+        expect(suggestions.map((task) => task.description)).toEqual(['Some other task']);
+    });
+
+    it('does offer DONE, CANCELLED and NON_TASK tasks', () => {
+        const markdown = `
+- [ ] Task being edited
+- [ ] Todo
+- [/] In Progress
+- [-] Cancelled
+- [x] Done
+- [Q] Non-Task
+`;
+        const allTasks = createTasksFromMarkdown(markdown, 'filename.md', 'header');
+        const suggestions = searchForCandidateTasksForDependency('', allTasks, allTasks[0], [], []);
+
+        expect(suggestions.map((task) => task.description)).toEqual([
+            'Todo',
+            'In Progress',
+            'Cancelled',
+            'Done',
+            'Non-Task',
+        ]);
+    });
+});
