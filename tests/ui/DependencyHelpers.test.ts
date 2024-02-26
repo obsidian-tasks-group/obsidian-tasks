ca5417154d8dcb1d64fe3bec4a811e39b3a7cec4
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2024-02-26 13:42:38 +0000

    test: Check that tasks in file being edited sort before other tasks

--- tests/ui/DependencyHelpers.test.ts
+++ tests/ui/DependencyHelpers.test.ts
@@ -2,12 +2,21 @@ import { createTasksFromMarkdown } from '../TestingTools/TestHelpers';
 import { searchForCandidateTasksForDependency } from '../../src/ui/DependencyHelpers';
 import { StatusRegistry } from '../../src/Statuses/StatusRegistry';
 import { StatusConfiguration, StatusType } from '../../src/Statuses/StatusConfiguration';
+import type { Task } from '../../src/Task/Task';
+
+function offersTheseCandidatesForTasks(
+    taskBeingEdited: Task,
+    allTasks: Task[],
+    descriptionsOfCandidateTasks: string[],
+) {
+    const suggestions = searchForCandidateTasksForDependency('', allTasks, taskBeingEdited, [], []);
+
+    expect(suggestions.map((task) => task.description)).toEqual(descriptionsOfCandidateTasks);
+}
 
 function offersTheseCandidates(markdown: string, descriptionsOfCandidateTasks: string[]) {
     const allTasks = createTasksFromMarkdown(markdown, 'filename.md', 'header');
-    const suggestions = searchForCandidateTasksForDependency('', allTasks, allTasks[0], [], []);
-
-    expect(suggestions.map((task) => task.description)).toEqual(descriptionsOfCandidateTasks);
+    offersTheseCandidatesForTasks(allTasks[0], allTasks, descriptionsOfCandidateTasks);
 }
 
 describe('searching for tasks', () => {
@@ -29,6 +38,35 @@ describe('searching for tasks', () => {
             `,
             ['Some other task'],
         );
+    });
+
+    it('should sort files in same file before those in others', () => {
+        // Arrange
+        const taskInOtherFile = createTasksFromMarkdown(
+            `
+                - [ ] Task from other file - should be offered after task from file being edited
+            `,
+            'other file.md',
+            'header',
+        );
+        const tasksInFileBeingEdited = createTasksFromMarkdown(
+            `
+                - [ ] Task being edited
+                - [ ] In same file as task being edited - should be offered first
+            `,
+            'this file.md',
+            'header',
+        );
+
+        const allTasks = [...taskInOtherFile].concat(tasksInFileBeingEdited);
+        // Check that task from other file is first, to avoid passing by luck:
+        expect(allTasks[0]).toBe(taskInOtherFile[0]);
+
+        // Act, Assert:
+        offersTheseCandidatesForTasks(tasksInFileBeingEdited[0], allTasks, [
+            'In same file as task being edited - should be offered first',
+            'Task from other file - should be offered after task from file being edited',
+        ]);
     });
 
     it('should not offer DONE, CANCELLED and NON_TASK tasks', () => {
