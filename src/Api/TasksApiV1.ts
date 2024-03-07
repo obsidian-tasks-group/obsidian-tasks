b1b41ce387146ab5070c019bad7ce10eeb6d0543
stevendikowitz <14956188+stevendkwtz@users.noreply.github.com>
2024-03-07 05:54:48 -0500

    Add toggle task endpoint

--- src/Api/TasksApiV1.ts
+++ src/Api/TasksApiV1.ts
@@ -1,3 +1,4 @@
+import type { Task } from '../Task/Task';
 /**
  * Tasks API v1 interface
  */
@@ -9,4 +10,10 @@ export interface TasksApiV1 {
      * an empty string, if data entry was cancelled.
      */
     createTaskLineModal(): Promise<string>;
+    /**
+     * @param task - task to toggle
+     *
+     * @returns {void} A promise that contains the Markdown string for the task entered
+     */
+    toggleTask(task: Task): void;
 }
