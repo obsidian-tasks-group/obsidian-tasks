8579952f6bbdf7be7fc1c984e2bdcc505fea789b
stevendikowitz <14956188+stevendkwtz@users.noreply.github.com>
2024-03-07 10:05:26 -0500

    Make toggleTask return tasks

--- src/Api/toggleTask.ts
+++ src/Api/toggleTask.ts
@@ -1,10 +1,13 @@
 import { replaceTaskWithTasks } from 'Obsidian/File';
 import type { Task } from '../Task/Task';
 
-export const toggleTask = (task: Task): void => {
+export const toggleTask = async (task: Task): Promise<Task[]> => {
     const toggledTasks = task.toggleWithRecurrenceInUsersOrder();
-    replaceTaskWithTasks({
+
+    await replaceTaskWithTasks({
         originalTask: task,
         newTasks: toggledTasks,
     });
+
+    return toggledTasks;
 };
