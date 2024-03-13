1c8c194b061bfd53b525bc5013a72fe94fe7609b
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2024-03-13 16:47:38 +0000

    test: Add consistency check that BooleanField.preprocessExpression2() does not modify line

--- tests/Query/Filter/BooleanField.test.ts
+++ tests/Query/Filter/BooleanField.test.ts
@@ -215,8 +215,20 @@ describe('boolean query - exhaustive tests', () => {
         verifyBooleanExpressionPreprocessing(BooleanField.preprocessExpression);
     });
 
+    // Add a consistency check to BooleanField.preprocessExpression2(), for the stage where
+    // we are simply dividing up the input line at operator boundaries.
+    // This would detect any accidental swallowing or adding of characters during the processing.
+    function newPreprocessAndCheckTextUnchanged(input: string): string[] {
+        const output = BooleanField.preprocessExpression2(input);
+        const collatedOutput = output.join('');
+        if (collatedOutput !== input) {
+            output.push('ERROR: Unintentional modification of instruction line');
+        }
+        return output;
+    }
+
     it('preprocess - rewrite', () => {
-        verifyBooleanExpressionPreprocessing(BooleanField.preprocessExpression2);
+        verifyBooleanExpressionPreprocessing(newPreprocessAndCheckTextUnchanged);
     });
 
     it('explain', () => {
