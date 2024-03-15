c1fbe51ecf4498c0ec1c4b8f4d25d5d3f15f54b5
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2024-03-15 11:14:21 +0000

    refactor: - Instruction can now store continuation lines.

--- tests/Query/Instruction.test.ts
+++ tests/Query/Instruction.test.ts
@@ -2,7 +2,11 @@ import { Instruction } from '../../src/Query/Instruction';
 
 describe('Instruction', () => {
     it('should store the instruction', () => {
-        const instruction = new Instruction('hello world');
+        const raw = `hello \\
+world`;
+        const final = 'hello world';
+        const instruction = new Instruction(raw, final);
+        expect(instruction.rawInstruction).toEqual(raw);
         expect(instruction.instruction).toEqual('hello world');
     });
 });
