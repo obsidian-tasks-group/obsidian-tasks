d4deb66113acab2be5988b3bbab3504193aff939
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2024-03-15 19:30:11 +0000

    refactor: Make fields in Statement private.

--- src/Query/Statement.ts
+++ src/Query/Statement.ts
@@ -3,12 +3,13 @@
  *
  * This may represent multiple lines with continuation characters.
  *
- * {@link anyContinuationLinesRemoved} is the final line, after continuation lines have been applied.
- * If continuation lines were used, {@link rawInstruction} represents the multi-line input.
+ * - {@link anyContinuationLinesRemoved} is the statement after continuation lines have been applied.
+ *                                       It may contain placeholders, however.
+ * - If continuation lines were used, {@link rawInstruction} represents the multi-line input.
  */
 export class Statement {
-    public readonly rawInstruction: string;
-    public readonly anyContinuationLinesRemoved: string;
+    private readonly _rawInstruction: string;
+    private readonly _anyContinuationLinesRemoved: string;
 
     /**
      *
@@ -16,7 +17,25 @@ export class Statement {
      * @param instruction - whitespace is trimmed
      */
     constructor(rawInstruction: string, instruction: string) {
-        this.rawInstruction = rawInstruction;
-        this.anyContinuationLinesRemoved = instruction.trim();
+        this._rawInstruction = rawInstruction;
+        this._anyContinuationLinesRemoved = instruction.trim();
+    }
+
+    /**
+     * The original text of this statement, with all original whitespace.
+     *
+     * If there were continuation lines in the query, this will contain newline characters.
+     */
+    public get rawInstruction(): string {
+        return this._rawInstruction;
+    }
+
+    /**
+     * The text of this statement after any continuation lines have been removed.
+     *
+     * Currently, the line is trimmed, although that may change.
+     */
+    public get anyContinuationLinesRemoved(): string {
+        return this._anyContinuationLinesRemoved;
     }
 }
