84c044ff40765ae0ab51f7780937c9ae8f164d65
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2024-03-15 13:47:10 +0000

    refactor: . Rename Instruction to Statement, to reduce ambiguity

--- src/Query/Statement.ts
+++ src/Query/Statement.ts
@@ -1,12 +1,12 @@
 /**
- * A single logical input line in a Tasks Query block.
+ * A single logical input statement in a Tasks Query block.
  *
  * This may represent multiple lines with continuation characters.
  *
  * {@link instruction} is the final line, after configuration lines and placeholders have been applied.
  * If continuation lines were used, {@link rawInstruction} represents the multi-line input.
  */
-export class Instruction {
+export class Statement {
     public readonly rawInstruction: string;
     public readonly instruction: string;
 
