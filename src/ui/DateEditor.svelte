6a32e39694278bff92841133d5633e0a06e3cde3
Ilyas Landikov <ilyas.landikov@icloud.com>
2024-03-17 16:06:43 +0600

    refactor: ! add parameters to DateEditor

--- src/ui/DateEditor.svelte
+++ src/ui/DateEditor.svelte
@@ -1,6 +1,10 @@
 <script lang='ts'>
     export let dateSymbol: string;
+    export let date: string;
     export let parsedDate: string;
+    export let isDateValid: boolean;
+    export let accesskey: string | null;
+    export let placeholder: string;
 </script>
 
 <code class="results">{dateSymbol} {@html parsedDate}</code>
