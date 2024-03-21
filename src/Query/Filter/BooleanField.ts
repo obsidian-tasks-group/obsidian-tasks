fc5658cceacbbd53b8fcc2b37aca687485b810e6
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2024-03-21 20:29:31 +0000

    refactor: . in BooleanField.parseLine(), extract parseLineV1()
    This is in preparation for experimenting with an alternative implementation

--- src/Query/Filter/BooleanField.ts
+++ src/Query/Filter/BooleanField.ts
@@ -56,6 +56,15 @@ export class BooleanField extends Field {
         if (line.length === 0) {
             return FilterOrErrorMessage.fromError(line, 'empty line');
         }
+        return this.parseLineV1(line);
+    }
+
+    /**
+     * Legacy Boolean filter parser
+     * @param line
+     * @private
+     */
+    private parseLineV1(line: string) {
         const preprocessed = BooleanField.preprocessExpression(line);
         try {
             // Convert the (preprocessed) line into a postfix logical expression
