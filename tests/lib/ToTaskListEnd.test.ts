4715ac9d8af45081bdb57718cb98ad145a12a5fe
Thomas Herden <tom.herden@dfa.state.ny.us>
2024-03-24 18:18:49 -0400

    Add toTaskListEnd function to insert line at the end of task list identified by heading

--- tests/lib/ToTaskListEnd.test.ts
+++ tests/lib/ToTaskListEnd.test.ts
@@ -1,47 +1,86 @@
 describe('ToTasklistEnd', () => {
-    //     it('should insert line at end of list when list followed by blank line', () => {
-    //         const initialContentNoNewLine = '';
-    //         const expectedContent = '';
-    //         const targetListHeading = '## MY TASK LIST';
-    //         const textToAppend = '- [-] A COMPLETED TASK TO INSERT AT END OF NAMED LIST';
-    //         const newFile = toTasklistEnd(initialContentNoNewLine, targetListHeading, textToAppend);
-    //         expect(newFile).toEqual(expectedContent);
-    //     });
-    //     it('should insert line at end of list when list followed by regular text', () => {
-    //         const initialContentNoNewLine = '';
-    //         const expectedContent = '';
-    //         const targetListHeading = '## MY TASK LIST';
-    //         const textToAppend = '- [-] A COMPLETED TASK TO INSERT AT END OF NAMED LIST';
-    //         const newFile = toTasklistEnd(initialContentNoNewLine, targetListHeading, textToAppend);
-    //         expect(newFile).toEqual(expectedContent);
-    //     });
-    //     it('should insert line at end of list when list followed by a markdown heading', () => {
-    //         const initialContentNoNewLine = '';
-    //         const expectedContent = '';
-    //         const targetListHeading = '## MY TASK LIST';
-    //         const textToAppend = '- [-] A COMPLETED TASK TO INSERT AT END OF NAMED LIST';
-    //         const newFile = toTasklistEnd(initialContentNoNewLine, targetListHeading, textToAppend);
-    //         expect(newFile).toEqual(expectedContent);
-    //     });
-    //     it('should insert line at end of list when end of list is last line in note file', () => {
-    //         const initialContentNoNewLine = '';
-    //         const expectedContent = '';
-    //         const targetListHeading = '## MY TASK LIST';
-    //         const textToAppend = '- [-] A COMPLETED TASK TO INSERT AT END OF NAMED LIST';
-    //         const newFile = toTasklistEnd(initialContentNoNewLine, targetListHeading, textToAppend);
-    //         expect(newFile).toEqual(expectedContent);
-    //     });
-    it('placeholder to allow initial Commit of this file', () => {
-        expect(1).toEqual(1);
+    it('should insert line at end of list when list followed by blank line', () => {
+        const initialContentNoNewLine = `Sed ipsam libero qui consequuntur quaerat non atque quia ab praesentium explicabo.
+## MY TASK LIST
+- [ ] An incomplete task
+
+Final line in note file.`;
+        const expectedContent = `Sed ipsam libero qui consequuntur quaerat non atque quia ab praesentium explicabo.
+## MY TASK LIST
+- [ ] An incomplete task
+- [-] A COMPLETED TASK TO INSERT AT END OF NAMED LIST
+
+Final line in note file.`;
+        const targetListHeading = '## MY TASK LIST';
+        const textToAppend = '- [-] A COMPLETED TASK TO INSERT AT END OF NAMED LIST';
+        const newFile = toTasklistEnd(initialContentNoNewLine, targetListHeading, textToAppend);
+        expect(newFile).toEqual(expectedContent);
+    });
+    it('should insert line at end of list when list followed by regular text', () => {
+        const initialContentNoNewLine = `Sed ipsam libero qui consequuntur quaerat non atque quia ab praesentium explicabo.
+## MY TASK LIST
+- [ ] An incomplete task
+A line of regular text.`;
+        const expectedContent = `Sed ipsam libero qui consequuntur quaerat non atque quia ab praesentium explicabo.
+## MY TASK LIST
+- [ ] An incomplete task
+- [-] A COMPLETED TASK TO INSERT AT END OF NAMED LIST
+A line of regular text.`;
+        const targetListHeading = '## MY TASK LIST';
+        const textToAppend = '- [-] A COMPLETED TASK TO INSERT AT END OF NAMED LIST';
+        const newFile = toTasklistEnd(initialContentNoNewLine, targetListHeading, textToAppend);
+        expect(newFile).toEqual(expectedContent);
+    });
+    it('should insert line at end of list when list followed by a markdown heading', () => {
+        const initialContentNoNewLine = `Sed ipsam libero qui consequuntur quaerat non atque quia ab praesentium explicabo.
+## MY TASK LIST
+- [ ] An incomplete task
+## Another list heading`;
+        const expectedContent = `Sed ipsam libero qui consequuntur quaerat non atque quia ab praesentium explicabo.
+## MY TASK LIST
+- [ ] An incomplete task
+- [-] A COMPLETED TASK TO INSERT AT END OF NAMED LIST
+## Another list heading`;
+        const targetListHeading = '## MY TASK LIST';
+        const textToAppend = '- [-] A COMPLETED TASK TO INSERT AT END OF NAMED LIST';
+        const newFile = toTasklistEnd(initialContentNoNewLine, targetListHeading, textToAppend);
+        expect(newFile).toEqual(expectedContent);
+    });
+    it('should insert line at end of list when end of list is last line in note file', () => {
+        const initialContentNoNewLine = `Sed ipsam libero qui consequuntur quaerat non atque quia ab praesentium explicabo.
+## MY TASK LIST
+- [ ] An incomplete task`;
+        const expectedContent = `Sed ipsam libero qui consequuntur quaerat non atque quia ab praesentium explicabo.
+## MY TASK LIST
+- [ ] An incomplete task
+- [-] A COMPLETED TASK TO INSERT AT END OF NAMED LIST`;
+        const targetListHeading = '## MY TASK LIST';
+        const textToAppend = '- [-] A COMPLETED TASK TO INSERT AT END OF NAMED LIST';
+        const newFile = toTasklistEnd(initialContentNoNewLine, targetListHeading, textToAppend);
+        expect(newFile).toEqual(expectedContent);
     });
 });
-//
-// function toTasklistEnd(initialContent: string, targetListHeading: string, textToAppend: string) {
-//     console.log(initialContent, targetListHeading, textToAppend);
-//     return false;
-// }
 
-// const NOT_TASK_REGEX = '/^(?! *(- [)).*$/dm';
+const NEWLINE = '\n';
+// const NOT_TASK_REGEX = new RegExp('^(?! *(- [)).*$');
+const TASK_REGEX = new RegExp('^( *(- [.])).*');
+function toTasklistEnd(initialContent: string, targetListHeading: string, textToAppend: string) {
+    const linesArray = initialContent.split('\n');
+    const headingLineNumber = linesArray.indexOf(targetListHeading);
+    let thisLine = '';
+    let insertionLine = headingLineNumber + 1;
+    for (thisLine in linesArray.slice(insertionLine)) {
+        if (thisLine.search(TASK_REGEX) > -1) {
+            insertionLine += 1;
+        } else break;
+    }
+    if (insertionLine > linesArray.length) {
+        insertionLine = -1;
+    }
+    linesArray[insertionLine] += NEWLINE + textToAppend;
+    return linesArray.join(NEWLINE);
+}
+
 //
 // // source of following code:  https://gist.github.com/reporter123/187a412821f829e1a818fd3bf2996cba
 // //install helper function not found in javascript
