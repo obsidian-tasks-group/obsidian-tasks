2decb524c0096df651d8f45baaa8ac9d9a8ff07c
Thomas Herden <tom.herden@dfa.state.ny.us>
2024-03-25 10:58:06 -0400

    Remove (unused, commented out) code intended to add IndexOf method to arrays

--- tests/lib/ToTaskListEnd.test.ts
+++ tests/lib/ToTaskListEnd.test.ts
@@ -97,24 +97,3 @@ function toTasklistEnd(initialContent: string, targetListHeading: string, textTo
     linesArray[insertionLine] += NEWLINE + textToAppend;
     return linesArray.join(NEWLINE);
 }
-
-//
-// // source of following code:  https://gist.github.com/reporter123/187a412821f829e1a818fd3bf2996cba
-// //install helper function not found in javascript
-// /** * Regular Expression IndexOf for Arrays
-//  * This little addition to the Array prototype will iterate over array
-//  * and return the index of the first element which matches the provided
-//  * regular expression.
-//  * Note: This will not match on objects.
-//  * @param  {RegEx}   rx The regular expression to test with. E.g. /-ba/gim
-//  * @return {Numeric} -1 means not found */
-// if (typeof Array.prototype.reIndexOf === 'undefined') {
-//     Array.prototype.reIndexOf = function (rx) {
-//         for (const i in this) {
-//             if (this[i].toString().match(rx)) {
-//                 return i;
-//             }
-//         }
-//         return -1;
-//     };
-// }
