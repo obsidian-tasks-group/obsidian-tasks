76264faedcd451fde9c4f254a4f1a3a09544aba0
Thomas Herden <tom.herden@dfa.state.ny.us>
2024-03-26 16:47:14 -0400

    Move tests for EndOfList action to OnCompletion.test.ts

--- tests/Task/OnCompletion.test.ts
+++ tests/Task/OnCompletion.test.ts
@@ -289,3 +289,83 @@ describe('OnCompletion-Delete', () => {
         expect(tasks.length).toEqual(0);
     });
 });
+
+describe('OnCompletion-EndOfList', () => {
+    it('should insert line at end of list when list followed by blank line', () => {
+        const initialContentNoNewLine = `Sed ipsam libero qui consequuntur quaerat non atque quia ab praesentium explicabo.
+## MY TASK LIST
+- [ ] An incomplete task
+
+Final line in note file.`;
+        const expectedContent = `Sed ipsam libero qui consequuntur quaerat non atque quia ab praesentium explicabo.
+## MY TASK LIST
+- [ ] An incomplete task
+- [-] A COMPLETED TASK TO INSERT AT END OF NAMED LIST
+
+Final line in note file.`;
+        const targetListHeading = '## MY TASK LIST';
+        const textToAppend = '- [-] A COMPLETED TASK TO INSERT AT END OF NAMED LIST';
+        const newFile = writeLineToListEnd(initialContentNoNewLine, targetListHeading, textToAppend);
+        expect(newFile).toEqual(expectedContent);
+    });
+
+    it('should insert line at end of list when list followed by regular text', () => {
+        const initialContentNoNewLine = `Sed ipsam libero qui consequuntur quaerat non atque quia ab praesentium explicabo.
+## MY TASK LIST
+- [ ] An incomplete task
+A line of regular text.`;
+        const expectedContent = `Sed ipsam libero qui consequuntur quaerat non atque quia ab praesentium explicabo.
+## MY TASK LIST
+- [ ] An incomplete task
+- [-] A COMPLETED TASK TO INSERT AT END OF NAMED LIST
+A line of regular text.`;
+        const targetListHeading = '## MY TASK LIST';
+        const textToAppend = '- [-] A COMPLETED TASK TO INSERT AT END OF NAMED LIST';
+        const newFile = writeLineToListEnd(initialContentNoNewLine, targetListHeading, textToAppend);
+        expect(newFile).toEqual(expectedContent);
+    });
+
+    it('should insert line at end of list when list followed by a markdown heading', () => {
+        const initialContentNoNewLine = `Sed ipsam libero qui consequuntur quaerat non atque quia ab praesentium explicabo.
+## MY TASK LIST
+- [ ] An incomplete task
+## Another list heading`;
+        const expectedContent = `Sed ipsam libero qui consequuntur quaerat non atque quia ab praesentium explicabo.
+## MY TASK LIST
+- [ ] An incomplete task
+- [-] A COMPLETED TASK TO INSERT AT END OF NAMED LIST
+## Another list heading`;
+        const targetListHeading = '## MY TASK LIST';
+        const textToAppend = '- [-] A COMPLETED TASK TO INSERT AT END OF NAMED LIST';
+        const newFile = writeLineToListEnd(initialContentNoNewLine, targetListHeading, textToAppend);
+        expect(newFile).toEqual(expectedContent);
+    });
+
+    it('should insert line at end of list when end of list is last line in note file', () => {
+        const initialContentNoNewLine = `Sed ipsam libero qui consequuntur quaerat non atque quia ab praesentium explicabo.
+## MY TASK LIST
+- [ ] An incomplete task`;
+        const expectedContent = `Sed ipsam libero qui consequuntur quaerat non atque quia ab praesentium explicabo.
+## MY TASK LIST
+- [ ] An incomplete task
+- [-] A COMPLETED TASK TO INSERT AT END OF NAMED LIST`;
+        const targetListHeading = '## MY TASK LIST';
+        const textToAppend = '- [-] A COMPLETED TASK TO INSERT AT END OF NAMED LIST';
+        const newFile = writeLineToListEnd(initialContentNoNewLine, targetListHeading, textToAppend);
+        expect(newFile).toEqual(expectedContent);
+    });
+
+    it('should recognize indented tasks as part of list', () => {
+        const initialContentNoNewLine = `Sed ipsam libero qui consequuntur quaerat non atque quia ab praesentium explicabo.
+## MY TASK LIST
+    - [ ] An INDENTED incomplete task`;
+        const expectedContent = `Sed ipsam libero qui consequuntur quaerat non atque quia ab praesentium explicabo.
+## MY TASK LIST
+    - [ ] An INDENTED incomplete task
+- [-] A COMPLETED TASK TO INSERT AT END OF NAMED LIST`;
+        const targetListHeading = '## MY TASK LIST';
+        const textToAppend = '- [-] A COMPLETED TASK TO INSERT AT END OF NAMED LIST';
+        const newFile = writeLineToListEnd(initialContentNoNewLine, targetListHeading, textToAppend);
+        expect(newFile).toEqual(expectedContent);
+    });
+});
