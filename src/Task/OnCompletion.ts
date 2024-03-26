72b037db6083f6beb2b83bb2faa37a68dd4f5ad6
Thomas Herden <tom.herden@dfa.state.ny.us>
2024-03-26 16:20:41 -0400

    Extract function that effects deletion of completed task

--- src/Task/OnCompletion.ts
+++ src/Task/OnCompletion.ts
@@ -20,8 +20,12 @@ export function handleOnCompletion(task: Task, tasks: Task[]): Task[] {
 
     // experimentally copy completed task instance to archive.md in vault root
 
-    if (taskString.includes('🏁 Delete')) {
+    function returnWithoutCompletedInstance() {
         return tasks.filter((task) => task !== changedStatusTask);
+    }
+
+    if (taskString.includes('🏁 Delete')) {
+        return returnWithoutCompletedInstance();
     }
     // const errorMessage = 'Unknown "On Completion" action: ' + ocAction;
     const errorMessage = 'Unknown "On Completion" action';
@@ -33,3 +37,8 @@ export function handleOnCompletion(task: Task, tasks: Task[]): Task[] {
     // console.log('Uh-oh -- we should never actually get here...  :( ');
     // throw new Error('Something went wrong');
 }
+
+// your getting your current code finalised (such as moving the functions out of test files)
+// and put in to a new PR,
+// which I can then review,
+// and we can hopefully get merged before next week....
