c325c99cf23ccb39a3f7d9b60244e1c57fd5577b
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2024-03-28 20:36:09 +0000

    fix: Ensure backwards compatibility of behaviour when preprocessing (...)AND(...)
    and similar...
    
    In testing later steps, I found this was necessary to ensure that
    we will report errors about missing spaces around Boolean operators.

--- src/Query/Filter/BooleanField.ts
+++ src/Query/Filter/BooleanField.ts
@@ -137,15 +137,18 @@ export class BooleanField extends Field {
 
         // This will now correctly split up almost all valid Boolean instructions - many more cases than the
         // original preprocessExpression() method.
-        // The one current exception is that any Spaces ) at the end of sub-expressions/filters are interpreted
-        //  as part ofthe Boolean condition, not the filter.
+        // The one current exception is that any Spaces and ) at the end of sub-expressions/filters are interpreted
+        // as part of the Boolean condition, not the filter.
 
         // Escape special regex characters for Binary boolean operators and create a regex pattern to match
         // operators and capture surrounding parentheses.
+        // To retain backwards compatibility, we match expressions that have missing spaces around AND, OR etc.
         // This matches text such as:
+        //   ')AND('
         //   ') AND ('
+        //   ')AND  NOT('
         //   ')  AND  NOT  ('
-        const binaryOperatorsRegex = /(\)\s+(?:AND|OR|AND +NOT|OR +NOT|XOR)\s+\()/g;
+        const binaryOperatorsRegex = /(\)\s*(?:AND|OR|AND +NOT|OR +NOT|XOR)\s*\()/g;
 
         // Divide up line, split at binary operator boundaries
         const substrings = line.split(binaryOperatorsRegex);
@@ -153,10 +156,11 @@ export class BooleanField extends Field {
         // Escape special regex characters for Unary boolean operators and create a regex pattern to match
         // operators and capture surrounding parentheses.
         // This matches text such as:
+        //   'NOT('
         //   'NOT ('
         //   'NOT  ('
         // TODO Ensure that NOT is at the start of a word - and perhaps is preceded by spaces.
-        const unaryOperatorsRegex = /(NOT\s+\()/g;
+        const unaryOperatorsRegex = /(NOT\s*\()/g;
 
         // Divide up the divided components, this time splitting at unary operator boundaries.
         // flatMap() divides and then flattens the result.
