04f9578b0f609a2b7983f0922181fb220edc4928
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2024-04-06 22:19:52 +0100

    refactor: - Add BooleanDelimiters.fromInstructionLine()

--- src/Query/Filter/BooleanDelimiters.ts
+++ src/Query/Filter/BooleanDelimiters.ts
@@ -23,4 +23,22 @@ export class BooleanDelimiters {
     public static allSupportedDelimiters(): BooleanDelimiters {
         return new BooleanDelimiters('("', ')"', '()"');
     }
+
+    public static fromInstructionLine(instruction: string) {
+        const trimmedInstruction = instruction.trim();
+        const firstChar = trimmedInstruction[0];
+        const lastChar = trimmedInstruction.slice(-1);
+
+        if (firstChar === '(' && lastChar === ')') {
+            return new BooleanDelimiters('(', ')', '()');
+        }
+
+        if (firstChar === '"' && lastChar === '"') {
+            return new BooleanDelimiters('"', '"', '"');
+        }
+
+        throw new Error(
+            "All filters in a Boolean instruction be surrounded with either '(' and ')' or '\"'. Combinations of those delimiters are no longer supported.",
+        );
+    }
 }
