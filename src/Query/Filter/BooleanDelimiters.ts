6c9229e6532b1b76c274635186ddfe8d76dbee4e
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2024-04-07 10:34:59 +0100

    jsdoc: Document that only the outer delimiters are checked

--- src/Query/Filter/BooleanDelimiters.ts
+++ src/Query/Filter/BooleanDelimiters.ts
@@ -2,6 +2,13 @@ export function anyOfTheseChars(allowedChars: string): string {
     return new RegExp('[' + allowedChars + ']').source;
 }
 
+/**
+ * A class to try to identify the type of delimiter used between Boolean operators.
+ *
+ * Note that this only checks the first and last non-operator characters on the line,
+ * so where there is more than one binary operator, it is still possible for the user
+ * to mix delimiters, and for the error to not be detected until later in the parsing process.
+ */
 export class BooleanDelimiters {
     public readonly openFilterChars;
     public readonly openFilter;
