796d8574685c42837109c3c2a81095a23f268965
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2024-04-08 08:59:51 +0100

    refactor: - New class BooleanPreprocessor now recognises AND
    This is the start of rewriting the parsing of Boolean instructions

---
+++ src/Query/Filter/BooleanPreprocessor.ts
@@ -0,0 +1,17 @@
+import { BooleanDelimiters } from './BooleanDelimiters';
+
+export class BooleanPreprocessor {
+    public static splitLine(line: string) {
+        const delimiters = BooleanDelimiters.allSupportedDelimiters();
+
+        // Here, we split the input line in to separate operators-plus-adjacent-delimiters
+        // and the remaining filter text.
+        const binaryOperatorsRegex = new RegExp(
+            '(' + delimiters.closeFilter + ' AND ' + delimiters.openFilter + ')',
+            'g',
+        );
+
+        // Divide up line, split at binary operator boundaries
+        return line.split(binaryOperatorsRegex);
+    }
+}
---
+++ tests/Query/Filter/BooleanPreprocessor.test.ts
@@ -0,0 +1,33 @@
+import { BooleanPreprocessor } from '../../../src/Query/Filter/BooleanPreprocessor';
+
+function split(line: string) {
+    return BooleanPreprocessor.splitLine(line);
+}
+
+describe('BooleanPreprocessor', () => {
+    it('single sub-expression', () => {
+        expect(split('(not done)')).toMatchInlineSnapshot(`
+            [
+              "(not done)",
+            ]
+        `);
+    });
+
+    it('simple AND', () => {
+        expect(split('(done) AND (has done date)')).toMatchInlineSnapshot(`
+            [
+              "(done",
+              ") AND (",
+              "has done date)",
+            ]
+        `);
+    });
+
+    it('simple OR', () => {
+        expect(split('(done) OR (has done date)')).toMatchInlineSnapshot(`
+            [
+              "(done) OR (has done date)",
+            ]
+        `);
+    });
+});
