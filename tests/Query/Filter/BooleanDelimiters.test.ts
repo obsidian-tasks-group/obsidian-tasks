1bd434d658bf9dc64c5c64d7a013a29f739ba68c
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2024-04-09 13:28:37 +0100

    test: - Group related tests together

--- tests/Query/Filter/BooleanDelimiters.test.ts
+++ tests/Query/Filter/BooleanDelimiters.test.ts
@@ -13,36 +13,38 @@ describe('BooleanDelimiters', () => {
         expect(delimiters.openAndCloseFilterChars).toEqual('()"');
     });
 
-    it('from line with () delimiters', () => {
-        const delimiters = BooleanDelimiters.fromInstructionLine('NOT (not done)');
+    describe('construct from line starting with NOT', () => {
+        it('from line with () delimiters', () => {
+            const delimiters = BooleanDelimiters.fromInstructionLine('NOT (not done)');
 
-        expect(delimiters.openFilterChars).toEqual('(');
-        expect(delimiters.closeFilterChars).toEqual(')');
-        expect(delimiters.openAndCloseFilterChars).toEqual('()');
-    });
+            expect(delimiters.openFilterChars).toEqual('(');
+            expect(delimiters.closeFilterChars).toEqual(')');
+            expect(delimiters.openAndCloseFilterChars).toEqual('()');
+        });
 
-    it('from line with "" delimiters', () => {
-        const delimiters = BooleanDelimiters.fromInstructionLine('NOT "not done"');
+        it('from line with "" delimiters', () => {
+            const delimiters = BooleanDelimiters.fromInstructionLine('NOT "not done"');
 
-        expect(delimiters.openFilterChars).toEqual('"');
-        expect(delimiters.closeFilterChars).toEqual('"');
-        expect(delimiters.openAndCloseFilterChars).toEqual('"');
-    });
+            expect(delimiters.openFilterChars).toEqual('"');
+            expect(delimiters.closeFilterChars).toEqual('"');
+            expect(delimiters.openAndCloseFilterChars).toEqual('"');
+        });
 
-    it.failing('from line with mixed delimiters', () => {
-        const t = () => {
-            BooleanDelimiters.fromInstructionLine('NOT (not done"');
-        };
-        expect(t).toThrow(Error);
-    });
+        it.failing('from line with mixed delimiters', () => {
+            const t = () => {
+                BooleanDelimiters.fromInstructionLine('NOT (not done"');
+            };
+            expect(t).toThrow(Error);
+        });
 
-    it('from line with unknown delimiters', () => {
-        const t = () => {
-            BooleanDelimiters.fromInstructionLine('NOT {not done}');
-        };
-        expect(t).toThrow(Error);
-        expect(t).toThrowError(
-            "All filters in a Boolean instruction be surrounded with either '(' and ')' or '\"'. Combinations of those delimiters are no longer supported.",
-        );
+        it('from line with unknown delimiters', () => {
+            const t = () => {
+                BooleanDelimiters.fromInstructionLine('NOT {not done}');
+            };
+            expect(t).toThrow(Error);
+            expect(t).toThrowError(
+                "All filters in a Boolean instruction be surrounded with either '(' and ')' or '\"'. Combinations of those delimiters are no longer supported.",
+            );
+        });
     });
 });
