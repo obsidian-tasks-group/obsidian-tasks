0e3582e316eab677a0b359f2522faf005ef1447e
Thomas Herden <tom.herden@dfa.state.ny.us>
2024-04-15 09:08:37 -0400

    Improve variable name

--- src/lib/FileWriter.ts
+++ src/lib/FileWriter.ts
@@ -15,18 +15,18 @@ export function appendToEndOfFile(initialContent: string, textToAppend: string) 
     return result;
 }
 
-export function appendToListWithinFile(initialContent: string, targetListHeading: string, textToAppend: string) {
+export function appendToListWithinFile(initialFileContent: string, targetListHeading: string, textToAppend: string) {
     if (textToAppend.length === 0) {
-        return initialContent;
+        return initialFileContent;
     }
     if (targetListHeading === '') {
         throw Error('Cannot move line to list as empty target list heading was supplied');
     }
-    const linesArray = initialContent.split('\n');
+    const linesArray = initialFileContent.split('\n');
     const headingLine = linesArray.indexOf(targetListHeading);
 
     if (headingLine === -1) {
-        let result = initialContent;
+        let result = initialFileContent;
         if (result.length > 0 && !result.endsWith(newLineChar)) {
             result += newLineChar;
         }
