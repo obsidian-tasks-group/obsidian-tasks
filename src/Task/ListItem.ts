88d23d7fcebdf139ec622c2d4855aeb4a23ac30f
Stanislav Markin <stasmarkin@gmail.com>
2024-05-13 08:48:02 -0300

    refactor: add parent field

--- src/Task/ListItem.ts
+++ src/Task/ListItem.ts
@@ -1,5 +1,6 @@
 export class ListItem {
     public readonly children: ListItem[];
+    public readonly parent: ListItem | null = null;
 
     constructor(children: ListItem[]) {
         this.children = children;
