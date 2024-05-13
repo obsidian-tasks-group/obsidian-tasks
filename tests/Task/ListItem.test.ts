4213a174480eb833be86ff74d2b8130a79c4f316
Stanislav Markin <stasmarkin@gmail.com>
2024-05-13 08:44:13 -0300

    refactor: added ListItem. children

--- tests/Task/ListItem.test.ts
+++ tests/Task/ListItem.test.ts
@@ -2,7 +2,14 @@ import { ListItem } from '../../src/Task/ListItem';
 
 describe('list item tests', () => {
     it('should create list item', () => {
-        const listItem = new ListItem();
+        const listItem = new ListItem([]);
         expect(listItem).toBeDefined();
+    });
+
+    it('should create list item with a child', () => {
+        const childItem = new ListItem([]);
+        const listItem = new ListItem([childItem]);
+        expect(listItem).toBeDefined();
+        expect(listItem.children).toEqual([childItem]);
     });
 });
