cb554fe659ee3505fb4a2ad02ac10daae1c01158
Ilyas Landikov <ilyas.landikov@icloud.com>
2024-05-14 00:40:34 +0600

    refactor: . rename parameter to labelText

--- src/ui/EditTaskHelpers.ts
+++ src/ui/EditTaskHelpers.ts
@@ -1,24 +1,24 @@
-export function editTaskLabelContent(id: string, accessKey: string | null) {
+export function editTaskLabelContent(labelText: string, accessKey: string | null) {
     if (accessKey === null) {
-        return capitalizeFirstLetter(id);
+        return capitalizeFirstLetter(labelText);
     }
 
-    if (!id.toLowerCase().includes(accessKey)) {
-        return `${capitalizeFirstLetter(id)} (<span class="accesskey">${accessKey}</span>)`;
+    if (!labelText.toLowerCase().includes(accessKey)) {
+        return `${capitalizeFirstLetter(labelText)} (<span class="accesskey">${accessKey}</span>)`;
     }
 
-    const accessKeyIndex = id.toLowerCase().indexOf(accessKey);
-    let labelContent = id.substring(0, accessKeyIndex);
+    const accessKeyIndex = labelText.toLowerCase().indexOf(accessKey);
+    let labelContent = labelText.substring(0, accessKeyIndex);
     labelContent += '<span class="accesskey">';
 
     if (accessKeyIndex === 0) {
-        labelContent += id.substring(accessKeyIndex, accessKeyIndex + 1).toUpperCase();
+        labelContent += labelText.substring(accessKeyIndex, accessKeyIndex + 1).toUpperCase();
     } else {
-        labelContent += id.substring(accessKeyIndex, accessKeyIndex + 1);
+        labelContent += labelText.substring(accessKeyIndex, accessKeyIndex + 1);
     }
 
     labelContent += '</span>';
-    labelContent += id.substring(accessKeyIndex + 1);
+    labelContent += labelText.substring(accessKeyIndex + 1);
     labelContent = capitalizeFirstLetter(labelContent);
     return labelContent;
 }
