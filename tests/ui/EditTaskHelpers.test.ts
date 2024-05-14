91cf0a1a3beb46806138a5c7e8482b09566d5661
Ilyas Landikov <ilyas.landikov@icloud.com>
2024-05-14 15:26:13 +0600

    test: - convert all tests to it.each()

--- tests/ui/EditTaskHelpers.test.ts
+++ tests/ui/EditTaskHelpers.test.ts
@@ -1,34 +1,17 @@
 import { labelContentWithAccessKey } from '../../src/ui/EditTaskHelpers';
 
 describe('labelContentWithAccessKey() tests', () => {
-    it.each([['first letter', 'f', '<span class="accesskey">F</span>irst letter']])(
-        "label text '%s' with access key '%s' should have label content '%s'",
-        (labelText, accessKey, labelContent) => {
-            expect(labelContentWithAccessKey(labelText, accessKey)).toEqual(labelContent);
-        },
-    );
-
-    it('should add access key span if it matches not the first letter', () => {
-        const labelContent = labelContentWithAccessKey('make this x the access key', 'x');
-        expect(labelContent).toMatchInlineSnapshot('"Make this <span class="accesskey">x</span> the access key"');
-    });
-
-    it('should treat a capitalised access key in the same way as a regular one', () => {
-        const labelContent = labelContentWithAccessKey('make this u the access key too', 'U');
-        expect(labelContent).toMatchInlineSnapshot(
-            '"Make this u the access key too (<span class="accesskey">u</span>)"',
-        );
-    });
-
-    it('should not add access key span after the label text if the access key is not present in the label text', () => {
-        const labelContent = labelContentWithAccessKey('the last letter of the alphabet is absent here', 'z');
-        expect(labelContent).toMatchInlineSnapshot(
-            '"The last letter of the alphabet is absent here (<span class="accesskey">z</span>)"',
-        );
-    });
-
-    it('should not add access key span if the it is null, but should capitalise the first word', () => {
-        const labelContent = labelContentWithAccessKey('access key is null', null);
-        expect(labelContent).toMatchInlineSnapshot('"Access key is null"');
+    it.each([
+        ['first letter', 'f', '<span class="accesskey">F</span>irst letter'],
+        ['make this x the access key', 'x', 'Make this <span class="accesskey">x</span> the access key'],
+        ['make this u the access key too', 'U', 'Make this u the access key too (<span class="accesskey">u</span>)'],
+        [
+            'the last letter of the alphabet is absent here',
+            'z',
+            'The last letter of the alphabet is absent here (<span class="accesskey">z</span>)',
+        ],
+        ['access key is null', null, 'Access key is null'],
+    ])("label text '%s' with access key '%s' should have label content '%s'", (labelText, accessKey, labelContent) => {
+        expect(labelContentWithAccessKey(labelText, accessKey)).toEqual(labelContent);
     });
 });
