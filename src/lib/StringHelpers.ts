d74fda22ff21091cac7c62ca750056ccb56d6f06
Ilyas Landikov <ilyas.landikov@icloud.com>
2024-05-14 15:43:53 +0600

    refactor: . rename parameter to text

--- src/lib/StringHelpers.ts
+++ src/lib/StringHelpers.ts
@@ -1,3 +1,3 @@
-export function capitalizeFirstLetter(word: string): string {
-    return word.charAt(0).toUpperCase() + word.slice(1);
+export function capitalizeFirstLetter(text: string): string {
+    return text.charAt(0).toUpperCase() + text.slice(1);
 }
