703846c39980b0d0499a9b37c18c6fe0aedc2780
Ilyas Landikov <ilyas.landikov@icloud.com>
2024-05-14 15:13:27 +0600

    test: - cover labelContentWithAccessKey() with tests

---
+++ tests/ui/EditTaskHelpers.test.ts
@@ -0,0 +1,25 @@
+import { labelContentWithAccessKey } from '../../src/ui/EditTaskHelpers';
+
+describe('labelContentWithAccessKey() tests', () => {
+    it('should add access key span if it matches the first letter and capitalise it', () => {
+        const labelContent = labelContentWithAccessKey('first letter', 'f');
+        expect(labelContent).toMatchInlineSnapshot('"<span class="accesskey">F</span>irst letter"');
+    });
+
+    it('should add access key span if it matches not the first letter', () => {
+        const labelContent = labelContentWithAccessKey('make this x the access key', 'x');
+        expect(labelContent).toMatchInlineSnapshot('"Make this <span class="accesskey">x</span> the access key"');
+    });
+
+    it('should not add access key span after the label text if the access key is not present in the label text', () => {
+        const labelContent = labelContentWithAccessKey('the last letter of the alphabet is absent here', 'z');
+        expect(labelContent).toMatchInlineSnapshot(
+            '"The last letter of the alphabet is absent here (<span class="accesskey">z</span>)"',
+        );
+    });
+
+    it('should not add access key span if the it is null, but should capitalise the first word', () => {
+        const labelContent = labelContentWithAccessKey('access key is null', null);
+        expect(labelContent).toMatchInlineSnapshot('"Access key is null"');
+    });
+});
