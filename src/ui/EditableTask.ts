6048e94533f2539d710e2c8ad7d23f339039a1fc
Ilyas Landikov <ilyas.landikov@icloud.com>
2024-05-19 04:20:19 +0600

    refactor: . inline EditableTask

--- src/ui/EditableTask.ts
+++ src/ui/EditableTask.ts
@@ -1,23 +1,6 @@
 import type { Status } from '../Statuses/Status';
 import type { Task } from '../Task/Task';
 
-export type EditableTask = {
-    // NEW_TASK_FIELD_EDIT_REQUIRED
-    description: string;
-    status: Status;
-    priority: 'none' | 'lowest' | 'low' | 'medium' | 'high' | 'highest';
-    recurrenceRule: string;
-    createdDate: string;
-    startDate: string;
-    scheduledDate: string;
-    dueDate: string;
-    doneDate: string;
-    cancelledDate: string;
-    forwardOnly: boolean;
-    blockedBy: Task[];
-    blocking: Task[];
-};
-
 export class AppleSauceTask {
     // NEW_TASK_FIELD_EDIT_REQUIRED
     description: string;
@@ -34,7 +17,22 @@ export class AppleSauceTask {
     blockedBy: Task[];
     blocking: Task[];
 
-    constructor(editableTask: EditableTask) {
+    constructor(editableTask: {
+        // NEW_TASK_FIELD_EDIT_REQUIRED
+        description: string;
+        status: Status;
+        priority: 'none' | 'lowest' | 'low' | 'medium' | 'high' | 'highest';
+        recurrenceRule: string;
+        createdDate: string;
+        startDate: string;
+        scheduledDate: string;
+        dueDate: string;
+        doneDate: string;
+        cancelledDate: string;
+        forwardOnly: boolean;
+        blockedBy: Task[];
+        blocking: Task[];
+    }) {
         this.description = editableTask.description;
         this.status = editableTask.status;
         this.priority = editableTask.priority;
