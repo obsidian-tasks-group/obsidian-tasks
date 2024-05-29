8d86766ad344bbf07c7c0148889539349bc2f746
Ilyas Landikov <ilyas.landikov@icloud.com>
2024-05-29 11:43:07 +0600

    test: - test fromTask().editableTask

---
+++ tests/ui/EditableTask.test.ts
@@ -0,0 +1,60 @@
+/**
+ * @jest-environment jsdom
+ */
+import moment from 'moment';
+import { EditableTask } from '../../src/ui/EditableTask';
+import { TaskBuilder } from '../TestingTools/TaskBuilder';
+
+window.moment = moment;
+
+describe('EditableTask tests', () => {
+    it('should create an editable task without dependencies', () => {
+        const taskToEdit = TaskBuilder.createFullyPopulatedTask();
+
+        const { editableTask } = EditableTask.fromTask(taskToEdit, [taskToEdit]);
+
+        expect(editableTask).toMatchInlineSnapshot(`
+            EditableTask {
+              "blockedBy": [],
+              "blocking": [],
+              "cancelledDate": "2023-07-06",
+              "createdDate": "2023-07-01",
+              "description": "Do exercises #todo #health",
+              "doneDate": "2023-07-05",
+              "dueDate": "2023-07-04",
+              "forwardOnly": true,
+              "priority": "medium",
+              "recurrenceRule": "every day when done",
+              "scheduledDate": "2023-07-03",
+              "startDate": "2023-07-02",
+              "status": Status {
+                "configuration": StatusConfiguration {
+                  "availableAsCommand": true,
+                  "name": "Todo",
+                  "nextStatusSymbol": "x",
+                  "symbol": " ",
+                  "type": "TODO",
+                },
+              },
+            }
+        `);
+    });
+
+    it('should create an editable task with dependencies', () => {
+        const taskToEdit = TaskBuilder.createFullyPopulatedTask();
+        const blockingTask = new TaskBuilder().description('I am blocking the task to edit').id('123456').build();
+        const blockedTask = new TaskBuilder()
+            .description('I am blocked by the task to edit')
+            .dependsOn(['abcdef'])
+            .build();
+        const allTasks = [taskToEdit, blockingTask, blockedTask];
+
+        const { editableTask } = EditableTask.fromTask(taskToEdit, allTasks);
+
+        const blockingTasks = editableTask.blocking;
+        const blockedByTasks = editableTask.blockedBy;
+
+        expect(blockingTasks).toEqual([blockedTask]);
+        expect(blockedByTasks).toEqual([blockingTask]);
+    });
+});
