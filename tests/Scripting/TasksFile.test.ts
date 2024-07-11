2311931a89e8888167eaede3ae18ee60b1ee336f
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2024-07-11 14:41:38 +0100

    test: - Show how 'kanban-plugin: basic' might be used

--- tests/Scripting/TasksFile.test.ts
+++ tests/Scripting/TasksFile.test.ts
@@ -11,6 +11,7 @@ import { yaml_tags_field_added_by_obsidian_but_not_populated } from '../Obsidian
 import { yaml_tags_had_value_then_was_emptied_by_obsidian } from '../Obsidian/__test_data__/yaml_tags_had_value_then_was_emptied_by_obsidian';
 import { yaml_tags_is_empty_list } from '../Obsidian/__test_data__/yaml_tags_is_empty_list';
 import { yaml_tags_is_empty } from '../Obsidian/__test_data__/yaml_tags_is_empty';
+import { example_kanban } from '../Obsidian/__test_data__/example_kanban';
 
 describe('TasksFile', () => {
     it('should provide access to path', () => {
@@ -108,6 +109,19 @@ describe('TasksFile - reading frontmatter', () => {
     it('should read file with multiple tags in yaml metadata', () => {
         const tasksFile = getTasksFileFromMockData(yaml_tags_has_multiple_values);
         expect(tasksFile.cachedMetadata.frontmatter?.tags).toEqual(['multiple1', 'multiple2']);
+    });
+
+    it('should detect whether file is a kanban plugin board', () => {
+        const tasksFile = getTasksFileFromMockData(example_kanban);
+
+        expect(tasksFile.frontmatter['kanban-plugin']).toEqual('basic');
+        // An example search might be:
+        //      filter by function task.file.frontmatter['kanban-plugin'] !== undefined
+
+        // TODO Consider sanitising property names:
+        //      See dataview docs for ideas:
+        //          https://blacksmithgu.github.io/obsidian-dataview/annotation/add-metadata/#field-names
+        expect(tasksFile.frontmatter.kanban_plugin).toBeUndefined();
     });
 
     // See property types: https://help.obsidian.md/Editing+and+formatting/Properties#Property+types
