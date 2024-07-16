c878c952bb6c44f452a5876591b6a16c2abb1c49
aigoncharov <andrey@goncharov.page>
2024-07-16 18:52:45 +0100

    Remove mentions of "spaced"

--- src/Suggestor/Suggestor.ts
+++ src/Suggestor/Suggestor.ts
@@ -365,7 +365,6 @@ function addRecurrenceSuggestions(
         'every week on Thursday',
         'every week on Friday',
         'every week on Saturday',
-        'spaced',
     ];
 
     const { postfix, insertSkip } = getAdjusters(dataviewMode, line, cursorPos);
