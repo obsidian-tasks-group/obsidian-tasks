039384dfe214fb33826de699c99530d17bdd4a13
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2024-07-20 15:34:20 +0100

    refactor: - Move the default frontmatter.tags default value to declaration

--- src/Scripting/TasksFile.ts
+++ src/Scripting/TasksFile.ts
@@ -8,7 +8,8 @@ export type OptionalTasksFile = TasksFile | undefined;
 export class TasksFile {
     private readonly _path: string;
     private readonly _cachedMetadata: CachedMetadata;
-    private readonly _frontmatter: FrontMatterCache = {} as FrontMatterCache;
+    // Always make TasksFile.frontmatter.tags exist and be empty, even if no frontmatter present:
+    private readonly _frontmatter = { tags: [] } as any;
 
     constructor(path: string, cachedMetadata: CachedMetadata = {}) {
         this._path = path;
@@ -18,9 +19,6 @@ export class TasksFile {
         if (rawFrontmatter !== undefined) {
             this._frontmatter = JSON.parse(JSON.stringify(rawFrontmatter));
             this._frontmatter.tags = parseFrontMatterTags(rawFrontmatter) ?? [];
-        } else {
-            // Always make TasksFile.frontmatter.tags exist and be empty, even if no frontmatter present.
-            this._frontmatter.tags = [];
         }
     }
 
