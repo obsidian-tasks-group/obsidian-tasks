c1f76dffe06a1eb838b59848ef8549adce0dc021
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2024-07-21 07:44:23 +0100

    test: - Visualise how each property type is represented
    See https://help.obsidian.md/Editing+and+formatting/Properties#Property+types

---
+++ resources/sample_vaults/Tasks-Demo/Test Data/yaml_all_property_types_empty.md
@@ -0,0 +1,11 @@
+---
+sample_checkbox_property: 
+sample_date_property: 
+sample_date_and_time_property: 
+sample_list_property: 
+sample_number_property: 
+sample_text_property:
+---
+# yaml_all_property_types_empty
+
+- [ ] #task Task in 'yaml_all_property_types_empty'
---
+++ tests/Obsidian/__test_data__/yaml_all_property_types_populated.ts
@@ -0,0 +1,145 @@
+export const yaml_all_property_types_populated = {
+    filePath: 'Test Data/yaml_all_property_types_populated.md',
+    fileContents:
+        '---\n' +
+        'sample_checkbox_property: true\n' +
+        'sample_date_property: 2024-07-21\n' +
+        'sample_date_and_time_property: 2024-07-21T12:37:00\n' +
+        'sample_list_property:\n' +
+        '  - Sample\n' +
+        '  - List\n' +
+        '  - Value\n' +
+        'sample_number_property: 246\n' +
+        'sample_text_property: Sample Text Value\n' +
+        '---\n' +
+        '\n' +
+        '# yaml_all_property_types_populated\n' +
+        '\n' +
+        "- [ ] #task Task in 'yaml_all_property_types_populated'\n",
+    cachedMetadata: {
+        tags: [
+            {
+                position: {
+                    start: {
+                        line: 14,
+                        col: 6,
+                        offset: 287,
+                    },
+                    end: {
+                        line: 14,
+                        col: 11,
+                        offset: 292,
+                    },
+                },
+                tag: '#task',
+            },
+        ],
+        headings: [
+            {
+                position: {
+                    start: {
+                        line: 12,
+                        col: 0,
+                        offset: 244,
+                    },
+                    end: {
+                        line: 12,
+                        col: 35,
+                        offset: 279,
+                    },
+                },
+                heading: 'yaml_all_property_types_populated',
+                level: 1,
+            },
+        ],
+        sections: [
+            {
+                type: 'yaml',
+                position: {
+                    start: {
+                        line: 0,
+                        col: 0,
+                        offset: 0,
+                    },
+                    end: {
+                        line: 10,
+                        col: 3,
+                        offset: 242,
+                    },
+                },
+            },
+            {
+                type: 'heading',
+                position: {
+                    start: {
+                        line: 12,
+                        col: 0,
+                        offset: 244,
+                    },
+                    end: {
+                        line: 12,
+                        col: 35,
+                        offset: 279,
+                    },
+                },
+            },
+            {
+                type: 'list',
+                position: {
+                    start: {
+                        line: 14,
+                        col: 0,
+                        offset: 281,
+                    },
+                    end: {
+                        line: 14,
+                        col: 55,
+                        offset: 336,
+                    },
+                },
+            },
+        ],
+        listItems: [
+            {
+                position: {
+                    start: {
+                        line: 14,
+                        col: 0,
+                        offset: 281,
+                    },
+                    end: {
+                        line: 14,
+                        col: 55,
+                        offset: 336,
+                    },
+                },
+                parent: -14,
+                task: ' ',
+            },
+        ],
+        frontmatter: {
+            sample_checkbox_property: true,
+            sample_date_property: '2024-07-21',
+            sample_date_and_time_property: '2024-07-21T12:37:00',
+            sample_list_property: ['Sample', 'List', 'Value'],
+            sample_number_property: 246,
+            sample_text_property: 'Sample Text Value',
+        },
+        frontmatterPosition: {
+            start: {
+                line: 0,
+                col: 0,
+                offset: 0,
+            },
+            end: {
+                line: 10,
+                col: 3,
+                offset: 242,
+            },
+        },
+        frontmatterLinks: [],
+    },
+    obsidianApiVersion: '1.6.7',
+    getAllTags: ['#task'],
+    parseFrontMatterTags: null,
+};
