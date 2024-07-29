c6f302447db88681a9565d100e05d6c3a6b98409
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2024-07-29 20:26:33 +0100

    test: Add another task and more meaningful tags
    ... to sample file docs_sample_for_task_properties_reference.md

--- tests/Obsidian/__test_data__/docs_sample_for_task_properties_reference.ts
+++ tests/Obsidian/__test_data__/docs_sample_for_task_properties_reference.ts
@@ -18,27 +18,75 @@ export const docs_sample_for_task_properties_reference = {
         'aliases:\n' +
         '  - YAML All Property Types Populated\n' +
         'tags:\n' +
-        '  - sample/tag/value\n' +
+        '  - tag-from-file-properties\n' +
         'creation date: 2024-05-25T15:17:00\n' +
         'project: Secret Project\n' +
         '---\n' +
         '\n' +
         '# docs_sample_for_task_properties_reference\n' +
         '\n' +
-        "- [ ] #task Task in 'docs_sample_for_task_properties_reference'\n",
+        '#tag-from-file-body\n' +
+        '\n' +
+        "- [ ] #task Task in 'docs_sample_for_task_properties_reference' #tag-from-task-line\n" +
+        "- [ ] #task Another task in 'docs_sample_for_task_properties_reference'\n",
     cachedMetadata: {
         tags: [
             {
                 position: {
                     start: {
                         line: 24,
-                        col: 6,
-                        offset: 601,
+                        col: 0,
+                        offset: 603,
                     },
                     end: {
                         line: 24,
+                        col: 19,
+                        offset: 622,
+                    },
+                },
+                tag: '#tag-from-file-body',
+            },
+            {
+                position: {
+                    start: {
+                        line: 26,
+                        col: 6,
+                        offset: 630,
+                    },
+                    end: {
+                        line: 26,
                         col: 11,
-                        offset: 606,
+                        offset: 635,
+                    },
+                },
+                tag: '#task',
+            },
+            {
+                position: {
+                    start: {
+                        line: 26,
+                        col: 64,
+                        offset: 688,
+                    },
+                    end: {
+                        line: 26,
+                        col: 83,
+                        offset: 707,
+                    },
+                },
+                tag: '#tag-from-task-line',
+            },
+            {
+                position: {
+                    start: {
+                        line: 27,
+                        col: 6,
+                        offset: 714,
+                    },
+                    end: {
+                        line: 27,
+                        col: 11,
+                        offset: 719,
                     },
                 },
                 tag: '#task',
@@ -50,12 +98,12 @@ export const docs_sample_for_task_properties_reference = {
                     start: {
                         line: 22,
                         col: 0,
-                        offset: 550,
+                        offset: 558,
                     },
                     end: {
                         line: 22,
                         col: 43,
-                        offset: 593,
+                        offset: 601,
                     },
                 },
                 heading: 'docs_sample_for_task_properties_reference',
@@ -74,7 +122,7 @@ export const docs_sample_for_task_properties_reference = {
                     end: {
                         line: 20,
                         col: 3,
-                        offset: 548,
+                        offset: 556,
                     },
                 },
             },
@@ -84,12 +132,27 @@ export const docs_sample_for_task_properties_reference = {
                     start: {
                         line: 22,
                         col: 0,
-                        offset: 550,
+                        offset: 558,
                     },
                     end: {
                         line: 22,
                         col: 43,
-                        offset: 593,
+                        offset: 601,
+                    },
+                },
+            },
+            {
+                type: 'paragraph',
+                position: {
+                    start: {
+                        line: 24,
+                        col: 0,
+                        offset: 603,
+                    },
+                    end: {
+                        line: 24,
+                        col: 19,
+                        offset: 622,
                     },
                 },
             },
@@ -97,14 +160,14 @@ export const docs_sample_for_task_properties_reference = {
                 type: 'list',
                 position: {
                     start: {
-                        line: 24,
+                        line: 26,
                         col: 0,
-                        offset: 595,
+                        offset: 624,
                     },
                     end: {
-                        line: 24,
-                        col: 63,
-                        offset: 658,
+                        line: 27,
+                        col: 71,
+                        offset: 779,
                     },
                 },
             },
@@ -113,17 +176,33 @@ export const docs_sample_for_task_properties_reference = {
             {
                 position: {
                     start: {
-                        line: 24,
+                        line: 26,
                         col: 0,
-                        offset: 595,
+                        offset: 624,
                     },
                     end: {
-                        line: 24,
-                        col: 63,
-                        offset: 658,
+                        line: 26,
+                        col: 83,
+                        offset: 707,
                     },
                 },
-                parent: -24,
+                parent: -26,
+                task: ' ',
+            },
+            {
+                position: {
+                    start: {
+                        line: 27,
+                        col: 0,
+                        offset: 708,
+                    },
+                    end: {
+                        line: 27,
+                        col: 71,
+                        offset: 779,
+                    },
+                },
+                parent: -26,
                 task: ' ',
             },
         ],
@@ -137,7 +216,7 @@ export const docs_sample_for_task_properties_reference = {
             sample_link_property: '[[yaml_all_property_types_populated]]',
             sample_link_list_property: ['[[yaml_all_property_types_populated]]', '[[yaml_all_property_types_empty]]'],
             aliases: ['YAML All Property Types Populated'],
-            tags: ['sample/tag/value'],
+            tags: ['tag-from-file-properties'],
             'creation date': '2024-05-25T15:17:00',
             project: 'Secret Project',
         },
@@ -150,7 +229,7 @@ export const docs_sample_for_task_properties_reference = {
             end: {
                 line: 20,
                 col: 3,
-                offset: 548,
+                offset: 556,
             },
         },
         frontmatterLinks: [
@@ -175,6 +254,6 @@ export const docs_sample_for_task_properties_reference = {
         ],
     },
     obsidianApiVersion: '1.6.7',
-    getAllTags: ['#sample/tag/value', '#task'],
-    parseFrontMatterTags: ['#sample/tag/value'],
+    getAllTags: ['#tag-from-file-properties', '#tag-from-file-body', '#task', '#tag-from-task-line', '#task'],
+    parseFrontMatterTags: ['#tag-from-file-properties'],
 };
