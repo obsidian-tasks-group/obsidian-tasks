9f4d677b431b7d0120329db1a34e797931723969
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2024-08-02 18:24:27 +0100

    refactor: . Flip if/else

--- src/Obsidian/Cache.ts
+++ src/Obsidian/Cache.ts
@@ -284,7 +284,9 @@ export class Cache {
                 const fallbackDate = new Lazy(() => DateFallback.fromPath(file.path));
 
                 this.tasks = this.tasks.map((task: Task): Task => {
-                    if (task.path === oldPath) {
+                    if (task.path !== oldPath) {
+                        return task;
+                    } else {
                         const taskLocation = task.taskLocation.fromRenamedFile(tasksFile);
                         if (!useFilenameAsScheduledDate) {
                             return new Task({
@@ -294,8 +296,6 @@ export class Cache {
                         } else {
                             return DateFallback.updateTaskPath(task, taskLocation, fallbackDate.value);
                         }
-                    } else {
-                        return task;
                     }
                 });
 
