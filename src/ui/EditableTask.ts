6af070e57d5eb87e098117b8fa371de85db0a7d4
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2024-08-06 21:20:17 +0100

    refactor: - Simplify code: this.onCompletion is always type OnCompletion

--- src/ui/EditableTask.ts
+++ src/ui/EditableTask.ts
@@ -2,7 +2,7 @@ import { GlobalFilter } from '../Config/GlobalFilter';
 import { parseTypedDateForSaving } from '../lib/DateTools';
 import { replaceTaskWithTasks } from '../Obsidian/File';
 import type { Status } from '../Statuses/Status';
-import { OnCompletion } from '../Task/OnCompletion';
+import type { OnCompletion } from '../Task/OnCompletion';
 import { Occurrence } from '../Task/Occurrence';
 import { Priority } from '../Task/Priority';
 import { Recurrence } from '../Task/Recurrence';
@@ -188,14 +188,7 @@ export class EditableTask {
                 parsedPriority = Priority.None;
         }
 
-        let parsedOnCompletion: OnCompletion;
-        switch (this.onCompletion) {
-            case 'delete':
-                parsedOnCompletion = OnCompletion.Delete;
-                break;
-            default:
-                parsedOnCompletion = OnCompletion.Ignore;
-        }
+        const parsedOnCompletion: OnCompletion = this.onCompletion;
 
         const blockedByWithIds = [];
 
