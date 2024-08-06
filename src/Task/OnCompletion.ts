2fed306ec2265da62196ef1ed47f19d8f168da63
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2024-08-06 20:07:40 +0100

    refactor: . Rename parameters to clarify their purpose.

--- src/Task/OnCompletion.ts
+++ src/Task/OnCompletion.ts
@@ -11,29 +11,29 @@ function returnWithoutCompletedInstance(tasks: Task[], changedStatusTask: Task) 
     return tasks.filter((task) => task !== changedStatusTask);
 }
 
-export function handleOnCompletion(task: Task, tasks: Task[]): Task[] {
-    const tasksArrayLength = tasks.length;
+export function handleOnCompletion(originalTask: Task, newTasks: Task[]): Task[] {
+    const tasksArrayLength = newTasks.length;
     if (tasksArrayLength === 0) {
-        return tasks;
+        return newTasks;
     }
-    const startStatus = task.status;
+    const startStatus = originalTask.status;
 
-    const changedStatusTask = tasks[tasksArrayLength - 1];
+    const changedStatusTask = newTasks[tasksArrayLength - 1];
     const endStatus = changedStatusTask.status;
 
-    if (!task.onCompletion || endStatus.type !== StatusType.DONE || endStatus.type === startStatus.type) {
-        return tasks;
+    if (!originalTask.onCompletion || endStatus.type !== StatusType.DONE || endStatus.type === startStatus.type) {
+        return newTasks;
     }
 
-    const ocAction = task.onCompletion.toLowerCase();
+    const ocAction = originalTask.onCompletion.toLowerCase();
 
     if ('delete' === ocAction) {
-        return returnWithoutCompletedInstance(tasks, changedStatusTask);
+        return returnWithoutCompletedInstance(newTasks, changedStatusTask);
     }
 
-    const errorText = 'Unknown "On Completion" action: ' + task.onCompletion;
+    const errorText = 'Unknown "On Completion" action: ' + originalTask.onCompletion;
     const hintText = '\nClick here to clear';
     const noticeText = errorText + hintText;
     new Notice(noticeText, 0);
-    return tasks;
+    return newTasks;
 }
