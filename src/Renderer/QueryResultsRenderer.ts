04db58e4bc79c890f7a1d5ebb5733e4027f5db81
Ilyas Landikov <ilyas.landikov@icloud.com>
2024-08-08 21:13:36 +0600

    refactor: ! move source field to QueryResultsRenderer

--- src/Renderer/QueryResultsRenderer.ts
+++ src/Renderer/QueryResultsRenderer.ts
@@ -1,7 +1,21 @@
 import { MarkdownRenderChild } from 'obsidian';
 
 export class QueryResultsRenderer extends MarkdownRenderChild {
-    constructor(container: HTMLElement) {
+    /**
+     * The complete text in the instruction block, such as:
+     * ```
+     *   not done
+     *   short mode
+     * ```
+     *
+     * This does not contain the Global Query from the user's settings.
+     * Use {@link getQueryForQueryRenderer} to get this value prefixed with the Global Query.
+     */
+    protected readonly source: string;
+
+    constructor(container: HTMLElement, source: string) {
         super(container);
+
+        this.source = source;
     }
 }
