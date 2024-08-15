59c299630a26e6eeeaaff1071b89865b699a8614
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2024-08-15 22:54:41 +0100

    test: Confirm that 'hide on completion' is hooked up correctly

--- tests/Query/Query.test.ts
+++ tests/Query/Query.test.ts
@@ -21,6 +21,7 @@ import type { FilteringCase } from '../TestingTools/FilterTestHelpers';
 import { shouldSupportFiltering } from '../TestingTools/FilterTestHelpers';
 import { TaskBuilder } from '../TestingTools/TaskBuilder';
 import { Priority } from '../../src/Task/Priority';
+import { TaskLayoutComponent } from '../../src/Layout/TaskLayoutOptions';
 
 window.moment = moment;
 
@@ -1286,6 +1287,18 @@ describe('Query', () => {
             // Assert
             // The task will match if the correct path.
             expect(results.totalTasksCount).toEqual(1);
+        });
+    });
+
+    describe('show and hide', () => {
+        it('should show "on completion" by default', () => {
+            const query = new Query('');
+            expect(query.taskLayoutOptions.isShown(TaskLayoutComponent.OnCompletion)).toEqual(true);
+        });
+
+        it('should allow to hide "on completion"', () => {
+            const query = new Query('hide on completion');
+            expect(query.taskLayoutOptions.isShown(TaskLayoutComponent.OnCompletion)).toEqual(false);
         });
     });
 
