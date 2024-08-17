553c9ec44247da9fc9df1076d0051517404895dd
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2024-08-17 22:39:21 +0100

    chore: Add flatpickr licence text to released main.js
    Version copied
    https://github.com/flatpickr/flatpickr/blob/548330e6e18871d6ca1ca78746c0e89cb12a0303/LICENSE.md

--- esbuild.config.mjs
+++ esbuild.config.mjs
@@ -1,5 +1,5 @@
-import { sassPlugin } from 'esbuild-sass-plugin';
 import process from 'process';
+import { sassPlugin } from 'esbuild-sass-plugin';
 import esbuild from 'esbuild';
 import builtins from 'builtin-modules';
 import esbuildSvelte from 'esbuild-svelte';
@@ -128,6 +128,31 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 */
+
+/*
+License flatpickr (included library):
+The MIT License (MIT)
+
+Copyright (c) 2017 Gregory Petrosyan
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
 `;
 
 const prod = process.argv[2] === 'production';
@@ -169,8 +194,8 @@ esbuild
                 preprocess: sveltePreprocess(),
             }),
             sassPlugin({
-                syntax: "scss",
-                style: "expanded",
+                syntax: 'scss',
+                style: 'expanded',
             }),
         ],
         sourcemap: prod ? false : 'inline',
