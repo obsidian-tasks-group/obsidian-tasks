e59e67af5538c564379992e8d25295c3beb40f37
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2024-08-19 10:53:48 +0100

    refactor: - Specify which date field to edit

--- src/ui/Menus/DateMenu.ts
+++ src/ui/Menus/DateMenu.ts
@@ -1,13 +1,20 @@
 import type { Task } from '../../Task/Task';
+import type { AllTaskDateFields } from '../../DateTime/DateFieldTypes';
 import { TaskEditingMenu, type TaskSaver, defaultTaskSaver } from './TaskEditingMenu';
 
 export class DateMenu extends TaskEditingMenu {
+    // @ts-expect-error Unused
+    private field: AllTaskDateFields;
+
     /**
      * Constructor, which sets up the menu items.
+     * @param field - the Date field to edit
      * @param _task - the Task to be edited.
      * @param taskSaver - an optional {@link TaskSaver} function. For details, see {@link TaskEditingMenu}.
      */
-    constructor(_task: Task, taskSaver: TaskSaver = defaultTaskSaver) {
+    constructor(field: AllTaskDateFields, _task: Task, taskSaver: TaskSaver = defaultTaskSaver) {
         super(taskSaver);
+
+        this.field = field;
     }
 }
