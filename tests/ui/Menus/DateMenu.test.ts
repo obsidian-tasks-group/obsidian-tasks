2a590cb3990c5e84fda129a6f762367c5ca3f0cd
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2024-08-19 11:06:26 +0100

    refactor: - DateMenu now offers one date: Today

--- tests/ui/Menus/DateMenu.test.ts
+++ tests/ui/Menus/DateMenu.test.ts
@@ -1,9 +1,28 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import moment from 'moment/moment';
+
 import { DateMenu } from '../../../src/ui/Menus/DateMenu';
 import { TaskBuilder } from '../../TestingTools/TaskBuilder';
 import { TaskLayoutComponent } from '../../../src/Layout/TaskLayoutOptions';
 import { TestableTaskSaver, menuToString } from './MenuTestingHelpers';
 
-export {};
+window.moment = moment;
+
+const farPast = '2022-01-17';
+const today = '2023-12-03';
+
+beforeEach(() => {
+    jest.useFakeTimers();
+    jest.setSystemTime(new Date(today));
+    TestableTaskSaver.reset();
+});
+
+afterEach(() => {
+    jest.useRealTimers();
+});
 
 describe('DateMenu', () => {
     beforeEach(() => {
@@ -12,17 +31,17 @@ describe('DateMenu', () => {
 
     it('should populate a menu for a specific field', () => {
         // Arrange
-        const task = new TaskBuilder().build();
+        const task = new TaskBuilder().dueDate(farPast).build();
 
         // Act
-        const field = TaskLayoutComponent.DoneDate;
+        const field = TaskLayoutComponent.ScheduledDate;
         const menu = new DateMenu(field, task);
 
         // Assert
         const itemsAsText = menuToString(menu);
         expect(itemsAsText).toMatchInlineSnapshot(`
             "
-            "
+              Set Date: Sun Dec 03 2023"
         `);
     });
 });
