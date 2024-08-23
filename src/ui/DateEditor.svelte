7a2f1f885195c50e5151094f5c7e0b1ce7eccf01
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2024-08-23 18:27:14 +0100

    feat: Add a calendar icon to the Edit Task modal date fields

--- src/ui/DateEditor.svelte
+++ src/ui/DateEditor.svelte
@@ -2,6 +2,8 @@
     import { doAutocomplete } from '../DateTime/DateAbbreviations';
     import { parseTypedDateForDisplayUsingFutureDate } from '../DateTime/DateTools';
     import { labelContentWithAccessKey } from './EditTaskHelpers';
+    // Import a calendar icon, or define an inline SVG for the calendar icon.
+    // If using an icon library, import it here. For demonstration, I'm using an inline SVG.
 
     export let id: 'start' | 'scheduled' | 'due' | 'done' | 'created' | 'cancelled';
     export let dateSymbol: string;
@@ -32,7 +34,28 @@
     placeholder={datePlaceholder}
     {accesskey}
 />
+
+<!-- Add the calendar icon here -->
+<svg
+    class="calendar-icon"
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 24 24"
+    fill="currentColor"
+    width="24px"
+    height="24px"
+>
+    <path
+        d="M19 4h-1V2h-2v2H8V2H6v2H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 16H5V9h14v11zM7 11h5v5H7z"
+    />
+</svg>
+
 <code class="tasks-modal-parsed-date">{dateSymbol} {@html parsedDate}</code>
 
 <style>
+    /* Add some basic styling for the calendar icon */
+    /* Commented out as it generates a main.css file at the root of the repo */
+    /*.calendar-icon {*/
+    /*    margin: 0 8px;*/
+    /*    vertical-align: middle;*/
+    /*}*/
 </style>
