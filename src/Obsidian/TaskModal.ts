765a128d479b6b9079b7ab88d86507ee113b7aa9
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2024-08-24 12:30:18 +0100

    fix: Prevent Escape key in date picker from closing both flatpickr & parent widget

--- src/Obsidian/TaskModal.ts
+++ src/Obsidian/TaskModal.ts
@@ -1,4 +1,6 @@
 import { App, Modal } from 'obsidian';
+import type flatpickr from 'flatpickr';
+
 import EditTask from '../ui/EditTask.svelte';
 import type { Task } from '../Task/Task';
 import { StatusRegistry } from '../Statuses/StatusRegistry';
@@ -8,6 +10,7 @@ export class TaskModal extends Modal {
     public readonly task: Task;
     public readonly onSubmit: (updatedTasks: Task[]) => void;
     public readonly allTasks: Task[];
+    private activeFlatpickrInstance: flatpickr.Instance | null = null;
 
     constructor({
         app,
@@ -41,7 +44,13 @@ export class TaskModal extends Modal {
 
         new EditTask({
             target: contentEl,
-            props: { task: this.task, statusOptions: statusOptions, onSubmit: this.onSubmit, allTasks: this.allTasks },
+            props: {
+                task: this.task,
+                statusOptions: statusOptions,
+                onSubmit: this.onSubmit,
+                allTasks: this.allTasks,
+                modal: this,
+            },
         });
     }
 
@@ -62,5 +71,26 @@ export class TaskModal extends Modal {
     public onClose(): void {
         const { contentEl } = this;
         contentEl.empty();
+    }
+
+    /**
+     * Make sure that if the date picker is open, an Escape key only closes
+     * the date-picker, and not the EditTask modal as well.
+     */
+    public onEscapeKey(): void {
+        if (this.activeFlatpickrInstance && this.activeFlatpickrInstance.isOpen) {
+            this.activeFlatpickrInstance.close();
+            this.activeFlatpickrInstance = null;
+        } else {
+            this.close();
+        }
+    }
+
+    public setActiveFlatpickrInstance(instance: flatpickr.Instance): void {
+        this.activeFlatpickrInstance = instance;
+    }
+
+    public clearActiveFlatpickrInstance(): void {
+        this.activeFlatpickrInstance = null;
     }
 }
