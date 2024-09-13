ae2bf10cbd6d8e4820223e68b136e262399bfb60
Ilyas Landikov <ilyas.landikov@icloud.com>
2024-09-13 23:22:38 +0600

    refactor: ! inline extracted()
    This had to be manual due to multiple exit points

--- src/DateTime/Postponer.ts
+++ src/DateTime/Postponer.ts
@@ -13,26 +13,22 @@ export class Postponer {
     }
 
     public shouldShowPostponeButton(): boolean {
-        return extracted(this.task);
-    }
-}
-
-function extracted(task: Task) {
-    // don't postpone if any invalid dates
-    for (const dateField of Task.allDateFields()) {
-        const taskElement = task[dateField] as Moment;
-        if (taskElement && !taskElement.isValid()) {
-            return false;
+        // don't postpone if any invalid dates
+        for (const dateField of Task.allDateFields()) {
+            const taskElement = this.task[dateField] as Moment;
+            if (taskElement && !taskElement.isValid()) {
+                return false;
+            }
         }
+
+        // require a valid happens date to postpone
+        const hasAValidHappensDate = this.task.happensDates.some((date) => {
+            return !!date?.isValid();
+        });
+
+        // only postpone not done tasks
+        return !this.task.isDone && hasAValidHappensDate;
     }
-
-    // require a valid happens date to postpone
-    const hasAValidHappensDate = task.happensDates.some((date) => {
-        return !!date?.isValid();
-    });
-
-    // only postpone not done tasks
-    return !task.isDone && hasAValidHappensDate;
 }
 
 export function shouldShowPostponeButton(task: Task) {
