14bcf1494fcb9327b88dd89526d35a1bfc1c7d10
Ilyas Landikov <ilyas.landikov@icloud.com>
2024-09-21 19:31:19 +0600

    refactor: - set the correct value explicitly

--- src/ui/StatusEditor.svelte
+++ src/ui/StatusEditor.svelte
@@ -24,7 +24,7 @@
         }
 
         if (!dateFieldIsEmpty && !isInStatus) {
-            editableTask[editableTaskDateField] = taskWithEditedStatusApplied[taskDateField].formatAsDate();
+            editableTask[editableTaskDateField] = '';
         }
     }
 
