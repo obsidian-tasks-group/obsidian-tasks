ee35f1ff1da111d3714439bf79d38f86eefc9b90
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2024-10-05 20:27:15 +0100

    feat: Add support for 'sort by random'
    Fixes #359
    Fixes #330
    
    Co-Authored-By: qelo <161508+qelo@users.noreply.github.com>

--- tests/Query/Filter/RandomField.test.ts
+++ tests/Query/Filter/RandomField.test.ts
@@ -1,4 +1,13 @@
+/**
+ * @jest-environment jsdom
+ */
+import moment from 'moment';
+
 import { RandomField } from '../../../src/Query/Filter/RandomField';
+import { fromLine } from '../../TestingTools/TestHelpers';
+import { expectTaskComparesEqual } from '../../CustomMatchers/CustomMatchersForSorting';
+
+window.moment = moment;
 
 const field = new RandomField();
 
@@ -9,8 +18,17 @@ describe('filtering by random', () => {
 });
 
 describe('sorting by random', () => {
-    it('should not support sorting', () => {
-        expect(field.supportsSorting()).toEqual(false);
+    it('should support sorting', () => {
+        expect(field.supportsSorting()).toEqual(true);
+    });
+
+    it('should sort identical tasks the same', () => {
+        // Arrange
+        const sorter = field.createNormalSorter();
+        const task1 = fromLine({ line: '- [ ] Some description' });
+
+        // Assert
+        expectTaskComparesEqual(sorter, task1, task1);
     });
 });
 
