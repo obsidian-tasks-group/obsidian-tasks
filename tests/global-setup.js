22a4d2ab9a8ca657dde99f7fbb780c0d6795111b
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2024-10-25 21:51:31 +0100

    test: Document how to override the time zone in Jest's global-setup.js

--- tests/global-setup.js
+++ tests/global-setup.js
@@ -1,3 +1,12 @@
 module.exports = async () => {
     process.env.TZ = 'UTC';
+
+    /*
+     * Below is an example alternative time zone, for experimentation.
+     * See https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
+     * Note: Currently many tests fail if the timezone is changed, due to
+     *       the use of `moment.toISOString()` in the `toEqualMoment()`
+     *       custom matcher in our tests.
+     */
+    // process.env.TZ = 'Pacific/Auckland';
 };
