34c4dd34874c0ef9c7adbbcdf76e66599aff2879
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2024-11-01 21:59:04 +0000

    test: - Remove early experiments in DateEditor.test.ts
    The later renderDateEditorWrapper() turned out to be much more useful.

--- tests/ui/DateEditor.test.ts
+++ tests/ui/DateEditor.test.ts
@@ -1,43 +1,13 @@
 /**
  * @jest-environment jsdom
  */
-import { type RenderResult, fireEvent, render } from '@testing-library/svelte';
+import { fireEvent, render } from '@testing-library/svelte';
 import moment from 'moment/moment';
-import { TASK_FORMATS } from '../../src/Config/Settings';
-import DateEditor from '../../src/ui/DateEditor.svelte';
 import DateEditorWrapper from './DateEditorWrapper.svelte';
 
 import { getAndCheckRenderedElement } from './RenderingTestHelpers';
 
 window.moment = moment;
-
-function renderDateEditor() {
-    const result: RenderResult<DateEditor> = render(DateEditor, {
-        id: 'due',
-        dateSymbol: TASK_FORMATS.tasksPluginEmoji.taskSerializer.symbols.dueDateSymbol,
-        date: '',
-        isDateValid: true,
-        forwardOnly: true,
-        accesskey: 'D',
-    });
-
-    const { container } = result;
-    expect(() => container).toBeTruthy();
-    return { result, container };
-}
-
-describe('date editor tests', () => {
-    it('should render and read input value', async () => {
-        const { container } = renderDateEditor();
-        const dueDateInput = getAndCheckRenderedElement<HTMLInputElement>(container, 'due');
-
-        expect(dueDateInput.value).toEqual('');
-
-        await fireEvent.input(dueDateInput, { target: { value: '2024-10-01' } });
-
-        expect(dueDateInput.value).toEqual('2024-10-01');
-    });
-});
 
 function renderDateEditorWrapper(componentOptions: { forwardOnly: boolean }) {
     const { container } = render(DateEditorWrapper, componentOptions);
