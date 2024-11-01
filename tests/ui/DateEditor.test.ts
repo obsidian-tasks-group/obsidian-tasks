6cb90b2b5a3aec964c224d6aa057aa21e3163e28
Ilyas Landikov <ilyas.landikov@icloud.com>
2024-11-01 20:43:42 +0600

    test: - test inputing a value

--- tests/ui/DateEditor.test.ts
+++ tests/ui/DateEditor.test.ts
@@ -1,7 +1,7 @@
 /**
  * @jest-environment jsdom
  */
-import { type RenderResult, render } from '@testing-library/svelte';
+import { type RenderResult, fireEvent, render } from '@testing-library/svelte';
 import moment from 'moment/moment';
 import { TASK_FORMATS } from '../../src/Config/Settings';
 import DateEditor from '../../src/ui/DateEditor.svelte';
@@ -26,10 +26,14 @@ function renderDateEditor() {
 }
 
 describe('date editor tests', () => {
-    it('should render and read input value', () => {
+    it('should render and read input value', async () => {
         const { container } = renderDateEditor();
         const dueDateInput = getAndCheckRenderedElement<HTMLInputElement>(container, 'due');
 
         expect(dueDateInput.value).toEqual('');
+
+        await fireEvent.input(dueDateInput, { target: { value: '2024-10-01' } });
+
+        expect(dueDateInput.value).toEqual('2024-10-01');
     });
 });
