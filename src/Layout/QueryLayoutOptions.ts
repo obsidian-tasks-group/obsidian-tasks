d377e38daba919dee52bf989cb0ae4e457700426
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2024-12-01 08:57:47 +0000

    refactor: . Move parseQueryShowHideOptions() to QueryLayoutOptions.ts

--- src/Layout/QueryLayoutOptions.ts
+++ src/Layout/QueryLayoutOptions.ts
@@ -13,3 +13,31 @@ export class QueryLayoutOptions {
     shortMode: boolean = false;
     explainQuery: boolean = false;
 }
+
+export function parseQueryShowHideOptions(queryLayoutOptions: QueryLayoutOptions, option: string, hide: boolean) {
+    if (option.startsWith('tree')) {
+        queryLayoutOptions.hideTree = hide;
+        return true;
+    }
+    if (option.startsWith('task count')) {
+        queryLayoutOptions.hideTaskCount = hide;
+        return true;
+    }
+    if (option.startsWith('backlink')) {
+        queryLayoutOptions.hideBacklinks = hide;
+        return true;
+    }
+    if (option.startsWith('postpone button')) {
+        queryLayoutOptions.hidePostponeButton = hide;
+        return true;
+    }
+    if (option.startsWith('edit button')) {
+        queryLayoutOptions.hideEditButton = hide;
+        return true;
+    }
+    if (option.startsWith('urgency')) {
+        queryLayoutOptions.hideUrgency = hide;
+        return true;
+    }
+    return false;
+}
