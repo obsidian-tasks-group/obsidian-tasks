400c0b1452e474c78c54ca9b1dcddadb6e299b7b
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2024-12-01 10:27:57 +0000

    refactor: - Remove repetition in parseQueryShowHideOptions()

--- src/Layout/QueryLayoutOptions.ts
+++ src/Layout/QueryLayoutOptions.ts
@@ -23,29 +23,20 @@ export class QueryLayoutOptions {
  * @see parseTaskShowHideOptions
  */
 export function parseQueryShowHideOptions(queryLayoutOptions: QueryLayoutOptions, option: string, hide: boolean) {
-    if (option.startsWith('tree')) {
-        queryLayoutOptions.hideTree = hide;
-        return true;
-    }
-    if (option.startsWith('task count')) {
-        queryLayoutOptions.hideTaskCount = hide;
-        return true;
-    }
-    if (option.startsWith('backlink')) {
-        queryLayoutOptions.hideBacklinks = hide;
-        return true;
-    }
-    if (option.startsWith('postpone button')) {
-        queryLayoutOptions.hidePostponeButton = hide;
-        return true;
-    }
-    if (option.startsWith('edit button')) {
-        queryLayoutOptions.hideEditButton = hide;
-        return true;
-    }
-    if (option.startsWith('urgency')) {
-        queryLayoutOptions.hideUrgency = hide;
-        return true;
+    const optionMap = new Map<string, keyof QueryLayoutOptions>([
+        ['tree', 'hideTree'],
+        ['task count', 'hideTaskCount'],
+        ['backlink', 'hideBacklinks'],
+        ['postpone button', 'hidePostponeButton'],
+        ['edit button', 'hideEditButton'],
+        ['urgency', 'hideUrgency'],
+    ]);
+
+    for (const [key, property] of optionMap.entries()) {
+        if (option.startsWith(key)) {
+            queryLayoutOptions[property] = hide;
+            return true;
+        }
     }
     return false;
 }
