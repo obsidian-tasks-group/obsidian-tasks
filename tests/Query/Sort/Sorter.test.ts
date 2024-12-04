99f026c113ed01a8055b587524d55491c4a34519
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2024-12-04 22:34:33 +0000

    test: - Test group.statement

--- tests/Query/Sort/Sorter.test.ts
+++ tests/Query/Sort/Sorter.test.ts
@@ -1,6 +1,7 @@
 import { type Comparator, Sorter } from '../../../src/Query/Sort/Sorter';
 import type { Task } from '../../../src/Task/Task';
 import type { SearchInfo } from '../../../src/Query/SearchInfo';
+import { Statement } from '../../../src/Query/Statement';
 
 describe('Sorter', () => {
     const comparator: Comparator = (a: Task, b: Task, _searchInfo: SearchInfo) => {
@@ -11,6 +12,16 @@ describe('Sorter', () => {
         const sorter = new Sorter('sort by lineNumber', 'lineNumber', comparator, false);
 
         expect(sorter.instruction).toBe('sort by lineNumber');
+        expect(sorter.statement.rawInstruction).toBe('sort by lineNumber');
+    });
+
+    it('should store a Statement object', () => {
+        const instruction = 'sort by lineNumber';
+        const statement = new Statement(instruction, instruction);
+        const sorter = new Sorter('sort by lineNumber', 'lineNumber', comparator, false);
+
+        sorter.setStatement(statement);
+
         expect(sorter.statement.rawInstruction).toBe('sort by lineNumber');
     });
 });
