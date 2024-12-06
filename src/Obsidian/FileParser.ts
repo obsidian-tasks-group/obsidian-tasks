b54bcc25a94184635eaf96fee9bf3c8a4edadbd1
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2024-12-06 09:32:56 +0000

    refactor: . Inline temporary variables

--- src/Obsidian/FileParser.ts
+++ src/Obsidian/FileParser.ts
@@ -33,26 +33,20 @@ export class FileParser {
     }
 
     public parseFileContent() {
-        const thisDotfilePath = this.filePath;
-        const thisDotfileContent = this.fileContent;
-        const thisDotlistItems = this.listItems;
-        const thisDotlogger = this.logger;
-        const thisDotfileCache = this.fileCache;
-        const thisDoterrorReporter = this.errorReporter;
         const tasks: Task[] = [];
-        if (thisDotlistItems === undefined) {
+        if (this.listItems === undefined) {
             // When called via Cache, this function would never be called or files without list items.
             // It is useful for tests to be act gracefully on sample Markdown files with no list items, however.
             return tasks;
         }
 
-        const tasksFile = new TasksFile(thisDotfilePath, thisDotfileCache);
-        const fileLines = thisDotfileContent.split('\n');
+        const tasksFile = new TasksFile(this.filePath, this.fileCache);
+        const fileLines = this.fileContent.split('\n');
         const linesInFile = fileLines.length;
 
         // Lazily store date extracted from filename to avoid parsing more than needed
         // this.logger.debug(`getTasksFromFileContent() reading ${file.path}`);
-        const dateFromFileName = new Lazy(() => DateFallback.fromPath(thisDotfilePath));
+        const dateFromFileName = new Lazy(() => DateFallback.fromPath(this.filePath));
 
         // We want to store section information with every task so
         // that we can use that when we post process the markdown
@@ -60,7 +54,7 @@ export class FileParser {
         let currentSection: SectionCache | null = null;
         let sectionIndex = 0;
         const line2ListItem: Map<number, ListItem> = new Map();
-        for (const listItem of thisDotlistItems) {
+        for (const listItem of this.listItems) {
             const lineNumber = listItem.position.start.line;
             if (lineNumber >= linesInFile) {
                 /*
@@ -74,15 +68,15 @@ export class FileParser {
                     when Obsidian started up, it got the new file content, but still had the old cached
                     data about locations of list items in the file.
                  */
-                thisDotlogger.debug(
-                    `${thisDotfilePath} Obsidian gave us a line number ${lineNumber} past the end of the file. ${linesInFile}.`,
+                this.logger.debug(
+                    `${this.filePath} Obsidian gave us a line number ${lineNumber} past the end of the file. ${linesInFile}.`,
                 );
                 return tasks;
             }
             if (currentSection === null || currentSection.position.end.line < lineNumber) {
                 // We went past the current section (or this is the first task).
                 // Find the section that is relevant for this task and the following of the same section.
-                currentSection = Cache.getSection(lineNumber, thisDotfileCache.sections);
+                currentSection = Cache.getSection(lineNumber, this.fileCache.sections);
                 sectionIndex = 0;
             }
 
@@ -93,7 +87,7 @@ export class FileParser {
 
             const line = fileLines[lineNumber];
             if (line === undefined) {
-                thisDotlogger.debug(`${thisDotfilePath}: line ${lineNumber} - ignoring 'undefined' line.`);
+                this.logger.debug(`${this.filePath}: line ${lineNumber} - ignoring 'undefined' line.`);
                 continue;
             }
 
@@ -102,7 +96,7 @@ export class FileParser {
                 lineNumber,
                 currentSection.position.start.line,
                 sectionIndex,
-                Cache.getPrecedingHeader(lineNumber, thisDotfileCache.headings),
+                Cache.getPrecedingHeader(lineNumber, this.fileCache.headings),
             );
             if (listItem.task !== undefined) {
                 let task;
@@ -127,7 +121,7 @@ export class FileParser {
                         line2ListItem.set(lineNumber, task);
                     }
                 } catch (e) {
-                    thisDoterrorReporter(e, thisDotfilePath, listItem, line);
+                    this.errorReporter(e, this.filePath, listItem, line);
                     continue;
                 }
 
