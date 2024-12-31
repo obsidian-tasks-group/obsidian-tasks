cf7b7fc919e4e96eb723d0d63cfa3f6b051ce121
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2024-12-31 10:08:19 +0000

    fix: Fix console error when 'Toggle task done' deletes the line
    Fixes #3256

--- src/Commands/ToggleDone.ts
+++ src/Commands/ToggleDone.ts
@@ -75,7 +75,7 @@ export const toggleLine = (line: string, path: string): EditorInsertion => {
     });
     if (task !== null) {
         const lines = task.toggleWithRecurrenceInUsersOrder().map((t) => t.toFileLineString());
-        const newLineNumber = lines.length - 1;
+        const newLineNumber = lines.length > 0 ? lines.length - 1 : 0;
         return { text: lines.join('\n'), moveTo: { line: newLineNumber } };
     } else {
         // If the task is null this means that we have one of:
