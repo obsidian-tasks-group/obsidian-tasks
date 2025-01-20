95d1c07308f15e6a0003e2bad1a657ac2e313590
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2025-01-20 22:22:12 +0000

    comment: Remove a TODO that I have fixed

--- tests/Obsidian/SimulatedFile.ts
+++ tests/Obsidian/SimulatedFile.ts
@@ -19,8 +19,6 @@ export interface SimulatedFile {
 /**
  For explanations on how to test code that is using Obsidian API
  refer to https://publish.obsidian.md/tasks-contributing/Testing/Using+Obsidian+API+in+tests
-
- TODO: Make the order of values in the generated code stable.
  */
 export function readTasksFromSimulatedFile(testData: SimulatedFile) {
     const logger = logging.getLogger('testCache');
