d0609806245983210301770d9848ba3b49cdb41d
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2025-01-22 08:38:37 +0000

    test: . Document how to initialise i18n in tests

--- src/i18n/i18n.ts
+++ src/i18n/i18n.ts
@@ -28,6 +28,14 @@ export const initializeI18n = async () => {
 export const i18n = new Proxy(i18next, {
     get(target, prop) {
         if (!isInitialized && prop === 't') {
+            /* If you get the following error in tests, add this code block before the first
+               test in the file.
+               (Or add the 'await' line to the existing first beforeAll).
+
+                    beforeAll(async () => {
+                        await initializeI18n();
+                    });
+             */
             throw new Error('i18n.t() called before initialization. Call initializeI18n() first.');
         }
         return Reflect.get(target, prop);
