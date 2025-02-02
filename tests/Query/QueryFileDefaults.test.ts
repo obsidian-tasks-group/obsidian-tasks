1285e41c477d486208d400d705364e5cfeba35ad
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2025-02-02 15:16:16 +0000

    test: . Extract generateQueryFileDefaultsSource()

--- tests/Query/QueryFileDefaults.test.ts
+++ tests/Query/QueryFileDefaults.test.ts
@@ -3,11 +3,15 @@ import { getTasksFileFromMockData } from '../TestingTools/MockDataHelpers';
 import query_file_defaults_all_options_null from '../Obsidian/__test_data__/query_file_defaults_all_options_null.json';
 import { QueryFileDefaults } from '../../src/Query/QueryFileDefaults';
 
+function generateQueryFileDefaultsSource(data: any) {
+    const tasksFile = getTasksFileFromMockData(data);
+    return new QueryFileDefaults().source(tasksFile);
+}
+
 describe('QueryFileDefaults', () => {
     it('should give empty query if no relevant properties present', () => {
         const data = query_file_defaults_all_options_null;
-        const tasksFile = getTasksFileFromMockData(data);
-        const source = new QueryFileDefaults().source(tasksFile);
+        const source = generateQueryFileDefaultsSource(data);
         expect(source).toEqual('');
     });
 });
