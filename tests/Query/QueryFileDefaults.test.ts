53de97dd1ac61e868e225ec0567f5803de7113e5
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2025-02-08 18:41:30 +0000

    refactor: Add QueryFileDefaults.allPropertyNames() and allPropertyNamesSorted()

--- tests/Query/QueryFileDefaults.test.ts
+++ tests/Query/QueryFileDefaults.test.ts
@@ -19,6 +19,64 @@ describe('QueryFileDefaults', () => {
         expect(generateQueryFileDefaultsSource(query_file_defaults_all_options_null)).toEqual('');
     });
 
+    it('should give the property names in original order', () => {
+        const names = new QueryFileDefaults().allPropertyNames();
+        expect(names.sort()).toMatchInlineSnapshot(`
+            [
+              "TQ_explain",
+              "TQ_extra_instructions",
+              "TQ_short_mode",
+              "TQ_show_backlink",
+              "TQ_show_cancelled_date",
+              "TQ_show_created_date",
+              "TQ_show_depends_on",
+              "TQ_show_done_date",
+              "TQ_show_due_date",
+              "TQ_show_edit_button",
+              "TQ_show_id",
+              "TQ_show_on_completion",
+              "TQ_show_postpone_button",
+              "TQ_show_priority",
+              "TQ_show_recurrence_rule",
+              "TQ_show_scheduled_date",
+              "TQ_show_start_date",
+              "TQ_show_tags",
+              "TQ_show_task_count",
+              "TQ_show_tree",
+              "TQ_show_urgency",
+            ]
+        `);
+    });
+
+    it('should give the property names in sorted order', () => {
+        const names = new QueryFileDefaults().allPropertyNamesSorted();
+        expect(names.sort()).toMatchInlineSnapshot(`
+            [
+              "TQ_explain",
+              "TQ_extra_instructions",
+              "TQ_short_mode",
+              "TQ_show_backlink",
+              "TQ_show_cancelled_date",
+              "TQ_show_created_date",
+              "TQ_show_depends_on",
+              "TQ_show_done_date",
+              "TQ_show_due_date",
+              "TQ_show_edit_button",
+              "TQ_show_id",
+              "TQ_show_on_completion",
+              "TQ_show_postpone_button",
+              "TQ_show_priority",
+              "TQ_show_recurrence_rule",
+              "TQ_show_scheduled_date",
+              "TQ_show_start_date",
+              "TQ_show_tags",
+              "TQ_show_task_count",
+              "TQ_show_tree",
+              "TQ_show_urgency",
+            ]
+        `);
+    });
+
     it('should generate instructions - all values false', () => {
         expect(generateQueryFileDefaultsSource(query_file_defaults_all_options_false)).toMatchInlineSnapshot(`
             "full mode
