95486c0a170c8fe2b3daa93fc527873980d52d9c
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2025-02-08 20:08:06 +0000

    test: - Remove unintentional calls to .sort()
    As well as fixing SonarCloud errors, it restores the
    intended behaviour of the tests.

--- tests/Query/QueryFileDefaults.test.ts
+++ tests/Query/QueryFileDefaults.test.ts
@@ -21,36 +21,36 @@ describe('QueryFileDefaults', () => {
 
     it('should give the property names in original order', () => {
         const names = new QueryFileDefaults().allPropertyNames();
-        expect(names.sort()).toMatchInlineSnapshot(`
+        expect(names).toMatchInlineSnapshot(`
             [
               "TQ_explain",
-              "TQ_extra_instructions",
               "TQ_short_mode",
-              "TQ_show_backlink",
-              "TQ_show_cancelled_date",
-              "TQ_show_created_date",
-              "TQ_show_depends_on",
-              "TQ_show_done_date",
-              "TQ_show_due_date",
-              "TQ_show_edit_button",
+              "TQ_show_tree",
+              "TQ_show_tags",
               "TQ_show_id",
-              "TQ_show_on_completion",
-              "TQ_show_postpone_button",
+              "TQ_show_depends_on",
               "TQ_show_priority",
               "TQ_show_recurrence_rule",
-              "TQ_show_scheduled_date",
+              "TQ_show_on_completion",
+              "TQ_show_created_date",
               "TQ_show_start_date",
-              "TQ_show_tags",
-              "TQ_show_task_count",
-              "TQ_show_tree",
+              "TQ_show_scheduled_date",
+              "TQ_show_due_date",
+              "TQ_show_cancelled_date",
+              "TQ_show_done_date",
               "TQ_show_urgency",
+              "TQ_show_backlink",
+              "TQ_show_edit_button",
+              "TQ_show_postpone_button",
+              "TQ_show_task_count",
+              "TQ_extra_instructions",
             ]
         `);
     });
 
     it('should give the property names in sorted order', () => {
         const names = new QueryFileDefaults().allPropertyNamesSorted();
-        expect(names.sort()).toMatchInlineSnapshot(`
+        expect(names).toMatchInlineSnapshot(`
             [
               "TQ_explain",
               "TQ_extra_instructions",
