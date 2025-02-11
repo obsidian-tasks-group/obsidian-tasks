b89f2e449f63e9cbc10c9e6b6b034d7a964c1f2d
dangehub <moruotaiqing@gmail.com>
2025-02-12 00:51:33 +0800

    refactor: remove `i18next-browser-languagedetector` dependency fix: resolve bug with Chinese translation not displaying refactor: optimize Chinese translations

--- src/i18n/i18n.ts
+++ src/i18n/i18n.ts
@@ -1,27 +1,30 @@
 import i18next from 'i18next';
-import LanguageDetector from 'i18next-browser-languagedetector';
 import en from './locales/en.json';
 import zh_cn from './locales/zh_cn.json';
 
 let isInitialized = false;
 
+// Get Obsidian language settings
+const getObsidianLanguage = (): string => {
+    return localStorage.getItem('language')?.toLowerCase() || 'en';
+};
+
 // Define a function to initialize i18next
 export const initializeI18n = async () => {
     if (!isInitialized) {
-        await i18next
-            .use(LanguageDetector) // Use language detector to determine user's language
-            .init({
-                // lng: 'zh_cn', // for testing, hard-code the language and remove the `use(LanguageDetector)` line
-                fallbackLng: 'en', // Fallback language if detection fails or translation is missing
-                returnEmptyString: false, // Use fallback language if i18next-parser put in empty value for untranslated text
-                resources: {
-                    en: { translation: en },
-                    zh_cn: { translation: zh_cn },
-                },
-                interpolation: {
-                    escapeValue: false, // Disable escaping of strings, like '&' -> '&amp;'
-                },
-            });
+        await i18next.init({
+            lng: getObsidianLanguage(),
+            fallbackLng: 'en', // Fallback language if detection fails or translation is missing
+            returnEmptyString: false, // Use fallback language if i18next-parser put in empty value for untranslated text
+            resources: {
+                en: { translation: en },
+                zh: { translation: zh_cn }, //Enter `localStorage.getItem("language")` in the console to check the current language code.
+            },
+            interpolation: {
+                escapeValue: false, // Disable escaping of strings, like '&' -> '&amp;'
+            },
+        });
+
         isInitialized = true;
     }
 };
