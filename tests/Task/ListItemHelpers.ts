beeae50526c8df1dbdbd574ad75e6266b8410870
Ilyas Landikov <ilyas.landikov@icloud.com>
2025-02-13 17:55:22 +0600

    test: ! prepare to remove parameter from createChildListItem()

--- tests/Task/ListItemHelpers.ts
+++ tests/Task/ListItemHelpers.ts
@@ -1,8 +1,9 @@
 import { ListItem } from '../../src/Task/ListItem';
 import type { TaskLocation } from '../../src/Task/TaskLocation';
 
-export function createChildListItem(originalMarkdown: string, parent: ListItem, taskLocation: TaskLocation | null) {
+export function createChildListItem(originalMarkdown: string, parent: ListItem, _taskLocation: TaskLocation | null) {
     // This exists purely to silence WebStorm about typescript:S1848
     // See https://sonarcloud.io/organizations/obsidian-tasks-group/rules?open=typescript%3AS1848&rule_key=typescript%3AS1848
-    new ListItem(originalMarkdown, parent, taskLocation);
+    // ListItem will incorrectly have line number from its parent
+    new ListItem(originalMarkdown, parent, parent.taskLocation);
 }
