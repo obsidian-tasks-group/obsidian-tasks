dc1cd1d515034fe3f7e07e1f35576b2c0b9c530c
Ilyas Landikov <ilyas.landikov@icloud.com>
2025-04-15 16:35:07 +0600

    refactor: - add hardcoded include instruction

--- tests/Scripting/Includes.test.ts
+++ tests/Scripting/Includes.test.ts
@@ -2,7 +2,7 @@ import { Query } from '../../src/Query/Query';
 import { TasksFile } from '../../src/Scripting/TasksFile';
 
 describe('include tests', () => {
-    it.failing('should accept whole-line include placeholder', () => {
+    it('should accept whole-line include placeholder', () => {
         const source = '{{include.not_done}}';
         const query = new Query(source, new TasksFile('stuff.md'));
 
