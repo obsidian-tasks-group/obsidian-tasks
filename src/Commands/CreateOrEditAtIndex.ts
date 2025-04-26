94d157279d92e04211e06bc75eeb83ee33e0b78e
vimiomori <vivian.muchen@gmail.com>
2025-04-26 14:35:40 +0900

    create by appending to current file

--- src/Commands/CreateOrEditAtIndex.ts
+++ src/Commands/CreateOrEditAtIndex.ts
@@ -1,21 +1,16 @@
-import type { App, TFile } from 'obsidian';
+import type { App } from 'obsidian';
 import type { TickTickApi } from 'TickTick/api';
 import { TaskModal } from '../Obsidian/TaskModal';
 import type { Task } from '../Task/Task';
 import { taskFromNothing } from './CreateOrEditTaskParser';
 
-// TEST COMMENT
 export const createAtIndex = async (app: App, allTasks: Task[], tickTickApi: TickTickApi) => {
-    const path = 'Tasks/Inbox.md';
-    const file = app.vault.getAbstractFileByPath(path) as TFile;
-    console.log('file', file);
+    const file = app.workspace.getActiveFile();
     if (!file) {
         return;
     }
-    console.log('got file');
 
-    let content = await app.vault.read(file);
-    const contentArray = content.split('\n');
+    const path = file.path;
     const task = taskFromNothing({ path });
 
     const onSubmit = async (updatedTasks: Task[], updatedTask?: Task): Promise<void> => {
@@ -36,10 +31,7 @@ export const createAtIndex = async (app: App, allTasks: Task[], tickTickApi: Tic
                 return task.toFileLineString();
             })
             .join('\n');
-        console.log('serialized', serialized);
-        contentArray.splice(0, 0, serialized);
-        content = contentArray.join('\n');
-        await app.vault.modify(file, content);
+        await app.vault.append(file, serialized);
     };
 
     const taskModal = new TaskModal({
