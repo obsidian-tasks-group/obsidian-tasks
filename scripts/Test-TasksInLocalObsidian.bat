d24510b95334f6afb596b65754c0b834bc72694c
Ryan <7444613+rhhub@users.noreply.github.com>
2025-04-30 10:35:30 -0700

    chore: add Test-TasksInLocalObsidian.bat

---
+++ scripts/Test-TasksInLocalObsidian.bat
@@ -0,0 +1,5 @@
+rem Should be run from root of repository
+copy "main.js"          "resources\sample_vaults\Tasks-Demo\.obsidian\plugins\obsidian-tasks-plugin\"
+copy "manifest.json"    "resources\sample_vaults\Tasks-Demo\.obsidian\plugins\obsidian-tasks-plugin\"
+copy "styles.css"       "resources\sample_vaults\Tasks-Demo\.obsidian\plugins\obsidian-tasks-plugin\"
+
