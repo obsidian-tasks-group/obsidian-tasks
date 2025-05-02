21ea3bd13f1ae94d43dccd7237a4ca44130d1c9d
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2025-05-02 08:50:21 +0100

    refactor: . Extract variable availableNames

--- src/Scripting/Includes.ts
+++ src/Scripting/Includes.ts
@@ -7,7 +7,8 @@ export function unknownIncludeErrorMessage(includeName: string, includes: Includ
     if (isIncludesEmpty) {
         message += `\nYou can define the instruction(s) for "${includeName}" in the Tasks settings.`;
     } else {
-        message += `\nThe following includes are defined: \n  ${Object.keys(includes).join('\n  ')}`;
+        const availableNames = Object.keys(includes).join('\n  ');
+        message += `\nThe following includes are defined: \n  ${availableNames}`;
     }
 
     return message;
