07f14aa53f07a344f9dc4e452253e090a4a99e95
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2025-05-08 20:34:59 +0100

    test: - Add describe() block, for consistency

--- tests/Config/IncludesSettingsService.test.ts
+++ tests/Config/IncludesSettingsService.test.ts
@@ -12,13 +12,14 @@ describe('IncludesSettingsService', () => {
             key2: 'value2',
         };
     });
+    describe('IncludesSettingsService - addInclude', () => {
+        it('should add a new include with a unique key', () => {
+            const result = service.addInclude(testIncludes);
 
-    it('should add a new include with a unique key', () => {
-        const result = service.addInclude(testIncludes);
-
-        expect(result.newKey).toBe('new_key_1');
-        expect(result.includes['new_key_1']).toBe('');
-        expect(Object.keys(result.includes).length).toBe(3);
+            expect(result.newKey).toBe('new_key_1');
+            expect(result.includes['new_key_1']).toBe('');
+            expect(Object.keys(result.includes).length).toBe(3);
+        });
     });
 
     describe('IncludesSettingsService - wouldCreateDuplicateKey', () => {
