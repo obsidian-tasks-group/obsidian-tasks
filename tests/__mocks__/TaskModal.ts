198f9c7da8c499e6bd7f0ff770cc9b9403755612
Kris Gray <kgray@justise.com>
2025-06-28 22:26:43 -0700

    Add TaskModal mock for public api tests

---
+++ tests/__mocks__/TaskModal.ts
@@ -0,0 +1,27 @@
+import type { App } from 'obsidian';
+import type { Task } from '../../src/Task/Task';
+
+/**
+ * {@link TaskModal} needs to be mocked, because it depends on {@link obsidian.Modal}, which is not available.
+ */
+export class TaskModal {
+    public static instance: TaskModal;
+    public readonly app: App;
+    public readonly task: Task;
+    public readonly onSubmit: (updateTasks: Task[]) => void;
+
+    public readonly open: () => void;
+
+    constructor({ app, task, onSubmit }: { app: App; task: Task; onSubmit: (updatedTasks: Task[]) => void }) {
+        this.app = app;
+        this.task = task;
+        this.onSubmit = onSubmit;
+        this.open = jest.fn();
+
+        TaskModal.instance = this;
+    }
+
+    public cancel(): void {
+        this.onSubmit([]);
+    }
+}
