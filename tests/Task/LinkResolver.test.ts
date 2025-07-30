8149cfab864b0a6af7b33e2ecd05f4a1636fddf4
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2025-07-30 21:21:56 +0100

    test: - Remove some repetition

--- tests/Task/LinkResolver.test.ts
+++ tests/Task/LinkResolver.test.ts
@@ -1,11 +1,15 @@
+import type { Reference } from 'obsidian';
 import link_in_file_body from '../Obsidian/__test_data__/link_in_file_body.json';
 import { LinkResolver } from '../../src/Task/LinkResolver';
 
 describe('LinkResolver', () => {
-    it('should resolve a link via local instance', () => {
-        const data = link_in_file_body;
-        const rawLink = data.cachedMetadata.links[0];
+    let rawLink: Reference;
 
+    beforeEach(() => {
+        rawLink = link_in_file_body.cachedMetadata.links[0];
+    });
+
+    it('should resolve a link via local instance', () => {
         const resolver = new LinkResolver();
         const link = resolver.resolve(rawLink, link_in_file_body.filePath);
 
@@ -14,9 +18,6 @@ describe('LinkResolver', () => {
     });
 
     it('should resolve a link via global instance', () => {
-        const data = link_in_file_body;
-        const rawLink = data.cachedMetadata.links[0];
-
         const link = LinkResolver.getInstance().resolve(rawLink, link_in_file_body.filePath);
 
         expect(link.originalMarkdown).toEqual('[[yaml_tags_is_empty]]');
