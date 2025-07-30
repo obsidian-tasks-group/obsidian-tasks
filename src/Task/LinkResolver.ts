e6a3ff01b38264202574eaaa96261ab0ac10f830
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2025-07-30 21:15:03 +0100

    refactor: - Implement LinkResolver.getInstance().

--- src/Task/LinkResolver.ts
+++ src/Task/LinkResolver.ts
@@ -2,7 +2,21 @@ import type { Reference } from 'obsidian';
 import { Link } from './Link';
 
 export class LinkResolver {
+    private static instance: LinkResolver;
+
     public resolve(rawLink: Reference, pathContainingLink: string, destinationPath?: string) {
         return new Link(rawLink, pathContainingLink, destinationPath);
+    }
+
+    /**
+     * Provides access to the single global instance of the LinkResolver.
+     * This should be used in the plugin code.
+     */
+    public static getInstance(): LinkResolver {
+        if (!LinkResolver.instance) {
+            LinkResolver.instance = new LinkResolver();
+        }
+
+        return LinkResolver.instance;
     }
 }
--- tests/Task/LinkResolver.test.ts
+++ tests/Task/LinkResolver.test.ts
@@ -2,12 +2,22 @@ import link_in_file_body from '../Obsidian/__test_data__/link_in_file_body.json'
 import { LinkResolver } from '../../src/Task/LinkResolver';
 
 describe('LinkResolver', () => {
-    it('should resolve a link', () => {
+    it('should resolve a link via local instance', () => {
         const data = link_in_file_body;
         const rawLink = data.cachedMetadata.links[0];
 
         const resolver = new LinkResolver();
         const link = resolver.resolve(rawLink, link_in_file_body.filePath);
+
+        expect(link.originalMarkdown).toEqual('[[yaml_tags_is_empty]]');
+        expect(link.destinationPath).toBeNull();
+    });
+
+    it('should resolve a link via global instance', () => {
+        const data = link_in_file_body;
+        const rawLink = data.cachedMetadata.links[0];
+
+        const link = LinkResolver.getInstance().resolve(rawLink, link_in_file_body.filePath);
 
         expect(link.originalMarkdown).toEqual('[[yaml_tags_is_empty]]');
         expect(link.destinationPath).toBeNull();
