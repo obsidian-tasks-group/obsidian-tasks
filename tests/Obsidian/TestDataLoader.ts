1d89d44d0ccaf377daf6c7051b069376a8fd34bb
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2025-09-25 16:55:25 +0100

    refactor: - Implement TestDataLoader.get()

--- tests/Obsidian/TestDataLoader.ts
+++ tests/Obsidian/TestDataLoader.ts
@@ -1,7 +1,15 @@
+import fs from 'fs';
+import path from 'path';
+
 import type { SimulatedFile } from './SimulatedFile';
 
 export class TestDataLoader {
     static get(filename: string): SimulatedFile {
-        throw new Error(`Test data not found: '${filename}'.`);
+        const filePath = path.resolve(__dirname, `./__test_data__/${filename}.json`);
+        if (!fs.existsSync(filePath)) {
+            throw new Error(`Test data not found: '${filename}'.`);
+        }
+
+        return JSON.parse(fs.readFileSync(filePath, 'utf8'));
     }
 }
