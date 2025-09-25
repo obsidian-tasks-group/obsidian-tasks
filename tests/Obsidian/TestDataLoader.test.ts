0a773817d2eadcf6804ba854516f1fb39b6141c0
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2025-09-25 16:55:25 +0100

    refactor: - Implement TestDataLoader.get()

--- tests/Obsidian/TestDataLoader.test.ts
+++ tests/Obsidian/TestDataLoader.test.ts
@@ -1,6 +1,14 @@
 import { TestDataLoader } from './TestDataLoader';
 
+import one_task from './__test_data__/one_task.json';
+
 describe('TestDataLoader', () => {
+    it('should return data for a known file', () => {
+        const readFromFile = TestDataLoader.get('one_task');
+
+        expect(readFromFile).toStrictEqual(one_task);
+    });
+
     it('should throw Error if unknown file requested', () => {
         const t = () => {
             TestDataLoader.get('i_do_not_exist');
