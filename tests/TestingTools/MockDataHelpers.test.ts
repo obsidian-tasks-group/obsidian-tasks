23090664a3c9195884027697de007b9bfc798995
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2025-09-26 21:24:12 +0100

    test: . Clarify test name in MockDataHelpers.test.ts

--- tests/TestingTools/MockDataHelpers.test.ts
+++ tests/TestingTools/MockDataHelpers.test.ts
@@ -2,7 +2,7 @@ import { AllMockDataNames } from '../Obsidian/AllCacheSampleData';
 import { listPathAndData } from './MockDataHelpers';
 
 describe('MockDataHelpers', () => {
-    it('should list all test file paths - version 2', () => {
+    it('should give the path to the Markdown file', () => {
         const allSamples = listPathAndData(AllMockDataNames);
         const firstSample = allSamples[0];
         expect(firstSample[0]).toBe('Test Data/all_link_types.md');
