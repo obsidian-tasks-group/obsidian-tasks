2e00226b62c9a079dd5eb5ec7b9344040fdcc42c
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2025-09-26 09:24:50 +0100

    test: . Rename TestDataLoader.get() parameter to testDataName

--- tests/Obsidian/TestDataLoader.ts
+++ tests/Obsidian/TestDataLoader.ts
@@ -5,10 +5,10 @@ import type { SimulatedFile } from './SimulatedFile';
 import type { TestDataName } from './AllCacheSampleData';
 
 export class TestDataLoader {
-    static get(filename: TestDataName): SimulatedFile {
-        const filePath = path.resolve(__dirname, `./__test_data__/${filename}.json`);
+    static get(testDataName: TestDataName): SimulatedFile {
+        const filePath = path.resolve(__dirname, `./__test_data__/${testDataName}.json`);
         if (!fs.existsSync(filePath)) {
-            throw new Error(`Test data not found: '${filename}'.`);
+            throw new Error(`Test data not found: '${testDataName}'.`);
         }
 
         return JSON.parse(fs.readFileSync(filePath, 'utf8'));
