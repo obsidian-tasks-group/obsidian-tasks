77a3ad1788bab6130e9d2134f6a9bb1076b29d50
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2025-09-26 09:54:52 +0100

    test: - Partially adopt TestDataLoader in TasksFile.test.ts

--- tests/Scripting/TasksFile.test.ts
+++ tests/Scripting/TasksFile.test.ts
@@ -1,28 +1,19 @@
 import { verifyAsJson } from 'approvals/lib/Providers/Jest/JestApprovals';
 import type { Reference } from 'obsidian';
 import { TasksFile } from '../../src/Scripting/TasksFile';
-import callouts_nested_issue_2890_unlabelled from '../Obsidian/__test_data__/callouts_nested_issue_2890_unlabelled.json';
 import no_yaml from '../Obsidian/__test_data__/no_yaml.json';
 import empty_yaml from '../Obsidian/__test_data__/empty_yaml.json';
-import yaml_capitalised_property_name from '../Obsidian/__test_data__/yaml_capitalised_property_name.json';
-import yaml_tags_has_multiple_values from '../Obsidian/__test_data__/yaml_tags_has_multiple_values.json';
 import yaml_custom_number_property from '../Obsidian/__test_data__/yaml_custom_number_property.json';
-import yaml_tags_with_one_value_on_new_line from '../Obsidian/__test_data__/yaml_tags_with_one_value_on_new_line.json';
 import yaml_tags_field_added_by_obsidian_but_not_populated from '../Obsidian/__test_data__/yaml_tags_field_added_by_obsidian_but_not_populated.json';
 import yaml_tags_had_value_then_was_emptied_by_obsidian from '../Obsidian/__test_data__/yaml_tags_had_value_then_was_emptied_by_obsidian.json';
 import yaml_tags_is_empty_list from '../Obsidian/__test_data__/yaml_tags_is_empty_list.json';
 import yaml_tags_is_empty from '../Obsidian/__test_data__/yaml_tags_is_empty.json';
 import example_kanban from '../Obsidian/__test_data__/example_kanban.json';
-import { getTasksFileFromMockData, listPathAndData } from '../TestingTools/MockDataHelpers';
+import { getTasksFileFromMockData, getTasksFileFromMockData2, listPathAndData } from '../TestingTools/MockDataHelpers';
 import jason_properties from '../Obsidian/__test_data__/jason_properties.json';
 import yaml_complex_example from '../Obsidian/__test_data__/yaml_complex_example.json';
-import yaml_complex_example_standardised from '../Obsidian/__test_data__/yaml_complex_example_standardised.json';
-import yaml_all_property_types_empty from '../Obsidian/__test_data__/yaml_all_property_types_empty.json';
-import yaml_all_property_types_populated from '../Obsidian/__test_data__/yaml_all_property_types_populated.json';
 import yaml_1_alias from '../Obsidian/__test_data__/yaml_1_alias.json';
 import yaml_2_aliases from '../Obsidian/__test_data__/yaml_2_aliases.json';
-import links_everywhere from '../Obsidian/__test_data__/links_everywhere.json';
-import link_in_yaml from '../Obsidian/__test_data__/link_in_yaml.json';
 import { LinkResolver } from '../../src/Task/LinkResolver';
 import { determineExpressionType, formatToRepresentType } from './ScriptingTestHelpers';
 
@@ -119,21 +110,21 @@ describe('TasksFile - reading frontmatter', () => {
     });
 
     it('should read file with no yaml metadata', () => {
-        const tasksFile = getTasksFileFromMockData(no_yaml);
+        const tasksFile = getTasksFileFromMockData2('no_yaml');
         expect(tasksFile.cachedMetadata.frontmatter).toBeUndefined();
         expect(tasksFile.frontmatter).toEqual({ tags: [] });
         expect(tasksFile.frontmatter.tags).toEqual([]);
     });
 
     it('should read file with empty yaml metadata', () => {
-        const tasksFile = getTasksFileFromMockData(empty_yaml);
+        const tasksFile = getTasksFileFromMockData2('empty_yaml');
         expect(tasksFile.cachedMetadata.frontmatter).toBeUndefined();
         expect(tasksFile.frontmatter).toEqual({ tags: [] });
         expect(tasksFile.frontmatter.tags).toEqual([]);
     });
 
     it('should provide an independent copy of frontmatter', () => {
-        const tasksFile = getTasksFileFromMockData(yaml_tags_has_multiple_values);
+        const tasksFile = getTasksFileFromMockData2('yaml_tags_has_multiple_values');
 
         expect(tasksFile.frontmatter).not.toBe(tasksFile.cachedMetadata.frontmatter);
 
@@ -146,12 +137,12 @@ describe('TasksFile - reading frontmatter', () => {
     });
 
     it('should read file with multiple tags in yaml metadata', () => {
-        const tasksFile = getTasksFileFromMockData(yaml_tags_has_multiple_values);
+        const tasksFile = getTasksFileFromMockData2('yaml_tags_has_multiple_values');
         expect(tasksFile.cachedMetadata.frontmatter?.tags).toEqual(['multiple1', 'multiple2']);
     });
 
     it('should detect whether file is a kanban plugin board', () => {
-        const tasksFile = getTasksFileFromMockData(example_kanban);
+        const tasksFile = getTasksFileFromMockData2('example_kanban');
 
         expect(tasksFile.frontmatter['kanban-plugin']).toEqual('basic');
         // An example search might be:
@@ -172,12 +163,12 @@ describe('TasksFile - reading frontmatter', () => {
     //  Date
     //  Date & time
     it('should read file with custom number property', () => {
-        const tasksFile = getTasksFileFromMockData(yaml_custom_number_property);
+        const tasksFile = getTasksFileFromMockData2('yaml_custom_number_property');
         expect(tasksFile.frontmatter?.custom_number_prop).toEqual(42);
     });
 
     it('should read JSON frontmatter', () => {
-        const tasksFile = getTasksFileFromMockData(jason_properties);
+        const tasksFile = getTasksFileFromMockData2('jason_properties');
         // Obsidian 1.9.x change in behaviour: See #3482
         // The tags line in frontmatter is:
         //        "tags": "journal",
@@ -192,7 +183,7 @@ describe('TasksFile - reading frontmatter', () => {
     });
 
     it('should read yaml_complex_example', () => {
-        const tasksFile = getTasksFileFromMockData(yaml_complex_example);
+        const tasksFile = getTasksFileFromMockData2('yaml_complex_example');
         // Obsidian 1.9.x change in behaviour: See #3482
         // The tags line in frontmatter is:
         //      TAG:
@@ -207,19 +198,19 @@ describe('TasksFile - reading frontmatter', () => {
     });
 
     it('should read yaml_complex_example_standardised', () => {
-        const tasksFile = getTasksFileFromMockData(yaml_complex_example_standardised);
+        const tasksFile = getTasksFileFromMockData2('yaml_complex_example_standardised');
         verifyAsJson(tasksFile.frontmatter);
     });
 
     it('should read yaml_all_property_types_empty', () => {
         // See https://help.obsidian.md/Editing+and+formatting/Properties#Property+types
-        const tasksFile = getTasksFileFromMockData(yaml_all_property_types_empty);
+        const tasksFile = getTasksFileFromMockData2('yaml_all_property_types_empty');
         verifyAsJson(tasksFile.frontmatter);
     });
 
     it('should read yaml_all_property_types_populated', () => {
         // See https://help.obsidian.md/Editing+and+formatting/Properties#Property+types
-        const tasksFile = getTasksFileFromMockData(yaml_all_property_types_populated);
+        const tasksFile = getTasksFileFromMockData2('yaml_all_property_types_populated');
         const frontmatter = tasksFile.frontmatter;
         verifyAsJson(frontmatter);
         const propertyValueTypes = Object.keys(frontmatter).map(
@@ -246,7 +237,7 @@ describe('TasksFile - reading frontmatter', () => {
 
 describe('TasksFile - accessing links', () => {
     it('should access all links in the file - both properties and body', () => {
-        const tasksFile = getTasksFileFromMockData(links_everywhere);
+        const tasksFile = getTasksFileFromMockData2('links_everywhere');
         expect(tasksFile.outlinks.length).toEqual(5);
         expect(tasksFile.outlinks.map((link) => link.originalMarkdown)).toMatchInlineSnapshot(`
             [
@@ -261,19 +252,19 @@ describe('TasksFile - accessing links', () => {
 
     it('should access all links in the file body', () => {
         {
-            const tasksFile = getTasksFileFromMockData(links_everywhere);
+            const tasksFile = getTasksFileFromMockData2('links_everywhere');
             expect(tasksFile.outlinksInBody.length).toEqual(3);
             expect(tasksFile.outlinksInBody[0].originalMarkdown).toEqual('[[link_in_file_body]]');
         }
 
         {
-            const tasksFile = getTasksFileFromMockData(link_in_yaml);
+            const tasksFile = getTasksFileFromMockData2('link_in_yaml');
             expect(tasksFile.outlinksInBody.length).toEqual(0);
         }
     });
 
     it('should access all links in properties', () => {
-        const tasksFile = getTasksFileFromMockData(link_in_yaml);
+        const tasksFile = getTasksFileFromMockData2('link_in_yaml');
         expect(tasksFile.outlinksInProperties.length).toEqual(1);
         expect(tasksFile.outlinksInProperties[0].originalMarkdown).toEqual('[[yaml_tags_is_empty]]');
         expect(tasksFile.outlinksInProperties[0].destinationPath).toBeNull();
@@ -284,7 +275,7 @@ describe('TasksFile - accessing links', () => {
             (_rawLink: Reference, _sourcePath: string) => 'Hello World.md',
         );
 
-        const tasksFile = getTasksFileFromMockData(link_in_yaml);
+        const tasksFile = getTasksFileFromMockData2('link_in_yaml');
         expect(tasksFile.outlinksInProperties[0].originalMarkdown).toEqual('[[yaml_tags_is_empty]]');
         expect(tasksFile.outlinksInProperties[0].destinationPath).toEqual('Hello World.md');
     });
@@ -292,7 +283,7 @@ describe('TasksFile - accessing links', () => {
 
 describe('TasksFile - reading tags', () => {
     it('should read a tag from body and the frontmatter', () => {
-        const tasksFile = getTasksFileFromMockData(yaml_tags_with_one_value_on_new_line);
+        const tasksFile = getTasksFileFromMockData2('yaml_tags_with_one_value_on_new_line');
         expect(tasksFile.tags).toEqual(['#single-value-new-line', '#task']);
         expect(tasksFile.frontmatter.tags).toEqual(['#single-value-new-line']);
     });
@@ -306,7 +297,7 @@ describe('TasksFile - reading tags', () => {
     });
 
     it('should read tags from body of file without duplication', () => {
-        const tasksFile = getTasksFileFromMockData(callouts_nested_issue_2890_unlabelled);
+        const tasksFile = getTasksFileFromMockData2('callouts_nested_issue_2890_unlabelled');
         expect(tasksFile.tags).toEqual(['#task']);
     });
 
@@ -326,7 +317,7 @@ describe('TasksFile - reading tags', () => {
 
 describe('TasksFile - properties', () => {
     it('should not have any properties in a file with empty frontmatter', () => {
-        const tasksFile = getTasksFileFromMockData(yaml_all_property_types_empty);
+        const tasksFile = getTasksFileFromMockData2('yaml_all_property_types_empty');
 
         Object.keys(tasksFile.frontmatter).forEach((key) => {
             if (key === 'tags') {
@@ -338,7 +329,7 @@ describe('TasksFile - properties', () => {
     });
 
     it('should have all properties in a file with populated frontmatter', () => {
-        const tasksFile = getTasksFileFromMockData(yaml_all_property_types_populated);
+        const tasksFile = getTasksFileFromMockData2('yaml_all_property_types_populated');
 
         Object.keys(tasksFile.frontmatter).forEach((key) => {
             expect(tasksFile.hasProperty(key)).toEqual(true);
@@ -346,28 +337,28 @@ describe('TasksFile - properties', () => {
     });
 
     it('should treat non-exising properties correctly', () => {
-        const tasksFile = getTasksFileFromMockData(no_yaml);
+        const tasksFile = getTasksFileFromMockData2('no_yaml');
         expect(tasksFile.hasProperty('appleSauce')).toEqual(false);
     });
 
     it('should obtain a string property value', () => {
-        const tasksFile = getTasksFileFromMockData(example_kanban);
+        const tasksFile = getTasksFileFromMockData2('example_kanban');
         expect(tasksFile.property('kanban-plugin')).toEqual('basic');
     });
 
     it('should return null for a missing property value', () => {
-        const tasksFile = getTasksFileFromMockData(no_yaml);
+        const tasksFile = getTasksFileFromMockData2('no_yaml');
         expect(tasksFile.property('kanban-plugin')).toEqual(null);
     });
 
     it('should remove nulls from a list property', () => {
-        const tasksFile = getTasksFileFromMockData(yaml_complex_example);
+        const tasksFile = getTasksFileFromMockData2('yaml_complex_example');
         expect(tasksFile.property('custom_list')).toEqual(['value 1', 'value 2']);
         expect(tasksFile.property('unknown_list')).toEqual([]);
     });
 
     it('should return properties of each type', () => {
-        const tasksFile = getTasksFileFromMockData(yaml_all_property_types_populated);
+        const tasksFile = getTasksFileFromMockData2('yaml_all_property_types_populated');
         expect(tasksFile.property('sample_checkbox_property')).toEqual(true);
         expect(tasksFile.property('sample_date_property')).toEqual('2024-07-21');
         expect(tasksFile.property('sample_date_and_time_property')).toEqual('2024-07-21T12:37:00');
@@ -385,13 +376,13 @@ describe('TasksFile - properties', () => {
     });
 
     it('should ignore the case of a lower case property name', () => {
-        const tasksFile = getTasksFileFromMockData(yaml_all_property_types_populated);
+        const tasksFile = getTasksFileFromMockData2('yaml_all_property_types_populated');
         expect(tasksFile.hasProperty('SAMPLE_CHECKBOX_PROPERTY')).toEqual(true);
         expect(tasksFile.property('SAMPLE_CHECKBOX_PROPERTY')).toEqual(true);
     });
 
     it('should ignore the case of an upper case property name', () => {
-        const tasksFile = getTasksFileFromMockData(yaml_capitalised_property_name);
+        const tasksFile = getTasksFileFromMockData2('yaml_capitalised_property_name');
         expect(tasksFile.hasProperty('capital_property')).toEqual(true);
         expect(tasksFile.property('capital_property')).toEqual('some value');
     });
