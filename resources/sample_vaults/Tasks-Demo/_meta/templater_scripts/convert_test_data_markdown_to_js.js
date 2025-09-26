e1bb1668166092ccb7ed3173222e8ed18c958956
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2025-09-26 20:02:25 +0100

    vault: Remove unused variable 'functions'

--- resources/sample_vaults/Tasks-Demo/_meta/templater_scripts/convert_test_data_markdown_to_js.js
+++ resources/sample_vaults/Tasks-Demo/_meta/templater_scripts/convert_test_data_markdown_to_js.js
@@ -125,8 +125,6 @@ async function convertMarkdownFileToTestFunction(filePath, tp) {
 async function writeListOfAllTestFunctions(files) {
     const basenames = files.map((file) => getBasename(file));
 
-    const functions = basenames.map((filename) => `        ${filename},`);
-
     const content = `// DO NOT EDIT!
 // This file is machine-generated in the test vault, by convert_test_data_markdown_to_js.js.
 
