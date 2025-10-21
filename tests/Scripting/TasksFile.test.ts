470c905a52868efc355c1882944599c9f69f86eb
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2025-10-21 17:34:19 +0100

    contrib: Add snippet showing use of listPathAndData()

--- tests/Scripting/TasksFile.test.ts
+++ tests/Scripting/TasksFile.test.ts
@@ -296,6 +296,7 @@ describe('TasksFile - reading tags', () => {
         expect(tasksFile.tags).toEqual(['#task']);
     });
 
+    // begin-snippet: iterate-over-multiple-SimulatedFiles
     it.each(
         listPathAndData([
             'yaml_custom_number_property', // no tags value in frontmatter
@@ -308,6 +309,7 @@ describe('TasksFile - reading tags', () => {
         const tasksFile = getTasksFileFromMockData(testDataName);
         expect(tasksFile.frontmatter.tags).toEqual([]);
     });
+    // end-snippet
 
     it('should be able to read all tags for any loaded SimulatedFile', () => {
         const file1 = getTasksFileFromMockData('yaml_tags_with_one_value_on_new_line');
