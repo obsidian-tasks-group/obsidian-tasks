2acca8020e08f937355c26b833acbae40f4c6051
Ilyas Landikov <ilyas.landikov@icloud.com>
2025-11-01 22:16:21 +0100

    refactor: - pull properties up

--- src/Renderer/HtmlQueryResultsRenderer.ts
+++ src/Renderer/HtmlQueryResultsRenderer.ts
@@ -1,1 +1,30 @@
-export class HtmlQueryResultsRenderer {}
+import type { App, Component } from 'obsidian';
+import type { TextRenderer } from './TaskLineRenderer';
+
+export class HtmlQueryResultsRenderer {
+    // Renders the description in TaskLineRenderer:
+    protected readonly textRenderer;
+
+    // Renders the group heading in this class:
+    protected readonly renderMarkdown;
+    protected readonly obsidianComponent: Component | null;
+    protected readonly obsidianApp: App;
+
+    constructor(
+        renderMarkdown: (
+            app: App,
+            markdown: string,
+            el: HTMLElement,
+            sourcePath: string,
+            component: Component,
+        ) => Promise<void>,
+        obsidianComponent: Component | null,
+        obsidianApp: App,
+        textRenderer: TextRenderer,
+    ) {
+        this.renderMarkdown = renderMarkdown;
+        this.obsidianComponent = obsidianComponent;
+        this.obsidianApp = obsidianApp;
+        this.textRenderer = textRenderer;
+    }
+}
