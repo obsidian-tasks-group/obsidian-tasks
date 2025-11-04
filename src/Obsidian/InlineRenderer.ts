bf9632fb1dcb3522b8152c211521e04e25bccbeb
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2025-11-04 20:52:20 +0000

    refactor: - TaskLineRenderer.renderTaskLine() now takes parentElement parameter

--- src/Obsidian/InlineRenderer.ts
+++ src/Obsidian/InlineRenderer.ts
@@ -139,6 +139,7 @@ export class InlineRenderer {
             const dataLine: string = renderedElement.getAttr('data-line') ?? '0';
             const taskIndex: number = Number.parseInt(dataLine, 10);
             const taskElement = await taskLineRenderer.renderTaskLine({
+                parentElement: element,
                 task,
                 taskIndex,
                 isTaskInQueryFile: true,
