85795979cc1fcb17dcf171f5c10a72e2e8830b48
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2025-11-07 10:56:12 +0000

    refactor: . Move QueryResultsRendererGetters to QueryResultsRendererBase.ts
    Co-Authored-By: Ilyas Landikov <93825870+ilandikov@users.noreply.github.com>

--- src/Renderer/QueryResultsRendererBase.ts
+++ src/Renderer/QueryResultsRendererBase.ts
@@ -1,1 +1,16 @@
+import type { TasksFile } from '../Scripting/TasksFile';
+import type { IQuery } from '../IQuery';
+
+/**
+ * Because properties in QueryResultsRenderer may be modified during the lifetime of this class,
+ * we pass in getter functions instead of storing duplicate copies of the values.
+ */
+
+// TODO Remove export
+export interface QueryResultsRendererGetters {
+    source: () => string;
+    tasksFile: () => TasksFile;
+    query: () => IQuery;
+}
+
 export class QueryResultsRendererBase {}
