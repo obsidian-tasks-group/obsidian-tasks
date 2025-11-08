affa0f536f95a2c1b7b55ee52f4e14f59d738b9d
Ilyas Landikov <ilyas.landikov@icloud.com>
2025-11-08 19:14:07 +0100

    refactor: . extract testMarkdown()

--- tests/Renderer/MarkdownQueryResultsRenderer.test.ts
+++ tests/Renderer/MarkdownQueryResultsRenderer.test.ts
@@ -1,4 +1,5 @@
 import moment from 'moment/moment';
+import type { Task } from 'Task/Task';
 import { State } from '../../src/Obsidian/Cache';
 import { Query } from '../../src/Query/Query';
 import { MarkdownQueryResultsRenderer } from '../../src/Renderer/MarkdownQueryResultsRenderer';
@@ -8,21 +9,28 @@ import { TaskBuilder } from '../TestingTools/TaskBuilder';
 
 window.moment = moment;
 
+async function testMarkdown(source: string, tasks: Task[], expectedMarkdown: string) {
+    const tasksFile = new TasksFile('query.md');
+
+    const query = new Query(source, tasksFile);
+
+    const renderer = new MarkdownQueryResultsRenderer({
+        query: () => query,
+        tasksFile: () => tasksFile,
+        source: () => source,
+    });
+
+    await renderer.renderQuery(State.Warm, tasks);
+
+    expect(renderer.markdown).toEqual(expectedMarkdown);
+}
+
 describe('MarkdownQueryResultsRenderer tests', () => {
     it('should render single task', async () => {
-        const source = 'hide tree';
-        const tasksFile = new TasksFile('query.md');
-        const query = new Query(source, tasksFile);
-
-        const renderer = new MarkdownQueryResultsRenderer({
-            query: () => query,
-            tasksFile: () => tasksFile,
-            source: () => source,
-        });
-
-        const tasks = [new TaskBuilder().description('hello').priority(Priority.Medium).build()];
-        await renderer.renderQuery(State.Warm, tasks);
-
-        expect(renderer.markdown).toEqual('- [ ] hello 🔼');
+        await testMarkdown(
+            'hide tree',
+            [new TaskBuilder().description('hello').priority(Priority.Medium).build()],
+            '- [ ] hello 🔼',
+        );
     });
 });
