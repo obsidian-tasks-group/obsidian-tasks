c96f461d167154b47260c8c9e84955d3fc296c55
Ilyas Landikov <ilyas.landikov@icloud.com>
2025-12-31 19:44:20 +0100

    test: - test saving data.json settings

--- tests/ui/ModalOptionsEditor.test.ts
+++ tests/ui/ModalOptionsEditor.test.ts
@@ -3,11 +3,11 @@ import { getSettings, resetSettings } from '../../src/Config/Settings';
 import ModalOptionsEditor from '../../src/ui/ModalOptionsEditor.svelte';
 import { verifyWithFileExtension } from '../TestingTools/ApprovalTestHelpers';
 import { prettifyHTML } from '../TestingTools/HTMLHelpers';
-import { checkAndClickApplyButton, getAndCheckRenderedElement, uncheckInput } from './RenderingTestHelpers';
+import { checkAndClickApplyButton, uncheckCheckbox } from './RenderingTestHelpers';
 
-function renderAndCheckModal() {
+function renderAndCheckModal(onSave: () => void = () => {}) {
     const result: RenderResult<ModalOptionsEditor> = render(ModalOptionsEditor, {
-        onSave: () => {},
+        onSave,
         onClose: () => {},
     });
     const { container } = result;
@@ -33,17 +33,19 @@ describe('ModalOptionsEditor snapshot tests', () => {
 });
 
 describe('ModalOptionsEditor settings edit tests', () => {
-    it('should set due as hidden', async () => {
-        const { result, container } = renderAndCheckModal();
+    it('should set due as hidden when Apply is clicked', async () => {
+        let savedSettings = getSettings();
+        const saveSettings = () => (savedSettings = getSettings());
+        const { result, container } = renderAndCheckModal(saveSettings);
 
-        const inputElement = getAndCheckRenderedElement<HTMLInputElement>(container, 'due');
-
-        await uncheckInput(inputElement);
-
+        await uncheckCheckbox(container, 'due');
+        // unchecking has not changed the global settings
         expect(getSettings().isShownInEditModal.due).toEqual(true);
 
         checkAndClickApplyButton(result);
-
+        // clicking the apply button actually saves the settings globally
         expect(getSettings().isShownInEditModal.due).toEqual(false);
+        // checking that the settings edit would be saved in data.json
+        expect(savedSettings.isShownInEditModal.due).toEqual(false);
     });
 });
