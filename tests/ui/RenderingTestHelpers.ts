5c2f726990eed6ef578735412a4e8b6b7cc6e844
Ilyas Landikov <ilyas.landikov@icloud.com>
2026-01-03 15:55:56 +0100

    test: . move helpers to dedicated file

--- tests/ui/RenderingTestHelpers.ts
+++ tests/ui/RenderingTestHelpers.ts
@@ -2,7 +2,6 @@ import { type RenderResult, fireEvent } from '@testing-library/svelte';
 import type { EditModalShowSettings } from '../../src/Config/EditModalShowSettings';
 import { getSettings } from '../../src/Config/Settings';
 import type EditTask from '../../src/ui/EditTask.svelte';
-import type ModalOptionsEditor from '../../src/ui/ModalOptionsEditor.svelte';
 
 /**
  * Find the element with the given id.
@@ -24,22 +23,6 @@ export function getAndCheckApplyButton(result: RenderResult<EditTask>): HTMLButt
     const submit = result.getByText('Apply') as HTMLButtonElement;
     expect(submit).toBeTruthy();
     return submit;
-}
-
-export function getAndCheckButton(result: RenderResult<ModalOptionsEditor>, buttonId: string): HTMLButtonElement {
-    const submit = result.getByText(buttonId) as HTMLButtonElement;
-    expect(submit).toBeTruthy();
-    return submit;
-}
-
-export function checkAndClickApplyButton(result: RenderResult<ModalOptionsEditor>) {
-    const apply = getAndCheckButton(result, 'Apply');
-    apply.click();
-}
-
-export function checkAndClickCancelButton(result: RenderResult<ModalOptionsEditor>) {
-    const cancel = getAndCheckButton(result, 'Cancel');
-    cancel.click();
 }
 
 export async function editInputElement(inputElement: HTMLInputElement, newValue: string | boolean) {
