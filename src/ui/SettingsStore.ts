0da0acb176cc789d0b6fc587ba35279e48cfcd84
Ilyas Landikov <ilyas.landikov@icloud.com>
2026-01-03 17:57:29 +0100

    refactor: - update global settings only in one place using store subscription

--- src/ui/SettingsStore.ts
+++ src/ui/SettingsStore.ts
@@ -1,5 +1,9 @@
 import { writable } from 'svelte/store';
-import { getSettings } from '../Config/Settings';
+import { getSettings, updateSettings } from '../Config/Settings';
 
 // This store is to be used by the UI only
 export const settingsStore = writable(getSettings());
+
+settingsStore.subscribe((settings) => {
+    updateSettings(settings);
+});
