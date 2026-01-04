124be6ade2afe68321af97c6cdbacf32ad7e5963
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2026-01-04 17:08:09 +0000

    refactor: - Remove unused plugin param from createTaskLineModal()

--- src/Api/createTaskLineModal.ts
+++ src/Api/createTaskLineModal.ts
@@ -1,5 +1,4 @@
 import type { App } from 'obsidian';
-import type TasksPlugin from '../main';
 import type { Task } from '../Task/Task';
 import { taskFromLine } from '../Commands/CreateOrEditTaskParser';
 import { TaskModal } from '../Obsidian/TaskModal';
@@ -9,15 +8,13 @@ import { TaskModal } from '../Obsidian/TaskModal';
  *
  * @param app - The Obsidian App
  * @param allTasks - All Tasks Plugin tasks in the vault
- * @param plugin - Tasks Plugin instance
- *
+ * @param onSaveSettings - function to save the plugin settings
  * @returns {Promise<string>} A promise that contains the Markdown string for the task entered or
  * an empty string, if data entry was cancelled.
  */
 export const createTaskLineModal = (
     app: App,
     allTasks: Task[],
-    _plugin: TasksPlugin,
     onSaveSettings: () => Promise<void>,
 ): Promise<string> => {
     let resolvePromise: (input: string) => void;
