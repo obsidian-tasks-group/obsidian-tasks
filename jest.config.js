60b727d68ac0070697e435e78d23d196addcff9d
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2026-03-09 20:47:06 +0000

    chore: Fix formatting in jest.config.js (instead of fighting against it)

--- jest.config.js
+++ jest.config.js
@@ -18,5 +18,5 @@ module.exports = {
     // A list of paths to modules that run some code to configure or
     // set up the testing framework before each test.
     setupFilesAfterEnv: ['<rootDir>/tests/CustomMatchers/jest.custom_matchers.setup.ts'],
-    globalSetup: "./tests/global-setup.js"
+    globalSetup: './tests/global-setup.js',
 };
