75fd180c8012672961adaca8b33994371fd320d9
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2026-04-04 17:49:06 +0100

    test: - Clarify a test name

--- tests/i18n/i18n.test.ts
+++ tests/i18n/i18n.test.ts
@@ -2,7 +2,7 @@ import * as fs from 'node:fs';
 import * as path from 'node:path';
 
 describe('i18n locale consistency', () => {
-    it('should have the same number of locales in i18n.ts and i18next-parser.config.js', () => {
+    it('should have the same locales in i18n.ts and i18next-parser.config.js', () => {
         // Find the names of JSON locale files imported by the plugin:
         const i18nSource = fs.readFileSync(path.resolve(__dirname, '../../src/i18n/i18n.ts'), 'utf-8');
         const i18nLocales = [...i18nSource.matchAll(/^import\s+\w+\s+from\s+'.*\/(\w+)\.json'/gm)].map((m) => m[1]);
