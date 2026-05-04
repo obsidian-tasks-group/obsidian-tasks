d3d62b9b9e091e1093067679e42506194533044a
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2026-05-04 11:32:54 +0100

    chore: Port ESLint config to flat config format
    ... in preparation for adopting eslint-plugin-obsidianmd

---
+++ eslint.config.mjs
@@ -0,0 +1,92 @@
+import js from '@eslint/js';
+import tseslint from '@typescript-eslint/eslint-plugin';
+import tsparser from '@typescript-eslint/parser';
+import importPlugin from 'eslint-plugin-import';
+import prettier from 'eslint-plugin-prettier';
+import prettierConfig from 'eslint-config-prettier';
+import svelteParser from 'svelte-eslint-parser';
+import globals from 'globals';
+
+const prettierRules = {
+    'prettier/prettier': [
+        'error',
+        {
+            trailingComma: 'all',
+            printWidth: 120,
+            tabWidth: 4,
+            useTabs: false,
+            singleQuote: true,
+            bracketSpacing: true,
+        },
+    ],
+};
+
+export default [
+    {
+        ignores: ['main.js', 'tests/.obsidian/**'],
+    },
+    js.configs.recommended,
+    prettierConfig,
+    {
+        files: ['**/*.ts'],
+        languageOptions: {
+            parser: tsparser,
+            parserOptions: {
+                ecmaVersion: 6,
+                sourceType: 'module',
+                ecmaFeatures: { modules: true },
+            },
+            globals: {
+                ...globals.es2015,
+                ...globals.node,
+                ...globals.browser,
+                Atomics: 'readonly',
+                SharedArrayBuffer: 'readonly',
+            },
+        },
+        plugins: {
+            '@typescript-eslint': tseslint,
+            import: importPlugin,
+            prettier,
+        },
+        rules: {
+            ...tseslint.configs['eslint-recommended'].overrides[0].rules,
+            ...prettierRules,
+            'linebreak-style': ['error', 'unix'],
+            quotes: ['error', 'single', { avoidEscape: true }],
+            '@typescript-eslint/no-unused-vars': 0, // Configured in tsconfig instead.
+            'no-unused-vars': 0, // Configured in tsconfig instead.
+            semi: ['error', 'always'],
+            'import/order': 'error',
+            'sort-imports': ['error', { ignoreDeclarationSort: true }],
+        },
+    },
+    {
+        files: ['**/*.svelte'],
+        languageOptions: {
+            parser: svelteParser,
+            parserOptions: {
+                parser: tsparser,
+            },
+            globals: {
+                ...globals.browser,
+            },
+        },
+        plugins: {
+            prettier,
+        },
+        rules: {
+            'no-inner-declarations': 0,
+            'no-unused-vars': 0, // Configured in tsconfig instead.
+        },
+    },
+    {
+        files: ['**/*.js'],
+        languageOptions: {
+            globals: {
+                ...globals.node,
+                ...globals.commonjs,
+            },
+        },
+    },
+];
