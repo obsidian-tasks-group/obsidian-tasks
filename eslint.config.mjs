fb558ab99b52f052fe7b3f2db056fa05eedfa61d
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2026-05-05 17:34:58 +0100

    chore: . Extract variable on_or_off, to allow quickly enabling all rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -36,54 +36,55 @@ const typescriptLanguageOptions = {
     },
 };
 
+const on_or_off = 0;
 const typescriptCommonRules = {
     ...prettierRules,
     'linebreak-style': ['error', 'unix'],
     quotes: ['error', 'single', {avoidEscape: true}],
-    '@typescript-eslint/no-unused-vars': 0, // Configured in tsconfig instead.
-    'no-unused-vars': 0, // Configured in tsconfig instead.
+    '@typescript-eslint/no-unused-vars': on_or_off, // Configured in tsconfig instead.
+    'no-unused-vars': on_or_off, // Configured in tsconfig instead.
     semi: ['error', 'always'],
     'import/order': 'error',
     'sort-imports': ['error', {ignoreDeclarationSort: true}],
 
     // Bulk-disable new rules that fail, in order to be able to enable them
     // incrementally later on.
-    '@microsoft/sdl/no-inner-html': 0,
-    '@typescript-eslint/no-base-to-string': 0,
-    '@typescript-eslint/no-deprecated': 0, // in tests: `toThrowError` is deprecated. in favor of `toThrow`
-    '@typescript-eslint/no-explicit-any': 0,
-    '@typescript-eslint/no-floating-promises': 0,
-    '@typescript-eslint/no-for-in-array': 0,
-    '@typescript-eslint/no-implied-eval': 0,
-    '@typescript-eslint/no-misused-promises': 0,
-    '@typescript-eslint/no-namespace': 0,
-    '@typescript-eslint/no-redundant-type-constituents': 0,
-    '@typescript-eslint/no-unnecessary-type-assertion': 0,
-    '@typescript-eslint/no-unsafe-argument': 0,
-    '@typescript-eslint/no-unsafe-assignment': 0,
-    '@typescript-eslint/no-unsafe-call': 0,
-    '@typescript-eslint/no-unsafe-function-type': 0,
-    '@typescript-eslint/no-unsafe-member-access': 0,
-    '@typescript-eslint/no-unsafe-return': 0,
-    '@typescript-eslint/no-wrapper-object-types': 0,
-    '@typescript-eslint/only-throw-error': 0,
-    '@typescript-eslint/restrict-plus-operands': 0,
-    '@typescript-eslint/restrict-template-expressions': 0,
-    '@typescript-eslint/unbound-method': 0,
-    'import/no-extraneous-dependencies': 0,
-    'no-restricted-globals': 0,
-    'no-undef': 0,
-    'no-unsanitized/method': 0,
-    'no-unsanitized/property': 0,
-    'obsidianmd/hardcoded-config-path': 0, // These were all 'https://publish.obsidian.md/tasks/' references!!!
-    'obsidianmd/no-nodejs-modules': 0, // Can disable this on test files
-    'obsidianmd/no-static-styles-assignment': 0, // This likely does need addressing
-    'obsidianmd/prefer-active-doc': 0,
-    'obsidianmd/prefer-active-window-timers': 0,
-    'obsidianmd/prefer-create-el': 0,
-    'obsidianmd/prefer-get-language': 0,
-    'obsidianmd/rule-custom-message': 0,
-    'obsidianmd/ui/sentence-case': 0,
+    '@microsoft/sdl/no-inner-html': on_or_off,
+    '@typescript-eslint/no-base-to-string': on_or_off,
+    '@typescript-eslint/no-deprecated': on_or_off, // in tests: `toThrowError` is deprecated. in favor of `toThrow`
+    '@typescript-eslint/no-explicit-any': on_or_off,
+    '@typescript-eslint/no-floating-promises': on_or_off,
+    '@typescript-eslint/no-for-in-array': on_or_off,
+    '@typescript-eslint/no-implied-eval': on_or_off,
+    '@typescript-eslint/no-misused-promises': on_or_off,
+    '@typescript-eslint/no-namespace': on_or_off,
+    '@typescript-eslint/no-redundant-type-constituents': on_or_off,
+    '@typescript-eslint/no-unnecessary-type-assertion': on_or_off,
+    '@typescript-eslint/no-unsafe-argument': on_or_off,
+    '@typescript-eslint/no-unsafe-assignment': on_or_off,
+    '@typescript-eslint/no-unsafe-call': on_or_off,
+    '@typescript-eslint/no-unsafe-function-type': on_or_off,
+    '@typescript-eslint/no-unsafe-member-access': on_or_off,
+    '@typescript-eslint/no-unsafe-return': on_or_off,
+    '@typescript-eslint/no-wrapper-object-types': on_or_off,
+    '@typescript-eslint/only-throw-error': on_or_off,
+    '@typescript-eslint/restrict-plus-operands': on_or_off,
+    '@typescript-eslint/restrict-template-expressions': on_or_off,
+    '@typescript-eslint/unbound-method': on_or_off,
+    'import/no-extraneous-dependencies': on_or_off,
+    'no-restricted-globals': on_or_off,
+    'no-undef': on_or_off,
+    'no-unsanitized/method': on_or_off,
+    'no-unsanitized/property': on_or_off,
+    'obsidianmd/hardcoded-config-path': on_or_off, // These were all 'https://publish.obsidian.md/tasks/' references!!!
+    'obsidianmd/no-nodejs-modules': on_or_off, // Can disable this on test files
+    'obsidianmd/no-static-styles-assignment': on_or_off, // This likely does need addressing
+    'obsidianmd/prefer-active-doc': on_or_off,
+    'obsidianmd/prefer-active-window-timers': on_or_off,
+    'obsidianmd/prefer-create-el': on_or_off,
+    'obsidianmd/prefer-get-language': on_or_off,
+    'obsidianmd/rule-custom-message': on_or_off,
+    'obsidianmd/ui/sentence-case': on_or_off,
 };
 
 export default defineConfig([
@@ -111,7 +112,7 @@ export default defineConfig([
         },
         rules:  {
             ...typescriptCommonRules,
-            'no-restricted-imports': 0, // It's fine for tests to import moment from moment.
+            'no-restricted-imports': on_or_off, // It's fine for tests to import moment from moment.
         },
     },
     ...svelte.configs['flat/recommended'],
