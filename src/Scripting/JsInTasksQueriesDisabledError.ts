0a69c1f593090e230925ff82beecdd77529a3c76
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2026-05-07 15:43:19 +0100

    feat: Better help message in queries when JS is disabled

--- src/Scripting/JsInTasksQueriesDisabledError.ts
+++ src/Scripting/JsInTasksQueriesDisabledError.ts
@@ -1,6 +1,10 @@
 export class JsInTasksQueriesDisabledError extends Error {
-    public static readonly helpMessage = 'JavaScript is disabled in Tasks queries';
-
+    public static readonly helpMessage =
+        'JavaScript is now disabled in Tasks queries.\n' +
+        '    This query uses JavaScript, for example via "filter by function", "sort by function", or "group by function".\n' +
+        '    JavaScript can run inside Obsidian and access or modify vault contents, local files, or other system resources.\n' +
+        '    Read the Tasks documentation page "JavaScript in Tasks Queries" before deciding whether to enable it:\n' +
+        '    https://publish.obsidian.md/tasks/Scripting/JavaScript+in+Tasks+Queries';
     constructor() {
         super(JsInTasksQueriesDisabledError.helpMessage);
         this.name = 'JsInTasksQueriesDisabledError';
