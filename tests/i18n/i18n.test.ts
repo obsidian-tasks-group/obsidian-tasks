ef2335c2e543a14030e37353f49d818b9b88e64b
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2026-06-05 20:22:42 +0100

    test: - Add some more explicit consistency tests

--- tests/i18n/i18n.test.ts
+++ tests/i18n/i18n.test.ts
@@ -3,6 +3,15 @@ import * as path from 'node:path';
 import i18next from 'i18next';
 import { initializeI18n } from '../../src/i18n/i18n';
 
+function getAllLocaleJsonFileBaseNames(): string[] {
+    const localesDir = path.resolve(__dirname, '../../src/i18n/locales');
+    return fs
+        .readdirSync(localesDir)
+        .filter((file) => file.endsWith('.json'))
+        .sort()
+        .map((file) => file.replace('.json', ''));
+}
+
 function readI18nJsonImports(): Readonly<string[]> {
     // Find the names of JSON locale files imported by the plugin:
     const i18nSource = fs.readFileSync(path.resolve(__dirname, '../../src/i18n/i18n.ts'), 'utf-8');
@@ -22,6 +31,7 @@ beforeAll(async () => {
 });
 
 describe('i18n locale consistency', () => {
+    const allJsonLocaleFileBaseNames = getAllLocaleJsonFileBaseNames();
     const i18nImports = readI18nJsonImports();
     const parserLocales = getI18nextParserLocales();
 
@@ -29,12 +39,28 @@ describe('i18n locale consistency', () => {
         expect(i18nImports).toBeSorted();
     });
 
+    it('"i18n.ts" should should import all JSON files', () => {
+        expect(i18nImports).toEqual(allJsonLocaleFileBaseNames);
+    });
+
     it('"i18n.ts" should list resources imports in alphabetical order', () => {
         expect(i18nResourceNames).toBeSorted();
     });
 
+    it('"i18n.ts" resources should reference all JSON files', () => {
+        // The resource names may differ from the JSON file names:
+        //     "pt_br" vs "pt-BR"
+        //     "zh_cn" vs "zh"
+        // So we just check the number of entries, rather than the string values:
+        expect(i18nResourceNames.length).toEqual(allJsonLocaleFileBaseNames.length);
+    });
+
     it('"i18next-parser.config.js" should list locales in alphabetical order', () => {
         expect(parserLocales).toBeSorted();
+    });
+
+    it('"i18next-parser.config.js" should reference all JSON files', () => {
+        expect(parserLocales).toEqual(allJsonLocaleFileBaseNames);
     });
 
     it('should have the same locales in i18n.ts and i18next-parser.config.js', () => {
