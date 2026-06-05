4898c3e5581ff16aca8c13be85207e3cc48cf47c
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2026-06-05 18:42:09 +0100

    test: - Ensure that i18nImports is read-only, for safety of tests

--- tests/i18n/i18n.test.ts
+++ tests/i18n/i18n.test.ts
@@ -1,7 +1,7 @@
 import * as fs from 'node:fs';
 import * as path from 'node:path';
 
-function readI18nJsonImports(): string[] {
+function readI18nJsonImports(): Readonly<string[]> {
     // Find the names of JSON locale files imported by the plugin:
     const i18nSource = fs.readFileSync(path.resolve(__dirname, '../../src/i18n/i18n.ts'), 'utf-8');
     return [...i18nSource.matchAll(/^import\s+\w+\s+from\s+'.*\/(\w+)\.json'/gm)].map((m) => m[1]);
