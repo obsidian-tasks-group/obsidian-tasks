9f6ae2a295f34b088f493bc7aea9ba2739af6ad3
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2026-06-05 18:39:48 +0100

    test: . Extract readI18nJsonImports()

--- tests/i18n/i18n.test.ts
+++ tests/i18n/i18n.test.ts
@@ -1,11 +1,15 @@
 import * as fs from 'node:fs';
 import * as path from 'node:path';
 
+function readI18nJsonImports(): string[] {
+    // Find the names of JSON locale files imported by the plugin:
+    const i18nSource = fs.readFileSync(path.resolve(__dirname, '../../src/i18n/i18n.ts'), 'utf-8');
+    return [...i18nSource.matchAll(/^import\s+\w+\s+from\s+'.*\/(\w+)\.json'/gm)].map((m) => m[1]);
+}
+
 describe('i18n locale consistency', () => {
     it('should have the same locales in i18n.ts and i18next-parser.config.js', () => {
-        // Find the names of JSON locale files imported by the plugin:
-        const i18nSource = fs.readFileSync(path.resolve(__dirname, '../../src/i18n/i18n.ts'), 'utf-8');
-        const i18nImports = [...i18nSource.matchAll(/^import\s+\w+\s+from\s+'.*\/(\w+)\.json'/gm)].map((m) => m[1]);
+        const i18nImports = readI18nJsonImports();
 
         // Find the locales used by 'yarn extract-i18n'
         const parserConfig = require('../../i18next-parser.config.js');
