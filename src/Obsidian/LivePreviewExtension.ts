d0688c8a62609c27cc453f605ae5f70f708c4269
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2026-06-09 22:04:30 +0100

    refactor: ! Add some vertical space for readability in warning message

--- src/Obsidian/LivePreviewExtension.ts
+++ src/Obsidian/LivePreviewExtension.ts
@@ -60,7 +60,8 @@ class LivePreviewExtension implements PluginValue {
                     const fragment = document.createDocumentFragment();
 
                     const msg =
-                        'obsidian-tasks-plugin warning: Tasks cannot add or remove completion dates or make the next copy of a recurring task for tasks written inside a callout when you click their checkboxes in Live Preview. \n' +
+                        'obsidian-tasks-plugin warning:\n\n' +
+                        'Tasks cannot add or remove completion dates or make the next copy of a recurring task for tasks written inside a callout when you click their checkboxes in Live Preview. \n\n' +
                         'If you wanted Tasks to do these things, please undo your change, then either click the line of the task and use the "Toggle Task Done" command, or switch to Reading View to click the checkbox.';
                     const message = document.createElement('div');
                     message.textContent = msg;
