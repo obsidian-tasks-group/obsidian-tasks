c565d09bef46ab2045a31a50f4bf68c77da1fbd5
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2026-06-10 18:31:45 +0100

    refactor: . Slide up, preparing to extract reusable notice dismissal logic

--- src/Obsidian/LivePreviewExtension.ts
+++ src/Obsidian/LivePreviewExtension.ts
@@ -70,15 +70,15 @@ class LivePreviewExtension implements PluginValue {
         if (ancestor) {
             if (ancestor.matches('div.callout-content')) {
                 const dontShowAgainKey = 'live-preview-callout-warning';
+                const msg =
+                    'obsidian-tasks-plugin warning:\n\n' +
+                    'Tasks cannot add or remove completion dates or make the next copy of a recurring task for tasks written inside a callout when you click their checkboxes in Live Preview. \n\n' +
+                    'If you wanted Tasks to do these things, please undo your change, then either click the line of the task and use the "Toggle Task Done" command, or switch to Reading View to click the checkbox.';
 
                 // TODO Extract this into a function that can be re-used by other notices in future.
                 if (!getSettings().dismissedNotices[dontShowAgainKey]) {
                     const fragment = document.createDocumentFragment();
 
-                    const msg =
-                        'obsidian-tasks-plugin warning:\n\n' +
-                        'Tasks cannot add or remove completion dates or make the next copy of a recurring task for tasks written inside a callout when you click their checkboxes in Live Preview. \n\n' +
-                        'If you wanted Tasks to do these things, please undo your change, then either click the line of the task and use the "Toggle Task Done" command, or switch to Reading View to click the checkbox.';
                     const message = document.createElement('div');
                     message.textContent = msg;
 
