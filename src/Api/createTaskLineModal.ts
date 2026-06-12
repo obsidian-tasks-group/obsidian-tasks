5dd1bfbcceccf17a729748853d04710b8bf02d89
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2026-06-12 18:46:05 +0100

    refactor: ! Reimplement createTaskLineModal() to use editTaskLineModal()
    After WebStorm pointed out that the code was almost indentical.
    
    Tested manually, in a build of the plugin, using instructions
    in 'QuickAdd Tasks API Demo.md'.

--- src/Api/createTaskLineModal.ts
+++ src/Api/createTaskLineModal.ts
@@ -1,7 +1,6 @@
 import type { App } from 'obsidian';
 import type { Task } from '../Task/Task';
-import { taskFromLine } from '../Commands/CreateOrEditTaskParser';
-import { TaskModal } from '../Obsidian/TaskModal';
+import { editTaskLineModal } from './editTaskLineModal';
 
 /**
  * Opens the Tasks UI and returns the Markdown string for the task entered.
@@ -17,31 +16,5 @@ export const createTaskLineModal = (
     allTasks: Task[],
     onSaveSettings: () => Promise<void>,
 ): Promise<string> => {
-    let resolvePromise: (input: string) => void;
-    const waitForClose = new Promise<string>((resolve, _) => {
-        resolvePromise = resolve;
-    });
-
-    const onSubmit = (updatedTasks: Task[]): void => {
-        const line = updatedTasks.map((task: Task) => task.toFileLineString()).join('\n');
-        resolvePromise(line);
-    };
-
-    const onCancel = (): void => {
-        resolvePromise('');
-    };
-
-    const task = taskFromLine({ line: '', path: '' });
-    const taskModal = new TaskModal({
-        app,
-        task,
-        onSaveSettings,
-        onSubmit,
-        onCancel,
-        allTasks,
-    });
-
-    taskModal.open();
-
-    return waitForClose;
+    return editTaskLineModal(app, '', allTasks, onSaveSettings);
 };
