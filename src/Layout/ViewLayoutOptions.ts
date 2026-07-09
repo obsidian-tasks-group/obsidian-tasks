67ac6b9915cecaeae26a64c6fb3c788df3302454
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2026-07-09 22:14:01 +0100

    refactor: - Add simple parsing of 'list' and 'columns' view modes

--- src/Layout/ViewLayoutOptions.ts
+++ src/Layout/ViewLayoutOptions.ts
@@ -4,3 +4,16 @@ export type QueryViewMode = (typeof queryViewModes)[number];
 export class ViewLayoutOptions {
     viewMode: QueryViewMode = 'list';
 }
+export function parseQueryViewMode(viewLayoutOptions: ViewLayoutOptions, viewMode: string): boolean {
+    if (viewMode === 'list') {
+        viewLayoutOptions.viewMode = 'list';
+        return true;
+    }
+
+    if (viewMode === 'columns') {
+        viewLayoutOptions.viewMode = 'columns';
+        return true;
+    }
+
+    return false;
+}
--- tests/Layout/ViewLayoutOptions.test.ts
+++ tests/Layout/ViewLayoutOptions.test.ts
@@ -1,4 +1,4 @@
-import { ViewLayoutOptions } from '../../src/Layout/ViewLayoutOptions';
+import { ViewLayoutOptions, parseQueryViewMode } from '../../src/Layout/ViewLayoutOptions';
 
 describe('storing view mode', () => {
     it('should default to list view', () => {
@@ -10,5 +10,38 @@ describe('storing view mode', () => {
         const options = new ViewLayoutOptions();
         options.viewMode = 'columns';
         expect(options.viewMode).toEqual('columns');
+    });
+});
+
+describe('parsing view mode', () => {
+    it('should parse list mode', () => {
+        const options = new ViewLayoutOptions();
+        options.viewMode = 'columns';
+
+        const result = parseQueryViewMode(options, 'list');
+        expect(result).toEqual(true);
+        expect(options.viewMode).toEqual('list');
+    });
+
+    it('should parse columns mode grouped by priority', () => {
+        const options = new ViewLayoutOptions();
+
+        const result = parseQueryViewMode(options, 'columns');
+
+        expect(result).toEqual(true);
+        expect(options.viewMode).toEqual('columns');
+    });
+
+    it('should report if unknown view mode supplied', () => {
+        const options = new ViewLayoutOptions();
+        const initialMode = 'columns';
+        options.viewMode = initialMode;
+
+        const result = parseQueryViewMode(options, 'invalid');
+
+        // Make sure the mode is not changed when there is an error:
+        expect(options.viewMode).toEqual(initialMode);
+
+        expect(result).toEqual(false);
     });
 });
