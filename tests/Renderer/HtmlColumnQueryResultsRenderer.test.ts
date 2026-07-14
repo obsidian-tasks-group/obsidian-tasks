6906187ec080de02ea0af97666cf938d0f3732ed
Ilyas Landikov <ilyas.landikov@icloud.com>
2026-07-14 20:37:48 +0600

    test: - add test with 2 columns

--- tests/Renderer/HtmlColumnQueryResultsRenderer.test.ts
+++ tests/Renderer/HtmlColumnQueryResultsRenderer.test.ts
@@ -3,6 +3,7 @@ import { GlobalQuery } from '../../src/Config/GlobalQuery';
 import { State } from '../../src/Obsidian/Cache';
 import { getQueryForQueryRenderer } from '../../src/Query/QueryRendererHelper';
 import { HtmlColumnQueryResultsRenderer } from '../../src/Renderer/HtmlColumnQueryResultsRenderer';
+import { Priority } from '../../src/Task/Priority';
 import type { Task } from '../../src/Task/Task';
 import { mockApp } from '../__mocks__/obsidian';
 import { TaskBuilder } from '../TestingTools/TaskBuilder';
@@ -39,6 +40,7 @@ function makeColumnRenderer(source: string, allTasks: Task[]) {
 
 describe('columns rendering', () => {
     const due_columns = 'view columns by due\nhide backlink\nhide edit button';
+    const priority_columns = 'view columns by priority\nhide backlink\nhide edit button';
 
     it('renders no due date column', () => {
         const allTasks = [new TaskBuilder().build()];
@@ -51,6 +53,16 @@ describe('columns rendering', () => {
         const allTasks = [new TaskBuilder().dueDate('2026-07-13').build()];
 
         const { query, renderer } = makeColumnRenderer(due_columns, allTasks);
+        verifyHtmlFromRenderer(renderer, State.Warm, query, allTasks);
+    });
+
+    it('renders two priority columns', () => {
+        const allTasks = [
+            new TaskBuilder().priority(Priority.Highest).build(),
+            new TaskBuilder().priority(Priority.Low).build(),
+        ];
+
+        const { query, renderer } = makeColumnRenderer(priority_columns, allTasks);
         verifyHtmlFromRenderer(renderer, State.Warm, query, allTasks);
     });
 });
