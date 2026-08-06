34de52c32d607f06a69fa43f4398ca7863859fec
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2026-08-06 12:03:44 +0100

    test: - Move test to new describe block, before adding more tests

--- tests/Commands/ToggleDone.test.ts
+++ tests/Commands/ToggleDone.test.ts
@@ -137,13 +137,15 @@ describe('ToggleDone', () => {
         testToggleLine('- [ ] I have a |proper description', '- [x] I have a |proper description');
     });
 
-    it('should preserve Markdown hard-break spaces when completing a task', () => {
-        const hardBreak = '  ';
-        const incomplete = `- [ ] foo${hardBreak}`;
-        const complete = `- [x] foo ✅ 2022-09-04${hardBreak}`;
+    describe('trailing spaces', () => {
+        it('should preserve Markdown hard-break spaces when completing a task', () => {
+            const hardBreak = '  ';
+            const incomplete = `- [ ] foo${hardBreak}`;
+            const complete = `- [x] foo ✅ 2022-09-04${hardBreak}`;
 
-        expect(toggleLine(incomplete, 'x.md').text).toStrictEqual(complete);
-        expect(toggleLine(complete, 'x.md').text).toStrictEqual(incomplete);
+            expect(toggleLine(incomplete, 'x.md').text).toStrictEqual(complete);
+            expect(toggleLine(complete, 'x.md').text).toStrictEqual(incomplete);
+        });
     });
 
     it('should un-complete a completed task', () => {
