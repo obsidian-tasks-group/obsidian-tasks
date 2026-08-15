ac2b3190462ec405fee22a28b1a409df88f33119
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2026-08-15 22:35:43 +0100

    test: - Add failing test to show el.createEl() does not accept single cls value

--- tests/TestingTools/DOMExtensions.test.ts
+++ tests/TestingTools/DOMExtensions.test.ts
@@ -45,6 +45,16 @@ describe('Obsidian DOM extensions in tests', () => {
         expect(child.classList).toContain('tasks-quick-search-result-checkbox');
     });
 
+    it.failing('createEl() should apply a class from the cls option', () => {
+        const parent = document.createElement('div') as HTMLElementWithCreateEl;
+
+        const child = parent.createEl('input', {
+            cls: 'single-class-value',
+        });
+
+        expect(child.classList).toContain('single-class-value');
+    });
+
     it('createDiv() should create a div, append it to the parent, and return it', () => {
         const parent = document.createElement('section') as HTMLElementWithCreateDiv;
 
