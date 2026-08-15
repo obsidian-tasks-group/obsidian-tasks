07f35cb5c7c75f5501a9caa75134fff82d7d09ff
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2026-08-15 21:53:45 +0100

    test: - Add failing test to show el.createDiv() not available in tests

--- tests/TestingTools/DOMExtensions.test.ts
+++ tests/TestingTools/DOMExtensions.test.ts
@@ -6,6 +6,10 @@ type HTMLElementWithCreateEl = HTMLElement & {
     createEl<K extends keyof HTMLElementTagNameMap>(tag: K): HTMLElementTagNameMap[K];
 };
 
+type HTMLElementWithCreateDiv = HTMLElement & {
+    createDiv(): HTMLDivElement;
+};
+
 describe('Obsidian DOM extensions in tests', () => {
     it('createEl() should create an element, append it to the parent, and return it', () => {
         const parent = document.createElement('div') as HTMLElementWithCreateEl;
@@ -14,6 +18,16 @@ describe('Obsidian DOM extensions in tests', () => {
         const child = parent.createEl('input');
 
         expect(child.tagName).toBe('INPUT');
+        expect(parent.children).toHaveLength(1);
+        expect(parent.firstElementChild).toBe(child);
+    });
+
+    it.failing('createDiv() should create a div, append it to the parent, and return it', () => {
+        const parent = document.createElement('section') as HTMLElementWithCreateDiv;
+
+        const child = parent.createDiv();
+
+        expect(child.tagName).toBe('DIV');
         expect(parent.children).toHaveLength(1);
         expect(parent.firstElementChild).toBe(child);
     });
