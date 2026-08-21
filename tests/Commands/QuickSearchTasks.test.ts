de8101ccb9518d13613db3437b7f377d12c1945f
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2026-08-21 21:44:50 +0100

    test: . Slide line down

--- tests/Commands/QuickSearchTasks.test.ts
+++ tests/Commands/QuickSearchTasks.test.ts
@@ -88,11 +88,11 @@ describe('Finding matching tasks, honouring the Global Query', () => {
     it('should honour single-instruction Global Query', () => {
         const query = 'release';
         const globalQuerySource = 'description includes Write';
-        GlobalQuery.getInstance().set(globalQuerySource);
-
         const descriptions = ['Write release notes', 'Review RELEASE checklist'];
         // Should not match task whose description is 'Review RELEASE checklist', and does not match the above Global Query
         const expectedFoundDescriptions = ['Write release notes'];
+
+        GlobalQuery.getInstance().set(globalQuerySource);
 
         const tasks = descriptions.map((description) => new TaskBuilder().description(description).build());
 
