e9c9ded6217bfc25386d00b6394559fea4c3b56b
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2026-08-28 21:24:13 +0100

    test: - Save current list of view instructions from docs
    See #4021

--- tests/ui/EditInstructions/CreateEditingInstructionForGroup.test.ts
+++ tests/ui/EditInstructions/CreateEditingInstructionForGroup.test.ts
@@ -2,6 +2,7 @@
  * @jest-environment jsdom
  */
 import moment from 'moment';
+import { verify } from 'approvals/lib/Providers/Jest/JestApprovals';
 import { TaskBuilder } from '../../TestingTools/TaskBuilder';
 import { Priority } from '../../../src/Task/Priority';
 import { createEditingInstructionForGroup } from '../../../src/ui/EditInstructions/CreateEditingInstructionForGroup';
@@ -23,5 +24,22 @@ describe('CreateEditingInstructionForGroup', () => {
 
         expect(instruction).not.toBeNull();
         expect(instruction?.instructionDisplayName()).toBe('Set Date: Fri Mar 14 2025');
+    });
+});
+
+describe('docs', () => {
+    const allViewInstructionsSupportingEditing = `
+view columns by priority
+
+view columns by cancelled
+view columns by created
+view columns by done
+view columns by due
+view columns by scheduled
+view columns by starts
+`;
+
+    it('all view groups that support editing', () => {
+        verify(allViewInstructionsSupportingEditing.trim());
     });
 });
