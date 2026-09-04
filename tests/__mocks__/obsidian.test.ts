deefe6de2790a8a18be823ae42f1f5bd1c08a24f
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2026-09-05 00:20:03 +0100

    test: - Record how to see Obsidian's actual prepareFuzzySearch() results

--- tests/__mocks__/obsidian.test.ts
+++ tests/__mocks__/obsidian.test.ts
@@ -1,5 +1,10 @@
 import { prepareFuzzySearch } from './obsidian';
 
+// To see Obsidian's actual prepareFuzzySearch() results in its console, use lines like:
+//      JSON.stringify(require('obsidian').prepareFuzzySearch('aaa')('ababab'))
+//
+// Note that the scores used here are different from Obsidian's, but they are good enough for testing purposes.
+
 describe('fake prepareFuzzySearch()', () => {
     it('should do case-insensitive match', () => {
         expect(prepareFuzzySearch('HELLO')('hello')).toEqual({ score: 1 });
