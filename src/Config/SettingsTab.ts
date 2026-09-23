0783f37fc67e0d8d0603e1800e8e4dfea808ae2f
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2026-09-23 16:51:33 +0100

    refactor: - Interim commit: pass single array element to paras() for Enable Custom Searches

--- src/Config/SettingsTab.ts
+++ src/Config/SettingsTab.ts
@@ -1167,14 +1167,16 @@ export class SettingsTab extends PluginSettingTab {
             .setName(i18n.t('settings.searches.enableCustomSearches.name'))
             .setDesc(
                 SettingsTab.createFragmentWithHTML(
-                    `<p>${i18n.t('settings.searches.enableCustomSearches.description.line1', {
-                        filterByFunction: '<code>filter by function</code>',
-                        sortByFunction: '<code>sort by function</code>',
-                        groupByFunction: '<code>group by function</code>',
-                    })}</p>` +
-                        `<p>${i18n.t('settings.searches.enableCustomSearches.description.line2')}</p>` +
-                        `<p><b>${i18n.t('settings.searches.enableCustomSearches.description.line3')}</b></p>` +
-                        `<p>${i18n.t('settings.searches.enableCustomSearches.description.line4')}</p>`,
+                    paras([
+                        `${i18n.t('settings.searches.enableCustomSearches.description.line1', {
+                            filterByFunction: '<code>filter by function</code>',
+                            sortByFunction: '<code>sort by function</code>',
+                            groupByFunction: '<code>group by function</code>',
+                        })}</p>` +
+                            `<p>${i18n.t('settings.searches.enableCustomSearches.description.line2')}</p>` +
+                            `<p><b>${i18n.t('settings.searches.enableCustomSearches.description.line3')}</b></p>` +
+                            `<p>${i18n.t('settings.searches.enableCustomSearches.description.line4')}`,
+                    ]),
                 ),
             )
             .addToggle((toggle) => {
