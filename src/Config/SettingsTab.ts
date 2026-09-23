e6d499297cbd0b7ce85c5037bc79823d32bb4e13
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2026-09-23 16:40:57 +0100

    refactor: - Fix mismatched HTML structure in pre-1.13 Global Filter settings

--- src/Config/SettingsTab.ts
+++ src/Config/SettingsTab.ts
@@ -1080,7 +1080,7 @@ export class SettingsTab extends PluginSettingTab {
                 SettingsTab.createFragmentWithHTML(
                     paras([
                         `<b>${i18n.t('settings.globalFilter.filter.description.line1')}</b></p>` +
-                            `<p>${i18n.t('settings.globalFilter.filter.description.line2')}<p>` +
+                            `<p>${i18n.t('settings.globalFilter.filter.description.line2')}</p>` +
                             `<p>${i18n.t('settings.globalFilter.filter.description.line3')}</br>` +
                             `${i18n.t('settings.globalFilter.filter.description.line4')}`,
                     ]) + this.seeTheDocumentation('https://publish.obsidian.md/tasks/Getting+Started/Global+Filter'),
