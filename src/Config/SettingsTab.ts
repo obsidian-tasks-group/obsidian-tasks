f39cbd5ba7e7824fcd71f4371361f7a440c0797b
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2026-09-23 18:40:25 +0100

    refactor: . Simplify addition of 'See the docs' paragraph

--- src/Config/SettingsTab.ts
+++ src/Config/SettingsTab.ts
@@ -1230,7 +1230,8 @@ export class SettingsTab extends PluginSettingTab {
                             instruction2: '<code>{{preset.name}}</code>',
                         }),
                         i18n.t('settings.presets.line2'),
-                    ]) + this.seeTheDocumentationPara('https://publish.obsidian.md/tasks/Queries/Presets'),
+                        this.seeTheDocumentation('https://publish.obsidian.md/tasks/Queries/Presets'),
+                    ]),
                 ),
             );
         // ---------------------------------------------------------------------------
